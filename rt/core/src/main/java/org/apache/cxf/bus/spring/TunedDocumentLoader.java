4305b96d5e321419a0f74379b666983b6518c98b
Benson Margulies <bimargulies@apache.org>
2008-05-23 20:55:14 +0000

    Try again to catch enough exceptions.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@659656 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/bus/spring/TunedDocumentLoader.java
+++ rt/core/src/main/java/org/apache/cxf/bus/spring/TunedDocumentLoader.java
@@ -78,7 +78,7 @@ class TunedDocumentLoader extends DefaultDocumentLoader {
             Class<?> cls = Class.forName("com.ctc.wstx.sax.WstxSAXParserFactory");
             saxParserFactory = (SAXParserFactory)cls.newInstance();
             nsasaxParserFactory = (SAXParserFactory)cls.newInstance();
-        } catch (Exception e) {
+        } catch (Throwable e) {
             //woodstox not found, use any other Stax parser
             saxParserFactory = SAXParserFactory.newInstance();
             nsasaxParserFactory = SAXParserFactory.newInstance();
@@ -88,7 +88,7 @@ class TunedDocumentLoader extends DefaultDocumentLoader {
             nsasaxParserFactory.setFeature("http://xml.org/sax/features/namespaces", true); 
             nsasaxParserFactory.setFeature("http://xml.org/sax/features/namespace-prefixes", 
                                            true);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             //ignore
         }
     }
@@ -126,7 +126,7 @@ class TunedDocumentLoader extends DefaultDocumentLoader {
         DocumentBuilderFactory factory = super.createDocumentBuilderFactory(validationMode, namespaceAware);
         try {
             factory.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", false);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             // we can get all kinds of exceptions from this
             // due to old copies of Xerces and whatnot.
         }
