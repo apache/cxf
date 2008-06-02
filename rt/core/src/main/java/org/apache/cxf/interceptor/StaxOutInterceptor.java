922054b9350b80b2a090a31b6e0308efcaab117d
Benson Margulies <bimargulies@apache.org>
2008-06-02 12:17:37 +0000

    Fix CXF-1622.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@662410 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/interceptor/StaxOutInterceptor.java
+++ rt/core/src/main/java/org/apache/cxf/interceptor/StaxOutInterceptor.java
@@ -59,15 +59,13 @@ public class StaxOutInterceptor extends AbstractPhaseInterceptor<Message> {
         if (os == null || writer != null) {
             return;
         }
-        // assert os != null;
 
-        // TODO: where does encoding constant go?
         String encoding = getEncoding(message);
         
         try {
             writer = getXMLOutputFactory(message).createXMLStreamWriter(os, encoding);
             if (Boolean.TRUE.equals(message.getContextualProperty(FORCE_START_DOCUMENT))) {
-                writer.writeStartDocument(encoding);
+                writer.writeStartDocument(encoding, "1.0");
             }
         } catch (XMLStreamException e) {
             throw new Fault(new org.apache.cxf.common.i18n.Message("STREAM_CREATE_EXC", BUNDLE), e);
