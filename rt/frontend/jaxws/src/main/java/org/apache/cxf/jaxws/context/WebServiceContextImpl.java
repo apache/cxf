643257023959231bfaf4abe4d8ce42dfc999e959
J. Daniel Kulp <dkulp@apache.org>
2008-08-28 18:52:49 +0000

    Remove some commented out code
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@689929 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/context/WebServiceContextImpl.java
+++ rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/context/WebServiceContextImpl.java
@@ -102,7 +102,6 @@ public class WebServiceContextImpl implements WebServiceContext {
     }
 
     public static void setMessageContext(MessageContext ctx) {
-        //ContextPropertiesMapping.mapCxf2Jaxws(ctx);
         context.set(ctx);
     }
 
