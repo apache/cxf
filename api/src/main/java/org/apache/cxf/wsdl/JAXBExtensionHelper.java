375a3cbb3c652eabc19033b8555f3bdea9c79540
J. Daniel Kulp <dkulp@apache.org>
2008-09-03 18:20:07 +0000

    Merged revisions 691602 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r691602 | sergeyb | 2008-09-03 09:07:27 -0400 (Wed, 03 Sep 2008) | 1 line
      
      Updating JaxbExtensionHelper to use ClassLoaderUtils instead of Class.forName
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes@691711 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/wsdl/JAXBExtensionHelper.java
+++ api/src/main/java/org/apache/cxf/wsdl/JAXBExtensionHelper.java
@@ -48,6 +48,7 @@ import javax.xml.stream.util.StreamReaderDelegate;
 
 import org.w3c.dom.Element;
 
+import org.apache.cxf.common.classloader.ClassLoaderUtils;
 import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.common.util.PackageUtils;
 import org.apache.cxf.common.util.StringUtils;
@@ -77,12 +78,13 @@ public class JAXBExtensionHelper implements ExtensionSerializer, ExtensionDeseri
         jaxbNamespace = ns;
     }
     
-    public static void addExtensions(ExtensionRegistry registry, String parentType, String elementType,
-                                     ClassLoader cl) throws JAXBException, ClassNotFoundException {
-        Class<?> parentTypeClass = Class.forName(parentType, true, cl);
+    public static void addExtensions(ExtensionRegistry registry, String parentType, String elementType)
+        throws JAXBException, ClassNotFoundException {
+        Class<?> parentTypeClass = ClassLoaderUtils.loadClass(parentType, JAXBExtensionHelper.class);
 
-        Class<? extends TExtensibilityElementImpl> elementTypeClass = Class.forName(elementType, true, cl)
-            .asSubclass(TExtensibilityElementImpl.class);
+        Class<? extends TExtensibilityElementImpl> elementTypeClass = 
+            ClassLoaderUtils.loadClass(elementType, JAXBExtensionHelper.class)
+                .asSubclass(TExtensibilityElementImpl.class);
         addExtensions(registry, parentTypeClass, elementTypeClass, null);
     }
     
