274f1b334c0468a0fbacecce2f68b0ff0538e5c8
J. Daniel Kulp <dkulp@apache.org>
2008-09-05 17:51:14 +0000

    Merged revisions 692499 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r692499 | dkulp | 2008-09-05 13:48:52 -0400 (Fri, 05 Sep 2008) | 3 lines
      
      Don't put the logging interceptor on the fault in as the normal in takes care of it before it's known to be a fault.
      If the soap message is a fault and the security stuff wasn't found, let the fault propogate.
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes@692500 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/feature/LoggingFeature.java
+++ rt/core/src/main/java/org/apache/cxf/feature/LoggingFeature.java
@@ -49,14 +49,12 @@ public class LoggingFeature extends AbstractFeature {
     protected void initializeProvider(InterceptorProvider provider, Bus bus) {
         if (limit == DEFAULT_LIMIT) {
             provider.getInInterceptors().add(IN);
-            provider.getInFaultInterceptors().add(IN);
             provider.getOutInterceptors().add(OUT);
             provider.getOutFaultInterceptors().add(OUT);
         } else {
             LoggingInInterceptor in = new LoggingInInterceptor(limit);
             LoggingOutInterceptor out = new LoggingOutInterceptor(limit);
             provider.getInInterceptors().add(in);
-            provider.getInFaultInterceptors().add(in);
             provider.getOutInterceptors().add(out);
             provider.getOutFaultInterceptors().add(out);
         }
