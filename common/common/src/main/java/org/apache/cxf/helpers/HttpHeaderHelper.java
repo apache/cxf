c70233b88268c1ada45a647caf4a8e880d344b6f
J. Daniel Kulp <dkulp@apache.org>
2008-09-12 17:14:21 +0000

    [CXF-1797] Make sure SOAPAction is properly mapped into the form expected by CXF internals.  (Juan Velez patch applied)
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@694744 13f79535-47bb-0310-9956-ffa450edef68

--- common/common/src/main/java/org/apache/cxf/helpers/HttpHeaderHelper.java
+++ common/common/src/main/java/org/apache/cxf/helpers/HttpHeaderHelper.java
@@ -54,6 +54,7 @@ public final class HttpHeaderHelper {
         internalHeaders.put("Transfer-Encoding", "transfer-encoding");
         internalHeaders.put("Connection", "connection");
         internalHeaders.put("authorization", "Authorization");
+        internalHeaders.put("soapaction", "SOAPAction");
     }
     
     private HttpHeaderHelper() {
