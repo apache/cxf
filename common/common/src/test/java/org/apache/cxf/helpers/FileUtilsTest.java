5523f72a5b3a4657c65cb5529b4e34dee28079f7
Sean O'Callaghan <seanoc@apache.org>
2008-12-05 17:04:09 +0000

    Reset java.io.tmpdir system property after test to avoid any potential weirdness.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@723791 13f79535-47bb-0310-9956-ffa450edef68

--- common/common/src/test/java/org/apache/cxf/helpers/FileUtilsTest.java
+++ common/common/src/test/java/org/apache/cxf/helpers/FileUtilsTest.java
@@ -11,12 +11,15 @@ public class FileUtilsTest extends Assert {
     
     @Test
     public void testTempIODirExists() throws Exception {
-
+        
+        String originaltmpdir = System.getProperty("java.io.tmpdir");
         try {
             System.setProperty("java.io.tmpdir", "dummy");
             FileUtils.createTempFile("foo", "bar");
         } catch (RuntimeException e) {
             assertTrue(e.toString().contains("please set java.io.tempdir to an existing directory"));
-        }    
+        } finally {
+            System.setProperty("java.io.tmpdir", originaltmpdir);
+        }
     }
 }
