52fc4660de1f91591dfe9c89ea759f7ef5b5f6ec
Benson Margulies <bimargulies@apache.org>
2008-12-08 23:57:02 +0000

    Add error message to stop users from trying to get useful work out of -npa until JAX-B supports it through the API we depend upon.
    This is relevant to CXF-1934.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@724557 13f79535-47bb-0310-9956-ffa450edef68

--- tools/common/src/main/java/org/apache/cxf/tools/common/ToolException.java
+++ tools/common/src/main/java/org/apache/cxf/tools/common/ToolException.java
@@ -18,7 +18,13 @@
  */
 
 package org.apache.cxf.tools.common;
+import java.util.logging.Logger;
+
 import org.apache.cxf.common.i18n.Message;
+
+/**
+ * Exception used for unrecoverable error in a CXF tool.
+ */
 public class ToolException extends RuntimeException {
 
     
@@ -44,7 +50,13 @@ public class ToolException extends RuntimeException {
     public ToolException(Throwable t) {
         super(t);
     }
-    
-
+    /**
+     * Construct message from message property bundle and logger.
+     * @param messageId
+     * @param logger
+     */
+    public ToolException(String messageId, Logger logger) {
+        this(new Message(messageId, logger));
+    }
 }
 
