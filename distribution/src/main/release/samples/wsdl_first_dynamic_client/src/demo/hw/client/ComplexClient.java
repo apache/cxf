840f071f25287bff88956a2722f7eccddee885e9
J. Daniel Kulp <dkulp@apache.org>
2009-01-16 21:01:52 +0000

    Merged revisions 733901 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r733901 | bimargulies | 2009-01-12 16:22:28 -0500 (Mon, 12 Jan 2009) | 2 lines
      
      Add a sample of a complex and messy Dynamic Client case.
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes@735128 13f79535-47bb-0310-9956-ffa450edef68

---
+++ distribution/src/main/release/samples/wsdl_first_dynamic_client/src/demo/hw/client/ComplexClient.java
@@ -0,0 +1,112 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package demo.hw.client;
+
+import java.beans.BeanDescriptor;
+import java.beans.PropertyDescriptor;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.ClientImpl;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+import org.apache.cxf.service.model.BindingInfo;
+import org.apache.cxf.service.model.BindingMessageInfo;
+import org.apache.cxf.service.model.BindingOperationInfo;
+import org.apache.cxf.service.model.MessagePartInfo;
+import org.apache.cxf.service.model.ServiceInfo;
+
+/**
+ * 
+ */
+public final class ComplexClient {
+    
+    private static final QName SERVICE_NAME 
+        = new QName("http://complex.demo.cxf.apache.org/", "ComplexImplService");
+    
+    private ComplexClient() {
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) { 
+            System.out.println("please specify wsdl");
+            System.exit(1); 
+        }
+
+        URL wsdlURL;
+        File wsdlFile = new File(args[0]);
+        if (wsdlFile.exists()) {
+            wsdlURL = wsdlFile.toURL();
+        } else {
+            wsdlURL = new URL(args[0]);
+        }
+        
+        System.out.println(wsdlURL);
+        
+        JaxWsDynamicClientFactory factory = JaxWsDynamicClientFactory.newInstance();
+        Client client = factory.createClient(wsdlURL.toExternalForm(), SERVICE_NAME);
+        ClientImpl clientImpl = (ClientImpl) client;
+        Endpoint endpoint = clientImpl.getEndpoint();
+        ServiceInfo serviceInfo = endpoint.getService().getServiceInfos().get(0);
+        QName bindingName = new QName("http://complex.demo.cxf.apache.org/", "ComplexImplServiceSoapBinding");
+        BindingInfo binding = serviceInfo.getBinding(bindingName);
+        //{
+        QName opName = new QName("http://Company.com/Application", "GetAgentDetails");
+        BindingOperationInfo boi = binding.getOperation(opName);
+        BindingMessageInfo inputMessageInfo = boi.getInput();
+        List<MessagePartInfo> parts = inputMessageInfo.getMessageParts();
+        // only one part.
+        MessagePartInfo partInfo = parts.get(0);
+        Class<?> partClass = partInfo.getTypeClass();
+        System.out.println(partClass.getCanonicalName()); // GetAgentDetails
+        Object inputObject = partClass.newInstance();
+        // Unfortunately, the slot inside of the part object is also called 'part'.
+        // this is the descriptor for get/set part inside the GetAgentDetails class.
+        PropertyDescriptor partPropertyDescriptor = new PropertyDescriptor("part", partClass);
+        // This is the type of the class which really contains all the parameter information.
+        Class<?> partPropType = partPropertyDescriptor.getPropertyType(); // AgentWSRequest
+        System.out.println(partPropType.getCanonicalName());
+        Object inputPartObject = partPropType.newInstance();
+        partPropertyDescriptor.getWriteMethod().invoke(inputObject, inputPartObject);
+        PropertyDescriptor numberPropertyDescriptor = new PropertyDescriptor("agentNumber", partPropType);
+        numberPropertyDescriptor.getWriteMethod().invoke(inputPartObject, new Integer(314159));
+
+        Object[] result = client.invoke(opName, inputObject);
+        Class<?> resultClass = result[0].getClass();
+        System.out.println(resultClass.getCanonicalName()); // GetAgentDetailsResponse
+        PropertyDescriptor resultDescriptor = new PropertyDescriptor("agentWSResponse", resultClass);
+        Object wsResponse = resultDescriptor.getReadMethod().invoke(result[0]);
+        Class<?> wsResponseClass = wsResponse.getClass();
+        System.out.println(wsResponseClass.getCanonicalName());
+        PropertyDescriptor agentNameDescriptor = new PropertyDescriptor("agentName", wsResponseClass);
+        String agentName = (String)agentNameDescriptor.getReadMethod().invoke(wsResponse);
+        System.out.println("Agent name: " + agentName);
+            
+    }
+
+}
---
+++ distribution/src/main/release/samples/wsdl_first_dynamic_client/src/org/apache/cxf/demo/complex/ComplexImpl.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.demo.complex;
+
+import agentwsresponse.agent.legacysystemservices.schemas.hitum.esb.company.AgentWSResponse;
+
+import com.company.application.CompanyESBApplicationBiztalkAgentDetails4405AgentDetailsPrtSoap;
+import com.company.application.GetAgentDetails;
+import com.company.application.GetAgentDetailsResponse;
+
+/**
+ * This is a trivial implementation of a service contributed in a bug report.  It's useful
+ * as an example of a hard case of using dynamic client.
+ */
+public class ComplexImpl implements CompanyESBApplicationBiztalkAgentDetails4405AgentDetailsPrtSoap {
+
+    /** {@inheritDoc}*/
+    public GetAgentDetailsResponse getAgentDetails(GetAgentDetails parameters) {
+        GetAgentDetailsResponse r = new GetAgentDetailsResponse();
+        AgentWSResponse awr = new AgentWSResponse();
+        
+        int number = parameters.getPart().getAgentNumber();
+        awr.setAgenceNumber(number);
+        awr.setAgentName("Orange");
+        
+        r.setAgentWSResponse(awr);
+        return r;
+    }
+
+}
