870489502d59155224c6a8b86326d68bf1f3cd01
J. Daniel Kulp <dkulp@apache.org>
2009-01-27 20:58:42 +0000

    Remove usued import
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@738244 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/DispatchImpl.java
+++ rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/DispatchImpl.java
@@ -27,7 +27,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 import java.util.logging.Level;
