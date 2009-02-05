eb152d58a025e3fd24d81437c56125735b15b132
J. Daniel Kulp <dkulp@apache.org>
2009-02-05 17:22:50 +0000

    Merged revisions 741188 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r741188 | dkulp | 2009-02-05 12:21:18 -0500 (Thu, 05 Feb 2009) | 2 lines
      
      [CXF-2021] Add detail from fault to SoapFalt
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes@741190 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/DispatchImpl.java
+++ rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/DispatchImpl.java
@@ -35,6 +35,7 @@ import java.util.logging.Logger;
 import javax.activation.DataSource;
 import javax.xml.bind.JAXBContext;
 import javax.xml.namespace.QName;
+import javax.xml.soap.Detail;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPFactory;
 import javax.xml.soap.SOAPFault;
@@ -52,6 +53,9 @@ import javax.xml.ws.http.HTTPBinding;
 import javax.xml.ws.http.HTTPException;
 import javax.xml.ws.soap.SOAPBinding;
 import javax.xml.ws.soap.SOAPFaultException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
@@ -194,6 +198,16 @@ public class DispatchImpl<T> extends BindingProviderImpl implements Dispatch<T>,
                         Fault fault = (Fault)exp;
                         soapFault.setFaultCode(fault.getFaultCode());
                         soapFault.setFaultString(fault.getMessage());
+                        if (fault.getDetail() != null) {
+                            Detail det = soapFault.addDetail();
+                            Element fd = fault.getDetail();
+                            Node child = fd.getFirstChild();
+                            while (child != null) {
+                                Node next = child.getNextSibling();
+                                det.appendChild(det.getOwnerDocument().importNode(child, true));
+                                child = next;
+                            }
+                        }
                         SOAPFaultException ex = new SOAPFaultException(soapFault);
                         ex.initCause(exp);
                         throw ex;
