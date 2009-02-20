6cdb104beb5ecff0585e61abf049549ec2a9e841
Eoghan Glynn <eglynn@apache.org>
2009-02-20 20:17:12 +0000

    ServerPersistenceTest fix.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@746357 13f79535-47bb-0310-9956-ffa450edef68

--- systests/src/test/java/org/apache/cxf/systest/ws/rm/ServerPersistenceTest.java
+++ systests/src/test/java/org/apache/cxf/systest/ws/rm/ServerPersistenceTest.java
@@ -157,7 +157,7 @@ public class ServerPersistenceTest extends AbstractBusClientServerTestBase {
     }
     
     void verifyMissingResponse(Response<GreetMeResponse> responses[]) throws Exception {
-        awaitMessages(4, 7, 20000);
+        awaitMessages(4, 6, 20000);
 
         // wait another while to prove that response to second request is indeed lost
         Thread.sleep(1000);
