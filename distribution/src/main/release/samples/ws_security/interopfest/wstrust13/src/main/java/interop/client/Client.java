b78e0b02ea3f7ccf65a17a3326223d985b918e34
J. Daniel Kulp <dkulp@apache.org>
2009-03-14 02:35:29 +0000

    Command line for trust clients. Remove server profiles since the trust servers don't work yet.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@753591 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/ws_security/interopfest/wstrust13/src/main/java/interop/client/Client.java
+++ distribution/src/main/release/samples/ws_security/interopfest/wstrust13/src/main/java/interop/client/Client.java
@@ -46,7 +46,7 @@ public final class Client {
     public static void main(String argv[])
         throws Exception {
         
-        if (argv.length < 1) {
+        if (argv.length < 1 || "ALL".equalsIgnoreCase(argv[0])) {
             argv = new String[] {
                 "CustomBinding_IPingServiceContract",
                 "OasisScenario2Binding_IPingServiceContract",  
