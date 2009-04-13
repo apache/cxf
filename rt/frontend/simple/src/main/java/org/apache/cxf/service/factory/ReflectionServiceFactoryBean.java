721622043092ec502fc4ae08afa8e2b8b14cca45
J. Daniel Kulp <dkulp@apache.org>
2009-04-13 19:53:13 +0000

    Merged revisions 764581 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r764581 | dkulp | 2009-04-13 15:40:36 -0400 (Mon, 13 Apr 2009) | 2 lines
      
      [CXF-2097] Rename response part to "result" if there is a header part to match what wsgen does
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes@764587 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/simple/src/main/java/org/apache/cxf/service/factory/ReflectionServiceFactoryBean.java
+++ rt/frontend/simple/src/main/java/org/apache/cxf/service/factory/ReflectionServiceFactoryBean.java
@@ -1495,6 +1495,13 @@ public class ReflectionServiceFactoryBean extends AbstractServiceFactoryBean {
                 break;
             }
         }
+        for (MessagePartInfo mpart : op.getOutput().getMessageParts()) {
+            if (Boolean.TRUE.equals(mpart.getProperty(HEADER))) {
+                partName = "result";
+                break;
+            }
+        }
+        
         if (partName == null) {
             partName = "parameters";
         }
