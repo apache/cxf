dbd8b8cd20a3e867d8db40f8eb2b4eba9d4147a9
Glen Mazza <gmazza@apache.org>
2009-04-19 23:58:27 +0000

    typo fixed.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@766548 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/tokenstore/SecurityToken.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/tokenstore/SecurityToken.java
@@ -81,7 +81,7 @@ public class SecurityToken {
     /**
      * The RequestedUnattachedReference element
      * NOTE : The oasis-200401-wss-soap-message-security-1.0 spec allows 
-     * an extensibility mechanism for wsse:SecurityTokenRefence and 
+     * an extensibility mechanism for wsse:SecurityTokenReference and 
      * wsse:Reference. Hence we cannot limit to the 
      * wsse:SecurityTokenReference\wsse:Reference case and only hold the URI and 
      * the ValueType values.
