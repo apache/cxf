d76a29de230156917572a26c266e77d42e510196
J. Daniel Kulp <dkulp@apache.org>
2009-05-14 14:36:23 +0000

    Merged revisions 773691 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r773691 | dkulp | 2009-05-11 16:56:05 -0400 (Mon, 11 May 2009) | 2 lines
      
      If reader created with fragment or document, ClassCast could happen
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes@774790 13f79535-47bb-0310-9956-ffa450edef68

--- common/common/src/main/java/org/apache/cxf/staxutils/W3CDOMStreamReader.java
+++ common/common/src/main/java/org/apache/cxf/staxutils/W3CDOMStreamReader.java
@@ -196,7 +196,7 @@ public class W3CDOMStreamReader extends AbstractDOMStreamReader<Node, Node> {
                 return frame.uris.get(index);
             }
 
-            if (frame.parent == null) {
+            if (frame.parent == null && frame.getElement() instanceof Element) {
                 return ((Element)frame.getElement()).lookupNamespaceURI(prefix);
             }
             frame = frame.parent;
