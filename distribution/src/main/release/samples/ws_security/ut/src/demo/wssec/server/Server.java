a37621a10dd01dc197a388526e57fc4eb0e273e7
J. Daniel Kulp <dkulp@apache.org>
2009-06-15 18:10:43 +0000

    First part of CXF-2285.  Stuff from Mayank Mishra added, but needs some changes for the maven poms to work.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@784893 13f79535-47bb-0310-9956-ffa450edef68

---
+++ distribution/src/main/release/samples/ws_security/ut/src/demo/wssec/server/Server.java
@@ -0,0 +1,80 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package demo.wssec.server;
+
+import java.net.URL;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
+
+
+public class Server {
+
+    protected Server() throws Exception {
+        System.out.println("Starting Server");
+
+
+        Object implementor = new GreeterImpl();
+        String address = "http://localhost:9000/SoapContext/GreeterPort";
+        Endpoint e = Endpoint.publish(address, implementor);
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        SpringBusFactory bf = new SpringBusFactory();
+        URL busFile = Server.class.getResource("wssec.xml");
+        Bus bus = bf.createBus(busFile.toString());
+
+
+        Map<String, Object> outProps = new HashMap<String, Object>();
+        outProps.put("action", "UsernameToken Timestamp");
+
+        outProps.put("passwordType", "PasswordText");
+        outProps.put("user", "Alice");
+        outProps.put("passwordCallbackClass", "demo.wssec.server.UTPasswordCallback");
+
+        bus.getOutInterceptors().add(new WSS4JOutInterceptor(outProps));
+
+        Map<String, Object> inProps = new HashMap<String, Object>();
+        inProps.put("action", "UsernameToken Timestamp");
+        inProps.put("passwordType", "PasswordDigest");
+        inProps.put("passwordCallbackClass", "demo.wssec.server.UTPasswordCallback");
+
+        bus.getInInterceptors().add(new WSS4JInInterceptor(inProps));
+
+        bf.setDefaultBus(bus);
+
+        new Server();
+        System.out.println("Server ready...");
+
+        Thread.sleep(5 * 60 * 1000);
+
+        bus.shutdown(true);
+        System.out.println("Server exiting");
+        System.exit(0);
+    }
+}
