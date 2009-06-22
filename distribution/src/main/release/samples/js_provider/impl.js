50ecf848ad0d330e85e73dd952fccbee09eda83f
Sergey Beryozkin <sergeyb@apache.org>
2009-06-22 16:22:25 +0000

    Fix for js_provider
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@787305 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/js_provider/impl.js
+++ distribution/src/main/release/samples/js_provider/impl.js
@@ -28,7 +28,7 @@ WebServiceProvider.invoke = function(document) {
     var ns4 = "http://apache.org/hello_world_soap_http/types";
     var list = document.getElementsByTagNameNS(ns4, "requestType");
     var name = list.item(0).getFirstChild().getNodeValue();
-    var newDoc = document.getOwnerDocument().getImplementation().createDocument(ns4, "ns4:greetMeResponse", null);
+    var newDoc = document.getImplementation().createDocument(ns4, "ns4:greetMeResponse", null);
     var el = newDoc.createElementNS(ns4, "ns4:responseType");
     var txt = newDoc.createTextNode("Hi " + name);
     el.insertBefore(txt, null);
