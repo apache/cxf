f5b39aab140d1d3415bcdb4faf129604b4efd23d
J. Daniel Kulp <dkulp@apache.org>
2009-07-08 19:25:20 +0000

    Merged revisions 792261 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r792261 | dkulp | 2009-07-08 15:01:39 -0400 (Wed, 08 Jul 2009) | 1 line
      
      [CXF-2186] Use the unordered check for checking for actions.
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.2.x-fixes@792276 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/WSS4JInInterceptor.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/WSS4JInInterceptor.java
@@ -264,9 +264,9 @@ public class WSS4JInInterceptor extends AbstractWSS4JInterceptor {
             }
 
             /*
-             * now check the security actions: do they match, in right order?
+             * now check the security actions: do they match, in any order?
              */
-            if (!ignoreActions && !checkReceiverResults(wsResult, actions)) {
+            if (!ignoreActions && !checkReceiverResultsAnyOrder(wsResult, actions)) {
                 LOG.warning("Security processing failed (actions mismatch)");
                 throw new WSSecurityException(WSSecurityException.INVALID_SECURITY);
             }
