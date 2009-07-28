2dfd67f2a219bda4ed5ad0af1f6847cef206f78c
J. Daniel Kulp <dkulp@apache.org>
2009-07-28 01:07:11 +0000

    [CXF-1079] Add option to allow element refs when checking for ability to unwrap.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@798344 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/tools/common/ToolConstants.java
+++ api/src/main/java/org/apache/cxf/tools/common/ToolConstants.java
@@ -89,6 +89,7 @@ public final class ToolConstants {
     public static final String CFG_LIB_REF = "library.references";
     public static final String CFG_ANT_PROP = "ant.prop";
     public static final String CFG_NO_ADDRESS_BINDING = "noAddressBinding";
+    public static final String CFG_ALLOW_ELEMENT_REFS = "allowElementReferences";
 
 
 
@@ -107,7 +108,6 @@ public final class ToolConstants {
     public static final String CFG_GEN_NEW_ONLY = "newonly";
 
     // Java2WSDL Constants
-
     public static final String CFG_CLASSPATH = "classpath";
     public static final String CFG_TNS = "tns";
     public static final String CFG_SERVICENAME = "servicename";
