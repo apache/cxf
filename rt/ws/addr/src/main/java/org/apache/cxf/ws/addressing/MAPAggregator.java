09c437136f723a91e54f4d4beee85c13f3f0c0bf
J. Daniel Kulp <dkulp@apache.org>
2009-10-26 17:01:30 +0000

    Merged revisions 829860 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r829860 | dkulp | 2009-10-26 12:54:34 -0400 (Mon, 26 Oct 2009) | 1 line
      
      [CXF-2496] Add addressingRequired attribute to ws-addressing schema
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.2.x-fixes@829863 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/addr/src/main/java/org/apache/cxf/ws/addressing/MAPAggregator.java
+++ rt/ws/addr/src/main/java/org/apache/cxf/ws/addressing/MAPAggregator.java
@@ -130,6 +130,7 @@ public class MAPAggregator extends AbstractPhaseInterceptor<Message> {
      * element is to be advisory
      */
     public void setUsingAddressingAdvisory(boolean advisory) {
+        usingAddressingAdvisory = advisory;
     }
     
     /**
