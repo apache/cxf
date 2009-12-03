2a3f7a9cf4f47cb0ab95c6d01e4521c9e9a2717c
J. Daniel Kulp <dkulp@apache.org>
2009-12-03 22:14:00 +0000

    Merged revisions 885212 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r885212 | cschneider | 2009-11-29 06:05:59 -0500 (Sun, 29 Nov 2009) | 1 line
      
      CXF-2275: Maven plugins: Support reading WSDLs from Maven repository - Should be complete now
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.2.x-fixes@886947 13f79535-47bb-0310-9956-ffa450edef68

--- systests/wsdl_maven/java2ws/src/main/java/org/apache/cxf/systests/java2ws/HelloWorld.java
+++ systests/wsdl_maven/java2ws/src/main/java/org/apache/cxf/systests/java2ws/HelloWorld.java
@@ -19,11 +19,13 @@
 
 package org.apache.cxf.systests.java2ws;
 
+import java.util.Date;
+
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 
 @WebService
 public interface HelloWorld {
-    String sayHi(@WebParam(name = "text") String text);
+    String sayHi(@WebParam(name = "text") String text, @WebParam(name = "date") Date date);
 }
