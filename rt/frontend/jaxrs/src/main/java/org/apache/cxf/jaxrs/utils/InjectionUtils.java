65432e1f766c5d86dd6cb7463eb3109f3feea219
Sergey Beryozkin <sergeyb@apache.org>
2010-03-16 13:58:53 +0000

    Merged revisions 923741 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r923741 | sergeyb | 2010-03-16 13:46:14 +0000 (Tue, 16 Mar 2010) | 1 line
      
      CXF-2717: fix for primitive(string) multipart/form-data values be decoded by default
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.2.x-fixes@923743 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/utils/InjectionUtils.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/utils/InjectionUtils.java
@@ -879,7 +879,7 @@ public final class InjectionUtils {
             || String.class == type;
     }
     
-    private static String decodeValue(String value, boolean decode, ParameterType param) {
+    public static String decodeValue(String value, boolean decode, ParameterType param) {
         if (!decode) {
             return value;
         }
