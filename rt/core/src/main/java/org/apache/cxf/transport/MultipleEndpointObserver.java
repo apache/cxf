be09500c37d3b9dab15c46ac007c43ddd51acac7
J. Daniel Kulp <dkulp@apache.org>
2010-03-31 15:01:41 +0000

    [CXF-2733] Make sure exchange is set on message
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@929567 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/transport/MultipleEndpointObserver.java
+++ rt/core/src/main/java/org/apache/cxf/transport/MultipleEndpointObserver.java
@@ -65,6 +65,7 @@ public class MultipleEndpointObserver implements MessageObserver {
             if (exchange == null) {
                 exchange = new ExchangeImpl();
                 exchange.setInMessage(message);
+                message.setExchange(exchange);
             }
             setExchangeProperties(exchange, message);
             
