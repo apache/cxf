2b4c7ddac355df1a44037c15eed8a048840591f3
Jim Ma <ema@apache.org>
2010-06-08 06:51:07 +0000

    [CXF-2840]:Process the wsdl port extenstion EPR
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@952550 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/EndpointReferenceBuilder.java
+++ rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/EndpointReferenceBuilder.java
@@ -19,16 +19,27 @@
 
 package org.apache.cxf.jaxws;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Logger;
+
+import javax.wsdl.extensions.ExtensibilityElement;
+import javax.wsdl.extensions.UnknownExtensibilityElement;
+import javax.xml.namespace.QName;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.ws.EndpointReference;
 import javax.xml.ws.WebServiceException;
 import javax.xml.ws.wsaddressing.W3CEndpointReference;
 import javax.xml.ws.wsaddressing.W3CEndpointReferenceBuilder;
 
+import org.w3c.dom.Element;
+
 import org.apache.cxf.common.i18n.Message;
 import org.apache.cxf.common.logging.LogUtils;
+import org.apache.cxf.helpers.DOMUtils;
 import org.apache.cxf.jaxws.binding.soap.SOAPBindingImpl;
 import org.apache.cxf.jaxws.support.JaxWsEndpointImpl;
+import org.apache.cxf.ws.addressing.Names;
 
 public class EndpointReferenceBuilder {
     private static final Logger LOG = LogUtils.getL7dLogger(EndpointReferenceBuilder.class);
@@ -38,6 +49,35 @@ public class EndpointReferenceBuilder {
         this.endpoint = e;
     }
     public EndpointReference getEndpointReference() {
+        
+        //if there is epr in wsdl, direct return this EPR
+        List<ExtensibilityElement> portExtensors = endpoint.getEndpointInfo()
+            .getExtensors(ExtensibilityElement.class);
+        if (portExtensors != null) {
+            Iterator<ExtensibilityElement> extensionElements = portExtensors.iterator();
+            QName wsaEpr = new QName(Names.WSA_NAMESPACE_NAME, "EndpointReference");
+            while (extensionElements.hasNext()) {
+                ExtensibilityElement ext = (ExtensibilityElement)extensionElements.next();
+                if (ext instanceof UnknownExtensibilityElement && wsaEpr.equals(ext.getElementType())) {
+                    Element eprEle = ((UnknownExtensibilityElement)ext).getElement();
+                    List<Element> addressElements = DOMUtils.getChildrenWithName(eprEle,
+                                                                                 Names.WSA_NAMESPACE_NAME,
+                                                                                 Names.WSA_ADDRESS_NAME);
+                    if (!addressElements.isEmpty()) {
+                        /*
+                         * [WSA-WSDL Binding] : in a SOAP 1.1 port described using WSDL 1.1, the location
+                         * attribute of a soap11:address element (if present) would have the same value as the
+                         * wsa:Address child element of the wsa:EndpointReference element.
+                         */
+                        addressElements.get(0).setTextContent(this.endpoint.getEndpointInfo().getAddress());
+                    }
+                    return W3CEndpointReference.readFrom(new DOMSource(eprEle));
+                }
+
+            }
+        }
+        
+        
         String bindingId = endpoint.getJaxwsBinding().getBindingID();   
         
         if (!SOAPBindingImpl.isSoapBinding(bindingId)) {
@@ -45,16 +85,17 @@ public class EndpointReferenceBuilder {
                                                                 LOG, bindingId).toString());
         }
         
-        W3CEndpointReferenceBuilder builder = new W3CEndpointReferenceBuilder();
-        
+        W3CEndpointReferenceBuilder builder = new W3CEndpointReferenceBuilder();      
         builder.address(this.endpoint.getEndpointInfo().getAddress());
+        
         builder.serviceName(this.endpoint.getService().getName());
         builder.endpointName(this.endpoint.getEndpointInfo().getName());
-        if (this.endpoint.getEndpointInfo().getService()
-                .getDescription() != null) {
-            builder.wsdlDocumentLocation(this.endpoint.getEndpointInfo().getService()
-                                         .getDescription().getBaseURI());
+
+        if (this.endpoint.getEndpointInfo().getService().getDescription() != null) {
+            builder.wsdlDocumentLocation(this.endpoint.getEndpointInfo().getService().getDescription()
+                .getBaseURI());
         }
+        
         return builder.build();
     }
 
