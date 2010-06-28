a4aa3e01a10c2df007c8be1b564ab0249b2991a3
Jim Ma <ema@apache.org>
2010-06-28 06:17:32 +0000

    [CXF-2860]:Accepts the soap fault request message from another endpoint whose FaultTo address is this endpoint
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@958470 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/uncategorized/src/test/java/org/apache/cxf/systest/soapfault/Server.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.soapfault;
+
+import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.testutil.common.AbstractBusTestServerBase;
+
+
+public class Server extends AbstractBusTestServerBase {
+    public static final String PORT = allocatePort(Server.class);
+    
+    protected void run() {
+        String address = "http://localhost:" + PORT + "/SoapFault";
+
+        Object implementor1 = new SOAPFaultImpl();
+        Endpoint.publish(address, implementor1);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Server s = new Server();
+            s.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(-1);
+        } finally {
+            System.out.println("done!");
+        }
+    }
+}
+
