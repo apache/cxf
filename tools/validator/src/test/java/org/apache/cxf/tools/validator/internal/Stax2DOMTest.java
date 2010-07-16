cdb434cc0c0cbdb7315808ba2ca49c8555a18d3f
J. Daniel Kulp <dkulp@apache.org>
2010-07-16 15:05:33 +0000

    Merged revisions 964818 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r964818 | dkulp | 2010-07-16 11:04:40 -0400 (Fri, 16 Jul 2010) | 4 lines
      
      [CXF-2900] WSDLValidator through Stax2DOM leaves input streams open
      which causes input files to be locked.
      
      Patch from Gary Gregory applied (with mods to not require woodstox)
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.2.x-fixes@964819 13f79535-47bb-0310-9956-ffa450edef68

--- tools/validator/src/test/java/org/apache/cxf/tools/validator/internal/Stax2DOMTest.java
+++ tools/validator/src/test/java/org/apache/cxf/tools/validator/internal/Stax2DOMTest.java
@@ -20,9 +20,14 @@
 package org.apache.cxf.tools.validator.internal;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import org.w3c.dom.Document;
 
+import org.apache.cxf.helpers.IOUtils;
 import org.apache.cxf.helpers.XMLUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -31,11 +36,63 @@ public class Stax2DOMTest extends Assert {
 
     @Test
     public void testGetDocument() throws Exception {
-        File wsdl = new File(getClass().getResource("/validator_wsdl/jms_test.wsdl").toURI());
-        Document doc = new Stax2DOM().getDocument(wsdl);
+        File wsdlFile = new File(getClass().getResource(
+                "/validator_wsdl/jms_test.wsdl").toURI());
+        Document doc = new Stax2DOM().getDocument(wsdlFile);
+        assertDocumentContent(doc);
+    }
+
+    public void assertDocumentContent(Document doc) {
         String content = XMLUtils.toString(doc);
-        assertTrue(content, content.indexOf("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"") != -1);
-        assertTrue(content, content.indexOf("xmlns:x1=\"http://cxf.apache.org/hello_world_jms/types\"")
-                                           != -1);
+        assertTrue(
+                content,
+                content.indexOf("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"") != -1);
+        assertTrue(
+                content,
+                content.indexOf("xmlns:x1=\"http://cxf.apache.org/hello_world_jms/types\"") != -1);
+    }
+
+    @Test
+    public void testGetDocumentURL() throws Exception {
+        File tempFile = createTempWsdlFile();
+        Document doc = new Stax2DOM().getDocument(tempFile.toURI().toURL());
+        assertDocumentContent(doc);
+        assertDelete(tempFile);
+    }
+
+    @Test
+    public void testGetDocumentString() throws Exception {
+        File tempFile = createTempWsdlFile();
+        Document doc = new Stax2DOM().getDocument(tempFile.toURI().toString());
+        assertDocumentContent(doc);
+        assertDelete(tempFile);
+    }
+
+    public void assertDelete(File tempFile) {
+        boolean deleted = tempFile.delete();
+        if (!deleted) {
+            tempFile.deleteOnExit();
+        }
+        Assert.assertTrue(
+                "Stax2DOM left the input stream open, file cannot be deleted: "
+                        + tempFile, deleted);
+    }
+
+    public File createTempWsdlFile() throws URISyntaxException, IOException {
+        File wsdlFile = new File(getClass().getResource(
+                "/validator_wsdl/jms_test.wsdl").toURI());
+        File tempFile = File.createTempFile("Stax2DOMTest", ".wsdl");
+        FileOutputStream output = new FileOutputStream(tempFile);
+        IOUtils.copyAndCloseInput(new FileInputStream(wsdlFile), output);
+        output.close();
+        return tempFile;
+    }
+
+    @Test
+    public void testGetDocumentFile() throws Exception {
+        File tempFile = createTempWsdlFile();
+        Document doc = new Stax2DOM().getDocument(tempFile);
+        assertDocumentContent(doc);
+        assertDelete(tempFile);
     }
 }
