349521707bca39afd449e664fcfe8308e3cfa4ee
J. Daniel Kulp <dkulp@apache.org>
2010-10-20 01:57:40 +0000

    Merged revisions 1024489 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1024489 | dkulp | 2010-10-19 21:52:33 -0400 (Tue, 19 Oct 2010) | 11 lines
      
      [CXF-2839] CXF HttpConduit doesn't read VM proxy settings
      Patch from Guillaume Sauthier applied
      
      * Add NonProxyHosts as a configuration parameter in HTTPConduit
      * Introduce PatternBuilder + TestCase
      * Only use Proxy when the hostname is not in the nonProxyHosts list
      * Directly map HTTPClientPolicy.nonProxyHosts as a Pattern
      * Use PatternBuilder as a JAXB Adapter
      * Introduce a systemProxyConfiguration in the HTTPConduit
      * Hopefully make the proxy selection code a little more readable
      * Use constants for HTTP proxy property names
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1024491 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/transports/http/src/test/java/org/apache/cxf/transport/http/PatternBuilderTest.java
@@ -0,0 +1,76 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.transport.http;
+
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A {@code PatternBuilderTest} is ...
+ *
+ * @author Guillaume Sauthier
+ */
+public class PatternBuilderTest extends Assert {
+
+    @Test
+    public void testPatternBuilder() {
+
+        Pattern pattern = null;
+
+        // Simple matching
+        pattern = PatternBuilder.build("localhost");
+
+        assertTrue(pattern.matcher("localhost").matches());
+        assertFalse(pattern.matcher("localhost-after").matches());
+        assertFalse(pattern.matcher("before-localhost").matches());
+
+        // List matching
+        pattern = PatternBuilder.build("localhost|othername");
+
+        assertTrue(pattern.matcher("localhost").matches());
+        assertTrue(pattern.matcher("othername").matches());
+        assertFalse(pattern.matcher("somename").matches());
+
+        // Name with dots matching
+        pattern = PatternBuilder.build("www.apache.org");
+
+        assertTrue(pattern.matcher("www.apache.org").matches());
+        assertFalse(pattern.matcher("www1apache1org").matches());
+
+        // Wildcards matching 1/2
+        pattern = PatternBuilder.build("*.apache.org");
+
+        assertTrue(pattern.matcher("www.apache.org").matches());
+        assertTrue(pattern.matcher("svn.apache.org").matches());
+        assertFalse(pattern.matcher("apache.org").matches());
+        assertTrue(pattern.matcher(".apache.org").matches()); // not very useful ...
+
+        // Wildcards matching 2/2
+        pattern = PatternBuilder.build("www.apache.*");
+
+        assertTrue(pattern.matcher("www.apache.org").matches());
+        assertTrue(pattern.matcher("www.apache.net").matches());
+        assertFalse(pattern.matcher("www.apache").matches());
+        assertTrue(pattern.matcher("www.apache.").matches()); // not very useful ...
+
+    }
+
+}
