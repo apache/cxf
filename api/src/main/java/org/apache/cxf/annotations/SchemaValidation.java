5bd9cf5962701ab82e0fc4ead9053b0838e51eca
Glen Mazza <gmazza@apache.org>
2010-10-25 15:19:19 +0000

    Typos fixed.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1027165 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/annotations/SchemaValidation.java
+++ api/src/main/java/org/apache/cxf/annotations/SchemaValidation.java
@@ -26,7 +26,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Enables SchemaValidation
+ * Enables schema validation
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
