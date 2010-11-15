86fd75aec3439a89a88346bb78fa9d7f19c2e7d0
J. Daniel Kulp <dkulp@apache.org>
2010-11-15 15:47:11 +0000

    Merged revisions 1035304 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes
    ................
      r1035304 | dkulp | 2010-11-15 10:29:08 -0500 (Mon, 15 Nov 2010) | 9 lines
      
      Merged revisions 1035203 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1035203 | ffang | 2010-11-15 05:12:28 -0500 (Mon, 15 Nov 2010) | 1 line
        
        [CXF-3122]Async Handler for dispatch client is called twice in case of bad response message
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.2.x-fixes@1035318 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/dispatch/MalformedResponseInterceptor.java
@@ -0,0 +1,63 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.dispatch;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.cxf.helpers.IOUtils;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.io.CachedOutputStream;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+
+public class MalformedResponseInterceptor extends AbstractPhaseInterceptor<Message> {
+
+    public MalformedResponseInterceptor() {
+        super(Phase.RECEIVE);
+        addAfter(LoggingInInterceptor.class.getName());
+    }
+
+    public void handleMessage(Message message) throws Fault {
+        InputStream is = message.getContent(InputStream.class);
+        if (is != null) {
+            CachedOutputStream bos = new CachedOutputStream();
+            try {
+                //intend to change response as malformed message
+                is = getClass().getClassLoader().getResourceAsStream(
+                        "org/apache/cxf/systest/dispatch/resources/GreetMeDocLiteralRespMalformed.xml");
+                IOUtils.copy(is, bos);
+
+                bos.flush();
+                is.close();
+                message.setContent(InputStream.class, bos.getInputStream());
+                bos.close();
+                message.setContent(InputStream.class, bos.getInputStream());
+                
+            } catch (IOException e) {
+                throw new Fault(e);
+            }
+        }
+
+    }
+
+}
+
