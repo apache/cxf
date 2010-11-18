ac77b82936807de96c52733bb1a25f0363a603af
Freeman Yue Fang <ffang@apache.org>
2010-11-18 11:00:21 +0000

    Merged revisions 1036397 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1036397 | ffang | 2010-11-18 18:53:33 +0800 (四, 18 11 2010) | 1 line
      
      [CXF-3132]asynchronous client can't get correct error if non-runtimeexcpetion happen
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1036401 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/dispatch/DispatchClientServerWithHugeResponseTest.java
@@ -0,0 +1,131 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.systest.dispatch;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Endpoint;
+import javax.xml.ws.Response;
+import javax.xml.ws.Service;
+
+
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.apache.cxf.testutil.common.AbstractBusTestServerBase;
+import org.apache.cxf.testutil.common.TestUtil;
+import org.apache.hello_world_soap_http.GreeterImpl;
+import org.apache.hello_world_soap_http.SOAPService;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DispatchClientServerWithHugeResponseTest extends AbstractBusClientServerTestBase {
+
+    private static final QName SERVICE_NAME 
+        = new QName("http://apache.org/hello_world_soap_http", "SOAPDispatchService");
+    private static final QName PORT_NAME 
+        = new QName("http://apache.org/hello_world_soap_http", "SoapDispatchPort");
+
+    private static String greeterPort = 
+        TestUtil.getPortNumber(DispatchClientServerWithHugeResponseTest.class); 
+    
+    public static class Server extends AbstractBusTestServerBase {        
+
+        protected void run() {
+            Object implementor = new GreeterImpl();
+            String address = "http://localhost:"
+                + TestUtil.getPortNumber(DispatchClientServerWithHugeResponseTest.class)
+                + "/SOAPDispatchService/SoapDispatchPort";
+            Endpoint.publish(address, implementor);
+
+        }
+
+        public static void main(String[] args) {
+            try {
+                Server s = new Server();
+                s.start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.exit(-1);
+            } finally {
+                System.out.println("done!");
+            }
+        }
+    }
+
+    @BeforeClass
+    public static void startServers() throws Exception {
+        assertTrue("server did not launch correctly", launchServer(Server.class));
+    }
+    
+    @org.junit.Before
+    public void setUp() {
+        BusFactory.getDefaultBus().getOutInterceptors().add(new LoggingOutInterceptor());
+        BusFactory.getDefaultBus().getInInterceptors().add(new LoggingInInterceptor());
+        BusFactory.getDefaultBus().getInInterceptors().add(new HugeResponseInterceptor());
+    }
+    
+    
+   
+    @Test
+    public void testSOAPMessageWithHugeResponse() throws Exception {
+
+        URL wsdl = getClass().getResource("/wsdl/hello_world.wsdl");
+        assertNotNull(wsdl);
+
+        SOAPService service = new SOAPService(wsdl, SERVICE_NAME);
+        assertNotNull(service);
+
+        Dispatch<SOAPMessage> disp = service
+            .createDispatch(PORT_NAME, SOAPMessage.class, Service.Mode.MESSAGE);
+        disp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                                     "http://localhost:" 
+                                     + greeterPort
+                                     + "/SOAPDispatchService/SoapDispatchPort");
+        
+        
+
+        InputStream is3 = getClass().getResourceAsStream("resources/GreetMeDocLiteralReq3.xml");
+        SOAPMessage soapReqMsg3 = MessageFactory.newInstance().createMessage(null, is3);
+        assertNotNull(soapReqMsg3);
+        Response<SOAPMessage> response = disp.invokeAsync(soapReqMsg3);
+        try {
+            response.get(10, TimeUnit.SECONDS);
+        } catch (TimeoutException te) {
+            fail("We should not have encountered a timeout, " 
+                + "should get some exception tell me stackoverflow");
+        } catch (Throwable e) {
+            assertTrue(e.getCause() instanceof StackOverflowError);
+        }
+        
+    }
+     
+
+}
