5b2a8869d3d076ad39b65f4d1bc90fc6cd6f0288
Freeman Yue Fang <ffang@apache.org>
2010-12-20 13:27:03 +0000

    [CXF-2810]wrong namespace in generated wsdl which conflict with aegis type annation namespace in SEI
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1051094 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/databinding/src/test/java/org/apache/cxf/systest/aegis/AegisWSDLNSTest.java
@@ -0,0 +1,98 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.systest.aegis;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+
+import org.w3c.dom.Document;
+
+import org.apache.cxf.aegis.databinding.AegisDatabinding;
+import org.apache.cxf.helpers.XMLUtils;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.test.TestUtilities;
+import org.apache.cxf.testutil.common.TestUtil;
+
+import org.junit.Test;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+/**
+ * 
+ */
+@ContextConfiguration(locations = { "classpath:aegisWSDLNSBeans.xml" })
+public class AegisWSDLNSTest extends AbstractJUnit4SpringContextTests {
+    static final String PORT = TestUtil.getPortNumber(AegisWSDLNSTest.class);
+
+    private AegisJaxWsWsdlNs client;
+    
+    public AegisWSDLNSTest() {
+    }
+    
+    private void setupForTest(boolean specifyWsdl) throws Exception {
+        
+        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        factory.setServiceClass(AegisJaxWsWsdlNs.class);
+        if (specifyWsdl) {
+            factory.setServiceName(new QName("http://v1_1_2.rtf2pdf.doc.ws.daisy.marbes.cz", 
+                                             "AegisJaxWsWsdlNsImplService")); 
+            factory.setWsdlLocation("http://localhost:" + PORT + "/aegisJaxWsWSDLNS?wsdl");
+        }
+        factory.getServiceFactory().setDataBinding(new AegisDatabinding());
+        factory.setAddress("http://localhost:" + PORT + "/aegisJaxWsWSDLNS"); 
+        client = (AegisJaxWsWsdlNs)factory.create();
+    }
+    
+    @Test
+    public void testWithInterface() throws Exception {
+        setupForTest(false);
+        AegisJaxWsWsdlNs.VO vo = new AegisJaxWsWsdlNs.VO();
+        vo.setStr("ffang");
+        client.updateVO(vo);
+    }
+    
+    @Test
+    public void testWithWsdl() throws Exception {
+        setupForTest(true);
+        AegisJaxWsWsdlNs.VO vo = new AegisJaxWsWsdlNs.VO();
+        vo.setStr("ffang");
+        client.updateVO(vo);        
+    }
+    
+    @Test
+    public void testGeneratedWsdlNs() throws Exception {
+        URL url = new URL("http://localhost:" + PORT + "/aegisJaxWsWSDLNS?wsdl");
+        Document dom = XMLUtils.parse(url.openStream());
+        TestUtilities util = new TestUtilities(this.getClass());
+        util.addDefaultNamespaces();
+        util.assertValid(
+                         "//wsdl:definitions[@targetNamespace" 
+                         + "='http://v1_1_2.rtf2pdf.doc.ws.daisy.marbes.cz']",
+                         dom);
+        //should be a targetNamespace for "http://wo.rtf2pdf.doc.ws.daisy.marbes.cz"
+        //as VO type specified in the SEI
+        util.assertValid("//wsdl:definitions/wsdl:types/xsd:schema[@targetNamespace"
+                         + "='http://wo.rtf2pdf.doc.ws.daisy.marbes.cz']",
+                         dom);
+    }
+   
+}
