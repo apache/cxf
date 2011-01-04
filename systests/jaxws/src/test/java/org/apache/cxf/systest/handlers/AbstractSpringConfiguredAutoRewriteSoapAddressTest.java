4454498dc3553f15ad0aae382e06e11b607c20c4
J. Daniel Kulp <dkulp@apache.org>
2011-01-04 04:23:28 +0000

    Merged revisions 1054890 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1054890 | dkulp | 2011-01-03 23:22:34 -0500 (Mon, 03 Jan 2011) | 2 lines
      
      [CXF-2770] Support for autoRewriteSoapAddress frmo Spring
      Patch from Jim Talbut applied
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1054891 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/handlers/AbstractSpringConfiguredAutoRewriteSoapAddressTest.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.systest.handlers;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.LinkedList;
+import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.apache.cxf.helpers.DOMUtils;
+import org.apache.cxf.test.AbstractCXFSpringTest;
+
+/**
+ *
+ */
+public abstract class AbstractSpringConfiguredAutoRewriteSoapAddressTest extends AbstractCXFSpringTest {
+
+    private Document retrieveWsdlDocument(String hostname, String port) throws Exception {
+        URL wsdlUrlLocalhost = new URL("http://" + hostname + ":" + port + "/SpringEndpoint?wsdl");
+        URLConnection urlConnection = wsdlUrlLocalhost.openConnection();
+        return DOMUtils.readXml(urlConnection.getInputStream());
+    }
+
+    protected List<String> findAllServiceUrlsFromWsdl(String hostname, String port) throws Exception {
+        Document wsdlDocument = retrieveWsdlDocument(hostname, port);
+        List<String> serviceUrls = new LinkedList<String>();
+        List<Element> serviceList = DOMUtils.findAllElementsByTagNameNS(wsdlDocument.getDocumentElement(),
+                                                          "http://schemas.xmlsoap.org/wsdl/",
+                                                          "service");
+        for (Element serviceEl : serviceList) {
+            List<Element> portList = DOMUtils.findAllElementsByTagNameNS(serviceEl,
+                                                          "http://schemas.xmlsoap.org/wsdl/",
+                                                          "port");
+            for (Element portEl : portList) {
+                List<Element> addressList = DOMUtils.findAllElementsByTagNameNS(portEl,
+                                                          "http://schemas.xmlsoap.org/wsdl/soap/",
+                                                          "address");
+                for (Element addressEl : addressList) {
+                    serviceUrls.add(addressEl.getAttribute("location"));
+                }
+            }
+        }
+        return serviceUrls;
+    }
+
+}
