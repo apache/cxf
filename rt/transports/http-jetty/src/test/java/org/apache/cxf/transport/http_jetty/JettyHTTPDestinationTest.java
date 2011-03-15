b8e2429e6dfb9d85e1264fd7a54567ad9ead99bf
Willem Ning Jiang <ningjiang@apache.org>
2011-03-15 08:43:07 +0000

    Fixed the unit test error of JettyHTTPDestinationTest
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1081683 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/http-jetty/src/test/java/org/apache/cxf/transport/http_jetty/JettyHTTPDestinationTest.java
+++ rt/transports/http-jetty/src/test/java/org/apache/cxf/transport/http_jetty/JettyHTTPDestinationTest.java
@@ -700,6 +700,7 @@ public class JettyHTTPDestinationTest extends Assert {
                 EasyMock.expect(request.getContextPath()).andReturn("/bar");
                 EasyMock.expect(request.getPathInfo()).andReturn("/foo");
                 EasyMock.expect(request.getRequestURI()).andReturn("/foo");
+                EasyMock.expect(request.getRequestURL()).andReturn(new StringBuffer("http://test/request"));
                 EasyMock.expect(request.getCharacterEncoding()).andReturn("UTF-8");
                 EasyMock.expect(request.getQueryString()).andReturn(query);    
                 EasyMock.expect(request.getHeader("Accept")).andReturn("*/*");  
