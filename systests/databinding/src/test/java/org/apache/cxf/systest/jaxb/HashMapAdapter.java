bc307a47c1347795c6e6d3f816633a41384f6be9
J. Daniel Kulp <dkulp@apache.org>
2011-03-15 02:45:43 +0000

    [CXF-3383] In some very strange situations with several small attachments and read(buf, off, len) called with off != 0, the data at the end of various buffers can get lost and boundaries might not be found.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1081648 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/databinding/src/test/java/org/apache/cxf/systest/jaxb/HashMapAdapter.java
@@ -0,0 +1,103 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.systest.jaxb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * 
+ */
+public class HashMapAdapter extends XmlAdapter<HashMapAdapter.HashMapType, Map<String, byte[]>> {
+    
+    @XmlType()
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class HashMapEntryType {
+        @XmlAttribute
+        private String key;
+        @XmlValue
+        private byte[] value;
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public byte[] getValue() {
+            return value;
+        }
+
+        public void setValue(byte[] value) {
+            this.value = value;
+        }
+    }
+
+    public static class HashMapType {
+        private List<HashMapEntryType> entry = new ArrayList<HashMapEntryType>();
+
+        public List<HashMapEntryType> getEntry() {
+            return entry;
+        }
+
+        public void setEntry(List<HashMapEntryType> entry) {
+            this.entry = entry;
+        }
+    }
+    
+    public HashMapType marshal(Map<String, byte[]> arg0) throws Exception {
+        HashMapType myHashMapType = new HashMapType();
+        if (arg0 != null && arg0.size() > 0) {
+            for (Map.Entry<String, byte[]> entry : arg0.entrySet()) {
+                if (entry != null) {
+                    HashMapEntryType myHashEntryType = new HashMapEntryType();
+                    myHashEntryType.key = entry.getKey();
+                    myHashEntryType.value = entry.getValue();
+                    myHashMapType.entry.add(myHashEntryType);
+                }
+            }
+        }
+        return myHashMapType;
+    }
+
+    public Map<String, byte[]> unmarshal(HashMapType arg0) throws Exception {
+        Map<String, byte[]> hashMap = new HashMap<String, byte[]>();
+        if (arg0 != null && arg0.entry != null) {
+            for (HashMapEntryType myHashEntryType : arg0.entry) {
+                if (myHashEntryType.key != null) {
+                    hashMap.put(myHashEntryType.key, myHashEntryType.value);
+                }
+            }
+        }
+        return hashMap;
+    }
+
+}
