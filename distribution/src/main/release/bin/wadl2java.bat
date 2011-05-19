9c2e16e264de7f1cddb02a5117f495076473e28f
Sergey Beryozkin <sergeyb@apache.org>
2011-05-19 17:13:28 +0000

    [CXF-3948] Adding wadl2java batch files
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1125003 13f79535-47bb-0310-9956-ffa450edef68

---
+++ distribution/src/main/release/bin/wadl2java.bat
@@ -0,0 +1,58 @@
+@echo off
+rem
+rem
+rem    Licensed to the Apache Software Foundation (ASF) under one
+rem    or more contributor license agreements. See the NOTICE file
+rem    distributed with this work for additional information
+rem    regarding copyright ownership. The ASF licenses this file
+rem    to you under the Apache License, Version 2.0 (the
+rem    "License"); you may not use this file except in compliance
+rem    with the License. You may obtain a copy of the License at
+rem
+rem    http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem    Unless required by applicable law or agreed to in writing,
+rem    software distributed under the License is distributed on an
+rem    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+rem    KIND, either express or implied. See the License for the
+rem    specific language governing permissions and limitations
+rem    under the License.
+rem
+rem
+
+rem 
+rem  invoke the Apache CXF wadl2java tool
+rem 
+@setlocal
+
+if not defined CXF_HOME goto set_cxf_home
+                                                                                                                                                             
+:cont
+if not defined JAVA_HOME goto no_java_home
+
+set TOOLS_JAR=%JAVA_HOME%\lib\tools.jar;
+
+if not exist "%CXF_HOME%\lib\cxf-manifest.jar" goto no_cxf_jar
+
+set CXF_JAR=%CXF_HOME%\lib\cxf-manifest.jar
+
+
+"%JAVA_HOME%\bin\java" -Xmx128M -Djava.endorsed.dirs="%CXF_HOME%\lib\endorsed" -cp "%CXF_JAR%;%TOOLS_JAR%;%CLASSPATH%" -Djava.util.logging.config.file="%CXF_HOME%\etc\logging.properties" org.apache.cxf.tools.wadlto.WADLToJava %*
+
+@endlocal
+
+goto end
+
+:no_cxf_jar
+echo ERROR: Unable to find cxf-manifest.jar in %cxf_home/lib
+goto end
+
+:no_java_home
+echo ERROR: Set JAVA_HOME to the path where the JDK (5.0 or higher) is installed
+goto end 
+
+:set_cxf_home
+set CXF_HOME=%~dp0..
+goto cont
+
+:end
