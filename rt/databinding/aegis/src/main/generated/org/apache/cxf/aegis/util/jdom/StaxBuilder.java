5ca380df33a63b9f4642683c92c337f0f21b6962
J. Daniel Kulp <dkulp@apache.org>
2011-06-29 08:51:27 +0000

    Remove some svn attribute things that don't really work with GIT and complicate various diffs and merges and such.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1141009 13f79535-47bb-0310-9956-ffa450edef68

--- rt/databinding/aegis/src/main/generated/org/apache/cxf/aegis/util/jdom/StaxBuilder.java
+++ rt/databinding/aegis/src/main/generated/org/apache/cxf/aegis/util/jdom/StaxBuilder.java
@@ -77,8 +77,6 @@ import org.jdom.UncheckedJDOMFactory;
  * Builds a JDOM {@link org.jdom.Document org.jdom.Document} using a
  * {@link javax.xml.stream.XMLStreamReader}.
  * 
- * @version $Revision$, $Date: 2006-02-15 15:21:25 -0500 (Wed, 15 Feb
- *          2006) $
  * @author Tatu Saloranta
  * @author Bradley S. Huffman
  * @author Benson I. Margulies, mods for CXF to allow reading a portion of a stream.
