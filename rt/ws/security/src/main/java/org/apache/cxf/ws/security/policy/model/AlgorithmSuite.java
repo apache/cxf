104e587fbe93e0b14b606d1afdc04739faae672b
Colm O Heigeartaigh <coheigea@apache.org>
2011-08-15 14:33:55 +0000

    Correcting a bug in AlgorithmSuite
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1157862 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/policy/model/AlgorithmSuite.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/policy/model/AlgorithmSuite.java
@@ -510,7 +510,7 @@ public class AlgorithmSuite extends AbstractSecurityAssertion {
             this.encryptionDerivedKeyLength = 128;
             this.signatureDerivedKeyLength = 128;
             this.minimumSymmetricKeyLength = 128;
-            this.maximumSymmetricKeyLength = 192;
+            this.maximumSymmetricKeyLength = 128;
         } else if (SPConstants.ALGO_SUITE_TRIPLE_DES_SHA256_RSA15.equals(algoSuite)) {
             this.digest = SPConstants.SHA256;
             this.encryption = SPConstants.TRIPLE_DES;
