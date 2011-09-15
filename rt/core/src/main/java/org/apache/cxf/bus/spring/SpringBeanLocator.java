69632ff18f98d6a9351e7329375a5c9ad9f2d932
J. Daniel Kulp <dkulp@apache.org>
2011-09-15 19:09:19 +0000

    Merged revisions 1171224 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1171224 | dkulp | 2011-09-15 15:03:31 -0400 (Thu, 15 Sep 2011) | 2 lines
      
      [CXF-3805] Fix problem with initializing all the factories when not
      needed
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1171230 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/bus/spring/SpringBeanLocator.java
+++ rt/core/src/main/java/org/apache/cxf/bus/spring/SpringBeanLocator.java
@@ -100,7 +100,7 @@ public class SpringBeanLocator implements ConfiguredBeanLocator {
     public List<String> getBeanNamesOfType(Class<?> type) {
         Set<String> s = new LinkedHashSet<String>(Arrays.asList(context.getBeanNamesForType(type,
                                                                                          false,
-                                                                                         true)));
+                                                                                         false)));
         s.removeAll(passThroughs);
         s.addAll(orig.getBeanNamesOfType(type));
         return new ArrayList<String>(s);
@@ -110,7 +110,7 @@ public class SpringBeanLocator implements ConfiguredBeanLocator {
     public <T> Collection<? extends T> getBeansOfType(Class<T> type) {
         Set<String> s = new LinkedHashSet<String>(Arrays.asList(context.getBeanNamesForType(type,
                                                                                             false,
-                                                                                            true)));
+                                                                                            false)));
         s.removeAll(passThroughs);
         List<T> lst = new LinkedList<T>();
         for (String n : s) {
@@ -150,7 +150,7 @@ public class SpringBeanLocator implements ConfiguredBeanLocator {
                                        BeanLoaderListener<T> listener) {
         List<String> list = new ArrayList<String>(Arrays.asList(context.getBeanNamesForType(type,
                                                                                             false,
-                                                                                            true)));
+                                                                                            false)));
         list.removeAll(passThroughs);
         Collections.reverse(list);
         boolean loaded = false;
