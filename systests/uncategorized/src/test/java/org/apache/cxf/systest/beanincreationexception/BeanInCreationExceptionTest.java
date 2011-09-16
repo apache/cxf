b478219e37f0492d8f50dd7d5102b365fd2ca52d
J. Daniel Kulp <dkulp@apache.org>
2011-09-16 16:53:28 +0000

    [CXF-3805] Add testcase for CXF-3805 Test from Tomasz Bech added
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1171659 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/uncategorized/src/test/java/org/apache/cxf/systest/beanincreationexception/BeanInCreationExceptionTest.java
@@ -0,0 +1,43 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.systest.beanincreationexception;
+
+import org.apache.cxf.test.AbstractCXFSpringTest;
+import org.apache.cxf.testutil.common.TestUtil;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class BeanInCreationExceptionTest extends AbstractCXFSpringTest {
+    static String port = TestUtil.getPortNumber("springport");
+
+    @Override
+    protected String[] getConfigLocations() {
+        return new String[] {"classpath:/org/apache/cxf/systest/beanincreationexception/beans.xml"};
+    }
+
+    @Test
+    public void testBeanInCreationExceptionTest() throws Exception {
+        //CXF-3805
+        //if it gets in here, creation of the context worked.   
+    }
+}
---
+++ systests/uncategorized/src/test/java/org/apache/cxf/systest/beanincreationexception/DummyPlatformTransactionManager.java
@@ -0,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.beanincreationexception;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
+
+public class DummyPlatformTransactionManager implements PlatformTransactionManager {
+
+    @Override
+    public void commit(TransactionStatus arg0) throws TransactionException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public TransactionStatus getTransaction(TransactionDefinition arg0) throws TransactionException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void rollback(TransactionStatus arg0) throws TransactionException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
