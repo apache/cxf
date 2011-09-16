3336ba8306ff08e3a420a8b4abbce9b82252e472
J. Daniel Kulp <dkulp@apache.org>
2011-09-16 21:30:06 +0000

    Remove property added to workaround Camel issue that is now fixed in Camel
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1171799 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/message/Message.java
+++ api/src/main/java/org/apache/cxf/message/Message.java
@@ -68,7 +68,6 @@ public interface Message extends StringMap {
     String DECOUPLED_CHANNEL_MESSAGE = "decoupled.channel.message";
     String PARTIAL_RESPONSE_MESSAGE = "org.apache.cxf.partial.response";
     String EMPTY_PARTIAL_RESPONSE_MESSAGE = "org.apache.cxf.partial.response.empty";
-    String HANDLING_PARTIAL_RESPONSE_MESSAGE = "org.apache.cxf.partial.response.handling";
     String ONE_WAY_REQUEST = "OnewayRequest";
     
 
