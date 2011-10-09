5f800c1a61984f4dc93e9d248d703f875cb343e7
Sergey Beryozkin <sergeyb@apache.org>
2011-10-09 16:57:40 +0000

    Merged revisions 1180649 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1180649 | sergeyb | 2011-10-09 17:54:55 +0100 (Sun, 09 Oct 2011) | 1 line
      
      [CXF-3849] Removing printing the stack trace
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1180650 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/JAXBElementProvider.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/JAXBElementProvider.java
@@ -168,7 +168,8 @@ public class JAXBElementProvider extends AbstractJAXBProvider  {
         } catch (WebApplicationException e) {
             throw e;
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.warning("Exception has occurred: " + e.getClass().getName() 
+                    + ", message: " + e.getMessage());
             throw new WebApplicationException(e);        
         }
         // unreachable
