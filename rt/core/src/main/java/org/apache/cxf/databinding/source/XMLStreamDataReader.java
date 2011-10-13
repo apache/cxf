fc1a13de2fd66327dc1df3b301def02d5d473c87
Willem Ning Jiang <ningjiang@apache.org>
2011-10-13 09:25:32 +0000

    Merged revisions 1182730 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes
    ................
      r1182730 | ningjiang | 2011-10-13 17:06:18 +0800 (Thu, 13 Oct 2011) | 9 lines
      
      Merged revisions 1182716 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1182716 | ningjiang | 2011-10-13 16:31:07 +0800 (Thu, 13 Oct 2011) | 1 line
        
        CXF-3858 Added the cause exception message when throwing the fault from XMLStreamDataReader and XMLStreamDataWriter
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1182741 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/databinding/source/XMLStreamDataReader.java
+++ rt/core/src/main/java/org/apache/cxf/databinding/source/XMLStreamDataReader.java
@@ -140,9 +140,9 @@ public class XMLStreamDataReader implements DataReader<XMLStreamReader> {
         } catch (IOException e) {
             throw new Fault("COULD_NOT_READ_XML_STREAM", LOG, e);
         } catch (XMLStreamException e) {
-            throw new Fault("COULD_NOT_READ_XML_STREAM", LOG, e);
+            throw new Fault("COULD_NOT_REDA_XML_STREAM_CAUSED_BY", LOG, e, e.getMessage());
         } catch (SAXException e) {
-            throw new Fault("COULD_NOT_READ_XML_STREAM", LOG, e);
+            throw new Fault("COULD_NOT_REDA_XML_STREAM_CAUSED_BY", LOG, e, e.getMessage());
         }
     }
     
@@ -228,7 +228,7 @@ public class XMLStreamDataReader implements DataReader<XMLStreamReader> {
                 return new DOMSource(document);
             }
         } catch (XMLStreamException e) {
-            throw new Fault("COULD_NOT_READ_XML_STREAM", LOG, e);
+            throw new Fault("COULD_NOT_REDA_XML_STREAM_CAUSED_BY", LOG, e, e.getMessage());
         }
     }
     
