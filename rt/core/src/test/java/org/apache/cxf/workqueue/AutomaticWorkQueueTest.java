a0bfd4bd529464febe55f2bb12a37a5d45ebda65
Willem Ning Jiang <ningjiang@apache.org>
2011-10-14 03:12:50 +0000

    Merged revisions 1183212 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1183212 | ningjiang | 2011-10-14 10:53:08 +0800 (Fri, 14 Oct 2011) | 1 line
      
      Try to fix the AutomaticWorkQueueTest test failure
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1183215 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/test/java/org/apache/cxf/workqueue/AutomaticWorkQueueTest.java
+++ rt/core/src/test/java/org/apache/cxf/workqueue/AutomaticWorkQueueTest.java
@@ -147,7 +147,7 @@ public class AutomaticWorkQueueTest extends Assert {
                 }
             }
 
-            while (workqueue.getActiveCount() < DEFAULT_HIGH_WATER_MARK - 1) {
+            while (workqueue.getActiveCount() < DEFAULT_HIGH_WATER_MARK) {
                 try {
                     Thread.sleep(250);
                 } catch (InterruptedException ex) {
