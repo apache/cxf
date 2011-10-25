0171c8b8ab3f85b20d867504b041f7c7a1c80c8e
Sergey Beryozkin <sergeyb@apache.org>
2011-10-25 13:26:17 +0000

    Reenabling one of the two disabled transform tests
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1188648 13f79535-47bb-0310-9956-ffa450edef68

--- systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/JAXRSSoapBookTest.java
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/JAXRSSoapBookTest.java
@@ -153,7 +153,6 @@ public class JAXRSSoapBookTest extends AbstractBusClientServerTestBase {
         assertEquals("0", getStringFromInputStream(in));
                 
     }
-    @Ignore
     @Test
     public void testGetBookTransform() throws Exception {
         
