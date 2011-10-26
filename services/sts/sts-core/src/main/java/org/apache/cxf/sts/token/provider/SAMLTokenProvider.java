e83dff85e100ebf4b96794a9d6eaa14eb4178d76
Colm O Heigeartaigh <coheigea@apache.org>
2011-10-26 18:06:53 +0000

    Fixing problem with last commit.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1189353 13f79535-47bb-0310-9956-ffa450edef68

--- services/sts/sts-core/src/main/java/org/apache/cxf/sts/token/provider/SAMLTokenProvider.java
+++ services/sts/sts-core/src/main/java/org/apache/cxf/sts/token/provider/SAMLTokenProvider.java
@@ -201,14 +201,14 @@ public class SAMLTokenProvider implements TokenProvider {
     public void setAuthDecisionStatementProviders(
         List<AuthDecisionStatementProvider> authDecisionStatementProviders
     ) {
-        this.authenticationDecisionStatementProviders = authDecisionStatementProviders;
+        this.authDecisionStatementProviders = authDecisionStatementProviders;
     }
     
     /**
      * Get the List of AuthDecisionStatementProviders.
      */
     public List<AuthDecisionStatementProvider> getAuthDecisionStatementProviders() {
-        return authenticationDecisionStatementProviders;
+        return authDecisionStatementProviders;
     }
 
     /**
