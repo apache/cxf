dc847cde3a6d5f66498526e09d781533ce1710a8
Benson Margulies <bimargulies@apache.org>
2011-11-14 19:03:56 +0000

    CXF-3912: add an archetype for JAX-RS.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1201826 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-plugins/archetypes/cxf-jaxrs-service/src/main/resources/archetype-resources/src/test/java/HelloWorldIT.java
@@ -0,0 +1,54 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.helpers.IOUtils;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.MappingJsonFactory;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HelloWorldIT {
+	private static String endpointUrl;
+	
+	@BeforeClass
+	public static void beforeClass() {
+		endpointUrl = System.getProperty("service.url");
+	}
+	
+	@Test
+	public void testPing() throws Exception {
+		WebClient client = WebClient.create(endpointUrl + "/hello/echo/SierraTangoNevada");
+		Response r = client.accept("text/plain").get();
+		assertEquals(Response.Status.OK.getStatusCode(), r.getStatus());
+		String value = IOUtils.toString((InputStream)r.getEntity());
+		assertEquals("SierraTangoNevada", value);
+	}
+	
+	@Test
+	public void testJsonRoundtrip() throws Exception {
+		 List<Object> providers = new ArrayList<Object>();
+	    providers.add(new org.codehaus.jackson.jaxrs.JacksonJsonProvider());
+	    JsonBean inputBean = new JsonBean();
+	    inputBean.setVal1("Maple");
+		WebClient client = WebClient.create(endpointUrl + "/hello/jsonBean", providers);
+		Response r = client.accept("application/json")
+				.type("application/json")
+				.post(inputBean);
+		assertEquals(Response.Status.OK.getStatusCode(), r.getStatus());
+		MappingJsonFactory factory = new MappingJsonFactory();
+		JsonParser parser = factory.createJsonParser((InputStream)r.getEntity());
+		JsonBean output = parser.readValueAs(JsonBean.class);
+		assertEquals("Maple", output.getVal2());
+	}
+}
