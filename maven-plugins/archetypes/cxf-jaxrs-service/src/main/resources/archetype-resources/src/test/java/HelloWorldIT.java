46210b61c7694624db9c8e39ef90971d9e4c308b
Sergey Beryozkin <sergeyb@apache.org>
2011-11-16 10:42:29 +0000

    [CXF-3912,CXF-3907] Few more minor updates
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1202624 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/archetypes/cxf-jaxrs-service/src/main/resources/archetype-resources/src/test/java/HelloWorldIT.java
+++ maven-plugins/archetypes/cxf-jaxrs-service/src/main/resources/archetype-resources/src/test/java/HelloWorldIT.java
@@ -1,7 +1,4 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package};
+package ${packageName};
 
 import static org.junit.Assert.assertEquals;
 
