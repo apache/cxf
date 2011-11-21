46bba290b9252436c8958b4b349331352a233368
Akitoshi Yoshida <ay@apache.org>
2011-11-21 16:43:29 +0000

    Merged revisions 1204500,1204537 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1204500 | ay | 2011-11-21 14:59:23 +0100 (Mon, 21 Nov 2011) | 1 line
      
      [CXF-3921] makingRMTxStore's tabel-exists error state configurable
    ........
      r1204537 | ay | 2011-11-21 16:19:36 +0100 (Mon, 21 Nov 2011) | 1 line
      
      fixing jdk15 build for CXF-3921
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1204585 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/rm/src/main/java/org/apache/cxf/ws/rm/persistence/jdbc/RMTxStore.java
+++ rt/ws/rm/src/main/java/org/apache/cxf/ws/rm/persistence/jdbc/RMTxStore.java
@@ -148,6 +148,9 @@ public class RMTxStore implements RMStore {
     private String userName;
     private String password;
     
+    private String tableExistsState = DERBY_TABLE_EXISTS_STATE;
+    private int tableExistsCode = ORACLE_TABLE_EXISTS_CODE;
+    
     // configuration
     
     public void setDriverClassName(String dcn) {
@@ -182,6 +185,22 @@ public class RMTxStore implements RMStore {
         return userName;
     }
    
+    public String getTableExistsState() {
+        return tableExistsState;
+    }
+
+    public void setTableExistsState(String tableExistsState) {
+        this.tableExistsState = tableExistsState;
+    }
+
+    public int getTableExistsCode() {
+        return tableExistsCode;
+    }
+
+    public void setTableExistsCode(int tableExistsCode) {
+        this.tableExistsCode = tableExistsCode;
+    }
+
     public void setConnection(Connection c) {
         connection = c;
     }
@@ -742,8 +761,9 @@ public class RMTxStore implements RMStore {
     }
 
     protected boolean isTableExistsError(SQLException ex) {
-        return DERBY_TABLE_EXISTS_STATE.equals(ex.getSQLState())
-                || ORACLE_TABLE_EXISTS_CODE == ex.getErrorCode();
+        // we could be deriving the state/code from the driver url to avoid explicit setting of them
+        return (null != tableExistsState && tableExistsState.equals(ex.getSQLState()))
+                || tableExistsCode == ex.getErrorCode();
     }
 
 
