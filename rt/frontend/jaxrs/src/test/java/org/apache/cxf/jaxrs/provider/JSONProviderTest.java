04986de986e6eb7c6c365b24ea11e0d1450ad547
Sergey Beryozkin <sergeyb@apache.org>
2011-12-23 12:29:00 +0000

    Merged revisions 1222654 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes
    ................
      r1222654 | sergeyb | 2011-12-23 12:16:53 +0000 (Fri, 23 Dec 2011) | 9 lines
      
      Merged revisions 1222652 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1222652 | sergeyb | 2011-12-23 11:59:06 +0000 (Fri, 23 Dec 2011) | 1 line
        
        Minor update to JSONProviderTest
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1222656 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/provider/JSONProviderTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/provider/JSONProviderTest.java
@@ -456,7 +456,6 @@ public class JSONProviderTest extends Assert {
     }
     
     @Test
-    @Ignore("Enable once http://jira.codehaus.org/browse/JETTISON-104 gets resolved")
     public void testReadQualifiedArray() throws Exception {
         String data = "{\"ns1.thetag\":[{\"group\":\"b\",\"name\":\"a\"}"
             + ",{\"group\":\"d\",\"name\":\"c\"}]}";
@@ -530,9 +529,9 @@ public class JSONProviderTest extends Assert {
     }
     
     @Test
-    @Ignore("This is hitting http://jira.codehaus.org/browse/JETTISON-44")
+    @Ignore("Enable once http://jira.codehaus.org/browse/JETTISON-75 gets fixed")
     public void testWriteMixedContent() throws Exception {
-        doTestMixedContent("{\"Book\":{\"name\":\"CXF\",\"id\":125,\"$\":\"\\n     \\n\"}}",
+        doTestMixedContent("{\"Book\":{\"name\":\"CXF\",\"id\":125}}",
                            false, "book.xml");
     }
     
