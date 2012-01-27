295e42700d2f12380c7b0758f2f50c46d680e103
Sergey Beryozkin <sergeyb@apache.org>
2012-01-27 15:40:22 +0000

    Removing redundant oauth-test module
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1236720 13f79535-47bb-0310-9956-ffa450edef68

--- systests/rs-security/src/test/java/org/apache/cxf/systest/jaxrs/security/oauth/OAuthTestUtils.java
+++ systests/rs-security/src/test/java/org/apache/cxf/systest/jaxrs/security/oauth/OAuthTestUtils.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.cxf.rs.security.oauth.test;
+package org.apache.cxf.systest.jaxrs.security.oauth;
 
 import java.io.BufferedReader;
 import java.io.IOException;
