b7658b31fe172eee288eaa6678ae87e33858ebba
Akitoshi Yoshida <ay@apache.org>
2012-02-01 22:29:19 +0000

    [CXF-4072] avoid NPE when a fault has no part
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1239373 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/interceptor/FaultOutInterceptor.java
+++ api/src/main/java/org/apache/cxf/interceptor/FaultOutInterceptor.java
@@ -168,7 +168,7 @@ public class FaultOutInterceptor extends AbstractPhaseInterceptor<Message> {
 
             FaultInfo faultInfo = bfi.getFaultInfo();
             Class<?> c = (Class<?>)faultInfo.getProperty(Class.class.getName());
-            if (c.isAssignableFrom(class1)) {
+            if (c != null && c.isAssignableFrom(class1)) {
                 return faultInfo;
             }
         }
