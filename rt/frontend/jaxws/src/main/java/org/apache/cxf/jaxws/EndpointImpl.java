97f8e63eb11ab8fdc0ca3e5f2f9f3d7fb435402c
Freeman Yue Fang <ffang@apache.org>
2012-02-06 03:38:35 +0000

    Merged revisions 1240882 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes
    ................
      r1240882 | ffang | 2012-02-06 10:45:47 +0800 (一, 06  2 2012) | 9 lines
      
      Merged revisions 1240879 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1240879 | ffang | 2012-02-06 10:22:21 +0800 (一, 06  2 2012) | 1 line
        
        [CXF-4085]use SystemPropertyAction.getProperty but not System.getProperty
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1240885 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/EndpointImpl.java
+++ rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/EndpointImpl.java
@@ -51,6 +51,7 @@ import org.apache.cxf.binding.BindingConfiguration;
 import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.common.util.ClassHelper;
 import org.apache.cxf.common.util.ModCountCopyOnWriteArrayList;
+import org.apache.cxf.common.util.SystemPropertyAction;
 import org.apache.cxf.configuration.Configurable;
 import org.apache.cxf.configuration.Configurer;
 import org.apache.cxf.databinding.DataBinding;
@@ -490,7 +491,7 @@ public class EndpointImpl extends javax.xml.ws.Endpoint
         SecurityManager sm = System.getSecurityManager();
         boolean checkPublishEndpointPermissionWithSecurityManager
             = Boolean.valueOf(
-                      System.getProperty(
+                      SystemPropertyAction.getProperty(
                                          CHECK_PUBLISH_ENDPOINT_PERMISSON_PROPERTY_WITH_SECURITY_MANAGER, 
                                          "true"));
         if (checkPublishEndpointPermissionWithSecurityManager && sm != null) {
