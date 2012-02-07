5622fe8d4dd0ef6b993bc9e4afc280b0863c95a5
Sergey Beryozkin <sergeyb@apache.org>
2012-02-07 10:27:37 +0000

    Merged revisions 1241412 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes
    ................
      r1241412 | sergeyb | 2012-02-07 10:24:34 +0000 (Tue, 07 Feb 2012) | 9 lines
      
      Merged revisions 1241195 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1241195 | sergeyb | 2012-02-06 21:40:21 +0000 (Mon, 06 Feb 2012) | 1 line
        
        [CXF-4088] Checking getGenericSuperclass when selecting providers
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1241413 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/provider/ProviderFactoryTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/provider/ProviderFactoryTest.java
@@ -391,7 +391,6 @@ public class ProviderFactoryTest extends Assert {
     }
     
     @Test
-    @Ignore
     public void testExceptionMappersHierarchyWithGenerics() throws Exception {
         ProviderFactory pf = ProviderFactory.getInstance();
         RuntimeExceptionMapper1 exMapper1 = new RuntimeExceptionMapper1(); 
