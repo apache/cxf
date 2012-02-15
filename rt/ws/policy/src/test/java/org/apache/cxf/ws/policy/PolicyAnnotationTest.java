4ea888bf447181559f178cb562f24004e0690634
J. Daniel Kulp <dkulp@apache.org>
2012-02-15 15:51:04 +0000

    Merged revisions 1244539 via  svn merge from https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes
    ........
      r1244539 | dkulp | 2012-02-15 10:34:27 -0500 (Wed, 15 Feb 2012) | 9 lines
      
      Merged revisions 1244529 via  git cherry-pick from
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1244529 | dkulp | 2012-02-15 10:19:16 -0500 (Wed, 15 Feb 2012) | 2 lines
      
        Fix compile failure with non-eclipse compiler
      
      ........
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1244548 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/policy/src/test/java/org/apache/cxf/ws/policy/PolicyAnnotationTest.java
+++ rt/ws/policy/src/test/java/org/apache/cxf/ws/policy/PolicyAnnotationTest.java
@@ -226,7 +226,7 @@ public class PolicyAnnotationTest extends Assert {
     
     
     @Policies({
-        @Policy(uri = "annotationpolicies/TestImplPolicy.xml"),
+        @Policy(uri = "annotationpolicies/TestImplPolicy.xml")
     }
     )
     @WebService(endpointInterface = "org.apache.cxf.ws.policy.PolicyAnnotationTest$TestInterface")
