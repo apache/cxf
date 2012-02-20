05dd1a7a9bfdc19314cfbd6176f4db597d11b3b2
Willem Ning Jiang <ningjiang@apache.org>
2012-02-20 08:34:34 +0000

    CXF-4120 JMS Transport content-type setting should be consistent with the HTTP one
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1291146 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/jms/src/main/java/org/apache/cxf/transport/jms/JMSUtils.java
+++ rt/transports/jms/src/main/java/org/apache/cxf/transport/jms/JMSUtils.java
@@ -393,7 +393,8 @@ public final class JMSUtils {
         String enc = (String)message.get(org.apache.cxf.message.Message.ENCODING);
         // add the encoding information
         if (null != contentType) {
-            if (enc != null && contentType.indexOf("charset=") == -1) {
+            if (enc != null && contentType.indexOf("charset=") == -1
+                && !contentType.toLowerCase().contains("multipart/related")) {
                 contentType = contentType + "; charset=" + enc;
             }
         } else if (enc != null) {
@@ -428,7 +429,8 @@ public final class JMSUtils {
         String enc = (String)message.get(org.apache.cxf.message.Message.ENCODING);
         // add the encoding information
         if (null != contentType) {
-            if (enc != null && contentType.indexOf("charset=") == -1) {
+            if (enc != null && contentType.indexOf("charset=") == -1&& 
+                !contentType.toLowerCase().contains("multipart/related")) {
                 contentType = contentType + "; charset=" + enc;
             }
         } else if (enc != null) {
