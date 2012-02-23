00dc8a3531ab335f936b78013a55d03be0a7e7d6
Akitoshi Yoshida <ay@apache.org>
2012-02-23 16:14:59 +0000

    [CXF-4084] Blueprint http (part 1 HTTPConduit)
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1292839 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/blueprint/HttpBPHandler.java
@@ -0,0 +1,72 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.transport.http.blueprint;
+
+import java.net.URL;
+import java.util.Set;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import org.apache.aries.blueprint.NamespaceHandler;
+//import org.apache.aries.blueprint.ParserContext;
+//import org.osgi.service.blueprint.container.BlueprintContainer;
+//import org.osgi.service.blueprint.reflect.ComponentMetadata;
+//import org.osgi.service.blueprint.reflect.Metadata;
+import org.apache.aries.blueprint.ParserContext;
+import org.osgi.service.blueprint.reflect.ComponentMetadata;
+import org.osgi.service.blueprint.reflect.Metadata;
+
+/**
+ * 
+ */
+public class HttpBPHandler implements NamespaceHandler {
+    
+    public HttpBPHandler() {
+    }
+
+    public URL getSchemaLocation(String s) {
+        if ("http://cxf.apache.org/transports/http/configuration".equals(s)) {
+            return getClass().getClassLoader().
+                getResource("/schemas/configuration/http-conf.xsd");
+        }
+        return null;
+    }
+
+
+    public Metadata parse(Element element, ParserContext context) {
+        String s = element.getLocalName();
+
+        if ("conduit".equals(s)) {
+            return new HttpConduitBPBeanDefinitionParser().parse(element, context);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public Set<Class> getManagedClasses() {
+        return null;
+    }
+    
+    public ComponentMetadata decorate(Node node, ComponentMetadata component, ParserContext context) {
+        return null;
+    }
+    
+}
