4e89d0a6fc7718a4c1771acea5cb3e69ebe53b6e
Willem Ning Jiang <ningjiang@apache.org>
2012-02-25 08:42:03 +0000

    CXF-4138 Fixed the build hung of samples/jax_rs/minimal_osgi
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1293546 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/jax_rs/minimal_osgi/src/main/java/minimalosgi/Activator.java
+++ distribution/src/main/release/samples/jax_rs/minimal_osgi/src/main/java/minimalosgi/Activator.java
@@ -51,7 +51,7 @@ public class Activator implements BundleActivator {
                 public Object addingService(ServiceReference serviceReference) {
                     try {
                         HttpService service = (HttpService)_context.getService(serviceReference);
-                        Map<String, String> initParams = new Hashtable<String, String>();
+                        Hashtable<String, String> initParams = new Hashtable<String, String>();
                         initParams.put("javax.ws.rs.Application", SampleApplication.class.getName());
                         service.registerServlet(_path, new SampleServlet(), initParams, null);
                         return service;
