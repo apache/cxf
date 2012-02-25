bf49d869960a608c93d4a57289685b1e0044d483
Willem Ning Jiang <ningjiang@apache.org>
2012-02-25 09:13:26 +0000

    CXF-4138 clean up the code
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1293552 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/jax_rs/minimal_osgi/src/main/java/minimalosgi/Activator.java
+++ distribution/src/main/release/samples/jax_rs/minimal_osgi/src/main/java/minimalosgi/Activator.java
@@ -18,7 +18,7 @@
  */
 
 package minimalosgi;
-
+import java.util.Dictionary;
 import java.util.Hashtable;
 
 import org.osgi.framework.BundleActivator;
@@ -51,7 +51,7 @@ public class Activator implements BundleActivator {
                 public Object addingService(ServiceReference serviceReference) {
                     try {
                         HttpService service = (HttpService)_context.getService(serviceReference);
-                        Hashtable<String, String> initParams = new Hashtable<String, String>();
+                        Dictionary<String, String> initParams = new Hashtable<String, String>();
                         initParams.put("javax.ws.rs.Application", SampleApplication.class.getName());
                         service.registerServlet(_path, new SampleServlet(), initParams, null);
                         return service;
