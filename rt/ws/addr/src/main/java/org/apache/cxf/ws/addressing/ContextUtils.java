a2bdfd8a9c62e0b22de91af811ce1514ba8ec929
Akitoshi Yoshida <ay@apache.org>
2012-03-01 08:44:37 +0000

    Merged revisions 1295466 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1295466 | ay | 2012-03-01 09:30:00 +0100 (Thu, 01 Mar 2012) | 1 line
      
      [CXF4096] mark partialMessage as the output message after the robust invocation completes
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1295470 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/addr/src/main/java/org/apache/cxf/ws/addressing/ContextUtils.java
+++ rt/ws/addr/src/main/java/org/apache/cxf/ws/addressing/ContextUtils.java
@@ -397,7 +397,6 @@ public final class ContextUtils {
                 if (reference == null) {
                     reference = ContextUtils.getNoneEndpointReference();
                 }
-                exchange.setOutMessage(partialResponse);
                 Conduit backChannel = target.getBackChannel(inMessage,
                                                             partialResponse,
                                                             reference);
@@ -428,6 +427,7 @@ public final class ContextUtils {
                         fullResponse != null
                         ? fullResponse.getInterceptorChain()
                         : OutgoingChainInterceptor.getOutInterceptorChain(exchange);
+                    exchange.setOutMessage(partialResponse);                    
                     partialResponse.setInterceptorChain(chain);
                     exchange.put(ConduitSelector.class,
                                  new PreexistingConduitSelector(backChannel,
