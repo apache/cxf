4c814754ee25f354113b3194641d0e5fbc261781
Sergey Beryozkin <sergeyb@apache.org>
2012-03-01 17:19:46 +0000

    [CXF-4151] Prototyping the initial code for the client_credentials grant
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1295685 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/grants/clientcred/ClientCredentialsGrantHandler.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.rs.security.oauth2.grants.clientcred;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.cxf.rs.security.oauth2.common.Client;
+import org.apache.cxf.rs.security.oauth2.common.ServerAccessToken;
+import org.apache.cxf.rs.security.oauth2.grants.AbstractGrantHandler;
+import org.apache.cxf.rs.security.oauth2.provider.OAuthServiceException;
+import org.apache.cxf.rs.security.oauth2.tokens.bearer.BearerAccessToken;
+import org.apache.cxf.rs.security.oauth2.utils.OAuthConstants;
+
+public class ClientCredentialsGrantHandler  extends AbstractGrantHandler {
+    
+    public ClientCredentialsGrantHandler() {
+        super(OAuthConstants.CLIENT_CREDENTIALS_GRANT);
+    }
+
+    public ServerAccessToken createAccessToken(Client client, MultivaluedMap<String, String> params)
+        throws OAuthServiceException {
+        if (!client.isConfidential()) {
+            throw new OAuthServiceException(OAuthConstants.UNAUTHORIZED_CLIENT);    
+        }
+        BearerAccessToken token = new BearerAccessToken(client, 
+                                                        generateRandomTokenKey(),
+                                                        getTokenLifetime(), 
+                                                        System.currentTimeMillis() / 1000);
+        token.setGrantType(OAuthConstants.CLIENT_CREDENTIALS_GRANT);
+        // the OAuth filter will use Client.getLoginName() to initialize 
+        // the Principal when setting up the security context 
+        
+        return token;
+        
+    }
+
+
+}
