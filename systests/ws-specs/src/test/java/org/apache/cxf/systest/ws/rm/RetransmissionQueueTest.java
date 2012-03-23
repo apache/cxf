8c2069af72a1c84bb09ac2bd5305fb5b69a8cce8
J. Daniel Kulp <dkulp@apache.org>
2012-03-23 17:30:40 +0000

    Merged revisions 1304496 via  svn merge from https://svn.us.apache.org/repos/asf/cxf/branches/2.4.x-fixes
    ........
      r1304496 | dkulp | 2012-03-23 13:18:40 -0400 (Fri, 23 Mar 2012) | 17 lines
      
      Merged revisions 1304488 via  svn merge from
      https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes
      
      ........
        r1304488 | dkulp | 2012-03-23 13:08:01 -0400 (Fri, 23 Mar 2012) | 9 lines
        
        Merged revisions 1304482 via  git cherry-pick from
        https://svn.apache.org/repos/asf/cxf/trunk
        
        ........
          r1304482 | dkulp | 2012-03-23 12:58:32 -0400 (Fri, 23 Mar 2012) | 2 lines
        
          Use the correct ports
        
        ........
      ........
      
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1304519 13f79535-47bb-0310-9956-ffa450edef68

--- systests/ws-specs/src/test/java/org/apache/cxf/systest/ws/rm/RetransmissionQueueTest.java
+++ systests/ws-specs/src/test/java/org/apache/cxf/systest/ws/rm/RetransmissionQueueTest.java
@@ -120,7 +120,7 @@ public class RetransmissionQueueTest extends AbstractBusClientServerTestBase {
         
         GreeterService gs = new GreeterService();
         final Greeter greeter = gs.getGreeterPort();
-        updateAddressPort(greeter, DecoupledClientServerTest.PORT);
+        updateAddressPort(greeter, PORT);
         LOG.fine("Created greeter client.");
        
         ConnectionHelper.setKeepAliveConnection(greeter, true);
