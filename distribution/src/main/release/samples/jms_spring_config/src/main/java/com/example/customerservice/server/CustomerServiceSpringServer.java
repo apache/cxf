5b04cb87d6690bab891194f3f844a43ef57a2788
J. Daniel Kulp <dkulp@apache.org>
2012-03-23 14:33:52 +0000

    Cleanup the spring contexts
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1304374 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/jms_spring_config/src/main/java/com/example/customerservice/server/CustomerServiceSpringServer.java
+++ distribution/src/main/release/samples/jms_spring_config/src/main/java/com/example/customerservice/server/CustomerServiceSpringServer.java
@@ -26,7 +26,9 @@ public class CustomerServiceSpringServer {
     }
 
     public static void main(String args[]) throws Exception {
-        new ClassPathXmlApplicationContext("server-applicationContext.xml");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("server-applicationContext.xml");
         System.in.read();
+        ctx.stop();
+        ctx.destroy();
     }
 }
