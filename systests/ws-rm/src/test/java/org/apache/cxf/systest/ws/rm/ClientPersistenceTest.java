bc5ea6d8e7cefddb94cf7b40f5b708c0ef30a2d4
J. Daniel Kulp <dkulp@apache.org>
2012-04-06 14:30:00 +0000

    Merged revisions 1310147 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1310147 | dkulp | 2012-04-05 21:28:55 -0400 (Thu, 05 Apr 2012) | 2 lines
    
      Fix random failure with -T8 on my box
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1310346 13f79535-47bb-0310-9956-ffa450edef68

--- systests/ws-rm/src/test/java/org/apache/cxf/systest/ws/rm/ClientPersistenceTest.java
+++ systests/ws-rm/src/test/java/org/apache/cxf/systest/ws/rm/ClientPersistenceTest.java
@@ -259,7 +259,10 @@ public class ClientPersistenceTest extends AbstractBusClientServerTestBase {
         mf.verifyMessageNumbers(new String[] {"5"}, true);
         mf.verifyAcknowledgements(new boolean[1], true);
 
-        mf.verifyMessages(2, false);
+        // need in-exact as it COULD be 3 acks on a slow machine
+        //normally it will ack 1,3,5 and then 1-5, but on a slow machine,
+        //I've seen 1,3,5, then 1-3,5, and then 1-5
+        mf.verifyMessages(2, false, false);
 
         // we can't reliably predict how the three remaining messages are acknowledged
 //        expectedActions = new String[] {RM10Constants.SEQUENCE_ACKNOWLEDGMENT_ACTION,
