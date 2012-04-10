3ac2a5b9e463263c6c65b6c9f197e6f354ca8134
Akitoshi Yoshida <ay@apache.org>
2012-04-10 08:37:19 +0000

    Merged revisions 1310076 via  svn merge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1310076 | ay | 2012-04-05 23:31:25 +0200 (Thu, 05 Apr 2012) | 1 line
      
      [CXF-4084] Http Blueprint (Destination)
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1311635 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/blueprint/HttpDestinationBPBeanDefinitionParser.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.transport.http.blueprint;
+
+import javax.xml.namespace.QName;
+
+import org.w3c.dom.Element;
+
+import org.apache.aries.blueprint.ParserContext;
+import org.apache.aries.blueprint.mutable.MutableBeanMetadata;
+import org.apache.cxf.configuration.blueprint.AbstractBPBeanDefinitionParser;
+import org.apache.cxf.transport.http.AbstractHTTPDestination;
+import org.osgi.service.blueprint.reflect.Metadata;
+
+public class HttpDestinationBPBeanDefinitionParser extends AbstractBPBeanDefinitionParser {
+    private static final String HTTP_NS =
+        "http://cxf.apache.org/transports/http/configuration";
+
+    public Metadata parse(Element element, ParserContext context) {
+        MutableBeanMetadata bean = context.createMetadata(MutableBeanMetadata.class);
+        
+        bean.setRuntimeClass(AbstractHTTPDestination.class);
+
+        mapElementToJaxbProperty(context, bean, element,
+                new QName(HTTP_NS, "server"), "server", null);
+        mapElementToJaxbProperty(context, bean, element,
+                new QName(HTTP_NS, "fixedParameterOrder"), "fixedParameterOrder", null); 
+        mapElementToJaxbProperty(context, bean, element,
+                new QName(HTTP_NS, "contextMatchStrategy"), "contextMatchStrategy", null);
+        
+        parseAttributes(element, context, bean);
+        parseChildElements(element, context, bean);
+
+        bean.setScope(MutableBeanMetadata.SCOPE_PROTOTYPE);
+        
+        return bean;
+    }
+
+    @Override
+    protected void processNameAttribute(Element element, ParserContext context, MutableBeanMetadata bean,
+                                        String val) {
+        bean.setId(val);
+    }
+}
