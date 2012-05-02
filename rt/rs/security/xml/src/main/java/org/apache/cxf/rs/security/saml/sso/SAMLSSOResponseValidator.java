2741a6d95b5cb7561cd2418f04d143d7e68320e6
Colm O Heigeartaigh <coheigea@apache.org>
2012-05-02 08:26:08 +0000

    Fixed NotOnOrAfter validation in SubjectConfirmationData
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1332952 13f79535-47bb-0310-9956-ffa450edef68

--- rt/rs/security/xml/src/main/java/org/apache/cxf/rs/security/saml/sso/SAMLSSOResponseValidator.java
+++ rt/rs/security/xml/src/main/java/org/apache/cxf/rs/security/saml/sso/SAMLSSOResponseValidator.java
@@ -165,10 +165,9 @@ public class SAMLSSOResponseValidator {
             throw new WSSecurityException(WSSecurityException.FAILURE, "invalidSAMLsecurity");
         }
         
-        // TODO clock skew - future TTL required here.
         // We must have a NotOnOrAfter timestamp
         if (subjectConfData.getNotOnOrAfter() == null
-            || subjectConfData.getNotOnOrAfter().isAfterNow()) {
+            || subjectConfData.getNotOnOrAfter().isBeforeNow()) {
             LOG.fine("Subject Conf Data does not contain NotOnOrAfter or it has expired");
             throw new WSSecurityException(WSSecurityException.FAILURE, "invalidSAMLsecurity");
         }
