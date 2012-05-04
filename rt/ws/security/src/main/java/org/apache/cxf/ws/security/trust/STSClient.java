d153b8c7184d582098e4c8239701552a14b9bc7a
Daniel Kulp <dkulp@apache.org>
2012-05-04 17:57:33 +0000

    Merged revisions 1332691 via  svn merge from https://svn.us.apache.org/repos/asf/cxf/branches/2.4.x-fixes
    ........
      r1332691 | coheigea | 2012-05-01 11:08:48 -0400 (Tue, 01 May 2012) | 9 lines
      
      Merged revisions 1332683 via  git cherry-pick from
      https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes
      
      ........
        r1332683 | coheigea | 2012-05-01 16:03:28 +0100 (Tue, 01 May 2012) | 2 lines
      
        Only write wst:SecondaryParameters out if there are child Elements
      
      ........
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1334098 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/trust/STSClient.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/trust/STSClient.java
@@ -454,7 +454,7 @@ public class STSClient implements Configurable, InterceptorProvider {
         String keyTypeTemplate = null;
         String sptt = null;
         
-        if (template != null) {
+        if (template != null && DOMUtils.getFirstElement(template) != null) {
             if (this.useSecondaryParameters()) {
                 writer.writeStartElement("wst", "SecondaryParameters", namespace);
             }
