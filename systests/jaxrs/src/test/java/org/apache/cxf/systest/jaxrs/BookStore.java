45ffe4b1fd0c8002283e8d0421142b4cacf044b3
Sergey Beryozkin <sergeyb@apache.org>
2012-05-14 21:51:56 +0000

    Merged revisions 1338447 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes
    ................
      r1338447 | sergeyb | 2012-05-14 22:48:26 +0100 (Mon, 14 May 2012) | 16 lines
      
      Merged revisions 1338444 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes
      
      ................
        r1338444 | sergeyb | 2012-05-14 22:40:47 +0100 (Mon, 14 May 2012) | 9 lines
        
        Merged revisions 1338441 via svnmerge from 
        https://svn.apache.org/repos/asf/cxf/trunk
        
        ........
          r1338441 | sergeyb | 2012-05-14 22:35:37 +0100 (Mon, 14 May 2012) | 1 line
          
          Few updates to the jaxrs system tests to avoid the ambiguous matches
        ........
      ................
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes@1338449 13f79535-47bb-0310-9956-ffa450edef68

--- systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/BookStore.java
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/BookStore.java
@@ -524,7 +524,7 @@ public class BookStore {
     }
     
     @GET
-    @Path("/books/{bookId}/")
+    @Path("/books/text/xml/{bookId}")
     @Produces("text/xml")
     public Book getBookTextXml(@PathParam("bookId") String id) throws BookNotFoundFault {
         return doGetBook(id);
