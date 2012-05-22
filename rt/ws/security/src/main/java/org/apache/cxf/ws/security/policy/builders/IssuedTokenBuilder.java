9600ca630d9256211fd04a44f03b95aff0e84a04
Colm O Heigeartaigh <coheigea@apache.org>
2012-05-22 15:14:33 +0000

    Merged revisions 1341507 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1341507 | coheigea | 2012-05-22 15:59:25 +0100 (Tue, 22 May 2012) | 2 lines
    
      Also store the IssuerName in the IssuedToken policy
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1341518 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/policy/builders/IssuedTokenBuilder.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/policy/builders/IssuedTokenBuilder.java
@@ -87,6 +87,9 @@ public class IssuedTokenBuilder implements AssertionBuilder<Element> {
                     processAlternative(iterator.next(), issuedToken);
                     break; // since there should be only one alternative ..
                 }                
+            } else if (SPConstants.ISSUER_NAME.equals(ln)) {
+                String issuerName = child.getNodeValue();
+                issuedToken.setIssuerName(issuerName);
             }
             
             child = DOMUtils.getNextElement(child);
