cb6d077d7926f230112ed4b26edbdb367482f5ba
Willem Ning Jiang <ningjiang@apache.org>
2012-05-22 12:55:22 +0000

    Merged revisions 1341353,1341363 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes
    ........
      r1341353 | ningjiang | 2012-05-22 16:49:40 +0800 (Tue, 22 May 2012) | 1 line
      
      CXF-4326, CXF-4327 added blueprint support on the cxf clustering
    ........
      r1341363 | ningjiang | 2012-05-22 16:56:35 +0800 (Tue, 22 May 2012) | 1 line
      
      CXF-4327 updated the namespace of blueprint handler
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1341455 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/core/src/main/java/org/apache/cxf/clustering/blueprint/ClusteringBPNamespaceHandler.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.clustering.blueprint;
+
+import java.net.URL;
+import java.util.Set;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import org.apache.aries.blueprint.NamespaceHandler;
+import org.apache.aries.blueprint.ParserContext;
+import org.apache.cxf.clustering.FailoverFeature;
+import org.apache.cxf.clustering.LoadDistributorTargetSelector;
+import org.apache.cxf.configuration.blueprint.SimpleBPBeanDefinitionParser;
+import org.osgi.service.blueprint.reflect.ComponentMetadata;
+import org.osgi.service.blueprint.reflect.Metadata;
+
+public class ClusteringBPNamespaceHandler implements NamespaceHandler {
+    public ComponentMetadata decorate(Node node, ComponentMetadata component, ParserContext context) {
+        return null;
+    }
+    
+    public Metadata parse(Element element, ParserContext context) {
+        String s = element.getLocalName();
+        if ("failover".equals(s)) {
+            return new SimpleBPBeanDefinitionParser(FailoverFeature.class).parse(element, context);
+        } else if ("loadDistributor".equals(s)) {
+            return new SimpleBPBeanDefinitionParser(LoadDistributorTargetSelector.class).parse(element, context);
+        }
+        return null;
+    }
+    
+    @SuppressWarnings("rawtypes")
+    public Set<Class> getManagedClasses() {
+        return null;
+    }
+    
+    public URL getSchemaLocation(String namespace) {
+        return getClass().getClassLoader().getResource("/schemas/clustering.xsd");
+    }
+
+}
