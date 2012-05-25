9861f362fc8b892e741a4e8093f859d5ff4eee6f
Daniel Kulp <dkulp@apache.org>
2012-05-25 13:37:36 +0000

    Try to get the offline stuff working by having separate namespace handler in API for schemas found there
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1342620 13f79535-47bb-0310-9956-ffa450edef68

---
+++ api/src/main/java/org/apache/cxf/internal/CXFAPINamespaceHandler.java
@@ -0,0 +1,63 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.internal;
+
+import java.net.URL;
+import java.util.Set;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import org.apache.aries.blueprint.NamespaceHandler;
+import org.apache.aries.blueprint.ParserContext;
+import org.osgi.service.blueprint.reflect.ComponentMetadata;
+import org.osgi.service.blueprint.reflect.Metadata;
+
+/**
+ * 
+ */
+public class CXFAPINamespaceHandler implements NamespaceHandler {    
+    
+    public URL getSchemaLocation(String namespace) {
+        if ("http://cxf.apache.org/configuration/beans".equals(namespace)) {
+            return getClass().getClassLoader().getResource("/schemas/configuration/cxf-beans.xsd");           
+        } else if ("http://cxf.apache.org/configuration/parameterized-types".equals(namespace)) {
+            return getClass().getClassLoader().getResource("/schemas/configuration/parameterized-types.xsd");
+        } else if ("http://cxf.apache.org/configuration/security".equals(namespace)) {
+            return getClass().getClassLoader().getResource("/schemas/configuration/security.xsd");
+        }
+        return null;
+    }
+
+
+    public Metadata parse(Element element, ParserContext context) {
+        return null;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public Set<Class> getManagedClasses() {
+        //probably should have the various stuff in cxf-api in here?
+        return null;
+    }
+    public ComponentMetadata decorate(Node node, ComponentMetadata component, ParserContext context) {
+        return null;
+    }
+    
+}
--- rt/core/src/main/java/org/apache/cxf/bus/blueprint/CXFCoreNamespaceHandler.java
+++ rt/core/src/main/java/org/apache/cxf/bus/blueprint/CXFCoreNamespaceHandler.java
@@ -43,12 +43,6 @@ public class CXFCoreNamespaceHandler implements NamespaceHandler {
     public URL getSchemaLocation(String namespace) {
         if ("http://cxf.apache.org/blueprint/core".equals(namespace)) {
             return getClass().getClassLoader().getResource("/schemas/blueprint/core.xsd");
-        } else if ("http://cxf.apache.org/configuration/beans".equals(namespace)) {
-            return getClass().getClassLoader().getResource("/schemas/configuration/cxf-beans.xsd");           
-        } else if ("http://cxf.apache.org/configuration/parameterized-types".equals(namespace)) {
-            return getClass().getClassLoader().getResource("/schemas/configuration/parameterized-types.xsd");
-        } else if ("http://cxf.apache.org/configuration/security".equals(namespace)) {
-            return getClass().getClassLoader().getResource("/schemas/configuration/security.xsd");
         }
         return null;
     }
