7c06e5b34843d461f9be7468ca005b29a490e1f2
Colm O Heigeartaigh <coheigea@apache.org>
2012-05-25 18:55:21 +0000

    Fix for failing test
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1342771 13f79535-47bb-0310-9956-ffa450edef68

--- systests/ws-security/src/test/java/org/apache/cxf/systest/ws/fault/FaultTest.java
+++ systests/ws-security/src/test/java/org/apache/cxf/systest/ws/fault/FaultTest.java
@@ -27,7 +27,6 @@ import javax.xml.ws.Service;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBusFactory;
-import org.apache.cxf.systest.ws.common.SecurityTestUtil;
 import org.apache.cxf.systest.ws.fault.server.Server;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
 import org.example.contract.doubleit.DoubleItPortType;
@@ -54,7 +53,6 @@ public class FaultTest extends AbstractBusClientServerTestBase {
     
     @org.junit.AfterClass
     public static void cleanup() throws Exception {
-        SecurityTestUtil.cleanup();
         stopAllServers();
     }
 
