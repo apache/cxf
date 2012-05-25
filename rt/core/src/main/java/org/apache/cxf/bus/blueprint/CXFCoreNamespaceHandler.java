09109d0508afdc94e42ab9a32af075d8d4441ee7
Akitoshi Yoshida <ay@apache.org>
2012-05-25 16:58:14 +0000

    Merged revisions 1342232 via  svn merge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1342232 | ay | 2012-05-24 14:40:59 +0200 (Thu, 24 May 2012) | 1 line
      
      consolidate some BP schemas
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1342726 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/bus/blueprint/CXFCoreNamespaceHandler.java
+++ rt/core/src/main/java/org/apache/cxf/bus/blueprint/CXFCoreNamespaceHandler.java
@@ -47,6 +47,8 @@ public class CXFCoreNamespaceHandler implements NamespaceHandler {
             return getClass().getClassLoader().getResource("/schemas/configuration/cxf-beans.xsd");           
         } else if ("http://cxf.apache.org/configuration/parameterized-types".equals(namespace)) {
             return getClass().getClassLoader().getResource("/schemas/configuration/parameterized-types.xsd");
+        } else if ("http://cxf.apache.org/configuration/security".equals(namespace)) {
+            return getClass().getClassLoader().getResource("/schemas/configuration/security.xsd");
         }
         return null;
     }
