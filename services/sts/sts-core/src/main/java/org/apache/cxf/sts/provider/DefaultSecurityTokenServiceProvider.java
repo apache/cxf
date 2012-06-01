9a7ae0beaefb608465154a74836cd6bd0d9cef5f
Colm O Heigeartaigh <coheigea@apache.org>
2012-06-01 10:58:22 +0000

    [CXF-4344] - Provide simplifications and shortcuts in CXF STS configuration
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1345046 13f79535-47bb-0310-9956-ffa450edef68

---
+++ services/sts/sts-core/src/main/java/org/apache/cxf/sts/provider/DefaultSecurityTokenServiceProvider.java
@@ -0,0 +1,127 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.sts.provider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.transform.Source;
+
+import org.apache.cxf.sts.STSPropertiesMBean;
+import org.apache.cxf.sts.claims.ClaimsManager;
+import org.apache.cxf.sts.operation.AbstractOperation;
+import org.apache.cxf.sts.operation.TokenIssueOperation;
+import org.apache.cxf.sts.operation.TokenValidateOperation;
+import org.apache.cxf.sts.service.ServiceMBean;
+import org.apache.cxf.sts.token.provider.SAMLTokenProvider;
+import org.apache.cxf.sts.token.provider.TokenProvider;
+import org.apache.cxf.sts.token.validator.SAMLTokenValidator;
+import org.apache.cxf.sts.token.validator.TokenValidator;
+import org.apache.cxf.sts.token.validator.UsernameTokenValidator;
+import org.apache.cxf.sts.token.validator.X509TokenValidator;
+import org.apache.cxf.ws.security.sts.provider.SecurityTokenServiceProvider;
+import org.apache.cxf.ws.security.tokenstore.TokenStore;
+
+/**
+ * A "default" SecurityTokenServiceProvider implementation that defines the Issue and Validate
+ * Operations of the STS and adds support for issuing and validating SAML Assertions, and
+ * validating UsernameTokens and X.509 Tokens.
+ */
+public class DefaultSecurityTokenServiceProvider extends SecurityTokenServiceProvider {
+    
+    private STSPropertiesMBean stsProperties;
+    private boolean encryptIssuedToken;
+    private List<ServiceMBean> services;
+    private boolean returnReferences = true;
+    private TokenStore tokenStore;
+    private ClaimsManager claimsManager = new ClaimsManager();
+
+    public DefaultSecurityTokenServiceProvider() throws Exception {
+        super();
+    }
+    
+    public void setReturnReferences(boolean returnReferences) {
+        this.returnReferences = returnReferences;
+    }
+    
+    public void setTokenStore(TokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    public void setStsProperties(STSPropertiesMBean stsProperties) {
+        this.stsProperties = stsProperties;
+    }
+    
+    public void setEncryptIssuedToken(boolean encryptIssuedToken) {
+        this.encryptIssuedToken = encryptIssuedToken;
+    }
+    
+    public void setServices(List<ServiceMBean> services) {
+        this.services = services;
+    }
+    
+    public void setClaimsManager(ClaimsManager claimsManager) {
+        this.claimsManager = claimsManager;
+    }
+    
+    @Override
+    public Source invoke(Source request) {
+        if (getIssueOperation() == null) {
+            setIssueOperation(createTokenIssueOperation());
+        }
+        if (getValidateOperation() == null) {
+            setValidateOperation(createTokenValidateOperation());
+        }
+        return super.invoke(request);
+    }
+    
+    private TokenIssueOperation createTokenIssueOperation() {
+        TokenIssueOperation issueOperation = new TokenIssueOperation();
+        populateAbstractOperation(issueOperation);
+        
+        return issueOperation;
+    }
+
+    private TokenValidateOperation createTokenValidateOperation() {
+        TokenValidateOperation validateOperation = new TokenValidateOperation();
+        populateAbstractOperation(validateOperation);
+        
+        return validateOperation;
+    }
+    
+    private void populateAbstractOperation(AbstractOperation abstractOperation) {
+        List<TokenProvider> tokenProviders = new ArrayList<TokenProvider>();
+        tokenProviders.add(new SAMLTokenProvider());
+        
+        List<TokenValidator> tokenValidators = new ArrayList<TokenValidator>();
+        tokenValidators.add(new SAMLTokenValidator());
+        tokenValidators.add(new UsernameTokenValidator());
+        tokenValidators.add(new X509TokenValidator());
+        
+        abstractOperation.setTokenProviders(tokenProviders);
+        abstractOperation.setTokenValidators(tokenValidators);
+        abstractOperation.setStsProperties(stsProperties);
+        abstractOperation.setEncryptIssuedToken(encryptIssuedToken);
+        abstractOperation.setServices(services);
+        abstractOperation.setReturnReferences(returnReferences);
+        abstractOperation.setTokenStore(tokenStore);
+        abstractOperation.setClaimsManager(claimsManager);
+    }
+}
