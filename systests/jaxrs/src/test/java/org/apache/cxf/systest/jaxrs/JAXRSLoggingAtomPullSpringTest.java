09382972e49998cc3b744c4add2bce6d46bbb2cd
Daniel Kulp <dkulp@apache.org>
2012-06-06 17:57:14 +0000

    Merged revisions 1346559 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1346559 | dkulp | 2012-06-05 16:07:52 -0400 (Tue, 05 Jun 2012) | 2 lines
    
      Some java 7 test order and cleanup fixes.
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1347007 13f79535-47bb-0310-9956-ffa450edef68

--- systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/JAXRSLoggingAtomPullSpringTest.java
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/JAXRSLoggingAtomPullSpringTest.java
@@ -82,8 +82,10 @@ public class JAXRSLoggingAtomPullSpringTest extends AbstractClientServerTestBase
         System.setProperty("systemtests.jaxrs.logs.folder", resourceDir);
         launchServer(SpringServer.class, true);
         context = JAXBContext.newInstance(org.apache.cxf.management.web.logging.LogRecord.class);
+        createStaticBus();
+        System.clearProperty("systemtests.jaxrs.logs.folder");
     }
-
+    
     @Ignore
     public static class SpringServer extends AbstractSpringServer {
         public static final int PORT = allocatePortAsInt(SpringServer.class);
@@ -97,7 +99,6 @@ public class JAXRSLoggingAtomPullSpringTest extends AbstractClientServerTestBase
         Storage.clearRecords();
     }
 
-    
     @Test
     public void testFeed() throws Exception {
         String listing = WebClient.create("http://localhost:" + PORT + "/services").get(String.class);
@@ -135,6 +136,9 @@ public class JAXRSLoggingAtomPullSpringTest extends AbstractClientServerTestBase
     
     @Test
     public void testPagedFeed() throws Exception {
+        WebClient wcReset = WebClient.create("http://localhost:" + PORT + "/reset");
+        wcReset.post(null);
+        
         WebClient wc = WebClient.create("http://localhost:" + PORT + "/resource2/paged");
         wc.path("/log").get();
         Thread.sleep(3000);
@@ -324,6 +328,7 @@ public class JAXRSLoggingAtomPullSpringTest extends AbstractClientServerTestBase
     private String fillPagedEntries(List<Entry> entries, String href, int expected, 
                                     String rel, boolean relExpected) {
         Feed feed = getFeed(href);
+
         assertEquals(expected, feed.getEntries().size());
         entries.addAll(feed.getEntries());
         
