ee296438e4b8db1b2cf3f7758aa3675e9bcbb22c
Sergey Beryozkin <sergeyb@apache.org>
2012-06-19 12:18:52 +0000

    Merged revisions 1351663 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes
    ................
      r1351663 | sergeyb | 2012-06-19 13:06:58 +0100 (Tue, 19 Jun 2012) | 16 lines
      
      Merged revisions 1351659 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes
      
      ................
        r1351659 | sergeyb | 2012-06-19 13:01:22 +0100 (Tue, 19 Jun 2012) | 9 lines
        
        Merged revisions 1351657 via svnmerge from 
        https://svn.apache.org/repos/asf/cxf/trunk
        
        ........
          r1351657 | sergeyb | 2012-06-19 12:57:21 +0100 (Tue, 19 Jun 2012) | 1 line
          
          [CXF-4380] Support for reading custom JAXBElements
        ........
      ................
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.4.x-fixes@1351667 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/provider/JAXBElementProviderTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/provider/JAXBElementProviderTest.java
@@ -67,6 +67,8 @@ import org.w3c.dom.Node;
 import org.xml.sax.ContentHandler;
 
 import org.apache.cxf.jaxrs.ext.xml.XMLSource;
+import org.apache.cxf.jaxrs.fortest.jaxb.jaxbelement.ParamJAXBElement;
+import org.apache.cxf.jaxrs.fortest.jaxb.jaxbelement.ParamType;
 import org.apache.cxf.jaxrs.fortest.jaxb.packageinfo.Book2;
 import org.apache.cxf.jaxrs.fortest.jaxb.packageinfo.Book2NoRootElement;
 import org.apache.cxf.jaxrs.impl.MetadataMap;
@@ -584,6 +586,33 @@ public class JAXBElementProviderTest extends Assert {
         provider.writeTo(books, m.getReturnType(), m.getGenericReturnType(),
                        new Annotation[0], MediaType.TEXT_XML_TYPE, new MetadataMap<String, Object>(), bos);
         doReadUnqualifiedCollection(bos.toString(), "setBooks", List.class);
+    }
+    
+    @Test
+    public void testReadJAXBElement() throws Exception {
+        String xml = "<Book><id>123</id><name>CXF in Action</name></Book>";
+        JAXBElementProvider provider = new JAXBElementProvider();
+        JAXBElement jaxbElement = (JAXBElement)provider.readFrom((Class)JAXBElement.class, Book.class,
+             new Annotation[0], MediaType.TEXT_XML_TYPE, new MetadataMap<String, String>(),
+             new ByteArrayInputStream(xml.getBytes("UTF-8")));
+        Book book = (Book)jaxbElement.getValue();
+        assertEquals(123L, book.getId());
+        assertEquals("CXF in Action", book.getName());
+        
+    }
+    
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testReadParamJAXBElement() throws Exception {
+        String xml = "<param xmlns=\"http://jaxbelement/10\">"
+            + "<filter name=\"foo\"/><comment>a</comment></param>";
+        JAXBElementProvider provider = new JAXBElementProvider();
+        ParamJAXBElement jaxbElement = (ParamJAXBElement)provider.readFrom((Class)ParamJAXBElement.class,
+            ParamJAXBElement.class,
+            new Annotation[0], MediaType.TEXT_XML_TYPE, new MetadataMap<String, String>(),
+            new ByteArrayInputStream(xml.getBytes("UTF-8")));
+        ParamType param = (ParamType)jaxbElement.getValue();
+        assertEquals("a", param.getComment());
     }
     
     @Test
@@ -1488,4 +1517,5 @@ public class JAXBElementProviderTest extends Assert {
             return null;
         }
     }
+
 }
