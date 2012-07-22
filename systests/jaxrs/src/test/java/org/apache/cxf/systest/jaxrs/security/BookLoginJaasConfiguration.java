21b868b398a4496a58fa7ebeb7105adc88107900
Sergey Beryozkin <sergeyb@apache.org>
2012-07-22 22:57:00 +0000

    JAAS Configuration subclasses have to override refresh() in 1.5
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes@1364454 13f79535-47bb-0310-9956-ffa450edef68

--- systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/security/BookLoginJaasConfiguration.java
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/security/BookLoginJaasConfiguration.java
@@ -31,6 +31,10 @@ public class BookLoginJaasConfiguration extends Configuration {
                                       Collections.<String, String>emptyMap())
         };
     }
+
+    @Override
+    public void refresh() {
+    }
 }
 
 
