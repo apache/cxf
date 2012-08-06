3ced31226a45a0af15aeb81893e5cb63cbca30df
Akitoshi Yoshida <ay@apache.org>
2012-08-06 18:09:58 +0000

    [CXF-4454] JSSE KeyManagers and TrustManager XML configurations are ignored if they contain no keystore element
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1369901 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/configuration/jsse/TLSParameterJaxBUtils.java
+++ api/src/main/java/org/apache/cxf/configuration/jsse/TLSParameterJaxBUtils.java
@@ -249,10 +249,6 @@ public final class TLSParameterJaxBUtils {
 
         KeyStore keyStore = getKeyStore(kmc.getKeyStore());
 
-        if (keyStore == null) {
-            return null;
-        }
-
         String alg = kmc.isSetFactoryAlgorithm()
                      ? kmc.getFactoryAlgorithm()
                      : KeyManagerFactory.getDefaultAlgorithm();
@@ -285,9 +281,6 @@ public final class TLSParameterJaxBUtils {
                 : (tmc.isSetCertStore()
                     ? getKeyStore(tmc.getCertStore())
                     : (KeyStore) null);
-        if (keyStore == null) {
-            return null;
-        }
 
         String alg = tmc.isSetFactoryAlgorithm()
                      ? tmc.getFactoryAlgorithm()
