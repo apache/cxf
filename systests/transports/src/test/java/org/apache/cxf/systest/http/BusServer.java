d0b5673379cf8bd9e77b3105d5e911c720f3450e
Daniel Kulp <dkulp@apache.org>
2012-09-11 18:39:00 +0000

    Merged revisions 1381676 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1381676 | dkulp | 2012-09-06 12:40:56 -0400 (Thu, 06 Sep 2012) | 2 lines
    
      Updates to get all the tests passing with the proxy related tests
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes@1383524 13f79535-47bb-0310-9956-ffa450edef68

--- systests/transports/src/test/java/org/apache/cxf/systest/http/BusServer.java
+++ systests/transports/src/test/java/org/apache/cxf/systest/http/BusServer.java
@@ -19,24 +19,35 @@
 
 package org.apache.cxf.systest.http;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.testutil.common.AbstractBusTestServerBase;
+import org.apache.cxf.testutil.common.TestUtil;
 
 /**
  * This server just instantiates a Bus, full stop.
  * Everything else is designed to be spring-loaded.
  */
 public class BusServer extends AbstractBusTestServerBase {
-    public static final String PORT0 = allocatePort(BusServer.class, 0);
-    public static final String PORT1 = allocatePort(BusServer.class, 1);
-    public static final String PORT2 = allocatePort(BusServer.class, 2);
-    public static final String PORT3 = allocatePort(BusServer.class, 3);
-    public static final String PORT4 = allocatePort(BusServer.class, 4);
-    public static final String PORT5 = allocatePort(BusServer.class, 5);
-    public static final String PORT6 = allocatePort(BusServer.class, 6);
-    public static final String PORT7 = allocatePort(BusServer.class, 7);
-    public static final String PORT8 = allocatePort(BusServer.class, 8);
+    public static final Map<String, String> PORTMAP = new HashMap<String, String>();
+    public static void resetPortMap() {
+        PORTMAP.clear();
+        for (int x = 0; x < 9; x++) {
+            PORTMAP.put("PORT" + x, TestUtil.getNewPortNumber(BusServer.class, x));
+        }
+    }
+    public static String getPort(int x) {
+        if (PORTMAP.isEmpty()) {
+            for (int y = 0; y < 9; y++) {
+                PORTMAP.put("PORT" + y, TestUtil.getPortNumber(BusServer.class, y));
+            }
+        }
+        return PORTMAP.get("PORT" + x);
+    }
+    
     
     protected void run()  {
         //
