b911f54c3cc32038777174331acba3154afd4ea7
Daniel Kulp <dkulp@apache.org>
2012-09-12 17:54:35 +0000

    Remove println
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1384057 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/http-hc/src/main/java/org/apache/cxf/transport/http/asyncclient/AsyncHTTPConduitFactory.java
+++ rt/transports/http-hc/src/main/java/org/apache/cxf/transport/http/asyncclient/AsyncHTTPConduitFactory.java
@@ -103,7 +103,6 @@ public class AsyncHTTPConduitFactory implements BusLifeCycleListener, HTTPCondui
     
     public AsyncHTTPConduitFactory(Map<String, Object> conf) {
         super();
-        System.out.println(conf);
         config.setTcpNoDelay(true);
         setProperties(conf);
     }
