aed0dac9a6089041c26511006266ecf61988814e
Daniel Kulp <dkulp@apache.org>
2012-09-26 17:06:59 +0000

    Merged revisions 1390603 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes
    ........
      r1390603 | dkulp | 2012-09-26 13:05:24 -0400 (Wed, 26 Sep 2012) | 10 lines
    
      Merged revisions 1390598 via  git cherry-pick from
      https://svn.apache.org/repos/asf/cxf/trunk
    
      ........
        r1390598 | dkulp | 2012-09-26 12:54:20 -0400 (Wed, 26 Sep 2012) | 2 lines
    
        If using something other than Basic auth, create a AuthorizationPolicy object based on what we CAN retrieve from the HTTP Request
    
      ........
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1390607 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/http/src/main/java/org/apache/cxf/transport/http/AbstractHTTPDestination.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/AbstractHTTPDestination.java
@@ -145,7 +145,7 @@ public abstract class AbstractHTTPDestination
         return bus;
     }
 
-    private AuthorizationPolicy getAuthorizationPolicyFromMessage(String credentials) {
+    private AuthorizationPolicy getAuthorizationPolicyFromMessage(String credentials, Principal pp) {
         if (credentials == null || StringUtils.isEmpty(credentials.trim())) {
             return null;
         }
@@ -162,12 +162,28 @@ public abstract class AbstractHTTPDestination
                 AuthorizationPolicy policy = new AuthorizationPolicy();
                 policy.setUserName(username);
                 policy.setPassword(password);
+                policy.setAuthorizationType(authType);
                 return policy;
             } catch (Base64Exception ex) {
                 // Invalid authentication => treat as not authenticated
             }
+        } else if (pp != null) {
+            AuthorizationPolicy policy = new PrincipalAuthorizationPolicy(pp);
+            policy.setUserName(pp.getName());
+            policy.setAuthorization(credentials);
+            policy.setAuthorizationType(authType);
+            return policy;
         }
         return null;
+    }
+    public static final class PrincipalAuthorizationPolicy extends AuthorizationPolicy {
+        final Principal principal;
+        public PrincipalAuthorizationPolicy(Principal p) {
+            principal = p;
+        }
+        public Principal getPrincipal() {
+            return principal;
+        }
     }
     
     /** 
@@ -246,6 +262,7 @@ public abstract class AbstractHTTPDestination
                 super.cacheInput();
             }
         };
+        
         inMessage.setContent(DelegatingInputStream.class, in);
         inMessage.setContent(InputStream.class, in);
         inMessage.put(HTTP_REQUEST, req);
@@ -299,8 +316,8 @@ public abstract class AbstractHTTPDestination
         }
         inMessage.put(Message.FIXED_PARAMETER_ORDER, isFixedParameterOrder());
         inMessage.put(Message.ASYNC_POST_RESPONSE_DISPATCH, Boolean.TRUE);
+        final Principal pp = req.getUserPrincipal(); 
         inMessage.put(SecurityContext.class, new SecurityContext() {
-            private Principal pp = req.getUserPrincipal(); 
             public Principal getUserPrincipal() {
                 return pp;
             }
@@ -309,10 +326,11 @@ public abstract class AbstractHTTPDestination
             }
         });
         
+        
         Headers headers = new Headers(inMessage);
         headers.copyFromRequest(req);
         String credentials = headers.getAuthorization();
-        AuthorizationPolicy authPolicy = getAuthorizationPolicyFromMessage(credentials);
+        AuthorizationPolicy authPolicy = getAuthorizationPolicyFromMessage(credentials, pp);
         inMessage.put(AuthorizationPolicy.class, authPolicy);
         
         SSLUtils.propogateSecureSession(req, inMessage);
