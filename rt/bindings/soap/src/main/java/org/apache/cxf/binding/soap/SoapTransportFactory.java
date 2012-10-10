103181ecdb685ea4215a611533d304a55b77e9bb
Willem Ning Jiang <ningjiang@apache.org>
2012-10-10 15:32:00 +0000

    Merged revisions 1396628 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes
    ................
      r1396628 | ningjiang | 2012-10-10 23:02:43 +0800 (Wed, 10 Oct 2012) | 13 lines
      
      Merged revisions 1396595,1396621 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1396595 | ningjiang | 2012-10-10 21:37:39 +0800 (Wed, 10 Oct 2012) | 1 line
        
         CXF-4554 clean up the active namespace of SoapTransportFactory
      ........
        r1396621 | ningjiang | 2012-10-10 22:43:11 +0800 (Wed, 10 Oct 2012) | 1 line
        
        CXF-4554 Fixed the CS error
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1396645 13f79535-47bb-0310-9956-ffa450edef68

--- rt/bindings/soap/src/main/java/org/apache/cxf/binding/soap/SoapTransportFactory.java
+++ rt/bindings/soap/src/main/java/org/apache/cxf/binding/soap/SoapTransportFactory.java
@@ -73,11 +73,10 @@ public class SoapTransportFactory extends AbstractTransportFactory implements De
             "http://schemas.xmlsoap.org/soap/",
             "http://schemas.xmlsoap.org/wsdl/soap/",
             "http://schemas.xmlsoap.org/wsdl/soap12/",
-            "http://schemas.xmlsoap.org/soap/http/",
+            "http://schemas.xmlsoap.org/soap/http",
             "http://schemas.xmlsoap.org/wsdl/soap/http",
             "http://www.w3.org/2010/soapjms/",
-            "http://www.w3.org/2003/05/soap/bindings/HTTP/",
-            "http://schemas.xmlsoap.org/soap/http");
+            "http://www.w3.org/2003/05/soap/bindings/HTTP/");
     
     public SoapTransportFactory() {
         super(DEFAULT_NAMESPACES, null);
