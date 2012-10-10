b2021ac991a0d7ac29a308de8bd503a10f71227d
Willem Ning Jiang <ningjiang@apache.org>
2012-10-10 14:43:11 +0000

    CXF-4554 Fixed the CS error
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1396621 13f79535-47bb-0310-9956-ffa450edef68

--- rt/bindings/soap/src/main/java/org/apache/cxf/binding/soap/SoapTransportFactory.java
+++ rt/bindings/soap/src/main/java/org/apache/cxf/binding/soap/SoapTransportFactory.java
@@ -76,8 +76,7 @@ public class SoapTransportFactory extends AbstractTransportFactory implements De
             "http://schemas.xmlsoap.org/soap/http",
             "http://schemas.xmlsoap.org/wsdl/soap/http",
             "http://www.w3.org/2010/soapjms/",
-            "http://www.w3.org/2003/05/soap/bindings/HTTP/"
-            );
+            "http://www.w3.org/2003/05/soap/bindings/HTTP/");
     public static final Set<String> DEFAULT_PREFIXES 
         = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList(
             "soap.udp", "soap.tcp"
