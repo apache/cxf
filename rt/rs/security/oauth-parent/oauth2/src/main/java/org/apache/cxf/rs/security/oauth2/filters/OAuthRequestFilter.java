4f01a173f52aa964e1273aa7253462a1c374f870
Sergey Beryozkin <sergeyb@apache.org>
2012-11-06 11:54:51 +0000

    [CXF-4615] Updating OAuthRequestFilter to ignore CORS preflight requests to be handled by the service code
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1406106 13f79535-47bb-0310-9956-ffa450edef68

--- rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/filters/OAuthRequestFilter.java
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/filters/OAuthRequestFilter.java
@@ -33,6 +33,7 @@ import org.apache.cxf.common.security.SimplePrincipal;
 import org.apache.cxf.jaxrs.ext.RequestHandler;
 import org.apache.cxf.jaxrs.model.ClassResourceInfo;
 import org.apache.cxf.message.Message;
+import org.apache.cxf.message.MessageUtils;
 import org.apache.cxf.rs.security.oauth2.common.AccessTokenValidation;
 import org.apache.cxf.rs.security.oauth2.common.OAuthContext;
 import org.apache.cxf.rs.security.oauth2.common.OAuthPermission;
@@ -51,6 +52,11 @@ public class OAuthRequestFilter extends AbstractAccessTokenValidator implements 
     private boolean useUserSubject;
     
     public Response handleRequest(Message m, ClassResourceInfo resourceClass) {
+        
+        if (isCorsRequest(m)) {
+            return null;
+        }
+        
         // Get the access token
         AccessTokenValidation accessTokenV = getAccessTokenValidation(); 
         
@@ -149,5 +155,14 @@ public class OAuthRequestFilter extends AbstractAccessTokenValidator implements 
         };
     }
     
+    protected boolean isCorsRequest(Message m) {
+        //Redirection-based flows (Implicit Grant Flow specifically) may have 
+        //the browser issuing CORS preflight OPTIONS request. 
+        //org.apache.cxf.rs.security.cors.CrossOriginResourceSharingFilter can be
+        //used to handle preflights but local preflights (to be handled by the service code)
+        // will be blocked by this filter unless CORS filter has done the initial validation
+        // and set a message "local_preflight" property to true
+        return MessageUtils.isTrue(m.get("local_preflight"));
+    }
     
 }
