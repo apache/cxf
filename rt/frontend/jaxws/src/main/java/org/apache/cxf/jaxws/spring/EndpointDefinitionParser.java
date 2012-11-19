c9e58e3f6ceb22ebfd48d193a70123a0a12ad2c7
Freeman Yue Fang <ffang@apache.org>
2012-11-19 06:55:00 +0000

    [CXF-4640]still need apply same change as that on trunk, as this change also affect the all-in-on bundle maven-bundle-plugin so that package org.springframework.context.annotation can get added as well
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1411082 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/spring/EndpointDefinitionParser.java
+++ rt/frontend/jaxws/src/main/java/org/apache/cxf/jaxws/spring/EndpointDefinitionParser.java
@@ -48,6 +48,7 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
 
 
 public class EndpointDefinitionParser extends AbstractBeanDefinitionParser {
@@ -177,8 +178,7 @@ public class EndpointDefinitionParser extends AbstractBeanDefinitionParser {
     public static final void setBlocking(ApplicationContext ctx, EndpointImpl impl) {
         Class<?> cls = null;
         try {
-            cls = Class
-                .forName("org.springframework.context.annotation.CommonAnnotationBeanPostProcessor");
+            cls = Class.forName(CommonAnnotationBeanPostProcessor.class.getName());
         } catch (ClassNotFoundException e) {
             //ignore
         }
