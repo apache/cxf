a1fdf517cb3503be163ddc250af28df85f235001
Sergey Beryozkin <sergeyb@apache.org>
2012-11-21 13:58:55 +0000

    Merged revisions 1412091 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1412091 | sergeyb | 2012-11-21 13:38:31 +0000 (Wed, 21 Nov 2012) | 1 line
      
      Adding few simple grant beans
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes@1412103 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/grants/owner/ResourceOwnerGrant.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.rs.security.oauth2.grants.owner;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.cxf.jaxrs.impl.MetadataMap;
+import org.apache.cxf.rs.security.oauth2.common.AccessTokenGrant;
+import org.apache.cxf.rs.security.oauth2.utils.OAuthConstants;
+
+public class ResourceOwnerGrant implements AccessTokenGrant {
+    private String ownerName;
+    private String ownerPassword;
+    
+    public ResourceOwnerGrant(String name, String password) {
+        this.ownerName = name;
+        this.ownerPassword = password;
+    }
+    
+    public String getType() {
+        return OAuthConstants.RESOURCE_OWNER_GRANT;
+    }
+
+    @Override
+    public MultivaluedMap<String, String> toMap() {
+        MultivaluedMap<String, String> map = new MetadataMap<String, String>();
+        map.putSingle(OAuthConstants.GRANT_TYPE, OAuthConstants.RESOURCE_OWNER_GRANT);
+        map.putSingle(OAuthConstants.RESOURCE_OWNER_NAME, ownerName);
+        map.putSingle(OAuthConstants.RESOURCE_OWNER_PASSWORD, ownerPassword);
+        return map;
+    }
+
+}
