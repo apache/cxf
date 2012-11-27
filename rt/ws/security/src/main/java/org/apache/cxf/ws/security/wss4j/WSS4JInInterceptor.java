84f80faefbef2cdcf88b901446bf9bc45f1e1bd8
Colm O Heigeartaigh <coheigea@apache.org>
2012-11-27 16:21:43 +0000

    [CXF-4655] - Enforce SAML SubjectConfirmation requirements for the non WS-SecurityPolicy case
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1414247 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/WSS4JInInterceptor.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/WSS4JInInterceptor.java
@@ -281,7 +281,7 @@ public class WSS4JInInterceptor extends AbstractWSS4JInterceptor {
 
                 storeSignature(msg, reqData, wsResult);
                 storeTimestamp(msg, reqData, wsResult);
-                checkActions(msg, reqData, wsResult, actions);
+                checkActions(msg, reqData, wsResult, actions, SAAJUtils.getBody(doc));
                 doResults(
                     msg, actor, 
                     SAAJUtils.getHeader(doc),
@@ -310,7 +310,7 @@ public class WSS4JInInterceptor extends AbstractWSS4JInterceptor {
                               SAAJUtils.getBody(doc),
                               wsResult);
                 } else {
-                    checkActions(msg, reqData, wsResult, actions);
+                    checkActions(msg, reqData, wsResult, actions, SAAJUtils.getBody(doc));
                     doResults(msg, actor,
                               SAAJUtils.getHeader(doc),
                               SAAJUtils.getBody(doc),
@@ -342,7 +342,8 @@ public class WSS4JInInterceptor extends AbstractWSS4JInterceptor {
         SoapMessage msg, 
         RequestData reqData, 
         List<WSSecurityEngineResult> wsResult, 
-        List<Integer> actions
+        List<Integer> actions,
+        Element body
     ) throws WSSecurityException {
         if (ignoreActions) {
             // Not applicable for the WS-SecurityPolicy case
@@ -364,6 +365,16 @@ public class WSS4JInInterceptor extends AbstractWSS4JInterceptor {
                 + "SignatureCoverageChecker";
             LOG.warning(warning);
         }
+        
+        // Now check SAML SenderVouches + Holder Of Key requirements
+        boolean validateSAMLSubjectConf = 
+            MessageUtils.getContextualBoolean(
+                msg, SecurityConstants.VALIDATE_SAML_SUBJECT_CONFIRMATION, true
+            );
+        if (validateSAMLSubjectConf) {
+            SAMLUtils.validateSAMLResults(wsResult, msg, body);
+        }
+        
     }
     
     private void storeSignature(
