c0231c0ca513c830b49c898d13afa649df54b68f
Sergey Beryozkin <sergeyb@apache.org>
2012-11-29 15:57:02 +0000

    [CXF-4643] Updating ServletController to check for custom query handlers only if HTTP method is GET
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1415221 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/http/src/main/java/org/apache/cxf/transport/servlet/ServletController.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/servlet/ServletController.java
@@ -176,14 +176,12 @@ public class ServletController {
                             orig = ClassLoaderUtils.setThreadContextClassloader(loader);
                         }
                     }
+                    updateDestination(request, d);
+                    
                     QueryHandlerRegistry queryHandlerRegistry = bus.getExtension(QueryHandlerRegistry.class);
-
-                    if (!StringUtils.isEmpty(request.getQueryString()) && queryHandlerRegistry != null) {
-                        
-                        // update the EndPoint Address with request url
-                        if ("GET".equals(request.getMethod())) {
-                            updateDestination(request, d);
-                        }
+                    if ("GET".equals(request.getMethod()) 
+                        && !StringUtils.isEmpty(request.getQueryString()) 
+                        && queryHandlerRegistry != null) {
                         
                         String ctxUri = request.getPathInfo();
                         String baseUri = request.getRequestURL().toString()
@@ -196,8 +194,6 @@ public class ServletController {
                             respondUsingQueryHandler(selectedHandler, res, ei, ctxUri, baseUri);
                             return true;
                         }
-                    } else {
-                        updateDestination(request, d);
                     }
                     invokeDestination(request, res, d);
                 } finally {
