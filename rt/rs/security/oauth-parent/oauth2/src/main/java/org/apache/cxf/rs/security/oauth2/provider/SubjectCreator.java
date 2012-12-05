819d0f2e5342d463121d44dd2d3304a50316a33f
Sergey Beryozkin <sergeyb@apache.org>
2012-12-05 11:16:00 +0000

    [CXF-4675] Introducing SubjectCreator interface for users be able to customize the user subject creation 'closer' to the data
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1417360 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/provider/SubjectCreator.java
@@ -0,0 +1,39 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.rs.security.oauth2.provider;
+
+import org.apache.cxf.jaxrs.ext.MessageContext;
+import org.apache.cxf.rs.security.oauth2.common.UserSubject;
+
+/**
+ * Optional provider responsible for creating 
+ * resource owner subject representations
+ */
+public interface SubjectCreator {
+
+
+    /**
+     * Create a {@link UserSubject} 
+     * @param mc the {@link MessageContext} of this request
+     * @return {@link UserSubject}
+     * @throws OAuthServiceException
+     */
+    UserSubject createUserSubject(MessageContext mc) throws OAuthServiceException;
+}
