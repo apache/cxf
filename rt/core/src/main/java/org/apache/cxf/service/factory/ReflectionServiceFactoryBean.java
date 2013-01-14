944cb79b980c5be07779d517a7aa087f7148967a
Freeman Yue Fang <ffang@apache.org>
2013-01-14 04:52:49 +0000

    Merged revisions 1432800 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes
    ................
      r1432800 | ffang | 2013-01-14 12:44:04 +0800 (一, 14  1 2013) | 9 lines
      
      Merged revisions 1432798 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1432798 | ffang | 2013-01-14 12:32:40 +0800 (一, 14  1 2013) | 1 line
        
        [CXF-4676]Partial WSDL support in CXF
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes@1432803 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/service/factory/ReflectionServiceFactoryBean.java
+++ rt/core/src/main/java/org/apache/cxf/service/factory/ReflectionServiceFactoryBean.java
@@ -46,7 +46,9 @@ import java.util.concurrent.Executor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.wsdl.Definition;
 import javax.wsdl.Operation;
+import javax.wsdl.WSDLException;
 import javax.xml.bind.annotation.XmlAttachmentRef;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlList;
@@ -105,6 +107,7 @@ import org.apache.cxf.service.model.OperationInfo;
 import org.apache.cxf.service.model.SchemaInfo;
 import org.apache.cxf.service.model.ServiceInfo;
 import org.apache.cxf.service.model.UnwrappedOperationInfo;
+import org.apache.cxf.wsdl.WSDLManager;
 import org.apache.cxf.wsdl11.WSDLServiceBuilder;
 import org.apache.cxf.wsdl11.WSDLServiceFactory;
 import org.apache.ws.commons.schema.XmlSchema;
@@ -525,7 +528,7 @@ public class ReflectionServiceFactoryBean extends AbstractServiceFactoryBean {
     }
 
     protected boolean isFromWsdl() {
-        return !populateFromClass && getWsdlURL() != null;
+        return !populateFromClass && getWsdlURL() != null && !isEmptywsdl(getWsdlURL());
     }
 
     protected void initializeServiceModel() {
@@ -2581,5 +2584,17 @@ public class ReflectionServiceFactoryBean extends AbstractServiceFactoryBean {
 
     public void setSchemaLocations(List<String> schemaLocations) {
         this.schemaLocations = schemaLocations;
+    }
+    private boolean isEmptywsdl(String wsdlUrl) {
+        Definition definition;
+        try {
+            definition = getBus().getExtension(WSDLManager.class).getDefinition(wsdlUrl);
+        } catch (WSDLException e) {
+            return true;
+        }
+        if (definition.getPortTypes().isEmpty() && definition.getImports().isEmpty()) {
+            return true;
+        } 
+        return false;
     }
 }
