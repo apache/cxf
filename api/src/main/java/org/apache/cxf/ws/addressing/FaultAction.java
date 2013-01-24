e4f7ef81eb8874378728dbbd13c6fcba2795b461
Daniel Kulp <dkulp@apache.org>
2013-01-24 17:18:35 +0000

    [CXF-4756] Ability to specify action to use for unchecked exceptions via an annotation
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1438080 13f79535-47bb-0310-9956-ffa450edef68

---
+++ api/src/main/java/org/apache/cxf/ws/addressing/FaultAction.java
@@ -0,0 +1,39 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.ws.addressing;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * If applied on an exception type, the value specifies the WS-Addressing Action
+ * which will be applied if this exception is thrown from a WSA-enabled service.
+ * This setting overrides any other settings, for example from @javax.xml.ws.Action.
+ * If the value is null, this annotation is ignored.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Target(ElementType.TYPE)
+public @interface FaultAction {
+    String value();
+}
