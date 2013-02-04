ec55501ea8765bf46fd6c65843efff0a95c721b8
Freeman Yue Fang <ffang@apache.org>
2013-02-04 04:46:26 +0000

    Merged revisions 1442024 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes
    ................
      r1442024 | ffang | 2013-02-04 12:39:40 +0800 (一, 04  2 2013) | 16 lines
      
      Merged revisions 1442021 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes
      
      ................
        r1442021 | ffang | 2013-02-04 12:04:01 +0800 (一, 04  2 2013) | 9 lines
        
        Merged revisions 1442015 via svnmerge from 
        https://svn.apache.org/repos/asf/cxf/trunk
        
        ........
          r1442015 | ffang | 2013-02-04 11:06:19 +0800 (一, 04  2 2013) | 1 line
          
          [CXF-4790]Set lang and subcode for soapfault
        ........
      ................
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1442025 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/interceptor/Fault.java
+++ api/src/main/java/org/apache/cxf/interceptor/Fault.java
@@ -43,11 +43,12 @@ public class Fault extends UncheckedException {
     private Element detail;
     private String message;
     private QName code;
+    private String lang;
     /**
      * response http header status code
      */
     private int statusCode = DEFAULT_HTTP_RESPONSE_CODE;
-    
+
     public Fault(Message message, Throwable throwable) {
         super(message, throwable);
         this.message = message.toString();
@@ -183,5 +184,13 @@ public class Fault extends UncheckedException {
      */
     public void setStatusCode(int statusCode) {
         this.statusCode = statusCode;
+    }
+
+    public void setLang(String convertedLang) {
+        lang = convertedLang;
+    }
+
+    public String getLang() {
+        return lang;
     }
 }
