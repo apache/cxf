bec8047e153e64e5fa281b734aeab37c8cd0f6b9
Daniel Kulp <dkulp@apache.org>
2013-02-13 17:09:29 +0000

    Merged revisions 1445729 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes
    ........
      r1445729 | dkulp | 2013-02-13 11:48:38 -0500 (Wed, 13 Feb 2013) | 18 lines
    
      Merged revisions 1445377 via  git cherry-pick from
      https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes
    
      ........
        r1445377 | dkulp | 2013-02-12 16:16:06 -0500 (Tue, 12 Feb 2013) | 10 lines
    
        Merged revisions 1445283 via  git cherry-pick from
        https://svn.apache.org/repos/asf/cxf/trunk
    
        ........
          r1445283 | dkulp | 2013-02-12 12:30:13 -0500 (Tue, 12 Feb 2013) | 2 lines
    
          Assertions that are not found in the map should not be asserted.
    
        ........
    
      ........
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1445749 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/ws/policy/AssertionInfoMap.java
+++ api/src/main/java/org/apache/cxf/ws/policy/AssertionInfoMap.java
@@ -99,12 +99,19 @@ public class AssertionInfoMap extends HashMap<QName, Collection<AssertionInfo>> 
         } else if (assertion instanceof Assertion) {
             Assertion ass = (Assertion)assertion;
             Collection<AssertionInfo> ail = getAssertionInfo(ass.getName());
+            boolean found = false;
             for (AssertionInfo ai : ail) {
-                if (ai.getAssertion().equal(ass)
-                    && !ai.isAsserted() && !ass.isOptional()) {
-                    errors.add(ass.getName());
-                    pass = false;                    
+                if (ai.getAssertion().equal(ass)) {
+                    found = true;
+                    if (!ai.isAsserted() && !ass.isOptional()) {
+                        errors.add(ass.getName());
+                        pass = false;                    
+                    }
                 }
+            }
+            if (!found) {
+                errors.add(ass.getName());
+                return false;
             }
         }
         if (assertion instanceof PolicyContainingAssertion) {
@@ -148,6 +155,7 @@ public class AssertionInfoMap extends HashMap<QName, Collection<AssertionInfo>> 
         
         for (QName name : errors) {
             Collection<AssertionInfo> ais = getAssertionInfo(name);
+            boolean found = false;
             for (AssertionInfo ai : ais) {
                 if (!ai.isAsserted()) {
                     String s = name.toString();
@@ -155,7 +163,11 @@ public class AssertionInfoMap extends HashMap<QName, Collection<AssertionInfo>> 
                         s += ": " + ai.getErrorMessage();
                     }
                     msgs.add(s);
+                    found = true;
                 }
+            }
+            if (!found) {
+                msgs.add(name.toString());
             }
         }
         StringBuilder error = new StringBuilder();
