5d2d5ec98c1174d6a978591f0cd6e1596b5e1025
Colm O Heigeartaigh <coheigea@apache.org>
2013-02-20 11:09:16 +0000

    [CXF-4841] - STSClient AppliesTo is not working correctly in certain circumstances
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1448077 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/trust/AbstractSTSClient.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/trust/AbstractSTSClient.java
@@ -157,7 +157,7 @@ public abstract class AbstractSTSClient implements Configurable, InterceptorProv
     protected CallbackHandler claimsCallbackHandler;
     protected AlgorithmSuite algorithmSuite;
     protected String namespace = STSUtils.WST_NS_05_12;
-    protected String addressingNamespace;
+    protected String addressingNamespace = "http://www.w3.org/2005/08/addressing";
     protected Object onBehalfOf;
     protected boolean enableAppliesTo = true;
 
@@ -976,10 +976,6 @@ public abstract class AbstractSTSClient implements Configurable, InterceptorProv
             tokentype = namespace + "/RSTR/Status";
         }
 
-        if (addressingNamespace == null) {
-            addressingNamespace = "http://www.w3.org/2005/08/addressing";
-        }
-
         Policy validatePolicy = new Policy();
         ExactlyOne one = new ExactlyOne();
         validatePolicy.addPolicyComponent(one);
@@ -1030,10 +1026,6 @@ public abstract class AbstractSTSClient implements Configurable, InterceptorProv
     protected STSResponse cancel(SecurityToken token) throws Exception {
         createClient();
 
-        if (addressingNamespace == null) {
-            addressingNamespace = "http://www.w3.org/2005/08/addressing";
-        }
-
         client.getRequestContext().clear();
         client.getRequestContext().putAll(ctx);
         client.getRequestContext().put(SecurityConstants.TOKEN, token);
@@ -1069,13 +1061,19 @@ public abstract class AbstractSTSClient implements Configurable, InterceptorProv
             SignedEncryptedParts parts = new SignedEncryptedParts(true);
             parts.setOptional(true);
             parts.setBody(true);
-            parts.addHeader(new Header("To", addressingNamespace));
-            parts.addHeader(new Header("From", addressingNamespace));
-            parts.addHeader(new Header("FaultTo", addressingNamespace));
-            parts.addHeader(new Header("ReplyTo", addressingNamespace));
-            parts.addHeader(new Header("Action", addressingNamespace));
-            parts.addHeader(new Header("MessageID", addressingNamespace));
-            parts.addHeader(new Header("RelatesTo", addressingNamespace));
+            
+            String addrNamespace = addressingNamespace;
+            if (addrNamespace == null) {
+                addrNamespace = "http://www.w3.org/2005/08/addressing";
+            }
+            
+            parts.addHeader(new Header("To", addrNamespace));
+            parts.addHeader(new Header("From", addrNamespace));
+            parts.addHeader(new Header("FaultTo", addrNamespace));
+            parts.addHeader(new Header("ReplyTo", addrNamespace));
+            parts.addHeader(new Header("Action", addrNamespace));
+            parts.addHeader(new Header("MessageID", addrNamespace));
+            parts.addHeader(new Header("RelatesTo", addrNamespace));
             all.addPolicyComponent(parts);
             
             client.getRequestContext().put(PolicyConstants.POLICY_OVERRIDE, cancelPolicy);
