e88bb5e2267071732786f746ccc143ae7597baf4
Colm O Heigeartaigh <coheigea@apache.org>
2013-03-04 17:30:06 +0000

    Merged revisions 1452414 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes
    ........
      r1452414 | coheigea | 2013-03-04 17:29:03 +0000 (Mon, 04 Mar 2013) | 18 lines
    
      Merged revisions 1452411 via  git cherry-pick from
      https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes
    
      ........
        r1452411 | coheigea | 2013-03-04 17:23:48 +0000 (Mon, 04 Mar 2013) | 10 lines
    
        Merged revisions 1452410 via  git cherry-pick from
        https://svn.apache.org/repos/asf/cxf/trunk
    
        ........
          r1452410 | coheigea | 2013-03-04 17:22:15 +0000 (Mon, 04 Mar 2013) | 2 lines
    
          Separating client + server side caching tests
    
        ........
    
      ........
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1452416 13f79535-47bb-0310-9956-ffa450edef68

--- services/sts/systests/advanced/src/test/java/org/apache/cxf/systest/sts/caching/CachingTest.java
+++ services/sts/systests/advanced/src/test/java/org/apache/cxf/systest/sts/caching/CachingTest.java
@@ -19,15 +19,11 @@
 package org.apache.cxf.systest.sts.caching;
 
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
 import javax.xml.ws.soap.SOAPFaultException;
-
-import org.w3c.dom.Element;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusException;
@@ -40,10 +36,7 @@ import org.apache.cxf.systest.sts.common.SecurityTestUtil;
 import org.apache.cxf.systest.sts.deployment.STSServer;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
 import org.apache.cxf.ws.security.SecurityConstants;
-import org.apache.cxf.ws.security.tokenstore.SecurityToken;
-import org.apache.cxf.ws.security.tokenstore.TokenStore;
 import org.apache.cxf.ws.security.trust.STSClient;
-
 import org.example.contract.doubleit.DoubleItPortType;
 import org.junit.BeforeClass;
 
@@ -57,16 +50,7 @@ public class CachingTest extends AbstractBusClientServerTestBase {
     private static final String NAMESPACE = "http://www.example.org/contract/DoubleIt";
     private static final QName SERVICE_QNAME = new QName(NAMESPACE, "DoubleItService");
     
-    private static final String SAML1_TOKEN_TYPE = 
-        "http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV1.1";
-    private static final String PUBLIC_KEY_KEYTYPE = 
-        "http://docs.oasis-open.org/ws-sx/ws-trust/200512/PublicKey";
-    
     private static final String PORT = allocatePort(Server.class);
-    private static final String PORT2 = allocatePort(Server.class, 2);
-    
-    private static final String DEFAULT_ADDRESS = 
-        "https://localhost:" + PORT + "/doubleit/services/doubleittransportsaml1alternative";
 
     @BeforeClass
     public static void startServers() throws Exception {
@@ -172,167 +156,6 @@ public class CachingTest extends AbstractBusClientServerTestBase {
         
         ((java.io.Closeable)port).close();
         bus.shutdown(true);
-    }
-    
-    @org.junit.Test
-    public void testServerSideSAMLTokenCaching() throws Exception {
-
-        SpringBusFactory bf = new SpringBusFactory();
-        URL busFile = CachingTest.class.getResource("cxf-client.xml");
-
-        Bus bus = bf.createBus(busFile.toString());
-        SpringBusFactory.setDefaultBus(bus);
-        SpringBusFactory.setThreadDefaultBus(bus);
-
-        URL wsdl = CachingTest.class.getResource("DoubleIt.wsdl");
-        Service service = Service.create(wsdl, SERVICE_QNAME);
-        QName portQName = new QName(NAMESPACE, "DoubleItTransportSAML1AlternativePort");
-        DoubleItPortType port = 
-            service.getPort(portQName, DoubleItPortType.class);
-        updateAddressPort(port, PORT);
-        
-        // Make an initial successful invocation
-        doubleIt(port, 25);
-        
-        // Store the SAML Assertion that was obtained from the STS
-        Client client = ClientProxy.getClient(port);
-        Endpoint ep = client.getEndpoint();
-        String id = (String)ep.get(SecurityConstants.TOKEN_ID);
-        TokenStore store = (TokenStore)ep.getEndpointInfo().getProperty(TokenStore.class.getName());
-        SecurityToken tok = store.getToken(id);
-        assertNotNull(tok);
-        Element storedToken = tok.getToken();
-        
-        // Get another security token by invoking on the STS directly and save it on the client port
-        SecurityToken token = 
-            requestSecurityToken(SAML1_TOKEN_TYPE, PUBLIC_KEY_KEYTYPE, bus, DEFAULT_ADDRESS);
-        assertNotNull(token);
-        tok.setToken(token.getToken());
-        
-        // Try another invocation - this will fail as the STSClient on the server side is disabled
-        // after the first invocation
-        try {
-            doubleIt(port, 30);
-            fail("Failure expected as the STSClient on the server side is null");
-        } catch (Throwable ex) {
-            // expected
-        }
-        // Try again using the original SAML token - this should work as it should be cached by the service
-        tok.setToken(storedToken);
-        doubleIt(port, 35);
-        
-        ((java.io.Closeable)port).close();
-        bus.shutdown(true);
-    }
-    
-    @org.junit.Test
-    public void testServerSideUsernameTokenCaching() throws Exception {
-
-        SpringBusFactory bf = new SpringBusFactory();
-        URL busFile = CachingTest.class.getResource("cxf-client.xml");
-
-        Bus bus = bf.createBus(busFile.toString());
-        SpringBusFactory.setDefaultBus(bus);
-        SpringBusFactory.setThreadDefaultBus(bus);
-
-        URL wsdl = CachingTest.class.getResource("DoubleIt.wsdl");
-        Service service = Service.create(wsdl, SERVICE_QNAME);
-        QName portQName = new QName(NAMESPACE, "DoubleItTransportUTPort");
-        DoubleItPortType transportUTPort = 
-            service.getPort(portQName, DoubleItPortType.class);
-        updateAddressPort(transportUTPort, PORT);
-        
-        // Make an initial successful invocation
-        doubleIt(transportUTPort, 25);
-        
-        BindingProvider p = (BindingProvider)transportUTPort;
-        try {
-            // The STSClient on the server side is disabled after the first invocation
-            p.getRequestContext().put(SecurityConstants.USERNAME, "bob");
-            doubleIt(transportUTPort, 30);
-            fail("Failure expected as the STSClient on the server side is null");
-        } catch (Exception ex) {
-            // expected
-        }
-        
-        // This will pass as the UsernameToken should be cached
-        p.getRequestContext().put(SecurityConstants.USERNAME, "alice");
-        doubleIt(transportUTPort, 35);
-        
-        ((java.io.Closeable)transportUTPort).close();
-        bus.shutdown(true);
-    }
-    
-    @org.junit.Test
-    public void testServerSideBinarySecurityTokenCaching() throws Exception {
-
-        SpringBusFactory bf = new SpringBusFactory();
-        URL busFile = CachingTest.class.getResource("cxf-client.xml");
-
-        Bus bus = bf.createBus(busFile.toString());
-        SpringBusFactory.setDefaultBus(bus);
-        SpringBusFactory.setThreadDefaultBus(bus);
-
-        URL wsdl = CachingTest.class.getResource("DoubleIt.wsdl");
-        Service service = Service.create(wsdl, SERVICE_QNAME);
-        QName portQName = new QName(NAMESPACE, "DoubleItAsymmetricBSTPort");
-        DoubleItPortType bstPort = 
-            service.getPort(portQName, DoubleItPortType.class);
-        updateAddressPort(bstPort, PORT2);
-        
-        // Make an initial successful invocation
-        doubleIt(bstPort, 25);
-        
-        BindingProvider p = (BindingProvider)bstPort;
-        try {
-            // The STSClient on the server side is disabled after the first invocation
-            p.getRequestContext().put(SecurityConstants.SIGNATURE_USERNAME, "myservicekey");
-            p.getRequestContext().put(SecurityConstants.SIGNATURE_PROPERTIES, "serviceKeystore.properties");
-            doubleIt(bstPort, 30);
-            fail("Failure expected as the STSClient on the server side is null");
-        } catch (Exception ex) {
-            // expected
-        }
-        
-        // This will pass as the BinarySecurityToken should be cached
-        p.getRequestContext().put(SecurityConstants.SIGNATURE_USERNAME, "myclientkey");
-        p.getRequestContext().put(SecurityConstants.SIGNATURE_PROPERTIES, "clientKeystore.properties");
-        doubleIt(bstPort, 35);
-        
-        ((java.io.Closeable)bstPort).close();
-        bus.shutdown(true);
-    }
-    
-    private SecurityToken requestSecurityToken(
-        String tokenType, 
-        String keyType,
-        Bus bus,
-        String endpointAddress
-    ) throws Exception {
-        STSClient stsClient = new STSClient(bus);
-        stsClient.setWsdlLocation("https://localhost:" + STSPORT + "/SecurityTokenService/Transport?wsdl");
-        stsClient.setServiceName("{http://docs.oasis-open.org/ws-sx/ws-trust/200512/}SecurityTokenService");
-        stsClient.setEndpointName("{http://docs.oasis-open.org/ws-sx/ws-trust/200512/}Transport_Port");
-
-        Map<String, Object> properties = new HashMap<String, Object>();
-        properties.put(SecurityConstants.USERNAME, "alice");
-        properties.put(
-            SecurityConstants.CALLBACK_HANDLER, 
-            "org.apache.cxf.systest.sts.common.CommonCallbackHandler"
-        );
-
-        if (PUBLIC_KEY_KEYTYPE.equals(keyType)) {
-            properties.put(SecurityConstants.STS_TOKEN_USERNAME, "myservicekey");
-            properties.put(SecurityConstants.STS_TOKEN_PROPERTIES, "serviceKeystore.properties");
-            stsClient.setUseCertificateForConfirmationKeyInfo(true);
-        }
-
-        stsClient.setProperties(properties);
-        stsClient.setTokenType(tokenType);
-        stsClient.setKeyType(keyType);
-        stsClient.setAddressingNamespace("http://www.w3.org/2005/08/addressing");
-
-        return stsClient.requestSecurityToken(endpointAddress);
     }
     
     private void clearSTSClient(BindingProvider p) throws BusException, EndpointException {
