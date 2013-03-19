9fadc0adc8ab0eb754d13c8c9e0c570aa38ad5db
Colm O Heigeartaigh <coheigea@apache.org>
2013-03-19 16:45:39 +0000

    [CXF-4904] - Incorrect validation of Layout LaxTimestampFirst + LaxTimestampLast
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1458397 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/policyvalidators/AbstractBindingPolicyValidator.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/policyvalidators/AbstractBindingPolicyValidator.java
@@ -152,7 +152,7 @@ public abstract class AbstractBindingPolicyValidator implements BindingPolicyVal
             if (results.isEmpty()) {
                 return false;
             }
-            Integer firstAction = (Integer)results.get(0).get(WSSecurityEngineResult.TAG_ACTION);
+            Integer firstAction = (Integer)results.get(results.size() - 1).get(WSSecurityEngineResult.TAG_ACTION);
             if (firstAction.intValue() != WSConstants.TS) {
                 return false;
             }
@@ -161,7 +161,7 @@ public abstract class AbstractBindingPolicyValidator implements BindingPolicyVal
                 return false;
             }
             Integer lastAction = 
-                (Integer)results.get(results.size() - 1).get(WSSecurityEngineResult.TAG_ACTION);
+                (Integer)results.get(0).get(WSSecurityEngineResult.TAG_ACTION);
             if (lastAction.intValue() != WSConstants.TS) {
                 return false;
             }
