94eb0aa577baeb8d2b51223a5c3282f4eef33a39
Colm O Heigeartaigh <coheigea@apache.org>
2013-03-20 12:24:30 +0000

    Reverting accidental change
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1458787 13f79535-47bb-0310-9956-ffa450edef68

--- systests/ws-security-examples/src/test/java/org/apache/cxf/systest/wssec/examples/saml/SamlTokenTest.java
+++ systests/ws-security-examples/src/test/java/org/apache/cxf/systest/wssec/examples/saml/SamlTokenTest.java
@@ -100,6 +100,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.1.2 (WSS1.0) SAML1.1 Assertion (Sender Vouches) over SSL
+     */
     @org.junit.Test
     public void testTLSSenderVouches() throws Exception {
 
@@ -125,6 +126,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.1.3 (WSS1.0) SAML1.1 Assertion (HK) over SSL
+     */
     @org.junit.Test
     public void testTLSHOKSignedEndorsing() throws Exception {
 
@@ -150,6 +152,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.1.4 (WSS1.0) SAML1.1 Sender Vouches with X.509 Certificates, Sign, Optional Encrypt
+     */
     @org.junit.Test
     public void testAsymmetricSigned() throws Exception {
 
@@ -175,6 +178,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.1.5 (WSS1.0) SAML1.1 Holder of Key, Sign, Optional Encrypt
+     */
     @org.junit.Test
     public void testAsymmetricInitiator() throws Exception {
 
@@ -201,6 +205,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.2.1 (WSS1.1) SAML 2.0 Bearer
+     */
     @org.junit.Test
     public void testAsymmetricSaml2Bearer() throws Exception {
 
@@ -226,6 +231,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.2.2 (WSS1.1) SAML2.0 Sender Vouches over SSL
+     */
     @org.junit.Test
     public void testTLSSenderVouchesSaml2() throws Exception {
 
@@ -251,6 +257,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.2.3 (WSS1.1) SAML2.0 HoK over SSL
+     */
     @org.junit.Test
     public void testTLSHOKSignedEndorsingSaml2() throws Exception {
 
@@ -276,6 +283,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.2.4 (WSS1.1) SAML1.1/2.0 Sender Vouches with X.509 Certificate, Sign, Encrypt
+     */
     @org.junit.Test
     public void testSymmetricSV() throws Exception {
 
@@ -301,6 +309,7 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
     
     /**
      * 2.3.2.5 (WSS1.1) SAML1.1/2.0 Holder of Key, Sign, Encrypt
+     */
     @org.junit.Test
     public void testSymmetricIssuedToken() throws Exception {
 
@@ -324,7 +333,6 @@ public class SamlTokenTest extends AbstractBusClientServerTestBase {
         ((java.io.Closeable)samlPort).close();
         bus.shutdown(true);
     }
-    */
     
     private static void updateSTSPort(BindingProvider p, String port) {
         STSClient stsClient = (STSClient)p.getRequestContext().get(SecurityConstants.STS_CLIENT);
