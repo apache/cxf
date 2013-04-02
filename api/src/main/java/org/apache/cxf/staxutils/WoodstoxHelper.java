1644c0fba2df316c80813eb2ed34c3cb080a8d7c
Daniel Kulp <dkulp@apache.org>
2013-04-02 15:42:04 +0000

    Update to make things work if woodstox isn't found and insecure parsing is turned on.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1463600 13f79535-47bb-0310-9956-ffa450edef68

---
+++ api/src/main/java/org/apache/cxf/staxutils/WoodstoxHelper.java
@@ -0,0 +1,43 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.staxutils;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+
+import org.codehaus.stax2.XMLStreamReader2;
+
+/**
+ * 
+ */
+final class WoodstoxHelper {
+
+    private WoodstoxHelper() {
+    }
+    
+    public static XMLInputFactory createInputFactory() {
+        return new com.ctc.wstx.stax.WstxInputFactory();
+    }
+
+    public static void setProperty(XMLStreamReader reader, String p, Object v) {
+        ((XMLStreamReader2)reader).setProperty(p, v);
+    }
+
+}
