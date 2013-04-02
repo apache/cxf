2c992cb9aeca97515454e863fff44f88210a079c
Freeman Yue Fang <ffang@apache.org>
2013-04-02 05:01:49 +0000

    [CXF-4936]Expose FastInfoset serializer tuning properties
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1463382 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/interceptor/FIStaxOutInterceptor.java
+++ api/src/main/java/org/apache/cxf/interceptor/FIStaxOutInterceptor.java
@@ -24,12 +24,15 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
 import com.sun.xml.fastinfoset.stax.StAXDocumentSerializer;
 
+import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.helpers.CastUtils;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.message.MessageUtils;
@@ -42,11 +45,19 @@ import org.apache.cxf.phase.Phase;
  */
 public class FIStaxOutInterceptor extends AbstractPhaseInterceptor<Message> {
     public static final String FI_ENABLED = "org.apache.cxf.fastinfoset.enabled";
+
+    private static final Logger LOG = LogUtils.getL7dLogger(FIStaxOutInterceptor.class);
     private static final String OUTPUT_STREAM_HOLDER = FIStaxOutInterceptor.class.getName() + ".outputstream";
     private static final StaxOutEndingInterceptor ENDING = new StaxOutEndingInterceptor(OUTPUT_STREAM_HOLDER);
 
     boolean force;
-    
+    private Integer serializerAttributeValueMapMemoryLimit;
+    private Integer serializerMinAttributeValueSize;
+    private Integer serializerMaxAttributeValueSize;
+    private Integer serializerCharacterContentChunkMapMemoryLimit;
+    private Integer serializerMinCharacterContentChunkSize;
+    private Integer serializerMaxCharacterContentChunkSize;
+
     public FIStaxOutInterceptor() {
         super(Phase.PRE_STREAM);
         addAfter(AttachmentOutInterceptor.class.getName());
@@ -138,8 +149,140 @@ public class FIStaxOutInterceptor extends AbstractPhaseInterceptor<Message> {
         }
         return serializer;
         */
-        return new StAXDocumentSerializer(out);
+        final StAXDocumentSerializer stAXDocumentSerializer = new StAXDocumentSerializer(out);
+        if (serializerAttributeValueMapMemoryLimit != null && serializerAttributeValueMapMemoryLimit.intValue() > 0) {
+            stAXDocumentSerializer.setAttributeValueMapMemoryLimit(serializerAttributeValueMapMemoryLimit.intValue());
+        }
+        if (serializerMinAttributeValueSize != null && serializerMinAttributeValueSize.intValue() > 0) {
+            stAXDocumentSerializer.setMinAttributeValueSize(serializerMinAttributeValueSize.intValue());
+        }
+        if (serializerMaxAttributeValueSize != null && serializerMaxAttributeValueSize.intValue() > 0) {
+            stAXDocumentSerializer.setMaxAttributeValueSize(serializerMaxAttributeValueSize.intValue());
+        }
+        if (serializerCharacterContentChunkMapMemoryLimit != null
+                && serializerCharacterContentChunkMapMemoryLimit.intValue() > 0) {
+            stAXDocumentSerializer
+                    .setCharacterContentChunkMapMemoryLimit(serializerCharacterContentChunkMapMemoryLimit.intValue());
+        }
+        if (serializerMinCharacterContentChunkSize != null && serializerMinCharacterContentChunkSize.intValue() > 0) {
+            stAXDocumentSerializer.setMinCharacterContentChunkSize(serializerMinCharacterContentChunkSize.intValue());
+        }
+        if (serializerMaxCharacterContentChunkSize != null && serializerMaxCharacterContentChunkSize.intValue() > 0) {
+            stAXDocumentSerializer.setMaxCharacterContentChunkSize(serializerMaxCharacterContentChunkSize.intValue());
+        }
+        return stAXDocumentSerializer;
     }
-        
 
+    public Integer getSerializerMinAttributeValueSize() {
+        return serializerMinAttributeValueSize;
+    }
+
+    /**
+     * Sets the property <code>minAttributeValueSize</code> on FastInfoset StAX Serializers created and used
+     * by this interceptor. The property controls the <b>minimum</b> size of attribute values to be indexed.
+     *
+     * @param serializerMinAttributeValueSize
+     *         The <b>minimum</b> size for attribute values to be indexed,
+     *         measured as a number of characters. The default is typically 0.
+     */
+    public void setSerializerMinAttributeValueSize(Integer serializerMinAttributeValueSize) {
+        logSetter("serializerMinAttributeValueSize", serializerMinAttributeValueSize);
+        this.serializerMinAttributeValueSize = serializerMinAttributeValueSize;
+    }
+
+    public Integer getSerializerMaxAttributeValueSize() {
+        return serializerMaxAttributeValueSize;
+    }
+
+    /**
+     * Sets the property <code>maxAttributeValueSize</code> on FastInfoset StAX Serializers created and used
+     * by this interceptor. The property controls the <b>maximum</b> size of attribute values to be indexed.
+     * Tests have shown that setting this property to lower values reduces CPU burden of processing, at the expense
+     * of larger sizes of resultant encoded Fast Infoset data.
+     *
+     * @param serializerMaxAttributeValueSize
+     *         The <b>maximum</b> size for attribute values to be indexed,
+     *         measured as a number of characters. The default is typically 32.
+     */
+    public void setSerializerMaxAttributeValueSize(Integer serializerMaxAttributeValueSize) {
+        logSetter("serializerMaxAttributeValueSize", serializerMaxAttributeValueSize);
+        this.serializerMaxAttributeValueSize = serializerMaxAttributeValueSize;
+    }
+
+    public Integer getSerializerCharacterContentChunkMapMemoryLimit() {
+        return serializerCharacterContentChunkMapMemoryLimit;
+    }
+
+    /**
+     * Sets the property <code>characterContentChunkMapMemoryLimit</code> on FastInfoset StAX Serializers created and
+     * used by this interceptor. The property controls character content chunk map size and can be used to control the
+     * memory and (indirectly) CPU footprint of processing.
+     *
+     * @param serializerCharacterContentChunkMapMemoryLimit
+     *         The value for the limit, measured as a number of Unicode characters.
+     */
+    public void setSerializerCharacterContentChunkMapMemoryLimit(
+            Integer serializerCharacterContentChunkMapMemoryLimit) {
+        logSetter("serializerCharacterContentChunkMapMemoryLimit", serializerCharacterContentChunkMapMemoryLimit);
+        this.serializerCharacterContentChunkMapMemoryLimit = serializerCharacterContentChunkMapMemoryLimit;
+    }
+
+    public Integer getSerializerMinCharacterContentChunkSize() {
+        return serializerMinCharacterContentChunkSize;
+    }
+
+    /**
+     * Sets the property <code>minCharacterContentChunkSize</code> on FastInfoset StAX Serializers created and used
+     * by this interceptor. The property controls the <b>minimum</b> size of character content chunks to be indexed.
+     *
+     * @param serializerMinCharacterContentChunkSize
+     *         The <b>minimum</b> size for character content chunks to be indexed,
+     *         measured as a number of characters. The default is typically 0.
+     */
+    public void setSerializerMinCharacterContentChunkSize(Integer serializerMinCharacterContentChunkSize) {
+        logSetter("serializerMinCharacterContentChunkSize", serializerMinCharacterContentChunkSize);
+        this.serializerMinCharacterContentChunkSize = serializerMinCharacterContentChunkSize;
+    }
+
+    public Integer getSerializerMaxCharacterContentChunkSize() {
+        return serializerMaxCharacterContentChunkSize;
+    }
+
+    /**
+     * Sets the property <code>maxCharacterContentChunkSize</code> on FastInfoset StAX Serializers created and used
+     * by this interceptor. The property controls the <b>maximum</b> size of character content chunks to be indexed.
+     * Tests have shown that setting this property to lower values reduces CPU burden of processing, at the expense
+     * of larger sizes of resultant encoded Fast Infoset data.
+     *
+     * @param serializerMaxCharacterContentChunkSize
+     *         The <b>maximum</b> size for character content chunks to be indexed,
+     *         measured as a number of characters. The default is typically 32.
+     */
+    public void setSerializerMaxCharacterContentChunkSize(Integer serializerMaxCharacterContentChunkSize) {
+        logSetter("serializerMaxCharacterContentChunkSize", serializerMaxCharacterContentChunkSize);
+        this.serializerMaxCharacterContentChunkSize = serializerMaxCharacterContentChunkSize;
+    }
+
+    public Integer getSerializerAttributeValueMapMemoryLimit() {
+        return serializerAttributeValueMapMemoryLimit;
+    }
+
+    /**
+     * Sets the property <code>attributeValueMapMemoryLimit</code> on FastInfoset StAX Serializers created and used
+     * by this interceptor. The property controls attribute value map size and can be used to control
+     * the memory and (indirectly) CPU footprint of processing.
+     *
+     * @param serializerAttributeValueMapMemoryLimit
+     *         The value for the limit, measured as a number of Unicode characters.
+     */
+    public void setSerializerAttributeValueMapMemoryLimit(Integer serializerAttributeValueMapMemoryLimit) {
+        logSetter("serializerAttributeValueMapMemoryLimit", serializerAttributeValueMapMemoryLimit);
+        this.serializerAttributeValueMapMemoryLimit = serializerAttributeValueMapMemoryLimit;
+    }
+
+    private void logSetter(String propertyName, Object propertyValue) {
+        if (LOG.isLoggable(Level.CONFIG)) {
+            LOG.config("Setting " + propertyName + " to " + propertyValue);
+        }
+    }
 }
