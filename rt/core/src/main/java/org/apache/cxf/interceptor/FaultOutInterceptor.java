1479378c59e61729f54ba8fef81a379d73baa92c
Freeman Yue Fang <ffang@apache.org>
2013-04-08 02:30:08 +0000

    Merged revisions 1465527 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes
    ................
      r1465527 | ffang | 2013-04-08 09:40:19 +0800 (一, 08  4 2013) | 20 lines
      
      Merged revisions 1465526 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes
      
      ................
        r1465526 | ffang | 2013-04-08 09:33:19 +0800 (一, 08  4 2013) | 13 lines
        
        Merged revisions 1465337,1465373 via svnmerge from 
        https://svn.apache.org/repos/asf/cxf/trunk
        
        ........
          r1465337 | ffang | 2013-04-07 15:42:23 +0800 (日, 07  4 2013) | 1 line
          
          [CXF-4940]add a testcase to demonstrate exception hierarchy works
        ........
          r1465373 | ffang | 2013-04-07 20:32:59 +0800 (日, 07  4 2013) | 1 line
          
          [CXF-4940]CXF not (always) responding with the correct exception when working with an exception hierarchy 
        ........
      ................
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1465529 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/interceptor/FaultOutInterceptor.java
+++ rt/core/src/main/java/org/apache/cxf/interceptor/FaultOutInterceptor.java
@@ -143,16 +143,20 @@ public class FaultOutInterceptor extends AbstractPhaseInterceptor<Message> {
      * @param class1
      * @return
      */
-    public FaultInfo getFaultForClass(BindingOperationInfo op, Class class1) {
+    public FaultInfo getFaultForClass(BindingOperationInfo op, Class<?> class1) {
+        FaultInfo selectedFaultInfo = null;
+        Class<?> selectedFaultInfoClass = null;
         for (BindingFaultInfo bfi : op.getFaults()) {
 
             FaultInfo faultInfo = bfi.getFaultInfo();
-            Class<?> c = (Class)faultInfo.getProperty(Class.class.getName());
-            if (c != null && c.isAssignableFrom(class1)) {
-                return faultInfo;
+            Class<?> c = (Class<?>)faultInfo.getProperty(Class.class.getName());
+            if (c != null && c.isAssignableFrom(class1)
+                && (selectedFaultInfo == null || selectedFaultInfoClass.isAssignableFrom(c))) {
+                selectedFaultInfo = faultInfo;
+                selectedFaultInfoClass = c;
+
             }
         }
-
-        return null;
+        return selectedFaultInfo;
     }
 }
