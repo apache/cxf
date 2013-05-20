13c9797c84f647dbecd1cef854721c7a274603c2
Sergey Beryozkin <sergeyb@apache.org>
2013-05-20 09:42:45 +0000

    Replacing Exception with Throwable in JAXRS client code to get another 1.1 and 2.0 API mix-up issue handled
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1484408 13f79535-47bb-0310-9956-ffa450edef68

--- rt/rs/client/src/main/java/org/apache/cxf/jaxrs/client/AbstractClient.java
+++ rt/rs/client/src/main/java/org/apache/cxf/jaxrs/client/AbstractClient.java
@@ -455,7 +455,7 @@ public abstract class AbstractClient implements Client {
         try {
             Constructor<?> ctr = exceptionClass.getConstructor(Response.class);
             return (WebApplicationException)ctr.newInstance(r);
-        } catch (Exception ex2) {
+        } catch (Throwable ex2) {
             return new WebApplicationException(ex2, r);
         }
     }
