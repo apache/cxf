925978effd034bbddc7ebe3716c4f6ed916e5b40
Colm O Heigeartaigh <coheigea@apache.org>
2013-05-22 15:14:07 +0000

    [CXF-5031] - Add support for SupportingToken policy assertions without a binding
    Conflicts:
    	rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/PolicyBasedWSS4JInInterceptor.java
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/wss4j2.0-port@1485241 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/policyhandlers/TransportBindingHandler.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/policyhandlers/TransportBindingHandler.java
@@ -148,8 +148,10 @@ public class TransportBindingHandler extends AbstractBindingBuilder {
                             addEncryptedKeyElement(cloneElement(el));
                         } 
                     }
+                }
                     
-                    handleNonEndorsingSupportingTokens();
+                handleNonEndorsingSupportingTokens();
+                if (transportTokenWrapper != null) {
                     handleEndorsingSupportingTokens();
                 }
             } else {
