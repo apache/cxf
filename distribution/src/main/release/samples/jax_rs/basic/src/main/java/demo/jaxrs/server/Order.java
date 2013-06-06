1223a2c432c7901be7254e358928e6db91db89aa
Sergey Beryozkin <sergeyb@apache.org>
2013-06-06 20:33:07 +0000

    Minor updates to get a demo working
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1490431 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/jax_rs/basic/src/main/java/demo/jaxrs/server/Order.java
+++ distribution/src/main/release/samples/jax_rs/basic/src/main/java/demo/jaxrs/server/Order.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -54,6 +55,7 @@ public class Order {
 
     @GET
     @Path("products/{productId}/")
+    @Produces("text/xml")   
     public Product getProduct(@PathParam("productId")int productId) {
         System.out.println("----invoking getProduct with id: " + productId);
         Product p = products.get(new Long(productId));
