e932b1f49139576c52dc21065ec4a75bfe2782a5
Freeman Yue Fang <ffang@apache.org>
2013-06-17 05:20:32 +0000

    [CXF-5061]a testcase to demonstrate that valid WSDL could get generated for List of Complex Type
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1493644 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/jaxws/TestService.java
@@ -0,0 +1,35 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.jaxws;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import org.apache.cxf.systest.jaxws.base.WrapperString;
+
+@WebService(name = "TestService", portName = "TestServicePort", 
+    serviceName = "TestService", targetNamespace = "http://cxf.apache.org/systest/jaxws")
+@SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
+public interface TestService {
+    @WebMethod(operationName = "getList")
+    List<WrapperString> getList();
+}
---
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/jaxws/TestServiceImpl.java
@@ -0,0 +1,36 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.jaxws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.cxf.systest.jaxws.base.WrapperString;
+
+public class TestServiceImpl implements TestService {
+    @Override
+    public List<WrapperString> getList() {
+        List<WrapperString> list = new ArrayList<WrapperString>();
+
+        list.add(new WrapperString("Test"));
+        list.add(new WrapperString("Test 2"));
+
+        return list;
+    }
+}
