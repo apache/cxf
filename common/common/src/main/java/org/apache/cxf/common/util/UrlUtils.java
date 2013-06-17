0e7c0482c35d5247aa37525f55dd1573074958aa
Daniel Kulp <dkulp@apache.org>
2013-06-17 18:53:23 +0000

    Fix another compile failure
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.5.x-fixes@1493884 13f79535-47bb-0310-9956-ffa450edef68

--- common/common/src/main/java/org/apache/cxf/common/util/UrlUtils.java
+++ common/common/src/main/java/org/apache/cxf/common/util/UrlUtils.java
@@ -20,7 +20,11 @@
 package org.apache.cxf.common.util;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.logging.Logger;
 
 import org.apache.cxf.common.logging.LogUtils;
@@ -44,7 +48,6 @@ public final class UrlUtils {
      * Decodes using URLDecoder - use when queries or form post values are decoded
      * @param value value to decode
      * @param enc encoding
-     * @return
      */
     public static String urlDecode(String value, String enc) {
         try {
@@ -63,7 +66,6 @@ public final class UrlUtils {
      * URL path segments may contain '+' symbols which should not be decoded into ' '
      * This method replaces '+' with %2B and delegates to URLDecoder
      * @param value value to decode
-     * @return
      */
     public static String pathDecode(String value) {
         // TODO: we actually need to do a proper URI analysis here according to
@@ -76,5 +78,44 @@ public final class UrlUtils {
         
         return urlDecode(value);
     }
+    
+    
+    /**
+     * Create a map from String to String that represents the contents of the query
+     * portion of a URL. For each x=y, x is the key and y is the value.
+     * @param s the query part of the URI.
+     * @return the map.
+     */
+    public static Map<String, String> parseQueryString(String s) {
+        Map<String, String> ht = new HashMap<String, String>();
+        StringTokenizer st = new StringTokenizer(s, "&");
+        while (st.hasMoreTokens()) {
+            String pair = st.nextToken();
+            int pos = pair.indexOf('=');
+            if (pos == -1) {
+                ht.put(pair.toLowerCase(), "");
+            } else {
+                ht.put(pair.substring(0, pos).toLowerCase(),
+                       pair.substring(pos + 1));
+            }
+        }
+        return ht;
+    }
+    
+    /**
+     * Return everything in the path up to the last slash in a URI.
+     * @param baseURI
+     * @return the trailing 
+     */
+    public static String getStem(String baseURI) {
+        URI uri = URI.create(baseURI);
+        baseURI = uri.getRawPath();
+        int idx = baseURI.lastIndexOf('/');
+        if (idx != -1) {
+            baseURI = baseURI.substring(0, idx);
+        }
+        return URI.create(baseURI).getPath();
+    }
+    
     
 }
