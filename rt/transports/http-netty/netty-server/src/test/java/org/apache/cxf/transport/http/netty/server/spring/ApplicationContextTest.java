1b4a1b6db93b6c3dbbf0da39c9621ef57b82f76b
Daniel Kulp <dkulp@apache.org>
2013-06-25 20:32:01 +0000

    Fix broken compile
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1496631 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/http-netty/netty-server/src/test/java/org/apache/cxf/transport/http/netty/server/spring/ApplicationContextTest.java
+++ rt/transports/http-netty/netty-server/src/test/java/org/apache/cxf/transport/http/netty/server/spring/ApplicationContextTest.java
@@ -108,7 +108,7 @@ public class ApplicationContextTest extends Assert {
         
         DestinationFactoryManager dfm = bus.getExtension(DestinationFactoryManager.class);
         DestinationFactory factory = dfm.getDestinationFactory("http://cxf.apache.org/transports/http");
-        Destination d = factory.getDestination(info);
+        Destination d = factory.getDestination(info, bus);
         assertTrue(d instanceof NettyHttpDestination);
         NettyHttpDestination jd = (NettyHttpDestination) d;        
         assertEquals("foobar", jd.getServer().getContentEncoding());   
@@ -119,16 +119,16 @@ public class ApplicationContextTest extends Assert {
         
         ConduitInitiatorManager cim = bus.getExtension(ConduitInitiatorManager.class);
         ConduitInitiator ci = cim.getConduitInitiator("http://cxf.apache.org/transports/http");
-        HTTPConduit conduit = (HTTPConduit) ci.getConduit(info);
+        HTTPConduit conduit = (HTTPConduit) ci.getConduit(info, bus);
         assertEquals(97, conduit.getClient().getConnectionTimeout());
         
         info.setName(new QName("urn:test:ns", "Bar"));
-        conduit = (HTTPConduit) ci.getConduit(info);
+        conduit = (HTTPConduit) ci.getConduit(info, bus);
         assertEquals(79, conduit.getClient().getConnectionTimeout());
 
         NettyHttpDestination jd2 = 
             (NettyHttpDestination)factory.getDestination(
-                getEndpointInfo("foo", "bar", "http://localhost:9001"));
+                getEndpointInfo("foo", "bar", "http://localhost:9001"), bus);
         
         engine = (NettyHttpServerEngine)jd2.getEngine();
         //assertEquals(40000, engine.getMaxIdleTime());
@@ -136,7 +136,7 @@ public class ApplicationContextTest extends Assert {
         
         NettyHttpDestination jd3 = 
             (NettyHttpDestination)factory.getDestination(
-                getEndpointInfo("sna", "foo", "https://localhost:9002"));
+                getEndpointInfo("sna", "foo", "https://localhost:9002"), bus);
         
         engine = (NettyHttpServerEngine)jd3.getEngine();
         assertEquals(engine.getTlsServerParameters().getClientAuthentication().isWant(), true);
@@ -144,7 +144,7 @@ public class ApplicationContextTest extends Assert {
         
         NettyHttpDestination jd4 = 
             (NettyHttpDestination)factory.getDestination(
-                getEndpointInfo("sna", "foo2", "https://localhost:9003"));
+                getEndpointInfo("sna", "foo2", "https://localhost:9003"), bus);
         
         engine = (NettyHttpServerEngine)jd4.getEngine();
         assertEquals(engine.getTlsServerParameters().getClientAuthentication().isWant(), false);
