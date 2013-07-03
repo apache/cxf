22fbed1d47cd41f92841b333905b5fec8dfbad75
Akitoshi Yoshida <ay@apache.org>
2013-07-03 09:56:20 +0000

    Merged revisions 1498415 via  svn merge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1498415 | ay | 2013-07-01 14:44:54 +0200 (Mon, 01 Jul 2013) | 1 line
    
      [CXF-5102] schema resource loading in various blueprint namespace handlers not working in some environment
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes@1499300 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/ws/rm/src/test/java/org/apache/cxf/ws/rm/blueprint/RMBPHandlerTest.java
@@ -0,0 +1,38 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.ws.rm.blueprint;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class RMBPHandlerTest extends Assert {
+    @Test
+    public void testGetSchemaLocation() {
+        RMBPHandler handler = new RMBPHandler();
+        
+        assertNotNull(handler.getSchemaLocation("http://cxf.apache.org/ws/rm/manager"));
+        assertNotNull(handler.getSchemaLocation("http://schemas.xmlsoap.org/ws/2005/02/rm/policy"));
+        assertNotNull(handler.getSchemaLocation("http://docs.oasis-open.org/ws-rx/wsrmp/200702"));
+    }
+
+}
