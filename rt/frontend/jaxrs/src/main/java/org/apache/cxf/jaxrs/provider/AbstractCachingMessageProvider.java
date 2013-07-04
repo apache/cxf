fff8cdee11ce9537cf3f19909ca5ce40929f94e4
Sergey Beryozkin <sergeyb@apache.org>
2013-07-04 10:04:36 +0000

    [CXF-5069] Adding basic caching JAX-RS providers
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1499710 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/AbstractCachingMessageProvider.java
@@ -0,0 +1,62 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.jaxrs.provider;
+
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.Provider;
+
+import org.apache.cxf.common.i18n.BundleUtils;
+import org.apache.cxf.common.logging.LogUtils;
+import org.apache.cxf.jaxrs.ext.MessageContext;
+
+@Provider
+public class AbstractCachingMessageProvider<T> extends AbstractConfigurableProvider {
+    
+    protected static final Logger LOG = LogUtils.getL7dLogger(AbstractCachingMessageProvider.class);
+    protected static final ResourceBundle BUNDLE = BundleUtils.getBundle(AbstractCachingMessageProvider.class);
+    protected static final String ACTIVE_JAXRS_PROVIDER_KEY = "active.jaxrs.provider";
+    
+    protected MessageContext mc;
+    private T object;
+    
+    @Context
+    public void setMessageContext(MessageContext context) {
+        this.mc = context;
+    }
+    
+    
+    protected boolean isProviderKeyNotSet() {
+        return mc.get(ACTIVE_JAXRS_PROVIDER_KEY) == null;
+    }
+
+    public T getObject() {
+        return object;
+    }
+
+    public void setObject(T object) {
+        this.object = object;
+    }
+    
+    
+        
+}
