cf388678325e27f7f485339b92d01e0c484d4d07
Colm O Heigeartaigh <coheigea@apache.org>
2013-07-16 10:12:33 +0000

    Send the Resources as a single Attribute
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1503645 13f79535-47bb-0310-9956-ffa450edef68

--- rt/security/src/main/java/org/apache/cxf/rt/security/xacml/DefaultXACMLRequestBuilder.java
+++ rt/security/src/main/java/org/apache/cxf/rt/security/xacml/DefaultXACMLRequestBuilder.java
@@ -72,12 +72,27 @@ public class DefaultXACMLRequestBuilder implements XACMLRequestBuilder {
 
     private ResourceType createResourceType(List<String> resources) {
         List<AttributeType> attributes = new ArrayList<AttributeType>();
+        
+        List<AttributeValueType> resourceAttributes = new ArrayList<AttributeValueType>();
         for (String resource : resources) {
             if (resource != null) {
-                attributes.add(createAttribute(XACMLConstants.RESOURCE_ID, XACMLConstants.XS_STRING, null,
-                                               resource));
+                AttributeValueType resourceAttributeValue = 
+                    RequestComponentBuilder.createAttributeValueType(resource);
+                resourceAttributes.add(resourceAttributeValue);
             }
         }
+
+        if (!resourceAttributes.isEmpty()) {
+            AttributeType resourceAttribute = 
+                createAttribute(
+                    XACMLConstants.RESOURCE_ID,
+                    XACMLConstants.XS_STRING,
+                    null,
+                    resourceAttributes
+                );
+            attributes.add(resourceAttribute);
+        }
+        
         return RequestComponentBuilder.createResourceType(attributes, null);
     }
 
