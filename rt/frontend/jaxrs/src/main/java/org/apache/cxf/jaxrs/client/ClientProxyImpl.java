9c9ce8e61332ed57db1f9551a65e9681b214405b
Sergey Beryozkin <sergeyb@apache.org>
2013-07-22 14:39:12 +0000

    Merged revisions 1505686 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1505686 | sergeyb | 2013-07-22 14:59:19 +0100 (Mon, 22 Jul 2013) | 1 line
    
      [CXF-5146] Updating RS proxy code to unwrap TypeVariables
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes@1505692 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/client/ClientProxyImpl.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/client/ClientProxyImpl.java
@@ -24,6 +24,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -592,9 +593,18 @@ public class ClientProxyImpl extends AbstractClient implements
                     && ((InputStream)r.getEntity()).available() == 0)) {
                 return r;
             }
+
+            Type genericType = method.getGenericReturnType();
+            if (genericType instanceof TypeVariable) {
+                genericType = InjectionUtils.getSuperType(method.getDeclaringClass(), 
+                                                   (TypeVariable<?>)genericType);
+            }
             
-            return readBody(r, outMessage, method.getReturnType(), 
-                            method.getGenericReturnType(), method.getDeclaredAnnotations());
+            return readBody(r, 
+                            outMessage, 
+                            method.getReturnType(), 
+                            genericType, 
+                            method.getDeclaredAnnotations());
         } finally {
             ProviderFactory.getInstance(outMessage).clearThreadLocalProxies();
         }
