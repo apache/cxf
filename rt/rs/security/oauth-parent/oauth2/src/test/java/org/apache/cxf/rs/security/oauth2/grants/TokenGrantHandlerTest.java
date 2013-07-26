6821f2e405be7dfa0876552dfda09f2847ca3888
Sergey Beryozkin <sergeyb@apache.org>
2013-07-26 12:44:20 +0000

    Merged revisions 1507293 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes
    ................
      r1507293 | sergeyb | 2013-07-26 13:39:53 +0100 (Fri, 26 Jul 2013) | 13 lines
      
      Merged revisions 1507286-1507287 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1507286 | sergeyb | 2013-07-26 13:31:12 +0100 (Fri, 26 Jul 2013) | 1 line
        
        [CXF-5161] Fixing AbstractGrantHandler typo and updating it to correctly work in a case where a concrete implementation wants to support more than one grant type
      ........
        r1507287 | sergeyb | 2013-07-26 13:33:15 +0100 (Fri, 26 Jul 2013) | 1 line
        
        Reverting a change to JAXRSXmlSecTest
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes@1507296 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/rs/security/oauth-parent/oauth2/src/test/java/org/apache/cxf/rs/security/oauth2/grants/TokenGrantHandlerTest.java
@@ -0,0 +1,131 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.rs.security.oauth2.grants;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.cxf.jaxrs.impl.MetadataMap;
+import org.apache.cxf.rs.security.oauth2.common.Client;
+import org.apache.cxf.rs.security.oauth2.common.ServerAccessToken;
+import org.apache.cxf.rs.security.oauth2.provider.OAuthServiceException;
+import org.apache.cxf.rs.security.oauth2.tokens.bearer.BearerAccessToken;
+import org.apache.cxf.rs.security.oauth2.utils.OAuthConstants;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TokenGrantHandlerTest extends Assert {
+
+    @Test
+    public void testSimpleGrantNotSupported() {
+        try {
+            new SimpleGrantHandler().createAccessToken(createClient("unsupported"), 
+                                                       createMap("a"));
+            fail("Unsupported Grant");
+        } catch (OAuthServiceException ex) {
+            assertEquals(OAuthConstants.UNAUTHORIZED_CLIENT, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testSimpleGrantBug() {
+        try {
+            new SimpleGrantHandler(Arrays.asList("a", "b")).createAccessToken(createClient("a"), 
+                                                       createMap("a"));
+            fail("Grant handler bug");
+        } catch (WebApplicationException ex) {
+            assertEquals(500, ex.getResponse().getStatus());
+        }
+    }
+    
+    @Test
+    public void testSimpleGrantSupported() {
+        ServerAccessToken t = new SimpleGrantHandler().createAccessToken(createClient("a"), 
+                                                                         createMap("a"));
+        assertTrue(t instanceof BearerAccessToken);
+    }
+    
+    @Test
+    public void testComplexGrantNotSupported() {
+        try {
+            new ComplexGrantHandler(Arrays.asList("a", "b"))
+                .createAccessToken(createClient("unsupported"), createMap("a"));
+            fail("Unsupported Grant");
+        } catch (OAuthServiceException ex) {
+            assertEquals(OAuthConstants.UNAUTHORIZED_CLIENT, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testComplexGrantSupported() {
+        ServerAccessToken t = new ComplexGrantHandler(Arrays.asList("a", "b"))
+            .createAccessToken(createClient("a"), createMap("a"));
+        assertTrue(t instanceof BearerAccessToken);
+    }
+    
+    private Client createClient(String... grants) {
+        Client c = new Client("alice", "password", true);
+        for (String grant : grants) { 
+            c.getAllowedGrantTypes().add(grant);
+        }
+        return c;
+    }
+    
+    private MultivaluedMap<String, String> createMap(String grant) {
+        MultivaluedMap<String, String> map = new MetadataMap<String, String>();
+        map.putSingle(OAuthConstants.GRANT_TYPE, grant);
+        return map;
+    }
+    
+    private static class SimpleGrantHandler extends AbstractGrantHandler {
+
+        public SimpleGrantHandler() {
+            super("a");
+        }
+        
+        public SimpleGrantHandler(List<String> grants) {
+            super(grants);
+        }
+        
+        public ServerAccessToken createAccessToken(Client client, MultivaluedMap<String, String> params)
+            throws OAuthServiceException {
+            super.checkIfGrantSupported(client);
+            return new BearerAccessToken(client, 3600L);
+        } 
+        
+    }
+    
+    private static class ComplexGrantHandler extends AbstractGrantHandler {
+
+        public ComplexGrantHandler(List<String> grants) {
+            super(grants);
+        }
+        
+        public ServerAccessToken createAccessToken(Client client, MultivaluedMap<String, String> params)
+            throws OAuthServiceException {
+            super.checkIfGrantSupported(client, params.getFirst(OAuthConstants.GRANT_TYPE));
+            return new BearerAccessToken(client, 3600L);
+        } 
+        
+    }
+}
