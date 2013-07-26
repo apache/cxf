ef81d6ad768f7d8e1537a160932b4a217b248f18
Jason Pell <jpell@apache.org>
2013-07-26 08:53:51 +0000

    CXF-5151 added support for gzip ?wsdl
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes@1507220 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/simple/src/main/java/org/apache/cxf/frontend/WSDLGetInterceptor.java
+++ rt/frontend/simple/src/main/java/org/apache/cxf/frontend/WSDLGetInterceptor.java
@@ -19,73 +19,35 @@
 
 package org.apache.cxf.frontend;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 
 import org.w3c.dom.Document;
 
-
 import org.apache.cxf.binding.soap.interceptor.EndpointSelectionInterceptor;
-import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.common.util.StringUtils;
 import org.apache.cxf.common.util.UrlUtils;
-import org.apache.cxf.helpers.IOUtils;
+import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.interceptor.Fault;
-import org.apache.cxf.io.CachedOutputStream;
+import org.apache.cxf.interceptor.Interceptor;
+import org.apache.cxf.interceptor.MessageSenderInterceptor;
+import org.apache.cxf.interceptor.OutgoingChainInterceptor;
+import org.apache.cxf.interceptor.StaxOutInterceptor;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.message.MessageImpl;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
 import org.apache.cxf.service.model.EndpointInfo;
-import org.apache.cxf.staxutils.StaxUtils;
-import org.apache.cxf.transport.Conduit;
+import org.apache.cxf.transport.common.gzip.GZIPOutInterceptor;
 
-/**
- * 
- */
 public class WSDLGetInterceptor extends AbstractPhaseInterceptor<Message> {
     public static final WSDLGetInterceptor INSTANCE = new WSDLGetInterceptor();
-       
-    private static final Logger LOG = LogUtils.getL7dLogger(WSDLGetInterceptor.class);
-    
+    public static final String DOCUMENT_HOLDER = WSDLGetInterceptor.class.getName() + ".documentHolder";
+
     public WSDLGetInterceptor() {
         super(Phase.READ);
         getAfter().add(EndpointSelectionInterceptor.class.getName());
     }
-    
-    public void doOutput(Message message, String base, Document doc, OutputStream out)
-        throws WSDLQueryException {
-        String enc = null;
-        try {
-            enc = doc.getXmlEncoding();
-        } catch (Exception ex) {
-            //ignore - not dom level 3
-        }
-        if (enc == null) {
-            enc = "utf-8";
-        }
-
-        XMLStreamWriter writer = StaxUtils.createXMLStreamWriter(out,
-                                                                 enc);
-        try {
-            StaxUtils.writeNode(doc, writer, true);
-            writer.flush();
-        } catch (XMLStreamException e) {
-            throw new WSDLQueryException(new org.apache.cxf.common.i18n.Message("COULD_NOT_PROVIDE_WSDL",
-                                                                                LOG,
-                                                                                base), e);
-        } finally {
-            StaxUtils.close(writer);
-        }
-    }
-
 
     public void handleMessage(Message message) throws Fault {
         String method = (String)message.get(Message.HTTP_REQUEST_METHOD);
@@ -93,106 +55,61 @@ public class WSDLGetInterceptor extends AbstractPhaseInterceptor<Message> {
         if (!"GET".equals(method) || StringUtils.isEmpty(query)) {
             return;
         }
+
         String baseUri = (String)message.get(Message.REQUEST_URL);
         String ctx = (String)message.get(Message.PATH_INFO);
-        
-        boolean setOutMessageToNull = false;
-        try {
-            CachedOutputStream cos = null;
 
-            //cannot have two wsdl's being written for the same endpoint at the same
-            //time as the addresses may get mixed up
-            synchronized (message.getExchange().getEndpoint()) {
-                Map<String, String> map = UrlUtils.parseQueryString(query);
-                if (isRecognizedQuery(map, baseUri, ctx, 
-                                      message.getExchange().getEndpoint().getEndpointInfo())) {
+        // cannot have two wsdl's being written for the same endpoint at the same
+        // time as the addresses may get mixed up
+        synchronized (message.getExchange().getEndpoint()) {
+            Map<String, String> map = UrlUtils.parseQueryString(query);
+            if (isRecognizedQuery(map, baseUri, ctx, message.getExchange().getEndpoint().getEndpointInfo())) {
+                Document doc = getDocument(message, baseUri, map, ctx, 
+                                           message.getExchange().getEndpoint().getEndpointInfo());
 
-                    setOutMessageToNull = true;
-                    try {
-                        Document doc = getDocument(message,
-                                      baseUri,
-                                      map,
-                                      ctx,
-                                      message.getExchange().getEndpoint().getEndpointInfo());
-                        String enc = null;
-                        try {
-                            enc = doc.getXmlEncoding();
-                        } catch (Exception ex) {
-                            //ignore - not dom level 3
-                        }
-                        if (enc == null) {
-                            enc = "utf-8";
-                        }
+                Endpoint e = message.getExchange().get(Endpoint.class);
+                Message mout = new MessageImpl();
+                mout.setExchange(message.getExchange());
+                mout = e.getBinding().createMessage(mout);
+                mout.setInterceptorChain(OutgoingChainInterceptor.getOutInterceptorChain(message
+                    .getExchange()));
+                message.getExchange().setOutMessage(mout);
 
-                        cos = new CachedOutputStream();
-                        XMLStreamWriter writer = StaxUtils.createXMLStreamWriter(cos,
-                                                                                 enc);
-                        StaxUtils.writeNode(doc, writer, true);
-                        writer.flush();
-                        writer.close();
+                mout.put(DOCUMENT_HOLDER, doc);
 
-                    } catch (XMLStreamException e) {
-                        throw new Fault(e);
+                // FIXME - how can I change this to provide a different interceptor chain that just has the
+                // stax, gzip and message sender components.
+                Iterator<Interceptor<? extends Message>> iterator = mout.getInterceptorChain().iterator();
+                while (iterator.hasNext()) {
+                    Interceptor<? extends Message> inInterceptor = iterator.next();
+                    if (!inInterceptor.getClass().equals(StaxOutInterceptor.class)
+                        && !inInterceptor.getClass().equals(GZIPOutInterceptor.class)
+                        && !inInterceptor.getClass().equals(MessageSenderInterceptor.class)) {
+                        mout.getInterceptorChain().remove(inInterceptor);
                     }
                 }
-            }
 
-            if (cos != null) {
-                try {
-                    Conduit c = message.getExchange().getDestination().getBackChannel(message, null, null);
-                    Message mout = new MessageImpl();
-                    mout.setExchange(message.getExchange());
-                    message.getExchange().setOutMessage(mout);
-                    mout.put(Message.CONTENT_TYPE, "text/xml");
-                    c.prepare(mout);
-                    OutputStream os = mout.getContent(OutputStream.class);
+                mout.getInterceptorChain().add(WSDLGetOutInterceptor.INSTANCE);
 
-                    InputStream cis = cos.getInputStream();
-                    IOUtils.copyAndCloseInput(cis, os);
-                    message.getInterceptorChain().abort();
-
-                    try {
-                        os.flush();
-                        os.close();
-                    } catch (IOException ex) {
-                        LOG.log(Level.FINE, "Failure writing full wsdl to the stream", ex);
-                        //we can ignore this.   Likely, whatever has requested the WSDL
-                        //has closed the connection before reading the entire wsdl.
-                        //WSDL4J has a tendency to not read the closing tags and such
-                        //and thus can sometimes hit this.   In any case, it's
-                        //pretty much ignorable and nothing we can do about it (cannot
-                        //send a fault or anything anyway
-                    }
-
-                    cos.close();
-                } catch (IOException e) {
-                    throw new Fault(e);
-                }
-            }
-
-        } finally {
-            if (setOutMessageToNull) {
-                message.getExchange().setOutMessage(null);
+                // skip the service executor and goto the end of the chain.
+                message.getInterceptorChain().doInterceptStartingAt(
+                        message,
+                        OutgoingChainInterceptor.class.getName());
             }
         }
     }
-    public Document getDocument(Message message,
-                                String base,
-                                Map<String, String> params,
-                                String ctxUri,
+
+    private Document getDocument(Message message, String base, Map<String, String> params, String ctxUri,
                                 EndpointInfo endpointInfo) {
         return new WSDLGetUtils().getDocument(message, base, params, ctxUri, endpointInfo);
     }
-    public boolean isRecognizedQuery(Map<String, String> map,
-                                     String baseUri,
-                                     String ctx, 
+
+    private boolean isRecognizedQuery(Map<String, String> map, String baseUri, String ctx,
                                      EndpointInfo endpointInfo) {
-        if (map.containsKey("wsdl")
-            || map.containsKey("xsd")) {
+
+        if (map.containsKey("wsdl") || map.containsKey("xsd")) {
             return true;
         }
         return false;
     }
-
 }
-
