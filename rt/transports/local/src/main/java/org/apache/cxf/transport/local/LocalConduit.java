0357ca0b30eb4d618cdf92f6196191b0f587538b
Sergey Beryozkin <sergeyb@apache.org>
2013-07-30 10:24:18 +0000

    Updating LocalTransport to check message contextual properties when deciding if it is the direct dispatch or not, so that this property can be used by injectable clients
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1508377 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/local/src/main/java/org/apache/cxf/transport/local/LocalConduit.java
+++ rt/transports/local/src/main/java/org/apache/cxf/transport/local/LocalConduit.java
@@ -34,6 +34,7 @@ import org.apache.cxf.message.Exchange;
 import org.apache.cxf.message.ExchangeImpl;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.message.MessageImpl;
+import org.apache.cxf.message.MessageUtils;
 import org.apache.cxf.transport.AbstractConduit;
 import org.apache.cxf.workqueue.SynchronousExecutor;
 
@@ -57,7 +58,7 @@ public class LocalConduit extends AbstractConduit {
     }
     
     public void prepare(final Message message) throws IOException {
-        if (!Boolean.TRUE.equals(message.get(DIRECT_DISPATCH))) {
+        if (!MessageUtils.isTrue(message.getContextualProperty(DIRECT_DISPATCH))) {
             dispatchViaPipe(message);
         } else {
             // prepare the stream here
@@ -71,7 +72,7 @@ public class LocalConduit extends AbstractConduit {
 
     @Override
     public void close(Message message) throws IOException {
-        if (Boolean.TRUE.equals(message.get(DIRECT_DISPATCH))
+        if (MessageUtils.isTrue(message.getContextualProperty(DIRECT_DISPATCH))
             && !Boolean.TRUE.equals(message.get(Message.INBOUND_MESSAGE))) {
             dispatchDirect(message);
         } 
