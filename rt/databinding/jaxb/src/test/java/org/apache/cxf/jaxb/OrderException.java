3e2e2c88b6a77000e50f60f422b6653c30c80d89
Jim Ma <ema@apache.org>
2013-08-01 09:20:55 +0000

    [CXF-5163]:Add missing test class
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1509142 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/databinding/jaxb/src/test/java/org/apache/cxf/jaxb/OrderException.java
@@ -0,0 +1,84 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.jaxb;
+
+import javax.xml.bind.annotation.XmlAccessOrder;
+import javax.xml.bind.annotation.XmlAccessorOrder;
+
+@XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
+public class OrderException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    private String info1;
+
+    private String info2;
+
+    private String aValue;
+
+    private int intVal;
+
+    private String detail;
+
+    public OrderException(String message) {
+        super(message);
+    }
+
+
+    public String getAValue() {
+        return aValue;
+    }
+
+    public void setAValue(String value) {
+        this.aValue = value;
+    }
+
+    public String getInfo1() {
+        return info1;
+    }
+
+    public void setInfo1(String info1) {
+        this.info1 = info1;
+    }
+
+    public String getInfo2() {
+        return info2;
+    }
+
+    public void setInfo2(String info2) {
+        this.info2 = info2;
+    }
+
+
+    public int getIntVal() {
+        return intVal;
+    }
+
+    public void setIntVal(int intVal) {
+        this.intVal = intVal;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+}
