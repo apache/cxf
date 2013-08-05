83052fac88126b39f1f32bd11b308613fa1af09c
Daniel Kulp <dkulp@apache.org>
2013-08-05 21:37:58 +0000

    fix failing test
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1510750 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/security/JAASAuthenticationFilter.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/security/JAASAuthenticationFilter.java
@@ -127,7 +127,7 @@ public class JAASAuthenticationFilter implements ContainerRequestFilter {
             StringBuilder sb = new StringBuilder();
             
             List<String> authHeader = headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
-            if (authHeader.size() > 0) {
+            if (authHeader != null && authHeader.size() > 0) {
                 // should HttpHeadersImpl do it ?
                 String[] authValues = StringUtils.split(authHeader.get(0), " ");
                 if (authValues.length > 0) {
