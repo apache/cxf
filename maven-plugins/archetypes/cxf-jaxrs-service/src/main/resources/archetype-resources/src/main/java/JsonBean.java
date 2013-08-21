d2fa4c1ae84a8431a487eab4ab0b7bba446771a4
Daniel Kulp <dkulp@apache.org>
2013-08-21 16:50:19 +0000

    Merged revisions 1516210 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes
    ........
      r1516210 | dkulp | 2013-08-21 12:46:59 -0400 (Wed, 21 Aug 2013) | 10 lines
    
      Merged revisions 1516207 via  git cherry-pick from
      https://svn.apache.org/repos/asf/cxf/trunk
    
      ........
        r1516207 | dkulp | 2013-08-21 12:43:54 -0400 (Wed, 21 Aug 2013) | 2 lines
    
        Remove a bunch of tab characters
    
      ........
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes@1516215 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/archetypes/cxf-jaxrs-service/src/main/resources/archetype-resources/src/main/java/JsonBean.java
+++ maven-plugins/archetypes/cxf-jaxrs-service/src/main/resources/archetype-resources/src/main/java/JsonBean.java
@@ -6,19 +6,19 @@ public class JsonBean {
     private String val2;
 
     public String getVal1() {
-	return val1;
+        return val1;
     }
 
     public void setVal1(String val1) {
-	this.val1 = val1;
+        this.val1 = val1;
     }
 
     public String getVal2() {
-	return val2;
+        return val2;
     }
 
     public void setVal2(String val2) {
-	this.val2 = val2;
+        this.val2 = val2;
     }
 
 }
--- maven-plugins/archetypes/cxf-jaxws-javafirst/src/main/resources/archetype-resources/src/test/java/HelloWorldImplTest.java
+++ maven-plugins/archetypes/cxf-jaxws-javafirst/src/main/resources/archetype-resources/src/test/java/HelloWorldImplTest.java
@@ -23,8 +23,8 @@ import static org.junit.Assert.assertEquals;
 public class HelloWorldImplTest {
 
     @Test
-	public void testSayHi() {
-        HelloWorldImpl helloWorldImpl = new HelloWorldImpl();            	
+    public void testSayHi() {
+        HelloWorldImpl helloWorldImpl = new HelloWorldImpl();
         String response = helloWorldImpl.sayHi("Sam");
         assertEquals("HelloWorldImpl not properly saying hi", "Hello Sam", response);
     }
