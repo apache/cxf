bc97ca55cf7e789cd26978dbe70d9da90d0e1ac6
Daniel Kulp <dkulp@apache.org>
2013-08-21 16:43:54 +0000

    Remove a bunch of tab characters
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1516207 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/archetypes/cxf-jaxrs-service/src/main/resources/archetype-resources/src/test/java/HelloWorldIT.java
+++ maven-plugins/archetypes/cxf-jaxrs-service/src/main/resources/archetype-resources/src/test/java/HelloWorldIT.java
@@ -16,36 +16,36 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class HelloWorldIT {
-	private static String endpointUrl;
-	
-	@BeforeClass
-	public static void beforeClass() {
-		endpointUrl = System.getProperty("service.url");
-	}
-	
-	@Test
-	public void testPing() throws Exception {
-		WebClient client = WebClient.create(endpointUrl + "/hello/echo/SierraTangoNevada");
-		Response r = client.accept("text/plain").get();
-		assertEquals(Response.Status.OK.getStatusCode(), r.getStatus());
-		String value = IOUtils.toString((InputStream)r.getEntity());
-		assertEquals("SierraTangoNevada", value);
-	}
-	
-	@Test
-	public void testJsonRoundtrip() throws Exception {
-		 List<Object> providers = new ArrayList<Object>();
-	    providers.add(new org.codehaus.jackson.jaxrs.JacksonJsonProvider());
-	    JsonBean inputBean = new JsonBean();
-	    inputBean.setVal1("Maple");
-		WebClient client = WebClient.create(endpointUrl + "/hello/jsonBean", providers);
-		Response r = client.accept("application/json")
-				.type("application/json")
-				.post(inputBean);
-		assertEquals(Response.Status.OK.getStatusCode(), r.getStatus());
-		MappingJsonFactory factory = new MappingJsonFactory();
-		JsonParser parser = factory.createJsonParser((InputStream)r.getEntity());
-		JsonBean output = parser.readValueAs(JsonBean.class);
-		assertEquals("Maple", output.getVal2());
-	}
+    private static String endpointUrl;
+
+    @BeforeClass
+    public static void beforeClass() {
+        endpointUrl = System.getProperty("service.url");
+    }
+
+    @Test
+    public void testPing() throws Exception {
+        WebClient client = WebClient.create(endpointUrl + "/hello/echo/SierraTangoNevada");
+        Response r = client.accept("text/plain").get();
+        assertEquals(Response.Status.OK.getStatusCode(), r.getStatus());
+        String value = IOUtils.toString((InputStream)r.getEntity());
+        assertEquals("SierraTangoNevada", value);
+    }
+
+    @Test
+    public void testJsonRoundtrip() throws Exception {
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new org.codehaus.jackson.jaxrs.JacksonJsonProvider());
+        JsonBean inputBean = new JsonBean();
+        inputBean.setVal1("Maple");
+        WebClient client = WebClient.create(endpointUrl + "/hello/jsonBean", providers);
+        Response r = client.accept("application/json")
+            .type("application/json")
+            .post(inputBean);
+        assertEquals(Response.Status.OK.getStatusCode(), r.getStatus());
+        MappingJsonFactory factory = new MappingJsonFactory();
+        JsonParser parser = factory.createJsonParser((InputStream)r.getEntity());
+        JsonBean output = parser.readValueAs(JsonBean.class);
+        assertEquals("Maple", output.getVal2());
+    }
 }
