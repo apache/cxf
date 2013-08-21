88b4bd9f90bcb4106a96fdf98949d5642c1a668d
Daniel Kulp <dkulp@apache.org>
2013-08-21 16:43:57 +0000

    [CXF-5225] Provide the Endpoint so conduit selectors can make a complete decision
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1516208 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/cxf/endpoint/ClientImpl.java
+++ core/src/main/java/org/apache/cxf/endpoint/ClientImpl.java
@@ -814,7 +814,7 @@ public class ClientImpl
         Message message = new MessageImpl();
         Exchange exchange = new ExchangeImpl();
         message.setExchange(exchange);
-        setExchangeProperties(exchange, null, null);
+        setExchangeProperties(exchange, getEndpoint(), null);
         return getConduitSelector().selectConduit(message);
     }
 
@@ -874,10 +874,11 @@ public class ClientImpl
         exchange.put(Client.class, this);
         exchange.put(Bus.class, bus);
 
-        if (endpoint != null && boi != null) {
-
+        if (endpoint != null) {
             EndpointInfo endpointInfo = endpoint.getEndpointInfo();
-            exchange.put(Message.WSDL_OPERATION, boi.getName());
+            if (boi != null) {
+                exchange.put(Message.WSDL_OPERATION, boi.getName());
+            }
 
             QName serviceQName = endpointInfo.getService().getName();
             exchange.put(Message.WSDL_SERVICE, serviceQName);
