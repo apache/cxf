5ea99941a438a63f4ed099b8aa7f0978895e43c7
Daniel Kulp <dkulp@apache.org>
2013-08-26 15:58:07 +0000

    Merged revisions 1517574 via  git cherry-pick from https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes
    ........
      r1517574 | dkulp | 2013-08-26 11:51:53 -0400 (Mon, 26 Aug 2013) | 10 lines
    
      Merged revisions 1517059 via  git cherry-pick from
      https://svn.apache.org/repos/asf/cxf/trunk
    
      ........
        r1517059 | dkulp | 2013-08-23 17:32:23 -0400 (Fri, 23 Aug 2013) | 2 lines
    
        [CXF-5217] Set namspace to null if no schema found (assumes unqualified elements which is default for JAX-WS/JAXB)
    
      ........
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes@1517581 13f79535-47bb-0310-9956-ffa450edef68

--- rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/JAXBEncoderDecoder.java
+++ rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/JAXBEncoderDecoder.java
@@ -354,12 +354,11 @@ public final class JAXBEncoderDecoder {
             
             SchemaInfo sch = part.getMessageInfo().getOperation().getInterface()
                 .getService().getSchema(namespace);
-            if (sch != null) {
-                if (!sch.isElementFormQualified()) {
-                    namespace = null;
-                }
-            } else {
+            if (sch == null) {
                 LOG.warning("Schema associated with " + namespace + " is null");
+                namespace = null;
+            } else if (!sch.isElementFormQualified()) {
+                namespace = null;
             }
             List<Member> combinedMembers = new ArrayList<Member>();
 
