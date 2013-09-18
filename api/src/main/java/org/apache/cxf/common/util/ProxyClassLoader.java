f2f0992022b4f92e92adba184404135fdac8c6b5
Daniel Kulp <dkulp@apache.org>
2013-09-18 13:46:33 +0000

    Make sure we return the EXACT class object for the names classes, check system classloader AFTER all the others.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes@1524411 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/common/util/ProxyClassLoader.java
+++ api/src/main/java/org/apache/cxf/common/util/ProxyClassLoader.java
@@ -28,19 +28,45 @@ import java.util.Set;
  * service class.    
  */
 public class ProxyClassLoader extends ClassLoader {
-    private Set<ClassLoader> loaders = new HashSet<ClassLoader>();
+    private final Class<?> classes[];
+    private final Set<ClassLoader> loaders = new HashSet<ClassLoader>();
+    private boolean checkSystem;
+
+    public ProxyClassLoader() {
+        classes = null;
+    }
+    public ProxyClassLoader(Class<?>[] cls) {
+        classes = cls;
+    }
 
     public void addLoader(ClassLoader loader) {
         if (loader == null) {
-            loader = getSystemClassLoader();
+            checkSystem = true;
+        } else {
+            loaders.add(loader);
         }
-        loaders.add(loader);
     }
 
     public Class<?> findClass(String name) throws ClassNotFoundException {
+        if (classes != null) {
+            for (Class<?> c : classes) {
+                if (name.equals(c.getName())) {
+                    return c;
+                }
+            }
+        }
         for (ClassLoader loader : loaders) {
             try {
                 return loader.loadClass(name);
+            } catch (ClassNotFoundException cnfe) {
+                // Try next
+            } catch (NoClassDefFoundError cnfe) {
+                // Try next
+            }
+        }
+        if (checkSystem) {
+            try {
+                return getSystemClassLoader().loadClass(name);
             } catch (ClassNotFoundException cnfe) {
                 // Try next
             } catch (NoClassDefFoundError cnfe) {
