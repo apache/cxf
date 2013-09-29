5db3d66129fe8e26a05c2db14c37bca76e968333
Jim Ma <ema@apache.org>
2013-09-29 05:06:21 +0000

    [CXF-5307]:Add DecoupledFaultHandler for request-response message when decoupled fault is supported
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1527271 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/addr/src/main/java/org/apache/cxf/ws/addressing/soap/MAPCodec.java
+++ rt/ws/addr/src/main/java/org/apache/cxf/ws/addressing/soap/MAPCodec.java
@@ -153,7 +153,8 @@ public class MAPCodec extends AbstractSoapInterceptor {
                     }
                 }
             }
-        } else if (MessageUtils.getContextualBoolean(message, DECOUPLED_FAULT_SUPPORT, false)) {
+        } 
+        if (MessageUtils.getContextualBoolean(message, DECOUPLED_FAULT_SUPPORT, false)) {
             new DecoupledFaultHandler().handleFault(message);
         }
     }
