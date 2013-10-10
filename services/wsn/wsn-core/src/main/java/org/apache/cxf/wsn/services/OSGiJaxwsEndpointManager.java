6a4ae1222f3e63a81a90e9e742cd71620da62bd2
Daniel Kulp <dkulp@apache.org>
2013-10-10 15:31:28 +0000

    [CXF-5329] Make sure the wsn bus gets registered as a service Patch from Sebastian Ruhl applied
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1531026 13f79535-47bb-0310-9956-ffa450edef68

--- services/wsn/wsn-core/src/main/java/org/apache/cxf/wsn/services/OSGiJaxwsEndpointManager.java
+++ services/wsn/wsn-core/src/main/java/org/apache/cxf/wsn/services/OSGiJaxwsEndpointManager.java
@@ -19,6 +19,8 @@
 
 package org.apache.cxf.wsn.services;
 
+import java.util.Dictionary;
+import java.util.Properties;
 import javax.management.MBeanServer;
 import javax.xml.ws.Endpoint;
 
@@ -26,8 +28,15 @@ import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.bus.blueprint.BlueprintBus;
 import org.apache.cxf.wsn.EndpointRegistrationException;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.Version;
 import org.osgi.service.blueprint.container.BlueprintContainer;
+
+import static org.apache.cxf.bus.osgi.OSGIBusListener.CONTEXT_NAME_PROPERTY;
+import static org.apache.cxf.bus.osgi.OSGIBusListener.CONTEXT_SYMBOLIC_NAME_PROPERTY;
+import static org.apache.cxf.bus.osgi.OSGIBusListener.CONTEXT_VERSION_PROPERTY;
 
 /**
  * 
@@ -99,7 +108,19 @@ public class OSGiJaxwsEndpointManager extends JaxwsEndpointManager {
         bp.setBlueprintContainer(container);
         bp.setId("WS-Notification");
         bp.initialize();
+        if (null != bundleContext) {
+            Properties props = new Properties();
+            props.put(CONTEXT_SYMBOLIC_NAME_PROPERTY, bundleContext.getBundle().getSymbolicName());
+            props.put(CONTEXT_VERSION_PROPERTY, getBundleVersion(bundleContext.getBundle()));
+            props.put(CONTEXT_NAME_PROPERTY, bp.getId());
+            bundleContext.registerService(Bus.class.getName(), bp, props);
+        }
         cxfBus = bp;
+    }
+    private Version getBundleVersion(Bundle bundle) {
+        Dictionary<?, ?> headers = bundle.getHeaders();
+        String version = (String) headers.get(Constants.BUNDLE_VERSION);
+        return (version != null) ? Version.parseVersion(version) : Version.emptyVersion;
     }
     
 }
