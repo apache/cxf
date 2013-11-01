ea30dd49bc132d6cf0fd3b30a4b8b48d9ee01183
Sergey Beryozkin <sergeyb@apache.org>
2013-11-01 14:08:06 +0000

    [CXF-5371] Updating JAXRSInInterceptor to skip the rest of the in chain if Response is ready
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1537939 13f79535-47bb-0310-9956-ffa450edef68

---
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/ResponseCheckInterceptor.java
@@ -0,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.jaxrs;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+
+public class ResponseCheckInterceptor extends AbstractPhaseInterceptor<Message> {
+
+    public ResponseCheckInterceptor() {
+        super(Phase.PRE_INVOKE);
+    }
+
+    public void handleMessage(Message message) throws Fault {
+        if (message.getExchange().get(Response.class) != null) {
+            throw new WebApplicationException(500);
+        }
+        
+        String query = (String)message.get(Message.QUERY_STRING);
+        if (query != null && query.contains("wadl")) {
+            throw new WebApplicationException(500);
+        }
+    }
+
+}
