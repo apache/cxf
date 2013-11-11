13a8e0f13d6fc68c53397f4096f3c47b9df51bef
Freeman Yue Fang <ffang@apache.org>
2013-11-11 05:26:02 +0000

    [CXF-5379]revert changes on CXF 2.6.x branch
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.6.x-fixes@1540613 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/AbstractJAXRSFactoryBean.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/AbstractJAXRSFactoryBean.java
@@ -114,7 +114,6 @@ public class AbstractJAXRSFactoryBean extends AbstractEndpointFactory {
         super.setBus(bus);
         checkBindingFactory(bus);
         serviceFactory.setBus(bus);
-        bus.setProperty(JAXRSServiceFactoryBean.class.getName(), serviceFactory);
     }
     
     /*
@@ -208,7 +207,6 @@ public class AbstractJAXRSFactoryBean extends AbstractEndpointFactory {
      */
     public void setServiceFactory(JAXRSServiceFactoryBean serviceFactory) {
         this.serviceFactory = serviceFactory;
-        getBus().setProperty(JAXRSServiceFactoryBean.class.getName(), serviceFactory);
     }
 
     protected Endpoint createEndpoint() throws BusException, EndpointException {
