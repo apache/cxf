71c14c19d2815c6272fe3d5090ab7263218e788b
Sergey Beryozkin <sergeyb@apache.org>
2013-11-19 13:59:28 +0000

    [CXF-5377] Updating LoggingInInterceptor to try to log the request URI even if REQUEST_URI is not set
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1543433 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/cxf/interceptor/LoggingInInterceptor.java
+++ api/src/main/java/org/apache/cxf/interceptor/LoggingInInterceptor.java
@@ -117,15 +117,17 @@ public class LoggingInInterceptor extends AbstractLoggingInterceptor {
         }
         String uri = (String)message.get(Message.REQUEST_URL);
         if (uri == null) {
+            String address = (String)message.get(Message.ENDPOINT_ADDRESS);
             uri = (String)message.get(Message.REQUEST_URI);
             if (uri != null && uri.startsWith("/")) {
-                String address = (String)message.get(Message.ENDPOINT_ADDRESS);
                 if (address != null && !address.startsWith(uri)) {
                     if (address.endsWith("/") && address.length() > 1) {
                         address = address.substring(0, address.length()); 
                     }
                     uri = address + uri;
                 }
+            } else {
+                uri = address;
             }
         } 
         if (uri != null) {
