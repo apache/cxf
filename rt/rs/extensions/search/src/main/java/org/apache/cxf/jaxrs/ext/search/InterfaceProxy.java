47d5bb448fbe8ab329cfd934e83fddff7086130c
Sergey Beryozkin <sergeyb@apache.org>
2013-12-11 16:19:30 +0000

    [CXF-5453] Support for beans with interfaces
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1550181 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt/rs/extensions/search/src/main/java/org/apache/cxf/jaxrs/ext/search/InterfaceProxy.java
@@ -0,0 +1,45 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.jaxrs.ext.search;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InterfaceProxy implements InvocationHandler {
+
+    private Map<String, Object> map = new HashMap<String, Object>(); 
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        String methodName = method.getName();
+        if (methodName.length() <= 3) {
+            throw new UnsupportedOperationException();
+        }
+        String property = methodName.substring(3);
+        boolean isGetter = "get".equals(methodName.substring(0,  3));
+        if (isGetter) {
+            return map.get(property);
+        } else {
+            map.put(property, args[0]);
+            return null;
+        }
+    }
+
+}
