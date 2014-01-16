b3b3cf8869f39161f8521a33508154799bb54e8e
Colm O Heigeartaigh <coheigea@apache.org>
2014-01-16 11:55:38 +0000

    Fixing sign_enc demo. Deleting ut_sign demo.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1558766 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/ws_security/sign_enc/src/main/java/demo/wssec/client/StaxClient.java
+++ distribution/src/main/release/samples/ws_security/sign_enc/src/main/java/demo/wssec/client/StaxClient.java
@@ -83,7 +83,7 @@ public final class StaxClient {
                 "http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p"
             );
             properties.addEncryptionPart(
-                new SecurePart(new QName(WSSConstants.NS_WSU10, "Timestamp"), SecurePart.Modifier.Element)
+                new SecurePart(new QName(WSSConstants.NS_WSSE10, "UsernameToken"), SecurePart.Modifier.Element)
             );
             properties.addEncryptionPart(
                 new SecurePart(new QName(WSSConstants.NS_SOAP11, "Body"), SecurePart.Modifier.Content)
