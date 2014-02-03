8797ffcf69fddaf3f6171b77e8e9f46487abe68f
Akitoshi Yoshida <ay@apache.org>
2014-02-03 11:16:33 +0000

    [CXF-5517] Incorrect check for VOID type
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1563835 13f79535-47bb-0310-9956-ffa450edef68

--- rt/rs/client/src/main/java/org/apache/cxf/jaxrs/client/ClientProxyImpl.java
+++ rt/rs/client/src/main/java/org/apache/cxf/jaxrs/client/ClientProxyImpl.java
@@ -358,7 +358,7 @@ public class ClientProxyImpl extends AbstractClient implements
                 accepts = InjectionUtils.isPrimitive(responseClass)
                     ? Collections.singletonList(MediaType.TEXT_PLAIN_TYPE)
                     : Collections.singletonList(MediaType.APPLICATION_XML_TYPE);        
-            } else if (responseClass == Void.class) {
+            } else if (responseClass == Void.class || responseClass == Void.TYPE) {
                 accepts = Collections.singletonList(MediaType.WILDCARD_TYPE);
             } else {
                 accepts = ori.getProduceTypes();
@@ -692,7 +692,7 @@ public class ClientProxyImpl extends AbstractClient implements
             
             Method method = outMessage.getExchange().get(Method.class);
             checkResponse(method, r, outMessage);
-            if (method.getReturnType() == Void.class || method.getReturnType() == void.class) { 
+            if (method.getReturnType() == Void.class || method.getReturnType() == Void.TYPE) { 
                 return null;
             }
             if (method.getReturnType() == Response.class
