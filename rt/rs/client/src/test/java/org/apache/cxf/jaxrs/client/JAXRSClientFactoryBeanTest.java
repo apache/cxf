0918a1bc99d40bc7fb3079570380b2b3094cae6b
Daniel Kulp <dkulp@apache.org>
2014-04-02 14:12:50 -0400

    Fix compile failure

--- rt/rs/client/src/test/java/org/apache/cxf/jaxrs/client/JAXRSClientFactoryBeanTest.java
+++ rt/rs/client/src/test/java/org/apache/cxf/jaxrs/client/JAXRSClientFactoryBeanTest.java
@@ -57,7 +57,7 @@ public class JAXRSClientFactoryBeanTest extends Assert {
     
     @Test
     public void testCreateClientCustomLoader() throws Exception {
-        ProxyClassLoader loader = new ProxyClassLoader();
+        ProxyClassLoader loader = new ProxyClassLoader(BookStore.class.getClassLoader());
         loader.addLoader(BookStore.class.getClassLoader());
         
         JAXRSClientFactoryBean bean = new JAXRSClientFactoryBean();
