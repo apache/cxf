9c20cb35a81d773ad40e48a33885e081462fb34d
Willem Jiang <willem.jiang@gmail.com>
2014-04-04 15:20:45 +0800

    CXF-5610 clean up the code

--- rt/transports/http-jetty/src/test/java/org/apache/cxf/transport/http_jetty/JettyHTTPServerEngineTest.java
+++ rt/transports/http-jetty/src/test/java/org/apache/cxf/transport/http_jetty/JettyHTTPServerEngineTest.java
@@ -199,21 +199,6 @@ public class JettyHTTPServerEngineTest extends Assert {
         } catch (Exception ex) {
             assertTrue("Get a wrong exception message", ex.getMessage().indexOf("hello/test") > 0);
         }
-        /*
-        try {
-            engine.addServant(new URL(urlStr + "/test"), handler2);
-            fail("We don't support to publish the two service at the same context path");
-        } catch (Exception ex) {
-            assertTrue("Get a wrong exception message", ex.getMessage().indexOf("hello/test/test") > 0);
-        }
-        
-        try {
-            engine.addServant(new URL("http://localhost:" + PORT1 + "/hello"), handler2);
-            fail("We don't support to publish the two service at the same context path");
-        } catch (Exception ex) {
-            assertTrue("Get a wrong exception message", ex.getMessage().indexOf("hello") > 0);
-        }
-        */
         
         engine.addServant(new URL(urlStr2), handler2);
         
