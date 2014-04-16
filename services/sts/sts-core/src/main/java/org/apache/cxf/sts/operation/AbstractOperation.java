115dc3aa448766b575b28924695a7b3ad4d51f18
Colm O hEigeartaigh <coheigea@apache.org>
2014-04-16 15:59:51 +0100

    [CXF-5693] - Support wsp:URI as part of wsp:AppliesTo in the STS

--- services/sts/sts-core/src/main/java/org/apache/cxf/sts/operation/AbstractOperation.java
+++ services/sts/sts-core/src/main/java/org/apache/cxf/sts/operation/AbstractOperation.java
@@ -429,7 +429,7 @@ public abstract class AbstractOperation {
     /**
      * Extract an address from an AppliesTo DOM element
      */
-    protected static String extractAddressFromAppliesTo(Element appliesTo) {
+    protected String extractAddressFromAppliesTo(Element appliesTo) {
         LOG.fine("Parsing AppliesTo element");
         if (appliesTo != null) {
             Element endpointRef = 
@@ -444,6 +444,15 @@ public abstract class AbstractOperation {
                 if (address != null) {
                     LOG.fine("Found address element");
                     return address.getTextContent();
+                }
+            } else if (appliesTo.getNamespaceURI() != null) {
+                Element uri = 
+                    DOMUtils.getFirstChildWithName(
+                        appliesTo, appliesTo.getNamespaceURI(), "URI"
+                    );
+                if (uri != null) {
+                    LOG.fine("Found URI element");
+                    return uri.getTextContent();
                 }
             }
         }
