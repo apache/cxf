5ff29eb828b101a5c4e3baa5006fab916215fab5
Willem Jiang <willem.jiang@gmail.com>
2014-04-30 16:41:32 +0800

    CXF-5718 Suppose to override the instance.id of the managed endpoint with persistent one

--- api/src/main/java/org/apache/cxf/endpoint/ManagedEndpoint.java
+++ api/src/main/java/org/apache/cxf/endpoint/ManagedEndpoint.java
@@ -36,6 +36,7 @@ import org.apache.cxf.management.annotation.ManagedResource;
 public class ManagedEndpoint implements ManagedComponent, ServerLifeCycleListener {
     public static final String ENDPOINT_NAME = "managed.endpoint.name";
     public static final String SERVICE_NAME = "managed.service.name";
+    public static final String INSTANCE_ID = "managed.instance.id";
 
     private Bus bus;
     private Endpoint endpoint;
@@ -108,8 +109,12 @@ public class ManagedEndpoint implements ManagedComponent, ServerLifeCycleListene
         }
         endpointName = ObjectName.quote(endpointName);
         buffer.append(ManagementConstants.PORT_NAME_PROP).append('=').append(endpointName).append(',');
+        String instanceId = (String)endpoint.get(INSTANCE_ID);
+        if (StringUtils.isEmpty(instanceId)) {
+            instanceId = new StringBuffer().append(endpoint.hashCode()).toString();
+        }
         // Added the instance id to make the ObjectName unique
-        buffer.append(ManagementConstants.INSTANCE_ID_PROP).append('=').append(endpoint.hashCode());
+        buffer.append(ManagementConstants.INSTANCE_ID_PROP).append('=').append(instanceId);
         
         //Use default domain name of server
         return new ObjectName(buffer.toString());
