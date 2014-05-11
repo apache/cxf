b06ce80f11b4499c4281976e10f7a46fb54a5b0b
Erich Duda <dudaerich@gmail.com>
2014-05-11 13:34:34 +0200

    Added InvalidRepresentation fault.

---
+++ rt/ws/transfer/src/main/java/org/apache/cxf/ws/transfer/shared/faults/InvalidRepresentation.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cxf.ws.transfer.shared.faults;
+
+import javax.xml.namespace.QName;
+import org.apache.cxf.ws.transfer.shared.TransferConstants;
+
+/**
+ *
+ * @author erich
+ */
+public class InvalidRepresentation extends WSTransferFault {
+    
+    private static final String SUBCODE = "UnknownDialect";
+    
+    private static final String REASON = "The specified Dialect IRI is not known.";
+    
+    public InvalidRepresentation() {
+        super(REASON, null,
+             new QName(TransferConstants.TRANSFER_2011_03_NAMESPACE, SUBCODE));
+    }
+    
+}
