2120019be400eddd8f90c3a90ec01baa9c1439f1
Colm O hEigeartaigh <coheigea@apache.org>
2014-05-12 12:49:54 +0100

    Adding a test-case for adding + parsing a BinarySecurityToken

---
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/BinarySecurityTokenInterceptor.java
@@ -0,0 +1,179 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.ws.security.wss4j;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.security.auth.callback.CallbackHandler;
+import javax.xml.namespace.QName;
+
+import org.w3c.dom.Element;
+
+import org.apache.cxf.binding.soap.SoapMessage;
+import org.apache.cxf.common.classloader.ClassLoaderUtils;
+import org.apache.cxf.headers.Header;
+import org.apache.cxf.helpers.CastUtils;
+import org.apache.cxf.helpers.DOMUtils;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.interceptor.security.DefaultSecurityContext;
+import org.apache.cxf.security.SecurityContext;
+import org.apache.cxf.ws.security.SecurityConstants;
+import org.apache.cxf.ws.security.tokenstore.SecurityToken;
+import org.apache.cxf.ws.security.tokenstore.TokenStore;
+import org.apache.wss4j.common.ext.WSSecurityException;
+import org.apache.wss4j.dom.WSConstants;
+import org.apache.wss4j.dom.WSDocInfo;
+import org.apache.wss4j.dom.WSSConfig;
+import org.apache.wss4j.dom.WSSecurityEngineResult;
+import org.apache.wss4j.dom.handler.RequestData;
+import org.apache.wss4j.dom.handler.WSHandlerConstants;
+import org.apache.wss4j.dom.handler.WSHandlerResult;
+import org.apache.wss4j.dom.processor.BinarySecurityTokenProcessor;
+import org.apache.wss4j.dom.validate.Validator;
+import org.apache.wss4j.policy.model.AbstractToken;
+
+/**
+ * An interceptor to add a BinarySecurityToken token to the security header of an outbound request, and to
+ * process a BinarySecurityToken on an inbound request. It takes the BinarySecurityToken from the message 
+ * context on the outbound side.
+ */
+public class BinarySecurityTokenInterceptor extends AbstractTokenInterceptor {
+
+    public BinarySecurityTokenInterceptor() {
+        super();
+    }
+    
+    protected void processToken(SoapMessage message) {
+        Header h = findSecurityHeader(message, false);
+        if (h == null) {
+            return;
+        }
+        Element el = (Element)h.getObject();
+        Element child = DOMUtils.getFirstElement(el);
+        while (child != null) {
+            if (WSConstants.BINARY_TOKEN_LN.equals(child.getLocalName())
+                && WSConstants.WSSE_NS.equals(child.getNamespaceURI())) {
+                try {
+                    List<WSSecurityEngineResult> bstResults = processToken(child, message);
+                    if (bstResults != null) {
+                        List<WSHandlerResult> results = CastUtils.cast((List<?>)message
+                                .get(WSHandlerConstants.RECV_RESULTS));
+                        if (results == null) {
+                            results = new ArrayList<WSHandlerResult>();
+                            message.put(WSHandlerConstants.RECV_RESULTS, results);
+                        }
+                        WSHandlerResult rResult = new WSHandlerResult(null, bstResults);
+                        results.add(0, rResult);
+
+                        assertTokens(message);
+                        
+                        Principal principal = 
+                            (Principal)bstResults.get(0).get(WSSecurityEngineResult.TAG_PRINCIPAL);
+                        message.put(WSS4JInInterceptor.PRINCIPAL_RESULT, principal);                   
+                        
+                        SecurityContext sc = message.get(SecurityContext.class);
+                        if (sc == null || sc.getUserPrincipal() == null) {
+                            message.put(SecurityContext.class, new DefaultSecurityContext(principal, null));
+                        }
+
+                    }
+                } catch (WSSecurityException ex) {
+                    throw new Fault(ex);
+                }
+            }
+            child = DOMUtils.getNextElement(child);
+        }
+    }
+    
+    private List<WSSecurityEngineResult> processToken(Element tokenElement, final SoapMessage message)
+        throws WSSecurityException {
+        WSDocInfo wsDocInfo = new WSDocInfo(tokenElement.getOwnerDocument());
+        RequestData data = new RequestData() {
+            public CallbackHandler getCallbackHandler() {
+                return getCallback(message);
+            }
+            public Validator getValidator(QName qName) throws WSSecurityException {
+                String key = SecurityConstants.BST_TOKEN_VALIDATOR;
+                Object o = message.getContextualProperty(key);
+                try {
+                    if (o instanceof Validator) {
+                        return (Validator)o;
+                    } else if (o instanceof Class) {
+                        return (Validator)((Class<?>)o).newInstance();
+                    } else if (o instanceof String) {
+                        return (Validator)ClassLoaderUtils.loadClass(o.toString(),
+                                                                     BinarySecurityTokenInterceptor.class)
+                                                                     .newInstance();
+                    }
+                } catch (RuntimeException t) {
+                    throw t;
+                } catch (Exception ex) {
+                    throw new WSSecurityException(WSSecurityException.ErrorCode.FAILURE, ex);
+                }
+                return super.getValidator(qName);
+            }
+        };
+        data.setWssConfig(WSSConfig.getNewInstance());
+        
+        BinarySecurityTokenProcessor p = new BinarySecurityTokenProcessor();
+        List<WSSecurityEngineResult> results = 
+            p.handleToken(tokenElement, data, wsDocInfo);
+        return results;
+    }
+    
+    protected AbstractToken assertTokens(SoapMessage message) {
+        // Assert tokens here if required
+        return null;
+    }
+
+    protected void addToken(SoapMessage message) {
+        SecurityToken securityToken = getSecurityToken(message);
+        if (securityToken == null || securityToken.getToken() == null) {
+            // No SecurityToken so just return
+            return;
+        }
+        
+        assertTokens(message);
+        Header h = findSecurityHeader(message, true);
+        Element el = (Element)h.getObject();
+        el.appendChild(el.getOwnerDocument().importNode(securityToken.getToken(), true));
+    }
+
+    private SecurityToken getSecurityToken(SoapMessage message) {
+        if (message.getContextualProperty(SecurityConstants.TOKEN) instanceof SecurityToken) {
+            return (SecurityToken)message.getContextualProperty(SecurityConstants.TOKEN);
+        }
+        
+        // Get the TokenStore
+        TokenStore tokenStore = getTokenStore(message);
+        if (tokenStore == null) {
+            return null;
+        }
+        
+        String id = (String)message.getContextualProperty(SecurityConstants.TOKEN_ID);
+        if (id != null) {
+            return tokenStore.getToken(id);
+        }
+        return null;
+    }
+    
+}
---
+++ systests/ws-security/src/test/java/org/apache/cxf/systest/ws/tokens/DoubleItBSTImpl.java
@@ -0,0 +1,72 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.ws.tokens;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+
+import org.apache.cxf.feature.Features;
+import org.apache.cxf.helpers.CastUtils;
+import org.apache.wss4j.dom.WSSecurityEngineResult;
+import org.apache.wss4j.dom.handler.WSHandlerConstants;
+import org.apache.wss4j.dom.handler.WSHandlerResult;
+import org.apache.wss4j.dom.message.token.BinarySecurity;
+import org.example.contract.doubleit.DoubleItFault;
+import org.example.contract.doubleit.DoubleItPortType;
+import org.junit.Assert;
+
+@WebService(targetNamespace = "http://www.example.org/contract/DoubleIt", 
+            serviceName = "DoubleItService", 
+            endpointInterface = "org.example.contract.doubleit.DoubleItPortType")
+@Features(features = "org.apache.cxf.feature.LoggingFeature")              
+public class DoubleItBSTImpl implements DoubleItPortType {
+    
+    @Resource
+    WebServiceContext wsContext;
+    
+    public int doubleIt(int numberToDouble) throws DoubleItFault {
+        if (numberToDouble == 0) {
+            throw new DoubleItFault("0 can't be doubled!");
+        }
+        
+        List<WSHandlerResult> results = 
+            CastUtils.cast((List<?>)wsContext.getMessageContext().get(WSHandlerConstants.RECV_RESULTS));
+        Assert.assertNotNull("Security Results cannot be null", results);
+        Assert.assertTrue(results.size() > 0);
+        
+        WSHandlerResult result = results.get(0);
+        List<WSSecurityEngineResult> securityResults = result.getResults();
+        Assert.assertNotNull("Security Results cannot be null", securityResults);
+        Assert.assertTrue(securityResults.size() > 0);
+        
+        WSSecurityEngineResult securityResult = securityResults.get(0);
+        BinarySecurity binarySecurityToken = 
+            (BinarySecurity)securityResult.get(WSSecurityEngineResult.TAG_BINARY_SECURITY_TOKEN);
+        Assert.assertNotNull(binarySecurityToken);
+        
+        Assert.assertTrue(Arrays.equals(binarySecurityToken.getToken(), "This is a token".getBytes()));
+        
+        return numberToDouble * 2;
+    }
+    
+}
