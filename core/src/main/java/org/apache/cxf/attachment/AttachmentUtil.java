39857567eaa4dab7fd986c85fed66130e8985724
Daniel Kulp <dkulp@apache.org>
2014-07-14 10:43:06 -0400

    Fix checkstyle issue

--- core/src/main/java/org/apache/cxf/attachment/AttachmentUtil.java
+++ core/src/main/java/org/apache/cxf/attachment/AttachmentUtil.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
