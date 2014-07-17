ffb30d5140171c22b3dbb00a1ea032a0076d4fa1
Sergey Beryozkin <sberyozkin@talend.com>
2014-07-17 15:46:02 +0100

    [CXF-5884] Using IOException to indicate that the sequence is malformed in OAuthJSONProvider

--- rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/provider/OAuthJSONProvider.java
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/provider/OAuthJSONProvider.java
@@ -30,7 +30,6 @@ import java.util.Map;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.client.ClientException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
@@ -158,7 +157,7 @@ public class OAuthJSONProvider implements MessageBodyWriter<Object>,
             return Collections.emptyMap();
         }
         if (!str.startsWith("{") || !str.endsWith("}")) {
-            throw new ClientException("JSON Sequence is broken");
+            throw new IOException("JSON Sequence is broken");
         }
         Map<String, String> map = new LinkedHashMap<String, String>();
         
