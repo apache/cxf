a76a9726421cd32dcdeb342da08023022a4498bb
Erich Duda <dudaerich@gmail.com>
2014-07-23 22:29:49 +0200

    Changed Dialect interface.

--- rt/ws/transfer/src/main/java/org/apache/cxf/ws/transfer/dialect/Dialect.java
+++ rt/ws/transfer/src/main/java/org/apache/cxf/ws/transfer/dialect/Dialect.java
@@ -19,6 +19,10 @@
 
 package org.apache.cxf.ws.transfer.dialect;
 
+import org.apache.cxf.ws.transfer.Create;
+import org.apache.cxf.ws.transfer.Delete;
+import org.apache.cxf.ws.transfer.Get;
+import org.apache.cxf.ws.transfer.Put;
 import org.apache.cxf.ws.transfer.Representation;
 
 /**
@@ -29,11 +33,34 @@ import org.apache.cxf.ws.transfer.Representation;
 public interface Dialect {
     
     /**
-     * Method for processing incoming SOAP message by Dialect extension.
-     * @param body SOAP body
-     * @param representation XML representation stored in the ResourceManager,
-     * null if there is no such representation
+     * Method for processing incoming Get message by Dialect extension.
+     * @param body Get body
+     * @param representation XML representation stored in the ResourceManager
+     * @return Representation, which will be returned in response.
      */
-    void process(Object body, Representation representation);
+    Representation processGet(Get body, Representation representation);
+    
+    /**
+     * Method for processing incoming Put message by Dialect extension.
+     * @param body Put body
+     * @param representation XML representation stored in the ResourceManager
+     * @return Representation, which will be stored in ResourceManager.
+     */
+    Representation processPut(Put body, Representation representation);
+    
+    /**
+     * Method for processing incoming Delete message by Dialect extension.
+     * @param body Delete body
+     * @param representation XML representation stored in the ResourceManager
+     * @return Representation, which will be stored in ResourceManager.
+     */
+    Representation processDelete(Delete body, Representation representation);
+    
+    /**
+     * Method for processing incoming Create message by Dialect extension.
+     * @param body Create body
+     * @return Representation, which will be stored in ResourceManager.
+     */
+    Representation processCreate(Create body);
     
 }
