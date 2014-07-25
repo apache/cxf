c0bbe48ec37104321bfa2988878184c0763856b8
Sergey Beryozkin <sberyozkin@talend.com>
2014-07-25 18:10:15 +0300

    [CXF-5844] Merging annotations on the output if the annotated and invoked methods are different

---
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/CustomJaxbElementProvider.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.jaxrs;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.cxf.annotations.SchemaValidation;
+import org.apache.cxf.jaxrs.provider.JAXBElementProvider;
+
+public class CustomJaxbElementProvider extends JAXBElementProvider<Book> {
+    @Context 
+    private UriInfo ui;
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] anns, MediaType mt) {
+        if (ui.getRequestUri().toString().endsWith("/test/5/bookstorestorage/thosebooks/123")) {
+            for (Annotation ann : anns) {
+                if (ann.annotationType() == SchemaValidation.class) {
+                    return super.isWriteable(type, genericType, anns, mt);
+                }
+            }
+            throw new RuntimeException();
+        } else {
+            return super.isWriteable(type, genericType, anns, mt);
+        }
+        
+    }
+}
