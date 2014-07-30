211c15f454d2a7a6ef271ada4b8b014b647d1996
Daniel Kulp <dkulp@apache.org>
2014-07-30 14:28:49 -0400

    [CXF-5862] Make sure we don't skip binding nodes in the binding file Fixes a potential NPE if there are an odd number of binding nodes

--- tools/wsdlto/frontend/jaxws/src/main/java/org/apache/cxf/tools/wsdlto/frontend/jaxws/customization/CustomizationParser.java
+++ tools/wsdlto/frontend/jaxws/src/main/java/org/apache/cxf/tools/wsdlto/frontend/jaxws/customization/CustomizationParser.java
@@ -300,7 +300,6 @@ public final class CustomizationParser {
                     }
                 }
                 appinfoNode.appendChild(cloneNode);
-                childNode = childNode.getNextSibling();
             }
         }
         
