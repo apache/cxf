6e6c139095a694011f8a115f5d74cb68ee87f89d
Colm O hEigeartaigh <coheigea@apache.org>
2014-08-06 12:21:20 +0100

    Adding support for WS-Security kerberos credential delegation + a system test

---
+++ services/sts/systests/advanced/src/test/java/org/apache/cxf/systest/sts/kerberos/IntermediaryPortTypeImpl.java
@@ -0,0 +1,90 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.sts.kerberos;
+
+import java.net.URL;
+import java.security.Principal;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
+import org.apache.cxf.feature.Features;
+import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.apache.cxf.ws.security.SecurityConstants;
+import org.apache.cxf.ws.security.trust.STSClient;
+import org.example.contract.doubleit.DoubleItPortType;
+import org.ietf.jgss.GSSCredential;
+import org.junit.Assert;
+
+@WebService(targetNamespace = "http://www.example.org/contract/DoubleIt", 
+            serviceName = "DoubleItService", 
+            endpointInterface = "org.example.contract.doubleit.DoubleItPortType")
+@Features(features = "org.apache.cxf.feature.LoggingFeature")              
+public class IntermediaryPortTypeImpl extends AbstractBusClientServerTestBase implements DoubleItPortType {
+    
+    private static final String NAMESPACE = "http://www.example.org/contract/DoubleIt";
+    private static final QName SERVICE_QNAME = new QName(NAMESPACE, "DoubleItService");
+    
+    @Resource
+    WebServiceContext wsc;
+
+    public int doubleIt(int numberToDouble) {
+        Principal pr = wsc.getUserPrincipal();
+        
+        Assert.assertNotNull("Principal must not be null", pr);
+        Assert.assertNotNull("Principal.getName() must not return null", pr.getName());
+        
+        URL wsdl = IntermediaryPortTypeImpl.class.getResource("DoubleIt.wsdl");
+        Service service = Service.create(wsdl, SERVICE_QNAME);
+        QName portQName = new QName(NAMESPACE, "DoubleItTransportSAML2Port");
+        DoubleItPortType transportPort = 
+            service.getPort(portQName, DoubleItPortType.class);
+        try {
+            updateAddressPort(transportPort, KerberosDelegationTokenTest.PORT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        // Retrieve delegated credential + set it on the outbound message
+        MessageContext messageContext = wsc.getMessageContext();
+        GSSCredential delegatedCredential = 
+            (GSSCredential)messageContext.get(SecurityConstants.DELEGATED_CREDENTIAL);
+        Map<String, Object> context = ((BindingProvider)transportPort).getRequestContext();
+        context.put(SecurityConstants.DELEGATED_CREDENTIAL, delegatedCredential);
+        
+        STSClient stsClient = (STSClient)context.get(SecurityConstants.STS_CLIENT);
+        if (stsClient != null) {
+            String location = stsClient.getWsdlLocation();
+            if (location.contains("8443")) {
+                stsClient.setWsdlLocation(
+                    location.replace("8443", KerberosDelegationTokenTest.STSPORT)
+                );
+            }
+        }
+        
+        return transportPort.doubleIt(numberToDouble);
+    }
+    
+}
--- services/sts/systests/advanced/src/test/java/org/apache/cxf/systest/sts/kerberos/KerberosDelegationTokenTest.java
+++ services/sts/systests/advanced/src/test/java/org/apache/cxf/systest/sts/kerberos/KerberosDelegationTokenTest.java
@@ -19,8 +19,6 @@
 package org.apache.cxf.systest.sts.kerberos;
 
 import java.net.URL;
-import java.util.Arrays;
-import java.util.Collection;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
@@ -29,44 +27,32 @@ import javax.xml.ws.Service;
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBusFactory;
 import org.apache.cxf.systest.sts.common.SecurityTestUtil;
-import org.apache.cxf.systest.sts.common.TestParam;
 import org.apache.cxf.systest.sts.common.TokenTestUtils;
 import org.apache.cxf.systest.sts.deployment.STSServer;
-import org.apache.cxf.systest.sts.deployment.StaxSTSServer;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
 import org.example.contract.doubleit.DoubleItPortType;
 import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized.Parameters;
 
 /**
- * In this test, a CXF client requests a SAML2 HOK Assertion from the STS, which has a policy of requiring
- * a KerberosToken over the TransportBinding. The CXF client retrieves a service ticket from the KDC and
- * inserts it into the security header of the request. The STS validates the ticket using the 
- * KerberosTokenValidator.
+ * This tests credential delegation. The client enables credential delegation + sends a Kerberos
+ * token to an Intermediary via WS-Security. The Intermediary validates the token, and then
+ * uses the delgated credential to obtain a ticket to in turn retrieve a SAML token from the
+ * STS. The SAML token is used to secure access to the backend service.
  * 
  * The tests are @Ignored by default, as a KDC is needed. To replicate the test scenario, set up a KDC with 
  * user principal "alice" (keytab in "/etc/alice.keytab"), and host service "bob@service.ws.apache.org" 
  * (keytab in "/etc/bob.keytab").
  */
-@RunWith(value = org.junit.runners.Parameterized.class)
 @org.junit.Ignore
-public class KerberosTokenTest extends AbstractBusClientServerTestBase {
+public class KerberosDelegationTokenTest extends AbstractBusClientServerTestBase {
     
     static final String STSPORT = allocatePort(STSServer.class);
-    static final String STAX_STSPORT = allocatePort(StaxSTSServer.class);
+    static final String PORT = allocatePort(Server.class);
+    static final String INTERMEDIARY_PORT = allocatePort(Intermediary.class);
     
     private static final String NAMESPACE = "http://www.example.org/contract/DoubleIt";
     private static final QName SERVICE_QNAME = new QName(NAMESPACE, "DoubleItService");
 
-    private static final String PORT = allocatePort(Server.class);
-    
-    final TestParam test;
-    
-    public KerberosTokenTest(TestParam type) {
-        this.test = type;
-    }
-    
     @BeforeClass
     public static void startServers() throws Exception {
         assertTrue(
@@ -85,18 +71,8 @@ public class KerberosTokenTest extends AbstractBusClientServerTestBase {
                    "Server failed to launch",
                    // run the server in the same process
                    // set this to false to fork
-                   launchServer(StaxSTSServer.class, true)
+                   launchServer(Intermediary.class, true)
         );
-    }
-    
-    @Parameters(name = "{0}")
-    public static Collection<TestParam[]> data() {
-       
-        return Arrays.asList(new TestParam[][] {{new TestParam(PORT, false, STSPORT)},
-                                                {new TestParam(PORT, true, STSPORT)},
-                                                {new TestParam(PORT, false, STAX_STSPORT)},
-                                                {new TestParam(PORT, true, STAX_STSPORT)},
-        });
     }
     
     @org.junit.AfterClass
@@ -109,20 +85,20 @@ public class KerberosTokenTest extends AbstractBusClientServerTestBase {
     public void testKerberosToken() throws Exception {
 
         SpringBusFactory bf = new SpringBusFactory();
-        URL busFile = KerberosTokenTest.class.getResource("cxf-client.xml");
+        URL busFile = KerberosDelegationTokenTest.class.getResource("cxf-intermediary-client.xml");
 
         Bus bus = bf.createBus(busFile.toString());
         SpringBusFactory.setDefaultBus(bus);
         SpringBusFactory.setThreadDefaultBus(bus);
         
-        URL wsdl = KerberosTokenTest.class.getResource("DoubleIt.wsdl");
+        URL wsdl = KerberosDelegationTokenTest.class.getResource("DoubleItIntermediary.wsdl");
         Service service = Service.create(wsdl, SERVICE_QNAME);
-        QName portQName = new QName(NAMESPACE, "DoubleItTransportSAML2Port");
+        QName portQName = new QName(NAMESPACE, "DoubleItTransportKerberosPort");
         DoubleItPortType transportSaml2Port = 
             service.getPort(portQName, DoubleItPortType.class);
-        updateAddressPort(transportSaml2Port, test.getPort());
+        updateAddressPort(transportSaml2Port, INTERMEDIARY_PORT);
         
-        TokenTestUtils.updateSTSPort((BindingProvider)transportSaml2Port, test.getStsPort());
+        TokenTestUtils.updateSTSPort((BindingProvider)transportSaml2Port, STSPORT);
 
         doubleIt(transportSaml2Port, 25);
         
