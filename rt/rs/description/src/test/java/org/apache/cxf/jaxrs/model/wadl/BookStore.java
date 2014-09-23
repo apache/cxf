854fd752671a801a4bcd3394a89021ba8de56ce6
Sergey Beryozkin <sberyozkin@talend.com>
2014-09-23 11:23:32 +0100

    Updating a WADLGenerator test

--- rt/rs/description/src/test/java/org/apache/cxf/jaxrs/model/wadl/BookStore.java
+++ rt/rs/description/src/test/java/org/apache/cxf/jaxrs/model/wadl/BookStore.java
@@ -181,7 +181,7 @@ public class BookStore implements BookDescription {
         public int getA() {
             return a;
         }
-        @PathParam("a")
+        @HeaderParam("a")
         public void setA(int a) {
             this.a = a;
         }
