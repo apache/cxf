6085471deb9974b1009eabcd2b067e7a5feec357
Colm O hEigeartaigh <coheigea@apache.org>
2014-10-06 12:14:24 +0100

    Fixing cache file location for SAML SSO

--- rt/rs/security/sso/saml/src/main/java/org/apache/cxf/rs/security/saml/sso/EHCacheTokenReplayCache.java
+++ rt/rs/security/sso/saml/src/main/java/org/apache/cxf/rs/security/saml/sso/EHCacheTokenReplayCache.java
@@ -45,7 +45,7 @@ public class EHCacheTokenReplayCache implements TokenReplayCache<String> {
     public static final long DEFAULT_TTL = 3600L;
     public static final long MAX_TTL = DEFAULT_TTL * 12L;
     public static final String CACHE_KEY = "cxf.samlp.replay.cache";
-    private static final String DEFAULT_CONFIG_URL = "cxf-samlp-ehcache.xml";
+    private static final String DEFAULT_CONFIG_URL = "/cxf-samlp-ehcache.xml";
     
     private Ehcache cache;
     private CacheManager cacheManager;
