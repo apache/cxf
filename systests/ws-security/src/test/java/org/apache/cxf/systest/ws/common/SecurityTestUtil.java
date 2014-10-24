623a827d65721c3fee9edc3a31b4e1b00bcf2451
Colm O hEigeartaigh <coheigea@apache.org>
2014-10-24 12:11:07 +0100

    Minor test change

--- systests/ws-security/src/test/java/org/apache/cxf/systest/ws/common/SecurityTestUtil.java
+++ systests/ws-security/src/test/java/org/apache/cxf/systest/ws/common/SecurityTestUtil.java
@@ -33,6 +33,27 @@ import org.example.contract.doubleit.DoubleItPortType;
  */
 public final class SecurityTestUtil {
     
+    private static final boolean UNRESTRICTED_POLICIES_INSTALLED;
+    static {
+        boolean ok = false;
+        try {
+            byte[] data = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+
+            SecretKey key192 = new SecretKeySpec(
+                new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+                            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17},
+                            "AES");
+            Cipher c = Cipher.getInstance("AES");
+            c.init(Cipher.ENCRYPT_MODE, key192);
+            c.doFinal(data);
+            ok = true;
+        } catch (Exception e) {
+            //
+        }
+        UNRESTRICTED_POLICIES_INSTALLED = ok;
+    }
+    
     private SecurityTestUtil() {
         // complete
     }
@@ -55,22 +76,7 @@ public final class SecurityTestUtil {
     }
     
     public static boolean checkUnrestrictedPoliciesInstalled() {
-        try {
-            byte[] data = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
-
-            SecretKey key192 = new SecretKeySpec(
-                new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-                            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-                            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17},
-                            "AES");
-            Cipher c = Cipher.getInstance("AES");
-            c.init(Cipher.ENCRYPT_MODE, key192);
-            c.doFinal(data);
-            return true;
-        } catch (Exception e) {
-            //
-        }
-        return false;
+        return UNRESTRICTED_POLICIES_INSTALLED;
     }
     
     public static void enableStreaming(DoubleItPortType port) {
