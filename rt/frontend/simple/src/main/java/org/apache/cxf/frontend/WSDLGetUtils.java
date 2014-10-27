754ebbdd7c2aab05b484cb1a0fe8bb48e4fe9376
Daniel Kulp <dkulp@apache.org>
2014-10-27 15:45:26 -0400

    [CXF-6034] Fix problems with mapping schema locations to resolvable locations when generating the live wsdl

--- rt/frontend/simple/src/main/java/org/apache/cxf/frontend/WSDLGetUtils.java
+++ rt/frontend/simple/src/main/java/org/apache/cxf/frontend/WSDLGetUtils.java
@@ -473,6 +473,9 @@ public class WSDLGetUtils {
                     if (!doneSchemas.containsKey(decodedStart)) {
                         String resolvedSchemaLocation = resolveWithCatalogs(catalogs, start, base);
                         if (resolvedSchemaLocation == null) {
+                            resolvedSchemaLocation = resolveWithCatalogs(catalogs, imp.getSchemaLocationURI(), base);
+                        }                        
+                        if (resolvedSchemaLocation == null) {
                             try {
                                 //check to see if it's already in a URL format.  If so, leave it.
                                 new URL(start);
@@ -484,6 +487,7 @@ public class WSDLGetUtils {
                         } else {
                             if (doneSchemas.put(decodedStart, imp) == null) {
                                 doneSchemas.put(resolvedSchemaLocation, imp);
+                                doneSchemas.put(imp.getSchemaLocationURI(), imp);
                                 updateSchemaImports(bus, imp.getReferencedSchema(), docBase, doneSchemas, base);
                             }
                         }
