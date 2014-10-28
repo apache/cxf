f56fbcebe75167228da960cf92eec65d62f9b745
Daniel Kulp <dkulp@apache.org>
2014-10-28 13:57:05 -0400

    [CXF-6058] NPE guard in Header logging
    Conflicts:
    	rt/transports/http/src/main/java/org/apache/cxf/transport/http/Headers.java

--- rt/transports/http/src/main/java/org/apache/cxf/transport/http/Headers.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/Headers.java
@@ -254,7 +254,8 @@ public class Headers {
         for (String header : headers.keySet()) {
             List<String> headerList = headers.get(header);
             for (String value : headerList) {
-                LOG.log(level, header + ": " + value);
+                LOG.log(level, header + ": "
+                    + (value == null ? "<null>" : value.toString()));
             }
         }
     }
