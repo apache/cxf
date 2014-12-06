2d5ca823a398948baf481b113367984176d625c2
paul <pkmcculloch@gmail.com>
2014-12-06 22:15:02 +0000

    CXF-6138 JAXB unmarshaller Properties get overwriten on a call to initialize

--- rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/JAXBDataBinding.java
+++ rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/JAXBDataBinding.java
@@ -304,8 +304,12 @@ public class JAXBDataBinding extends AbstractInterceptorProvidingDataBinding
 
 
         contextClasses = new LinkedHashSet<Class<?>>();
-        Map<String, Object> unmarshallerProps = new HashMap<String, Object>();
-        this.setUnmarshallerProperties(unmarshallerProps);
+        
+        if(this.getUnmarshallerProperties()==null||this.getUnmarshallerProperties().isEmpty()){
+        	Map<String, Object> unmarshallerProps = new HashMap<String, Object>();
+        	this.setUnmarshallerProperties(unmarshallerProps);
+        }
+        
         for (ServiceInfo serviceInfo : service.getServiceInfos()) {
             
             JAXBContextInitializer initializer
