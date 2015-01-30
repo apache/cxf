bfbce4beef8a5a9cc96c63f5d82cb3409221a466
Alessio Soldano <asoldano@redhat.com>
2015-01-30 12:41:15 +0100

    [CXF-6227] Avoid string concatenation when not required for the current log level
    Conflicts:
    	rt/transports/http/src/main/java/org/apache/cxf/transport/http/Headers.java

--- rt/transports/http/src/main/java/org/apache/cxf/transport/http/Headers.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/Headers.java
@@ -251,11 +251,13 @@ public class Headers {
      * @param headers The Message protocol headers.
      */
     void logProtocolHeaders(Level level) {
-        for (String header : headers.keySet()) {
-            List<String> headerList = headers.get(header);
-            for (String value : headerList) {
-                LOG.log(level, header + ": "
-                    + (value == null ? "<null>" : value.toString()));
+        if (LOG.isLoggable(level)) {
+            for (String header : headers.keySet()) {
+                List<String> headerList = headers.get(header);
+                for (String value : headerList) {
+                    LOG.log(level, header + ": "
+                            + (value == null ? "<null>" : value.toString()));
+                }
             }
         }
     }
