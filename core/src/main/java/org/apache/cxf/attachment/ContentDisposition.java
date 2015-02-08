5f125f514d00451b3644b25b0ecfd506664c958f
Sergey Beryozkin <sberyozkin@talend.com>
2015-02-08 18:54:33 +0000

    [CXF-6244] Support for ContentDisposition UTF8 filename parameters, patch from Mark Ford applied; This closes #52

--- core/src/main/java/org/apache/cxf/attachment/ContentDisposition.java
+++ core/src/main/java/org/apache/cxf/attachment/ContentDisposition.java
@@ -19,6 +19,7 @@
 
 package org.apache.cxf.attachment;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -26,45 +27,85 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ContentDisposition {
-    private static final String CD_HEADER_PARAMS_EXPRESSION = 
-        "(([\\w]+( )?=( )?\"[^\"]+\")|([\\w]+( )?=( )?[^;]+))";
+    private static final String CD_HEADER_PARAMS_EXPRESSION =
+        "(([\\w]+( )?\\*?=( )?\"[^\"]+\")|([\\w]+( )?\\*?=( )?[^;]+))";
     private static final Pattern CD_HEADER_PARAMS_PATTERN =
-        Pattern.compile(CD_HEADER_PARAMS_EXPRESSION);
+            Pattern.compile(CD_HEADER_PARAMS_EXPRESSION);
+
+    private static final String CD_HEADER_EXT_PARAMS_EXPRESSION =
+            "(UTF-8|ISO-8859-1)''((?:%[0-9a-f]{2}|\\S)+)";
+    private static final Pattern CD_HEADER_EXT_PARAMS_PATTERN =
+            Pattern.compile(CD_HEADER_EXT_PARAMS_EXPRESSION);
+
     private String value;
     private String type;
     private Map<String, String> params = new LinkedHashMap<String, String>();
-    
+
     public ContentDisposition(String value) {
         this.value = value;
-        
+
         String tempValue = value;
-        
+
         int index = tempValue.indexOf(';');
         if (index > 0 && !(tempValue.indexOf('=') < index)) {
             type = tempValue.substring(0, index).trim();
             tempValue = tempValue.substring(index + 1);
         }
-        
+
+        String extendedFilename = null;
         Matcher m = CD_HEADER_PARAMS_PATTERN.matcher(tempValue);
         while (m.find()) {
             String[] pair = m.group().trim().split("=");
-            params.put(pair[0].trim(), 
-                       pair.length == 2 ? pair[1].trim().replace("\"", "") : "");
+            String paramName = pair[0].trim();
+            String paramValue = pair.length == 2 ? pair[1].trim().replace("\"", "") : "";
+            // filename* looks like the only CD param that is human readable
+            // and worthy of the extended encoding support. Other parameters 
+            // can be supported if needed, see the complete list below
+            /*
+                http://www.iana.org/assignments/cont-disp/cont-disp.xhtml#cont-disp-2
+
+                filename            name to be used when creating file [RFC2183]
+                creation-date       date when content was created [RFC2183]
+                modification-date   date when content was last modified [RFC2183]
+                read-date           date when content was last read [RFC2183]
+                size                approximate size of content in octets [RFC2183]
+                name                original field name in form [RFC2388]
+                voice               type or use of audio content [RFC2421]
+                handling            whether or not processing is required [RFC3204]
+             */
+            if ("filename*".equals(paramName)) {
+                // try to decode the value if it matches the spec
+                try {
+                    Matcher matcher = CD_HEADER_EXT_PARAMS_PATTERN.matcher(paramValue);
+                    if (matcher.matches()) {
+                        String encodingScheme = matcher.group(1);
+                        String encodedValue = matcher.group(2);
+                        paramValue = Rfc5987Util.decode(encodedValue, encodingScheme);
+                        extendedFilename = paramValue;
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    // would be odd not to support UTF-8 or 8859-1
+                }
+            }
+            params.put(paramName, paramValue);
+        }
+        if (extendedFilename != null) {
+            params.put("filename", extendedFilename);
         }
     }
-    
+
     public String getType() {
         return type;
     }
-    
+
     public String getParameter(String name) {
         return params.get(name);
     }
-    
+
     public Map<String, String> getParameters() {
         return Collections.unmodifiableMap(params);
     }
-    
+
     public String toString() {
         return value;
     }
---
+++ core/src/test/java/org/apache/cxf/attachment/Rfc5987UtilTest.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.attachment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class Rfc5987UtilTest {
+    private final String input;
+    private final String expected;
+
+    public Rfc5987UtilTest(String input, String expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> params() throws Exception {
+        List<Object[]> params = new ArrayList<>();
+        params.add(new Object[] {"foo-ä-€.html", "foo-%c3%a4-%e2%82%ac.html"});
+        params.add(new Object[]{"世界ーファイル 2.jpg",
+            "%e4%b8%96%e7%95%8c%e3%83%bc%e3%83%95%e3%82%a1%e3%82%a4%e3%83%ab%202.jpg"});
+        params.add(new Object[]{"foo.jpg", "foo.jpg"});
+        return params;
+    }
+
+    @Test
+    public void test() throws Exception {
+        assertEquals(expected, Rfc5987Util.encode(input, "UTF-8"));
+
+        assertEquals(input, Rfc5987Util.decode(expected, "UTF-8"));
+    }
+}
