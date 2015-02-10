bbcbac00dd7a1bf5f0c68f9a77dae1497d969167
Colm O hEigeartaigh <coheigea@apache.org>
2015-02-10 16:24:27 +0000

    NPE fix

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/CryptoCoverageChecker.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/CryptoCoverageChecker.java
@@ -116,6 +116,10 @@ public class CryptoCoverageChecker extends AbstractSoapInterceptor {
             // return
         }
         
+        if (message.getContent(SOAPMessage.class) == null) {
+            throw new SoapFault("Error obtaining SOAP document", Fault.FAULT_CODE_CLIENT);
+        }
+        
         Element documentElement = null;
         try {
             SOAPMessage saajDoc = message.getContent(SOAPMessage.class);
