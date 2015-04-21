57f74751675a5339e0cacf48a480be994f4ee064
Colm O hEigeartaigh <coheigea@apache.org>
2015-04-21 15:18:29 +0100

    Also cater for null aliases

--- core/src/main/java/org/apache/cxf/common/util/crypto/CryptoUtils.java
+++ core/src/main/java/org/apache/cxf/common/util/crypto/CryptoUtils.java
@@ -637,8 +637,11 @@ public final class CryptoUtils {
     }
     public static Certificate loadCertificate(KeyStore keyStore, String alias) {
         try {
+            if (alias == null) {
+                throw new SecurityException("No keystore alias was defined");
+            }
             if (!keyStore.containsAlias(alias)) {
-                throw new SecurityException("No alias exists in the keystore for the given alias: " + alias);
+                throw new SecurityException("No alias exists in the keystore for: " + alias);
             }
             return keyStore.getCertificate(alias);
         } catch (Exception ex) { 
@@ -689,6 +692,12 @@ public final class CryptoUtils {
                                             char[] keyPassword, 
                                             String alias) {
         try {
+            if (alias == null) {
+                throw new SecurityException("No keystore alias was defined");
+            }
+            if (!keyStore.containsAlias(alias)) {
+                throw new SecurityException("No alias exists in the keystore for: " + alias);
+            }
             KeyStore.PrivateKeyEntry pkEntry = (KeyStore.PrivateKeyEntry)
                 keyStore.getEntry(alias, new KeyStore.PasswordProtection(keyPassword));
             return pkEntry.getPrivateKey();
