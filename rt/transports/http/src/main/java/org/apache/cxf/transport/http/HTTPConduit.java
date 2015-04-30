ed953346885462fdc81186a51e661335ec846279
Sergey Beryozkin <sberyozkin@talend.com>
2015-04-30 16:58:15 +0100

    [CXF-6007] Minor update to HttpConduit to support empty POSTs and DELETEs retransmits

--- rt/transports/http/src/main/java/org/apache/cxf/transport/http/HTTPConduit.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/HTTPConduit.java
@@ -1362,9 +1362,8 @@ public abstract class HTTPConduit
         protected void handleRetransmits() throws IOException {
             // If we have a cachedStream, we are caching the request.
             if (cachedStream != null
-                || KNOWN_HTTP_VERBS_WITH_NO_CONTENT.contains(getMethod()) 
-                   && (getClient().isAutoRedirect()) 
-                       || authSupplier != null && authSupplier.requiresRequestCaching()) {
+                || getClient().isAutoRedirect() && KNOWN_HTTP_VERBS_WITH_NO_CONTENT.contains(getMethod()) 
+                || authSupplier != null && authSupplier.requiresRequestCaching()) {
 
                 if (LOG.isLoggable(Level.FINE) && cachedStream != null) {
                     StringBuilder b = new StringBuilder(4096);
