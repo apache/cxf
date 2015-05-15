90b54f1154c45e132f69feff67d65ad2311ceb9b
Sergey Beryozkin <sberyozkin@talend.com>
2015-05-15 10:25:23 +0100

    [CXF-6402] preventing NPE in JettyHTTPServerEngine

--- rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/JettyHTTPServerEngine.java
+++ rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/JettyHTTPServerEngine.java
@@ -316,6 +316,9 @@ public class JettyHTTPServerEngine implements ServerEngine {
         if (shouldCheckUrl(handler.getBus())) {
             checkRegistedContext(url);
         }
+        if (contexts == null) {
+            contexts = new ContextHandlerCollection();
+        }
         
         SecurityHandler securityHandler = null;
         if (server == null) {
@@ -394,7 +397,6 @@ public class JettyHTTPServerEngine implements ServerEngine {
                     }
                 }
             }
-            contexts = new ContextHandlerCollection();
             /*
              * handlerCollection may be null here if is only one handler to deal with.
              * Which in turn implies that there can't be a 'defaultHander' to deal with.
