4e7d0b8dea372a596514d47f7bd1fdfa1f5d39ef
Sergey Beryozkin <sberyozkin@talend.com>
2015-05-18 17:39:24 +0100

    [CXf-6402] Registering contexts as a handler if the server is not null

--- rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/JettyHTTPServerEngine.java
+++ rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/JettyHTTPServerEngine.java
@@ -318,6 +318,9 @@ public class JettyHTTPServerEngine implements ServerEngine {
         }
         if (contexts == null) {
             contexts = new ContextHandlerCollection();
+            if (server != null) {
+                server.setHandler(contexts);
+            }
         }
         
         SecurityHandler securityHandler = null;
