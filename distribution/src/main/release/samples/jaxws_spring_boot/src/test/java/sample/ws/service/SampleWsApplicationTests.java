be4e40a46784d81c559b073b487255cf87334a77
Daniel Kulp <dkulp@apache.org>
2015-06-05 13:20:35 -0400

    Get jaxws_spring_boot to pass checkstyle

--- distribution/src/main/release/samples/jaxws_spring_boot/src/test/java/sample/ws/service/SampleWsApplicationTests.java
+++ distribution/src/main/release/samples/jaxws_spring_boot/src/test/java/sample/ws/service/SampleWsApplicationTests.java
@@ -1,17 +1,20 @@
-/*
- * Copyright 2012-2014 the original author or authors.
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package sample.ws.service;
 
@@ -31,10 +34,11 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.ws.client.core.WebServiceTemplate;
-import sample.ws.SampleWsApplication;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
+
+import sample.ws.SampleWsApplication;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -43,31 +47,36 @@ import static org.junit.Assert.assertThat;
 @IntegrationTest("server.port=0")
 public class SampleWsApplicationTests {
 
-	@Rule
-	public OutputCapture output = new OutputCapture();
+    //CHECKSTYLE:OFF
+    @Rule
+    public OutputCapture output = new OutputCapture(); // SUPPRESS CHECKSTYLE
+    //CHECKSTYLE:ON
 
-	private WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+    private WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
 
-	@Value("${local.server.port}")
-	private int serverPort;
+    @Value("${local.server.port}")
+    private int serverPort;
 
-	@Before
-	public void setUp() {
-		this.webServiceTemplate.setDefaultUri("http://localhost:" + this.serverPort
-				+ "/Service/Hello");
-	}
+    @Before
+    public void setUp() {
+        this.webServiceTemplate.setDefaultUri("http://localhost:" + this.serverPort + "/Service/Hello");
+    }
 
-	@Test
-	public void testHelloRequest() {
-		//final String request = "<q0:sayHello xmlns:q0=\"http://service.ws.sample\">Elan</q0:sayHello>";
-		final String request = "<q0:sayHello xmlns:q0=\"http://service.ws.sample/\"><myname>Elan</myname></q0:sayHello>";
-				
+    @Test
+    public void testHelloRequest() {
+        // final String request =
+        // "<q0:sayHello xmlns:q0=\"http://service.ws.sample\">Elan</q0:sayHello>";
+        String request = "<q0:sayHello xmlns:q0=\"http://service.ws.sample/\"><myname>Elan</myname></q0:sayHello>";
 
-		StreamSource source = new StreamSource(new StringReader(request));
-		StreamResult result = new StreamResult(System.out);
+        StreamSource source = new StreamSource(new StringReader(request));
+        StreamResult result = new StreamResult(System.out);
 
-		this.webServiceTemplate.sendSourceAndReceiveToResult(source, result);
-		assertThat(this.output.toString(), containsString("<?xml version=\"1.0\" encoding=\"UTF-8\"?><ns2:sayHelloResponse xmlns:ns2=\"http://service.ws.sample/\"><return>Hello, Welcome to CXF Spring boot Elan!!!</return></ns2:sayHelloResponse>"));
-	}
+        this.webServiceTemplate.sendSourceAndReceiveToResult(source, result);
+        assertThat(this.output.toString(),
+                   containsString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                                  + "<ns2:sayHelloResponse xmlns:ns2=\"http://service.ws.sample/\">"
+                                  + "<return>Hello, Welcome to CXF Spring boot Elan!!!</return>"
+                                  + "</ns2:sayHelloResponse>"));
+    }
 
 }
