7988c4a974118cc5c2a8bd956fe396a7d5e8bb5c
Akitoshi Yoshida <ay@apache.org>
2015-07-03 12:15:10 +0200

    [CXF-6476] Introduce Swagger 2.0 API to the swagger feature

---
+++ rt/rs/description/src/main/java/org/apache/cxf/jaxrs/swagger/AbstractSwaggerFeature.java
@@ -0,0 +1,128 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.jaxrs.swagger;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.feature.AbstractFeature;
+import org.apache.cxf.jaxrs.JAXRSServiceFactoryBean;
+import org.apache.cxf.jaxrs.model.AbstractResourceInfo;
+
+abstract class AbstractSwaggerFeature extends AbstractFeature {
+    protected boolean scan = true;
+    protected boolean runAsFilter;
+    private String resourcePackage;
+    private String version = "1.0.0";
+    private String basePath;
+    private String title = "Sample REST Application";
+    private String description = "The Application";
+    private String contact = "committer@apache.org";
+    private String license = "Apache 2.0 License";
+    private String licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.html";
+    
+    @Override
+    public void initialize(Server server, Bus bus) {
+        calculateDefaultResourcePackage(server);
+        calculateDefaultBasePath(server);
+        addSwaggerResource(server);
+        
+        initializeProvider(server.getEndpoint(), bus);
+    }
+
+    protected abstract void addSwaggerResource(Server server);
+
+    private void calculateDefaultResourcePackage(Server server) {
+        JAXRSServiceFactoryBean serviceFactoryBean = 
+            (JAXRSServiceFactoryBean)server.getEndpoint().get(JAXRSServiceFactoryBean.class.getName());
+        AbstractResourceInfo resourceInfo = serviceFactoryBean.getClassResourceInfo().get(0);
+        
+        if ((resourceInfo != null) 
+            && (getResourcePackage() == null || getResourcePackage().length() == 0)) {
+            setResourcePackage(resourceInfo.getServiceClass().getPackage().getName());
+        }
+    }
+    
+    private void calculateDefaultBasePath(Server server) {
+        if (getBasePath() == null || getBasePath().length() == 0) {
+            String address = server.getEndpoint().getEndpointInfo().getAddress();
+            setBasePath(address);
+        }
+    }
+    public String getResourcePackage() {
+        return resourcePackage;
+    }
+    public void setResourcePackage(String resourcePackage) {
+        this.resourcePackage = resourcePackage;
+    }
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    public String getBasePath() {
+        return basePath;
+    }
+    public void setBasePath(String basePath) {
+        this.basePath = basePath;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getContact() {
+        return contact;
+    }
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+    public String getLicense() {
+        return license;
+    }
+    public void setLicense(String license) {
+        this.license = license;
+    }
+    public String getLicenseUrl() {
+        return licenseUrl;
+    }
+    public void setLicenseUrl(String licenseUrl) {
+        this.licenseUrl = licenseUrl;
+    }
+    public boolean isScan() {
+        return scan;
+    }
+    public void setScan(boolean scan) {
+        this.scan = scan;
+    }
+
+    public boolean isRunAsFilter() {
+        return runAsFilter;
+    }
+    public void setRunAsFilter(boolean runAsFilter) {
+        this.runAsFilter = runAsFilter;
+    }
+}
