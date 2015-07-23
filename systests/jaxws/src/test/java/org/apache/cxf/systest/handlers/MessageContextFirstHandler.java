84c9e98aae2663e51ebe34f47746bdd939c7bf9e
Jim Ma <ema@apache.org>
2015-07-23 13:43:03 +0800

    [CXF-6506]:Add missing test classes

---
+++ systests/jaxws/src/test/java/org/apache/cxf/systest/handlers/MessageContextFirstHandler.java
@@ -0,0 +1,65 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.systest.handlers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+public class MessageContextFirstHandler implements SOAPHandler<SOAPMessageContext> {
+
+    @Override
+    public boolean handleMessage(SOAPMessageContext context) {
+        boolean isOutbound = (boolean)context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+        if (isOutbound) {
+            @SuppressWarnings("unchecked")
+            Map<String, List<String>> headerMap = (Map<String, List<String>>)context
+                .get(MessageContext.HTTP_REQUEST_HEADERS);
+            if (headerMap == null) {
+                headerMap = new HashMap<>();
+            }
+            // Add custom header.
+            headerMap.put("MY_HEADER", Arrays.asList("FIRST_VALUE"));
+            context.put(MessageContext.HTTP_REQUEST_HEADERS, headerMap);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean handleFault(SOAPMessageContext context) {
+        return true;
+    }
+
+    @Override
+    public void close(MessageContext context) {
+    }
+
+    @Override
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+}
