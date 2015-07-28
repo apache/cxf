dc0857bb19d1df53cdd9d0474d86b0574a216ed2
Sergey Beryozkin <sberyozkin@talend.com>
2015-07-28 10:23:31 +0300

    Logging a stack trace if a RS SAML validator fails to process the assertion

--- rt/rs/security/xml/src/main/java/org/apache/cxf/rs/security/saml/AbstractSamlInHandler.java
+++ rt/rs/security/xml/src/main/java/org/apache/cxf/rs/security/saml/AbstractSamlInHandler.java
@@ -238,7 +238,7 @@ public abstract class AbstractSamlInHandler implements ContainerRequestFilter {
     protected void throwFault(String error, Exception ex) {
         // TODO: get bundle resource message once this filter is moved 
         // to rt/rs/security
-        LOG.warning(error);
+        LOG.warning(error + ": " + ExceptionUtils.getStackTrace(ex));
         Response response = JAXRSUtils.toResponseBuilder(401).entity(error).build();
         throw ExceptionUtils.toNotAuthorizedException(null, response);
     }
