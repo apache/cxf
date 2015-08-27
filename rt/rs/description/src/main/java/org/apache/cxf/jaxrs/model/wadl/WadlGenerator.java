269dd5f9504087dc4a0fe243feae6f46cccc7ea5
Sergey Beryozkin <sberyozkin@gmail.com>
2015-08-27 15:32:46 +0100

    [CXF-6566] Adding a boolean convertResourcesToDOM property enabled by default

--- rt/rs/description/src/main/java/org/apache/cxf/jaxrs/model/wadl/WadlGenerator.java
+++ rt/rs/description/src/main/java/org/apache/cxf/jaxrs/model/wadl/WadlGenerator.java
@@ -150,11 +150,10 @@ public class WadlGenerator implements ContainerRequestFilter {
     }
     
     private String wadlNamespace;
-    private boolean ignoreMessageWriters = true;
+    
     private boolean singleResourceMultipleMethods = true;
     private boolean useSingleSlashResource;
     private boolean ignoreForwardSlash;
-    private boolean ignoreRequests;
     private boolean linkAnyMediaTypeToXmlSchema;
     private boolean useJaxbContextForQnames = true;
     private boolean supportCollections = true;
@@ -164,6 +163,11 @@ public class WadlGenerator implements ContainerRequestFilter {
     private boolean checkAbsolutePathSlash;
     private boolean keepRelativeDocLinks;
     private boolean usePathParamsToCompareOperations = true;
+    
+    
+    private boolean ignoreMessageWriters = true;
+    private boolean ignoreRequests;
+    private boolean convertResourcesToDOM = true;
     
     private List<String> externalSchemasCache;
     private List<URI> externalSchemaLinks;
@@ -182,6 +186,8 @@ public class WadlGenerator implements ContainerRequestFilter {
     private Bus bus;
     private List<DocumentationProvider> docProviders = new LinkedList<DocumentationProvider>();
     private ResourceIdGenerator idGenerator;     
+    
+    
     
     public WadlGenerator() {
     }
@@ -1146,7 +1152,7 @@ public class WadlGenerator implements ContainerRequestFilter {
                 try {
                     InputStream is = ResourceUtils.getResourceStream(loc, (Bus)ep.get(Bus.class.getName()));
                     if (is != null) {
-                        if (isJson(mt)) {
+                        if (!convertResourcesToDOM || isJson(mt)) {
                             return Response.ok(is, mt).build();
                         }
                         Document wadlDoc = StaxUtils.read(is);
@@ -2159,6 +2165,10 @@ public class WadlGenerator implements ContainerRequestFilter {
 
     public void setUsePathParamsToCompareOperations(boolean usePathParamsToCompareOperations) {
         this.usePathParamsToCompareOperations = usePathParamsToCompareOperations;
+    }
+
+    public void setConvertResourcesToDOM(boolean convertResourcesToDOM) {
+        this.convertResourcesToDOM = convertResourcesToDOM;
     }
 
     private static class SchemaConverter extends DelegatingXMLStreamWriter {
