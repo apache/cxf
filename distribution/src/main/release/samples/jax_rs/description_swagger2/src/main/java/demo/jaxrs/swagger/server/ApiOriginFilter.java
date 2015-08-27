1df8753ff461d263ea56835d91e42f17bb6a3a5f
Akitoshi Yoshida <ay@apache.org>
2015-08-27 10:20:38 +0200

    [CXF-6557] add a comment text about cxf's CORS filter in the swagger2 demos

--- distribution/src/main/release/samples/jax_rs/description_swagger2/src/main/java/demo/jaxrs/swagger/server/ApiOriginFilter.java
+++ distribution/src/main/release/samples/jax_rs/description_swagger2/src/main/java/demo/jaxrs/swagger/server/ApiOriginFilter.java
@@ -26,6 +26,12 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
 
+/**
+ * This is a simple optional CORS filter used for this demo to make the resources accessible
+ * from other origins. You may omit using this filter or use CXF's advanced CORS filter 
+ * org.apache.cxf.rs.security.cors.CrossOriginResourceSharingFilter  
+ * included in cxf-rt-rs-security-cors if you need a more comprehensive accessibility rules.
+ */
 public class ApiOriginFilter implements ContainerResponseFilter {
 
     @Override
