88d998449b0485eef5de60d9f63a6e9235652378
Colm O hEigeartaigh <coheigea@apache.org>
2015-08-28 15:55:51 +0100

    NPE fix when exception is null

--- rt/rs/security/xml/src/main/java/org/apache/cxf/rs/security/saml/AbstractSamlInHandler.java
+++ rt/rs/security/xml/src/main/java/org/apache/cxf/rs/security/saml/AbstractSamlInHandler.java
@@ -197,7 +197,11 @@ public abstract class AbstractSamlInHandler implements RequestHandler {
     protected void throwFault(String error, Exception ex) {
         // TODO: get bundle resource message once this filter is moved 
         // to rt/rs/security
-        LOG.warning(error + ": " + ExceptionUtils.getStackTrace(ex));
+        String errorMsg = error;
+        if (ex != null) {
+            errorMsg += ": " + ExceptionUtils.getStackTrace(ex);
+        }
+        LOG.warning(errorMsg);
         Response response = JAXRSUtils.toResponseBuilder(401).entity(error).build();
         throw ExceptionUtils.toNotAuthorizedException(null, response);
     }
