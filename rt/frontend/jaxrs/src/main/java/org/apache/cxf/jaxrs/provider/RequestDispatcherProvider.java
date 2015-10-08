7e8b16bf0bd305a2139f2f37d77111e1b961572d
Sergey Beryozkin <sberyozkin@gmail.com>
2015-10-08 10:17:10 +0100

    [CXF-6630] Fixing a typo with thanks to Jan Vondrouš

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/RequestDispatcherProvider.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/provider/RequestDispatcherProvider.java
@@ -349,7 +349,7 @@ public class RequestDispatcherProvider extends AbstractConfigurableProvider
         }
         String name = beanNames.get(bean.getClass().getName());
         if (name != null) {
-            return null;
+            return name;
         } 
         Class<?> resourceClass = bean.getClass();
         if (useClassNames && doGetClassResourceName(resourceClass) == null) {
