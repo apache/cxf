4f42074c5f4482c45670a7e727a3076c1dcae06d
Sergey Beryozkin <sberyozkin@gmail.com>
2015-10-09 16:34:47 +0100

    [CXF-6637] Making @GZIP/etc annotations @Inherited

--- api/src/main/java/org/apache/cxf/annotations/FastInfoset.java
+++ api/src/main/java/org/apache/cxf/annotations/FastInfoset.java
@@ -21,6 +21,7 @@ package org.apache.cxf.annotations;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -31,6 +32,7 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
+@Inherited
 public @interface FastInfoset {
     /**
      * Set to true in order for FastInfoset to be always used without negotiation
