934012437896e35955290c0c3de43f94b2a651c3
Sergey Beryozkin <sberyozkin@gmail.com>
2015-11-02 13:21:33 +0000

    Minor updates to AccessTokenValidatorService

--- rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/services/AccessTokenValidatorService.java
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/services/AccessTokenValidatorService.java
@@ -18,6 +18,8 @@
  */
 package org.apache.cxf.rs.security.oauth2.services;
 
+import java.util.logging.Logger;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Encoded;
 import javax.ws.rs.POST;
@@ -25,22 +27,51 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.SecurityContext;
 
+import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.rs.security.oauth2.common.AccessTokenValidation;
 import org.apache.cxf.rs.security.oauth2.utils.AuthorizationUtils;
 import org.apache.cxf.rs.security.oauth2.utils.OAuthConstants;
 
 @Path("validate")
 public class AccessTokenValidatorService extends AbstractAccessTokenValidator {
+    private static final Logger LOG = LogUtils.getL7dLogger(AccessTokenValidatorService.class);
+    private boolean blockUnsecureRequests;
+    private boolean blockUnauthorizedRequests = true;
     @POST
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public AccessTokenValidation getTokenValidationInfo(@Encoded MultivaluedMap<String, String> params) {
-        if (getMessageContext().getSecurityContext().getUserPrincipal() == null) {
-            AuthorizationUtils.throwAuthorizationFailure(supportedSchemes, realm);
-        }
+        checkSecurityContext();
         String authScheme = params.getFirst(OAuthConstants.AUTHORIZATION_SCHEME_TYPE);
         String authSchemeData  = params.getFirst(OAuthConstants.AUTHORIZATION_SCHEME_DATA);
         return super.getAccessTokenValidation(authScheme, authSchemeData, params);
+    }
+
+    private void checkSecurityContext() {
+        SecurityContext sc = getMessageContext().getSecurityContext();
+        if (!sc.isSecure() && blockUnsecureRequests) {
+            LOG.warning("Unsecure HTTP, Transport Layer Security is recommended");
+            AuthorizationUtils.throwAuthorizationFailure(supportedSchemes, realm);
+        }
+        if (sc.getUserPrincipal() == null && blockUnauthorizedRequests) {
+            //TODO: check client certificates
+            LOG.warning("Authenticated Principal is not available");
+            AuthorizationUtils.throwAuthorizationFailure(supportedSchemes, realm);
+        }
+        
+    }
+
+    public void setBlockUnsecureRequests(boolean blockUnsecureRequests) {
+        this.blockUnsecureRequests = blockUnsecureRequests;
+    }
+
+    public boolean isBlockUnauthorizedRequests() {
+        return blockUnauthorizedRequests;
+    }
+
+    public void setBlockUnauthorizedRequests(boolean blockUnauthorizedRequests) {
+        this.blockUnauthorizedRequests = blockUnauthorizedRequests;
     }
 }
