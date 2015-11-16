d2ed88ccb247d4105de34a5b675c0fc198795209
Sergey Beryozkin <sberyozkin@gmail.com>
2015-11-16 13:40:04 +0000

    Update to the redirect_uri validation code

--- rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/services/RedirectionBasedGrantService.java
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/services/RedirectionBasedGrantService.java
@@ -363,8 +363,8 @@ public abstract class RedirectionBasedGrantService extends AbstractOAuthService 
         
         List<String> uris = client.getRedirectUris();
         if (redirectUri != null) {
-            if (!uris.contains(redirectUri)) {
-                redirectUri = null;
+            if (!uris.isEmpty() && !uris.contains(redirectUri)) {
+                reportInvalidRequestError("Client Redirect Uri is invalid");
             } 
         } else if (uris.size() == 1 && useRegisteredRedirectUriIfPossible) {
             redirectUri = uris.get(0);
