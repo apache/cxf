bdd6b1b1bbfc039c11c0ca7891cf520cce614226
Akitoshi Yoshida <ay@apache.org>
2016-01-07 21:26:22 +0900

    [CXF-6720] AbstractHTTPDestination#WrappedOutputStream.close() calls flush after close

--- rt/transports/http/src/main/java/org/apache/cxf/transport/http/AbstractHTTPDestination.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/AbstractHTTPDestination.java
@@ -802,8 +802,8 @@ public abstract class AbstractHTTPDestination
                 }
             }
             if (wrappedStream != null) {
+                // closing the stream should indirectly call the servlet response's flushBuffer
                 wrappedStream.close();
-                response.flushBuffer();
             }
             /*
             try {
