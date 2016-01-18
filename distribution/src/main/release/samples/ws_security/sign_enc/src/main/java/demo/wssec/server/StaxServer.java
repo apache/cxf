6e7b79f6a8b6523b2248937275d92918e5682aa2
Colm O hEigeartaigh <coheigea@apache.org>
2016-01-18 11:55:02 +0000

    Fixing distribution

--- distribution/src/main/release/samples/ws_security/sign_enc/src/main/java/demo/wssec/server/StaxServer.java
+++ distribution/src/main/release/samples/ws_security/sign_enc/src/main/java/demo/wssec/server/StaxServer.java
@@ -88,7 +88,7 @@ public class StaxServer {
         
         properties.setSignatureCryptoProperties(decCryptoProperties);
         properties.setSignatureKeyIdentifier(
-            WSSecurityTokenConstants.KeyIdentifier_SecurityTokenDirectReference
+            WSSecurityTokenConstants.KEYIDENTIFIER_SECURITY_TOKEN_DIRECT_REFERENCE
         );
         properties.setSignatureAlgorithm("http://www.w3.org/2000/09/xmldsig#rsa-sha1");
         properties.addSignaturePart(
