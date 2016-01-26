2ba96091771fd8a15852b39373a22b4973d26ca1
Sergey Beryozkin <sberyozkin@gmail.com>
2016-01-26 16:01:55 +0000

    Fixing 3.1.x build failure

--- systests/rs-security/src/test/java/org/apache/cxf/systest/jaxrs/security/oauth2/OAuthDataProviderImpl.java
+++ systests/rs-security/src/test/java/org/apache/cxf/systest/jaxrs/security/oauth2/OAuthDataProviderImpl.java
@@ -120,13 +120,13 @@ public class OAuthDataProviderImpl implements OAuthDataProvider {
     }
 
     @Override
-    public List<ServerAccessToken> getAccessTokens(Client client) throws OAuthServiceException {
+    public List<ServerAccessToken> getAccessTokens(Client client, UserSubject sub) throws OAuthServiceException {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public List<RefreshToken> getRefreshTokens(Client client) throws OAuthServiceException {
+    public List<RefreshToken> getRefreshTokens(Client client, UserSubject sub) throws OAuthServiceException {
         // TODO Auto-generated method stub
         return null;
     }
