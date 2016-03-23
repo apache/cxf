ceff09ecbd3639f21574fe60662ec17af04a867a
Sergey Beryozkin <sberyozkin@gmail.com>
2016-03-23 16:34:59 +0300

    [CXF-6840] Updating SchemaHandler to correctly resolve with a null publicId, patch from Vjacheslav Borisov applied

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/utils/schemas/SchemaHandler.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/utils/schemas/SchemaHandler.java
@@ -129,7 +129,8 @@ public class SchemaHandler {
                                         resolvedLocation = catalogResolver.resolveURI(namespaceURI);
                                     }
                                     if (resolvedLocation == null) {
-                                        resolvedLocation = catalogResolver.resolvePublic(publicId, systemId);
+                                        resolvedLocation = catalogResolver.resolvePublic(
+                                            publicId != null ? publicId : namespaceURI, systemId);
                                     } 
                                     if (resolvedLocation != null) {
                                         InputStream resourceStream = 
