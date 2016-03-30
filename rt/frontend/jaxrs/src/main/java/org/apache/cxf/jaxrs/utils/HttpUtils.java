fd33546e44aa148f77b88094d8dfafdbe3640f9c
Jim Ma <ema@apache.org>
2016-03-30 20:57:24 +0800

    [CXF-6853]:Support encoded value in @ApplicationPath

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/utils/HttpUtils.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/utils/HttpUtils.java
@@ -22,6 +22,7 @@ package org.apache.cxf.jaxrs.utils;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -498,6 +499,15 @@ public final class HttpUtils {
     public static String getPathToMatch(String path, String address, boolean addSlash) {
         
         int ind = path.indexOf(address);
+        try {
+            String decodeAddr = URLDecoder.decode(address, "UTF-8");
+            if (ind == -1 && !address.equals(decodeAddr)) {
+                address = decodeAddr;
+                ind = path.indexOf(address);
+            }
+        } catch (UnsupportedEncodingException e) {
+            //
+        }
         if (ind == -1 && address.equals(path + "/")) {
             path += "/";
             ind = 0;
