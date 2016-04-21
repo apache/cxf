55e9924dd0694f09ef8b3343b566699dfd2f266f
Colm O hEigeartaigh <coheigea@apache.org>
2016-04-21 17:04:44 +0100

    NPE fix

--- rt/rs/security/sso/oidc/src/main/java/org/apache/cxf/rs/security/oidc/idp/OidcHybridService.java
+++ rt/rs/security/sso/oidc/src/main/java/org/apache/cxf/rs/security/oidc/idp/OidcHybridService.java
@@ -74,7 +74,7 @@ public class OidcHybridService extends OidcImplicitService {
         StringBuilder sb = super.prepareGrant(state, client, requestedScope, 
                                                           approvedScope, userSubject, preAuthorizedToken);
    
-        if (state.getResponseType().startsWith(OAuthConstants.CODE_RESPONSE_TYPE)) {
+        if (state.getResponseType() != null && state.getResponseType().startsWith(OAuthConstants.CODE_RESPONSE_TYPE)) {
             String code = codeService.getGrantCode(state, client, requestedScope,
                                                    approvedScope, userSubject, preAuthorizedToken);
             
