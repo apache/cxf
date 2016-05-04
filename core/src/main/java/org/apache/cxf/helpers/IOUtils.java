f6fd463d3a367aa84e4e9235d72d6ffa88a49413
Sergey Beryozkin <sberyozkin@gmail.com>
2016-05-04 14:18:46 +0100

    [CXF-6891] Avoiding wrapping InputStream into PushBackInputStream in IOUtils.isEmpty

--- core/src/main/java/org/apache/cxf/helpers/IOUtils.java
+++ core/src/main/java/org/apache/cxf/helpers/IOUtils.java
@@ -48,6 +48,15 @@ public final class IOUtils {
         if (is == null) {
             return true;
         }
+        try {
+            // if available is 0 it does not mean it is empty; it can also throw IOException
+            if (is.available() > 0) {
+                return false;
+            }
+        } catch (IOException ex) {
+            // ignore
+        }
+        
         final byte[] bytes = new byte[1];
         try {
             if (is.markSupported()) {
@@ -58,17 +67,13 @@ public final class IOUtils {
                     is.reset();
                 }
             }
-            // if available is 0 it does not mean it is empty; it can also throw IOException
-            if (is.available() > 0) {
-                return false;
-            }
         } catch (IOException ex) {
             // ignore
         }
-        // it may be an attachment stream
-        @SuppressWarnings("resource")
-        PushbackInputStream pbStream = 
-            is instanceof PushbackInputStream ? (PushbackInputStream)is : new PushbackInputStream(is);
+        if (!(is instanceof PushbackInputStream)) {
+            return false;
+        }
+        PushbackInputStream pbStream = (PushbackInputStream)is;
         boolean isEmpty = isEof(pbStream.read(bytes));
         if (!isEmpty) {
             pbStream.unread(bytes);
