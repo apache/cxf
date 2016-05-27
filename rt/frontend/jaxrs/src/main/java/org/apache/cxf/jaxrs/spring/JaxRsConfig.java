e36fd658456bfa1221b2365c9f7b30faca19d55d
Vedran Pavic <vedran.pavic@gmail.com>
2016-05-28 01:26:33 +0200

    [CXF-6869] Polish JAX-RS Spring components

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/spring/JaxRsConfig.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/spring/JaxRsConfig.java
@@ -18,10 +18,8 @@
  */
 package org.apache.cxf.jaxrs.spring;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-@Configuration
 @ImportResource({"classpath:META-INF/cxf/cxf.xml" })
 public class JaxRsConfig {
 }
