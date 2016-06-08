553be8f2afd1c54158f1fa2eec20c8b8571f8aef
Sergey Beryozkin <sberyozkin@gmail.com>
2016-06-08 17:01:52 +0100

    Fixing jaxrs spring boot scan demo

--- distribution/src/main/release/samples/jax_rs/jaxrs_spring_boot_scan/src/main/java/sample/rs/service/SampleScanRestApplication.java
+++ distribution/src/main/release/samples/jax_rs/jaxrs_spring_boot_scan/src/main/java/sample/rs/service/SampleScanRestApplication.java
@@ -22,7 +22,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class SampleScanRestApplication {
