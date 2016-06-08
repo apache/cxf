fb48f17d13fa46f8433dc5af367fe6dca16f977c
Sergey Beryozkin <sberyozkin@gmail.com>
2016-06-08 17:01:52 +0100

    Fixing jaxrs spring boot scan demo

--- distribution/src/main/release/samples/jax_rs/jaxrs_spring_boot_scan/src/main/java/sample/rs/client/SampleRestClientApplication.java
+++ distribution/src/main/release/samples/jax_rs/jaxrs_spring_boot_scan/src/main/java/sample/rs/client/SampleRestClientApplication.java
@@ -20,7 +20,7 @@ package sample.rs.client;
 
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 
-import sample.rs.service.hello1.HelloService;
+import sample.rs.service.hello1.HelloServiceImpl1;
 
 
 public final class SampleRestClientApplication {
@@ -28,8 +28,8 @@ public final class SampleRestClientApplication {
         
     }
     public static void main(String[] args) {
-        HelloService service = JAXRSClientFactory.create("http://localhost:8080/services/helloservice/", 
-                                  HelloService.class);
+        HelloServiceImpl1 service = JAXRSClientFactory.create("http://localhost:8080/services/helloservice/", 
+                                    HelloServiceImpl1.class);
         System.out.println(service.sayHello("ApacheCxfUser"));
     }  
 }
