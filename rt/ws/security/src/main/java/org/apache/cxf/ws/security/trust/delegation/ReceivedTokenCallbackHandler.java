e4ed06328ac14529170f69c1834c26a8e2a802b0
Colm O hEigeartaigh <coheigea@apache.org>
2016-06-21 11:17:46 +0100

    CXF-6949 - Add support to the ReceivedTokenCalbackHandler to return a transformed token

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/trust/delegation/ReceivedTokenCallbackHandler.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/trust/delegation/ReceivedTokenCallbackHandler.java
@@ -47,6 +47,8 @@ import org.apache.wss4j.dom.message.token.UsernameToken;
  */
 public class ReceivedTokenCallbackHandler implements CallbackHandler {
     
+    private boolean useTransformedToken = true;
+    
     @SuppressWarnings("unchecked")
     public void handle(Callback[] callbacks)
         throws IOException, UnsupportedCallbackException {
@@ -93,6 +95,13 @@ public class ReceivedTokenCallbackHandler implements CallbackHandler {
         List<WSSecurityEngineResult> wsSecEngineResults
     ) {
         for (WSSecurityEngineResult wser : wsSecEngineResults) {
+            // First check for a transformed token
+            Object transformedToken = wser.get(WSSecurityEngineResult.TAG_TRANSFORMED_TOKEN);
+            if (useTransformedToken && transformedToken instanceof SamlAssertionWrapper) {
+                return ((SamlAssertionWrapper)transformedToken).getElement();
+            }
+            
+            // Otherwise check the actions
             Integer actInt = (Integer)wser.get(WSSecurityEngineResult.TAG_ACTION);
             if (actInt.intValue() == WSConstants.ST_SIGNED
                 || actInt.intValue() == WSConstants.ST_UNSIGNED) {
@@ -111,6 +120,19 @@ public class ReceivedTokenCallbackHandler implements CallbackHandler {
             }
         }
         return null;
+    }
+
+    public boolean isUseTransformedToken() {
+        return useTransformedToken;
+    }
+
+    /**
+     * Set whether to use the transformed token if it is available from a previous security result.
+     * It false, it uses the original "received" token instead. The default is "true".
+     * @param useTransformedToken whether to use the transformed token if it is available
+     */
+    public void setUseTransformedToken(boolean useTransformedToken) {
+        this.useTransformedToken = useTransformedToken;
     }
     
 }
