97eb4f3e2db0678af3c2ae6cac999ce821574b10
Daniel Kulp <dkulp@apache.org>
2016-07-25 16:22:51 -0400

    FIx checkstyle issues
    # Conflicts:
    #	distribution/src/main/release/samples/jax_rs/spring_boot_scan/eureka-registry/src/main/java/registry/eureka/RegistryApplication.java

--- distribution/src/main/release/samples/jax_rs/spring_boot_scan/application/src/main/java/sample/rs/service/SampleRestApplication.java
+++ distribution/src/main/release/samples/jax_rs/spring_boot_scan/application/src/main/java/sample/rs/service/SampleRestApplication.java
@@ -60,4 +60,4 @@ public class SampleRestApplication {
     }
     
     
- }
+}
--- distribution/src/main/release/samples/jax_rs/spring_boot_scan/eureka-registry/src/main/java/registry/eureka/RegistryApplication.java
+++ distribution/src/main/release/samples/jax_rs/spring_boot_scan/eureka-registry/src/main/java/registry/eureka/RegistryApplication.java
@@ -6,7 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistryApplication {
+public final class RegistryApplication {
+    private RegistryApplication() {
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
     }
