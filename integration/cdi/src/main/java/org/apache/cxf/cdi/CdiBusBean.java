e0e2102796e65715819995896ea3be9faa9454db
John D. Ament <johndament@apache.org>
2016-08-02 20:34:08 -0400

    [CXF-6986] If no application classes found, install a default application.

--- integration/cdi/src/main/java/org/apache/cxf/cdi/CdiBusBean.java
+++ integration/cdi/src/main/java/org/apache/cxf/cdi/CdiBusBean.java
@@ -26,12 +26,9 @@ import java.util.Set;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.spi.CreationalContext;
-import javax.enterprise.inject.Any;
-import javax.enterprise.inject.Default;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.enterprise.inject.spi.InjectionTarget;
-import javax.enterprise.util.AnnotationLiteral;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.CXFBusFactory;
@@ -61,13 +58,9 @@ public final class CdiBusBean implements Bean< ExtensionManagerBus > {
         return CXF;
     }
 
-    @SuppressWarnings("serial")
     @Override
     public Set< Annotation > getQualifiers() {
-        Set<Annotation> qualifiers = new HashSet<Annotation>();
-        qualifiers.add(new AnnotationLiteral< Default >() { });
-        qualifiers.add(new AnnotationLiteral< Any >() { });
-        return qualifiers;
+        return CdiUtil.getQualifiers();
     }
 
     @Override
