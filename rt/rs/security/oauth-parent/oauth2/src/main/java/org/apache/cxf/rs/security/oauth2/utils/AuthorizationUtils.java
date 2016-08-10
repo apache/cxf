3c19fac24be0d39f67f7d62d5bf133e28fe90a33
Freeman Fang <freeman.fang@gmail.com>
2016-08-10 15:28:00 +0800

    [CXF-7006]add an util method in AuthorizationUtils which can take AuthorizationFailure cause
    (cherry picked from commit 395acbd2f16b03314f34ee0b6a212b708b375873)

--- rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/utils/AuthorizationUtils.java
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/utils/AuthorizationUtils.java
@@ -91,6 +91,10 @@ public final class AuthorizationUtils {
     }
     
     public static void throwAuthorizationFailure(Set<String> challenges, String realm) {
+        throwAuthorizationFailure(challenges, realm, null);
+    }
+
+    public static void throwAuthorizationFailure(Set<String> challenges, String realm, Throwable cause) {
         ResponseBuilder rb = JAXRSUtils.toResponseBuilder(401);
         
         StringBuilder sb = new StringBuilder();
@@ -109,8 +113,12 @@ public final class AuthorizationUtils {
             }
             rb.header(HttpHeaders.WWW_AUTHENTICATE, sb.toString());
         }
-        Response r = rb.build();
-        throw ExceptionUtils.toNotAuthorizedException(null, r);
+        Response r = null;
+        if (cause != null) {
+            r = rb.entity(cause.getMessage()).build(); 
+        } else {
+            r = rb.build();
+        }
+        throw ExceptionUtils.toNotAuthorizedException(cause, r);
     }
-
 }
