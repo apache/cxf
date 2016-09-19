042d1dbe5f236125c77db8348bf66d4a3d4d2385
Guillaume Nodet <gnodet@apache.org>
2016-09-19 14:19:26 +0200

    [CXF-7060] Provide Karaf 4 specific features and commands

---
+++ osgi/karaf4/commands/src/main/java/org/apache/cxf/karaf/commands/ListBussesCommand.java
@@ -0,0 +1,65 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.karaf.commands;
+
+import java.util.List;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.karaf.commands.internal.CXFController;
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
+/**
+ * 
+ */
+@Command(scope = "cxf", name = "list-busses", description = "Lists all CXF Busses.")
+@Service
+public class ListBussesCommand extends CXFController implements Action {
+    protected static final int DEFAULT_BUSID_LENGTH = 38;
+    protected String headerFormat = "%-40s %-20s";
+    protected String outputFormat = "[%-38s] [%-18s]";
+
+    @Override
+    public Object execute() throws Exception {
+        List<Bus> busses = getBusses();
+        renderFormat(busses);
+        System.out.println(String.format(headerFormat, "Name", "State"));
+
+        for (Bus bus : busses) {
+            String state = bus.getState().toString();
+            System.out.println(String.format(outputFormat, bus.getId(), state));
+        }
+        return null;
+    }
+
+    private void renderFormat(List<Bus> busses) {
+        int longestBusId = DEFAULT_BUSID_LENGTH;
+        for (Bus bus : busses) {
+            if (bus.getId().length() > longestBusId) {
+                longestBusId = bus.getId().length();
+            }
+        }
+        if (longestBusId > DEFAULT_BUSID_LENGTH) {
+            headerFormat = "%-" + (longestBusId + 2) + "s %-20s";
+            outputFormat = "[%-" + longestBusId + "s] [%-18s]";
+        }
+    }
+}
