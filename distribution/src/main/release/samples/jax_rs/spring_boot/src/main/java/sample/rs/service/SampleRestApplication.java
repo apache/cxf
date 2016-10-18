3510e3d345ba2186a228436d481c809e692d42a2
Sergey Beryozkin <sberyozkin@gmail.com>
2016-10-18 16:07:01 +0100

    Resoring the demo endpoint address

--- distribution/src/main/release/samples/jax_rs/spring_boot/src/main/java/sample/rs/service/SampleRestApplication.java
+++ distribution/src/main/release/samples/jax_rs/spring_boot/src/main/java/sample/rs/service/SampleRestApplication.java
@@ -45,7 +45,7 @@ public class SampleRestApplication {
         JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
         endpoint.setBus(bus);
         endpoint.setServiceBeans(Arrays.<Object>asList(new HelloServiceImpl1(), new HelloServiceImpl2()));
-        endpoint.setAddress("/a");
+        endpoint.setAddress("/");
         endpoint.setFeatures(Arrays.asList(new Swagger2Feature()));
         return endpoint.create();
     }
