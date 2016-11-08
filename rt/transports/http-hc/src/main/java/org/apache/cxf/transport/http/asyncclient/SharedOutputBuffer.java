c06cc6dcf2183eb9561a5d18af33b4563bf9a2fb
Freeman Fang <freeman.fang@gmail.com>
2016-11-08 15:56:18 +0800

    [CXF-7122]remove unnecessary Timer
    (cherry picked from commit caac861613b4df07eb3ff4023e1b9e53b07cd5ea)

--- rt/transports/http-hc/src/main/java/org/apache/cxf/transport/http/asyncclient/SharedOutputBuffer.java
+++ rt/transports/http-hc/src/main/java/org/apache/cxf/transport/http/asyncclient/SharedOutputBuffer.java
@@ -118,7 +118,6 @@ public class SharedOutputBuffer extends ExpandableBuffer {
 
     public int produceContent(final ContentEncoder encoder, final IOControl ioc) throws IOException {
         if (this.shutdown) {
-            encoder.complete();
             return -1;
         }
         this.lock.lock();
