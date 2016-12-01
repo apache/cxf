bdc67fbc286a3c8c84e3d4b044801ae6366e005d
Sergey Beryozkin <sberyozkin@gmail.com>
2016-12-01 13:56:14 +0000

    [CXF-6836] More SwaggerUtils work

--- rt/rs/description-swagger/src/test/java/org/apache/cxf/jaxrs/swagger/SwaggerUtilsTest.java
+++ rt/rs/description-swagger/src/test/java/org/apache/cxf/jaxrs/swagger/SwaggerUtilsTest.java
@@ -18,8 +18,11 @@
  */
 package org.apache.cxf.jaxrs.swagger;
 
+import java.util.Map;
+
 import org.apache.cxf.jaxrs.model.Parameter;
 import org.apache.cxf.jaxrs.model.ParameterType;
+import org.apache.cxf.jaxrs.model.UserApplication;
 import org.apache.cxf.jaxrs.model.UserOperation;
 import org.apache.cxf.jaxrs.model.UserResource;
 
@@ -29,46 +32,44 @@ import org.junit.Test;
 public class SwaggerUtilsTest extends Assert {
 
     @Test
-    public void testConvertSwagger12ToUserResource() {
-        UserResource ur = SwaggerUtils.getUserResource("/swagger12.json");
-        assertNotNull(ur);
-        assertEquals("/hello", ur.getPath());
-        assertEquals(1, ur.getOperations().size());
-        UserOperation op = ur.getOperations().get(0);
-        assertEquals("helloSubject", op.getName());
-        assertEquals("/{subject}", op.getPath());
-        assertEquals("GET", op.getVerb());
-        assertEquals(1, op.getParameters().size());
-        Parameter param = op.getParameters().get(0);
-        assertEquals("subject", param.getName());
-        assertEquals(ParameterType.PATH, param.getType());
-        assertEquals(String.class, param.getJavaType());
-    }
-    @Test
-    public void testConvertSwagger20ToUserResource() {
-        UserResource ur = SwaggerUtils.getUserResource("/swagger20.json");
-        assertNotNull(ur);
-        assertEquals("/base", ur.getPath());
-        assertEquals(1, ur.getOperations().size());
-        UserOperation op = ur.getOperations().get(0);
-        assertEquals("postOp", op.getName());
-        assertEquals("/somepath", op.getPath());
-        assertEquals("POST", op.getVerb());
-        assertEquals("application/x-www-form-urlencoded", op.getConsumes());
-        assertEquals("application/json", op.getProduces());
+    public void testConvertSwaggerToUserApp() {
+        UserApplication ap = SwaggerUtils.getUserApplication("/swagger20.json");
+        assertNotNull(ap);
+        assertEquals("/services/helloservice", ap.getBasePath());
+        Map<String, UserResource> map = ap.getResourcesAsMap();
+        assertEquals(2, map.size());
         
-        assertEquals(3, op.getParameters().size());
+        UserResource ur = map.get("sayHello");
+        assertNotNull(ur);
+        assertEquals("/sayHello", ur.getPath());
+        assertEquals(1, ur.getOperations().size());
+        UserOperation op = ur.getOperations().get(0);
+        assertEquals("sayHello", op.getName());
+        assertEquals("/{a}", op.getPath());
+        assertEquals("GET", op.getVerb());
+        assertEquals("text/plain", op.getProduces());
+        
+        assertEquals(1, op.getParameters().size());
         Parameter param1 = op.getParameters().get(0);
-        assertEquals("userName", param1.getName());
-        assertEquals(ParameterType.FORM, param1.getType());
+        assertEquals("a", param1.getName());
+        assertEquals(ParameterType.PATH, param1.getType());
         assertEquals(String.class, param1.getJavaType());
-        Parameter param2 = op.getParameters().get(1);
-        assertEquals("password", param2.getName());
-        assertEquals(ParameterType.FORM, param2.getType());
+        
+        UserResource ur2 = map.get("sayHello2");
+        assertNotNull(ur2);
+        assertEquals("/sayHello2", ur2.getPath());
+        assertEquals(1, ur2.getOperations().size());
+        UserOperation op2 = ur2.getOperations().get(0);
+        assertEquals("sayHello", op2.getName());
+        assertEquals("/{a}", op2.getPath());
+        assertEquals("GET", op2.getVerb());
+        assertEquals("text/plain", op2.getProduces());
+        
+        assertEquals(1, op2.getParameters().size());
+        Parameter param2 = op.getParameters().get(0);
+        assertEquals("a", param2.getName());
+        assertEquals(ParameterType.PATH, param2.getType());
         assertEquals(String.class, param2.getJavaType());
-        Parameter param3 = op.getParameters().get(2);
-        assertEquals("type", param3.getName());
-        assertEquals(ParameterType.MATRIX, param3.getType());
-        assertEquals(String.class, param3.getJavaType());
+        
     }
 }
