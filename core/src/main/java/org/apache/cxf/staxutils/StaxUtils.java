25e33a49cfd1df608b0d0d85b1e8e8369761ca0c
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2016-12-02 22:58:47 -0300

    [CXF-7162] Inconsistent reading of formatted xml when validating schema

--- core/src/main/java/org/apache/cxf/staxutils/StaxUtils.java
+++ core/src/main/java/org/apache/cxf/staxutils/StaxUtils.java
@@ -772,6 +772,7 @@ public final class StaxUtils {
                 }
                 break;
             case XMLStreamConstants.CHARACTERS:
+            case XMLStreamConstants.SPACE:
                 String s = reader.getText();
                 if (s != null) {
                     writer.writeCharacters(s);
