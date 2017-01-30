f8ee9192936110ae2daa419a52ac3818f46e00ca
Sergey Beryozkin <sberyozkin@gmail.com>
2017-01-30 10:10:04 +0000

    CXF-7236: Fix ArrayIndexOutOfBound when Accept header is not set, patch from Jeremy Kuhn applied, This closes #230

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/HttpHeadersImpl.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/HttpHeadersImpl.java
@@ -315,7 +315,7 @@ public class HttpHeadersImpl implements HttpHeaders {
 
     public Date getDate() {
         List<String> values = headers.get(HttpHeaders.DATE);
-        if (values == null || StringUtils.isEmpty(values.get(0))) {
+        if (values == null || values.isEmpty() || StringUtils.isEmpty(values.get(0))) {
             return null;
         }
         return HttpUtils.getHttpDate(values.get(0));
