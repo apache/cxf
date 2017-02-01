3bebf6b49283d9a9c36365c030533a3508f836c7
reta <drreta@gmail.com>
2017-01-31 21:43:00 -0500

    Enhanced CDI Extension to support JAX-RS Features (registered as providers) along with Apache CXF Features

--- integration/cdi/src/main/java/org/apache/cxf/cdi/JAXRSCdiResourceExtension.java
+++ integration/cdi/src/main/java/org/apache/cxf/cdi/JAXRSCdiResourceExtension.java
@@ -107,6 +107,8 @@ public class JAXRSCdiResourceExtension implements Extension {
             serviceBeans.add(event.getBean());
         } else if (event.getAnnotated().isAnnotationPresent(Provider.class)) {
             providerBeans.add(event.getBean());
+        } else if (event.getBean().getTypes().contains(javax.ws.rs.core.Feature.class)) {
+            providerBeans.add((Bean< ? extends Feature >)event.getBean());
         } else if (event.getBean().getTypes().contains(Feature.class)) {
             featureBeans.add((Bean< ? extends Feature >)event.getBean());
         } else if (CdiBusBean.CXF.equals(event.getBean().getName())
