9ab47ba086ff149adfa7fe4b55759646e99c5719
Sergey Beryozkin <sberyozkin@gmail.com>
2017-02-28 11:31:43 +0000

    Reverting LogMessageFormatter starting from the next line, but also dropped a 'Type' prefix to get a possibly more optimal output, can be put back easily

--- rt/features/logging/src/main/java/org/apache/cxf/ext/logging/event/LogMessageFormatter.java
+++ rt/features/logging/src/main/java/org/apache/cxf/ext/logging/event/LogMessageFormatter.java
@@ -34,9 +34,8 @@ public final class LogMessageFormatter {
     public static String format(LogEvent event) {
         StringBuilder b = new StringBuilder();
         // Start from the next line to have the output well-aligned
-        b.append('\n');
+        b.append(event.getType()).append('\n');
         
-        write(b, "Type", event.getType().toString());
         write(b, "Address", event.getAddress());
         write(b, "HttpMethod", event.getHttpMethod());
         write(b, "Content-Type", event.getContentType());
