3c0aa10207747c8e6c5524ff1aa1b0c224f0b990
Colm O hEigeartaigh <coheigea@apache.org>
2017-03-24 09:56:11 +0000

    CXF-4692 - Allow customization of Request Security Token Response
    # Conflicts:
    #	services/sts/sts-core/src/main/java/org/apache/cxf/sts/operation/TokenCancelOperation.java
    #	services/sts/sts-core/src/main/java/org/apache/cxf/sts/operation/TokenRenewOperation.java
    #	services/sts/sts-core/src/main/java/org/apache/cxf/sts/operation/TokenValidateOperation.java

--- services/sts/sts-core/src/main/java/org/apache/cxf/sts/operation/TokenRenewOperation.java
+++ services/sts/sts-core/src/main/java/org/apache/cxf/sts/operation/TokenRenewOperation.java
@@ -199,8 +199,13 @@ public class TokenRenewOperation extends AbstractOperation implements RenewOpera
             throw ex;
         }            
     }
+<<<<<<< HEAD
    
     private RequestSecurityTokenResponseType createResponse(
+=======
+
+    protected RequestSecurityTokenResponseType createResponse(
+>>>>>>> 4824bfe... CXF-4692 - Allow customization of Request Security Token Response
             EncryptionProperties encryptionProperties,
             TokenRenewerResponse tokenRenewerResponse, 
             TokenRequirements tokenRequirements,
