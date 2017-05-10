eff326a0140ac3f85d323f4f9fd596dda9067c2a
Sergey Beryozkin <sberyozkin@gmail.com>
2017-05-10 10:50:42 +0100

    [CXF-7316] Getting Swagger2Feature working with swagger-jaxrs 1.5.13, optional at the moment

--- rt/rs/description-swagger/src/main/java/org/apache/cxf/jaxrs/swagger/DefaultSwagger2Serializers.java
+++ rt/rs/description-swagger/src/main/java/org/apache/cxf/jaxrs/swagger/DefaultSwagger2Serializers.java
@@ -23,211 +23,76 @@ import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.apache.cxf.jaxrs.model.ClassResourceInfo;
-import org.apache.cxf.jaxrs.model.OperationResourceInfo;
 import org.apache.cxf.jaxrs.model.doc.DocumentationProvider;
 import org.apache.cxf.jaxrs.model.doc.JavaDocProvider;
-import org.apache.cxf.jaxrs.utils.JAXRSUtils;
 
 import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
-import io.swagger.models.HttpMethod;
-import io.swagger.models.Operation;
-import io.swagger.models.Path;
 import io.swagger.models.Swagger;
-import io.swagger.models.Tag;
-import io.swagger.models.parameters.Parameter;
 
 public class DefaultSwagger2Serializers extends SwaggerSerializers implements Swagger2Serializers {
-
-    protected boolean dynamicBasePath;
-
-    protected boolean replaceTags;
-
-    protected DocumentationProvider javadocProvider;
-
-    protected List<ClassResourceInfo> cris;
-
-    protected BeanConfig beanConfig;
-
+    private Swagger2Customizer customizer = new Swagger2Customizer();
+    
     @Override
     public void writeTo(
-            final Swagger data,
+            Swagger data,
             final Class<?> type,
             final Type genericType,
             final Annotation[] annotations,
             final MediaType mediaType,
             final MultivaluedMap<String, Object> headers,
             final OutputStream out) throws IOException {
-
-        if (dynamicBasePath) {
-            MessageContext ctx = createMessageContext();
-            String currentBasePath = StringUtils.substringBeforeLast(ctx.getHttpServletRequest().getRequestURI(), "/");
-            if (!currentBasePath.equals(beanConfig.getBasePath())) {
-                data.setBasePath(currentBasePath);
-                data.setHost(beanConfig.getHost());
-                data.setInfo(beanConfig.getInfo());
-            }
-            if (beanConfig.getSwagger() != null 
-                && beanConfig.getSwagger().getSecurityDefinitions() != null
-                && data.getSecurityDefinitions() == null) {
-                data.setSecurityDefinitions(beanConfig.getSwagger().getSecurityDefinitions());
-            }
-        }
-
-        if (replaceTags || javadocProvider != null) {
-            Map<String, ClassResourceInfo> operations = new HashMap<>();
-            Map<Pair<String, String>, OperationResourceInfo> methods = new HashMap<>();
-            for (ClassResourceInfo cri : cris) {
-                for (OperationResourceInfo ori : cri.getMethodDispatcher().getOperationResourceInfos()) {
-                    String normalizedPath = getNormalizedPath(
-                            cri.getURITemplate().getValue(), ori.getURITemplate().getValue());
-
-                    operations.put(normalizedPath, cri);
-                    methods.put(ImmutablePair.of(ori.getHttpMethod(), normalizedPath), ori);
-                }
-            }
-
-            if (replaceTags && data.getTags() != null) {
-                data.getTags().clear();
-            }
-            for (final Map.Entry<String, Path> entry : data.getPaths().entrySet()) {
-                Tag tag = null;
-                if (replaceTags && operations.containsKey(entry.getKey())) {
-                    ClassResourceInfo cri = operations.get(entry.getKey());
-
-                    tag = new Tag();
-                    tag.setName(cri.getURITemplate().getValue().replaceAll("/", "_"));
-                    if (javadocProvider != null) {
-                        tag.setDescription(javadocProvider.getClassDoc(cri));
-                    }
-
-                    data.addTag(tag);
-                }
-
-                for (Map.Entry<HttpMethod, Operation> subentry : entry.getValue().getOperationMap().entrySet()) {
-                    if (replaceTags && tag != null) {
-                        subentry.getValue().setTags(Collections.singletonList(tag.getName()));
-                    }
-
-                    Pair<String, String> key = ImmutablePair.of(subentry.getKey().name(), entry.getKey());
-                    if (methods.containsKey(key) && javadocProvider != null) {
-                        OperationResourceInfo ori = methods.get(key);
-
-                        subentry.getValue().setSummary(javadocProvider.getMethodDoc(ori));
-                        for (int i = 0; i < subentry.getValue().getParameters().size(); i++) {
-                            subentry.getValue().getParameters().get(i).
-                                    setDescription(javadocProvider.getMethodParameterDoc(ori, i));
-                        }
-                        addParameters(subentry.getValue().getParameters());
-
-                        if (subentry.getValue().getResponses() != null
-                                && !subentry.getValue().getResponses().isEmpty()) {
-
-                            subentry.getValue().getResponses().entrySet().iterator().next().getValue().
-                                    setDescription(javadocProvider.getMethodResponseDoc(ori));
-                        }
-                    }
-                }
-            }
-        }
-        if (replaceTags && data.getTags() != null) {
-            Collections.sort(data.getTags(), new Comparator<Tag>() {
-
-                @Override
-                public int compare(final Tag tag1, final Tag tag2) {
-                    return tag1.getName().compareTo(tag2.getName());
-                }
-            });
-        }
-
+        data = customizer.customize(data);
         super.writeTo(data, type, genericType, annotations, mediaType, headers, out);
-    }
-
-    private MessageContext createMessageContext() {
-        return JAXRSUtils.createContextValue(
-                               JAXRSUtils.getCurrentMessage(), null, MessageContext.class);
-    }
-
-    protected String getNormalizedPath(String classResourcePath, String operationResourcePath) {
-        StringBuilder normalizedPath = new StringBuilder();
-
-        String[] segments = StringUtils.split(classResourcePath + operationResourcePath, "/");
-        for (String segment : segments) {
-            if (!StringUtils.isEmpty(segment)) {
-                normalizedPath.append("/").append(segment);
-            }
-        }
-        // Adapt to Swagger's path expression
-        if (normalizedPath.toString().endsWith(":.*}")) {
-            normalizedPath.setLength(normalizedPath.length() - 4);
-            normalizedPath.append('}');
-        }
-        return StringUtils.EMPTY.equals(normalizedPath.toString()) ? "/" : normalizedPath.toString();
-    }
-
-    /**
-     * Allows to add parameters to the list, related to an {@link Operation} instance; the method is invoked
-     * for all instances available.
-     *
-     * @param parameters list of parameters defined for an {@link Operation}
-     * @see io.swagger.models.parameters.HeaderParameter
-     * @see io.swagger.models.parameters.CookieParameter
-     * @see io.swagger.models.parameters.PathParameter
-     * @see io.swagger.models.parameters.BodyParameter
-     * @see io.swagger.models.parameters.QueryParameter
-     * @see io.swagger.models.parameters.RefParameter
-     */
-    protected void addParameters(final List<Parameter> parameters) {
-        // does nothing by default
     }
 
     @Override
     public void setDynamicBasePath(final boolean dynamicBasePath) {
-        this.dynamicBasePath = dynamicBasePath;
+        customizer.setDynamicBasePath(dynamicBasePath);
     }
 
     public void setReplaceTags(final boolean replaceTags) {
-        this.replaceTags = replaceTags;
+        customizer.setReplaceTags(replaceTags);
     }
 
     public void setJavadocProvider(final DocumentationProvider javadocProvider) {
-        this.javadocProvider = javadocProvider;
+        customizer.setJavadocProvider(javadocProvider);
     }
 
     @Override
     public void setClassResourceInfos(final List<ClassResourceInfo> classResourceInfos) {
-        this.cris = classResourceInfos;
+        customizer.setClassResourceInfos(classResourceInfos);
     }
 
     public void setJavaDocPath(final String javaDocPath) throws Exception {
-        this.javadocProvider = new JavaDocProvider(javaDocPath);
+        customizer.setJavadocProvider(new JavaDocProvider(javaDocPath));
     }
 
     public void setJavaDocPaths(final String... javaDocPaths) throws Exception {
-        this.javadocProvider = new JavaDocProvider(javaDocPaths);
+        customizer.setJavadocProvider(new JavaDocProvider(javaDocPaths));
     }
 
     public void setJavaDocURLs(final URL[] javaDocURLs) {
-        this.javadocProvider = new JavaDocProvider(javaDocURLs);
+        customizer.setJavadocProvider(new JavaDocProvider(javaDocURLs));
     }
 
     @Override
     public void setBeanConfig(BeanConfig beanConfig) {
-        this.beanConfig = beanConfig;
+        customizer.setBeanConfig(beanConfig);
 
+    }
+
+    public Swagger2Customizer getCustomizer() {
+        return customizer;
+    }
+
+    public void setCustomizer(Swagger2Customizer customizer) {
+        this.customizer = customizer;
     }
 }
