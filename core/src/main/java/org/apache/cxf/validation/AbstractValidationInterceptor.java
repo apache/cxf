a6d18c83af6584cd9b5ec1215c9b3ef77906cd19
Sergey Beryozkin <sberyozkin@gmail.com>
2017-06-21 13:54:14 +0100

    [CXF-7418] Initial support for ValidateOnExecution

--- core/src/main/java/org/apache/cxf/validation/AbstractValidationInterceptor.java
+++ core/src/main/java/org/apache/cxf/validation/AbstractValidationInterceptor.java
@@ -23,6 +23,9 @@ import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
+import javax.validation.executable.ExecutableType;
+import javax.validation.executable.ValidateOnExecution;
+
 import org.apache.cxf.common.i18n.BundleUtils;
 import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.endpoint.Endpoint;
@@ -64,6 +67,14 @@ public abstract class AbstractValidationInterceptor extends AbstractPhaseInterce
         final Method method = getServiceMethod(message);
         if (method == null) {
             return;
+        }
+        
+        ValidateOnExecution validateOnExec = method.getAnnotation(ValidateOnExecution.class);
+        if (validateOnExec != null) {
+            ExecutableType[] execTypes = validateOnExec.type();
+            if (execTypes.length == 1 && execTypes[0] == ExecutableType.NONE) {
+                return;
+            }
         }
 
 
