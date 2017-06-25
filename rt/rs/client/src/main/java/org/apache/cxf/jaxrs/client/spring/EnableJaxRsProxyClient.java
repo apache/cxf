6819cc59fe842ce3cef4e148c0f6f94db6931c4a
Dennis Kieselhorst <deki@apache.org>
2017-06-25 15:00:22 +0200

    annotations should have Javadoc, copied documentation from https://cwiki.apache.org/confluence/display/CXF20DOC/JAXRSClientSpringBoot

--- rt/rs/client/src/main/java/org/apache/cxf/jaxrs/client/spring/EnableJaxRsProxyClient.java
+++ rt/rs/client/src/main/java/org/apache/cxf/jaxrs/client/spring/EnableJaxRsProxyClient.java
@@ -25,6 +25,15 @@ import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.Import;
 
+/**
+ * Allows autowiring of proxy clients
+ *
+ * It creates a proxy from the auto-discovered service class interface.
+ * JAX-RS providers (annotated with @Provider) and marked as Spring Components are added to proxy clients.
+ * The providers which are not marked as Spring Components can also be optionally auto-discovered.
+ * Proxy can also be configured with optional headers such as Accept and Content-Type
+ * (if JAX-RS @Produces and/or @Consumes are missing or need to be overridden) and made thread-safe.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Import(JaxRsProxyClientConfiguration.class)
