b4c3fa08369dec06af367879c9ddcc693ed87d8e
Sergey Beryozkin <sberyozkin@gmail.com>
2017-07-21 11:43:13 +0300

    Starting testing the pet shop resorce

--- rt/rs/description-swagger/src/test/java/org/apache/cxf/jaxrs/swagger/SwaggerUtilsTest.java
+++ rt/rs/description-swagger/src/test/java/org/apache/cxf/jaxrs/swagger/SwaggerUtilsTest.java
@@ -32,6 +32,95 @@ import org.junit.Test;
 public class SwaggerUtilsTest extends Assert {
 
     @Test
+    public void testConvertSwaggerPetShopToUserApp() {
+        UserApplication ap = SwaggerUtils.getUserApplication("/swagger2petShop.json");
+        assertNotNull(ap);
+        assertEquals("/v2", ap.getBasePath());
+        Map<String, UserResource> map = ap.getResourcesAsMap();
+        assertEquals(3, map.size());
+        
+        UserResource store = map.get("store");
+        verifyPetStoreResource(store);
+        
+        UserResource user = map.get("user");
+        verifyPetUserResource(user);
+        
+        UserResource pet = map.get("pet");
+        verifyPetResource(pet);
+        
+        
+    }
+    
+    
+    private void verifyPetStoreResource(UserResource ur) {
+        assertNotNull(ur);
+        assertEquals("/store", ur.getPath());
+        assertEquals(4, ur.getOperations().size());
+        //GET /store/inventory
+        UserOperation getInv = ur.getOperations().get(0);
+        assertEquals("getInventory", getInv.getName());
+        assertEquals("/inventory", getInv.getPath());
+        assertEquals("GET", getInv.getVerb());
+        assertEquals("application/json", getInv.getProduces());
+        assertNull(getInv.getConsumes());
+        assertEquals(0, getInv.getParameters().size());
+        //TODO: check 200 response type
+        
+        //POST /store/inventory
+        UserOperation placeOrder = ur.getOperations().get(1);
+        assertEquals("placeOrder", placeOrder.getName());
+        assertEquals("/order", placeOrder.getPath());
+        assertEquals("POST", placeOrder.getVerb());
+        assertEquals("application/xml,application/json", placeOrder.getProduces());
+        assertNull(placeOrder.getConsumes());
+        assertEquals(1, placeOrder.getParameters().size());
+        Parameter placeOrderParam1 = placeOrder.getParameters().get(0);
+        assertEquals("body", placeOrderParam1.getName());
+        assertEquals(ParameterType.REQUEST_BODY, placeOrderParam1.getType());
+        //TODO: check that placeOrderParam1 refers to Order model 
+        //TODO: check 200 response type
+        
+        //GET /store/order/{orderId}
+        UserOperation getOrderById = ur.getOperations().get(2);
+        assertEquals("getOrderById", getOrderById.getName());
+        assertEquals("/order/{orderId}", getOrderById.getPath());
+        assertEquals("GET", getOrderById.getVerb());
+        assertEquals("application/xml,application/json", getOrderById.getProduces());
+        assertNull(getOrderById.getConsumes());
+        assertEquals(1, getOrderById.getParameters().size());
+        Parameter getOrderByIdParam1 = getOrderById.getParameters().get(0);
+        assertEquals("orderId", getOrderByIdParam1.getName());
+        assertEquals(ParameterType.PATH, getOrderByIdParam1.getType());
+        assertEquals(long.class, getOrderByIdParam1.getJavaType());
+        //TODO: check 200 response type
+        
+        //DELETE /store/order/{orderId}
+        UserOperation deleteOrder = ur.getOperations().get(3);
+        assertEquals("deleteOrder", deleteOrder.getName());
+        assertEquals("/order/{orderId}", deleteOrder.getPath());
+        assertEquals("DELETE", deleteOrder.getVerb());
+        assertEquals("application/xml,application/json", deleteOrder.getProduces());
+        assertNull(deleteOrder.getConsumes());
+        assertEquals(1, getOrderById.getParameters().size());
+        Parameter deleteOrderParam1 = deleteOrder.getParameters().get(0);
+        assertEquals("orderId", deleteOrderParam1.getName());
+        assertEquals(ParameterType.PATH, deleteOrderParam1.getType());
+        assertEquals(long.class, deleteOrderParam1.getJavaType());
+        //TODO: check 200 response type
+        
+    }
+    
+    private void verifyPetUserResource(UserResource ur) {
+        assertNotNull(ur);
+        
+        
+    }
+    
+    private void verifyPetResource(UserResource ur) {
+        assertNotNull(ur);
+    }
+
+    @Test
     public void testConvertSwaggerToUserApp() {
         UserApplication ap = SwaggerUtils.getUserApplication("/swagger20.json");
         assertNotNull(ap);
