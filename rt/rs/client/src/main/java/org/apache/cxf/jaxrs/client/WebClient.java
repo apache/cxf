0d523a2dcb977a4a026627df759235628f1689ff
Sergey Beryozkin <sberyozkin@gmail.com>
2017-08-17 09:49:52 +0100

    [CXF-7478] Response stream can not be auto-closed if it is read implicitly

--- rt/rs/client/src/main/java/org/apache/cxf/jaxrs/client/WebClient.java
+++ rt/rs/client/src/main/java/org/apache/cxf/jaxrs/client/WebClient.java
@@ -1132,9 +1132,9 @@ public class WebClient extends AbstractClient {
                     entity = currentResponse.getEntity();
                 }
             }
-            rb = JAXRSUtils.fromResponse(currentResponse);
-            
-            rb.entity(entity instanceof Response 
+            rb = JAXRSUtils.fromResponse(currentResponse, false);
+
+            rb.entity(entity instanceof Response
                       ? ((Response)entity).getEntity() : entity);
             
             Response r = rb.build();
