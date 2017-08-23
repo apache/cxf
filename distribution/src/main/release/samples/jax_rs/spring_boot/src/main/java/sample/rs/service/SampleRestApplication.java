905f619067931ff340536e75f0c3d196d5fd334f
Sergey Beryozkin <sberyozkin@gmail.com>
2017-08-23 11:27:19 +0100

    [CXF-7482] Updating the spring_boot_scan application to use the properties file

--- distribution/src/main/release/samples/jax_rs/spring_boot/src/main/java/sample/rs/service/SampleRestApplication.java
+++ distribution/src/main/release/samples/jax_rs/spring_boot/src/main/java/sample/rs/service/SampleRestApplication.java
@@ -47,10 +47,19 @@ public class SampleRestApplication {
         endpoint.setBus(bus);
         endpoint.setServiceBeans(Arrays.<Object>asList(new HelloServiceImpl1(), new HelloServiceImpl2()));
         endpoint.setAddress("/");
-        Swagger2Feature swagger2Feature = new Swagger2Feature();
-        swagger2Feature.setTitle("Sample REST Application");
-        swagger2Feature.setDescription("The Application");
-        endpoint.setFeatures(Collections.singletonList(swagger2Feature));
+        endpoint.setFeatures(Collections.singletonList(createSwaggerFeature()));
         return endpoint.create();
+    }
+
+    public Swagger2Feature createSwaggerFeature() {
+        Swagger2Feature swagger2Feature = new Swagger2Feature();
+        swagger2Feature.setPrettyPrint(true);
+        swagger2Feature.setTitle("Spring Boot CXF REST Application");
+        swagger2Feature.setContact("The Apache CXF team");
+        swagger2Feature.setDescription("This sample project demonstrates how to use CXF JAX-RS services"
+                + " with Spring Boot. This demo has two JAX-RS class resources being"
+                + " deployed in a single JAX-RS endpoint.");
+        swagger2Feature.setVersion("1.0.0");
+        return swagger2Feature;
     }
 }
