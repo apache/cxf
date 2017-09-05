d0ea04e46b46cf233fb6cb2c4d889acdc7abd532
Sergey Beryozkin <sberyozkin@gmail.com>
2017-09-05 13:37:16 +0100

    [CXF-7492] Renaming the option to 'authorization'

--- tools/wadlto/jaxrs/src/main/java/org/apache/cxf/tools/wadlto/jaxrs/SourceGenerator.java
+++ tools/wadlto/jaxrs/src/main/java/org/apache/cxf/tools/wadlto/jaxrs/SourceGenerator.java
@@ -227,7 +227,7 @@ public class SourceGenerator {
     private boolean validateWadl;
     private SchemaCollection schemaCollection = new SchemaCollection();
     private String encoding;
-    private String namePassword;
+    private String authorization;
     private boolean createJavaDocs;
 
     public SourceGenerator() {
@@ -1861,10 +1861,10 @@ public class SourceGenerator {
             } 
             if (is == null) {
                 URL url = URI.create(href).toURL();
-                if (namePassword != null) {
+                if (authorization != null) {
                     HttpURLConnection conn = (HttpURLConnection)url.openConnection();
                     conn.setRequestMethod("GET");
-                    String encodedAuth = "Basic " + Base64Utility.encode(namePassword.getBytes());
+                    String encodedAuth = "Basic " + Base64Utility.encode(authorization.getBytes());
                     conn.setRequestProperty("Authorization", encodedAuth);
                     is = conn.getInputStream();
                 } else {
@@ -2035,8 +2035,8 @@ public class SourceGenerator {
         this.supportBeanValidation = supportBeanValidation;
     }
 
-    public void setNamePassword(String namePassword) {
-        this.namePassword = namePassword;
+    public void setAuthorization(String authorization) {
+        this.authorization = authorization;
     }
 
     private static class GrammarInfo {
