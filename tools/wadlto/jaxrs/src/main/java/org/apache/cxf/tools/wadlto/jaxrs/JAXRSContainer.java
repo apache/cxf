1ed21f665a36aa8ecfcfb97f6a3cfa48c078a7d0
Sergey Beryozkin <sberyozkin@gmail.com>
2017-09-05 12:55:28 +0100

    [CXF-7492] Adding a namePassword wadltojava option

--- tools/wadlto/jaxrs/src/main/java/org/apache/cxf/tools/wadlto/jaxrs/JAXRSContainer.java
+++ tools/wadlto/jaxrs/src/main/java/org/apache/cxf/tools/wadlto/jaxrs/JAXRSContainer.java
@@ -126,6 +126,7 @@ public class JAXRSContainer extends AbstractCXFToolContainer {
         sg.setPackageName((String)context.get(WadlToolConstants.CFG_PACKAGENAME));
         sg.setResourceName((String)context.get(WadlToolConstants.CFG_RESOURCENAME));
         sg.setEncoding((String)context.get(WadlToolConstants.CFG_ENCODING));
+        sg.setNamePassword((String)context.get(WadlToolConstants.CFG_NAME_PASSWORD));
 
         String wadlNs = (String)context.get(WadlToolConstants.CFG_WADL_NAMESPACE);
         if (wadlNs != null) {
