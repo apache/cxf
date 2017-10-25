a6b4f74e49873e89a24bce14b807f1fc52ede49a
Andy McCright <j.andrew.mccright@gmail.com>
2017-10-25 15:54:15 -0500

    Add doPrivs

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/model/BeanResourceInfo.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/model/BeanResourceInfo.java
@@ -17,11 +17,32 @@
  * under the License.
  */
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.cxf.jaxrs.model;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -59,7 +80,7 @@ public abstract class BeanResourceInfo extends AbstractResourceInfo {
         if (Object.class == cls || cls == null) {
             return;
         }
-        for (Field f : cls.getDeclaredFields()) {
+        for (Field f : getDeclaredFields(cls)) {
             for (Annotation a : f.getAnnotations()) {
                 if (AnnotationUtils.isParamAnnotationClass(a.annotationType())) {
                     if (paramFields == null) {
@@ -109,5 +130,14 @@ public abstract class BeanResourceInfo extends AbstractResourceInfo {
     public List<Field> getParameterFields() {
         return paramFields == null ? Collections.<Field>emptyList() 
                                     : Collections.unmodifiableList(paramFields);
+    }
+    
+    private static Field[] getDeclaredFields(final Class<?> cls) {
+        return AccessController.doPrivileged(new PrivilegedAction<Field[]>() {
+
+            @Override
+            public Field[] run() {
+                return cls.getDeclaredFields();
+            } });
     }
 }
