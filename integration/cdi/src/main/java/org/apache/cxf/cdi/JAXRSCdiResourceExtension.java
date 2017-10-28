336657c5ae672c7e0690f30f98a9e871f200aff9
Andriy Redko <drreta@gmail.com>
2017-10-28 08:52:45 -0400

    CXF-7501: Cannot inject field in ContainerRequestFilter (and generally, into any providers registered using FeatureContext) (#329)
    * CXF-7501: Cannot inject field in ContainerRequestFilter (and generally, into any providers registered using FeatureContext)
    
    * CXF-7501: Cannot inject field in ContainerRequestFilter (and generally, into any providers registered using FeatureContext). Added creational context disposal.

--- integration/cdi/src/main/java/org/apache/cxf/cdi/JAXRSCdiResourceExtension.java
+++ integration/cdi/src/main/java/org/apache/cxf/cdi/JAXRSCdiResourceExtension.java
@@ -49,9 +49,11 @@ import javax.ws.rs.ext.Provider;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.extension.ExtensionManagerBus;
+import org.apache.cxf.cdi.event.DisposableCreationalContext;
 import org.apache.cxf.cdi.extension.JAXRSServerFactoryCustomizationExtension;
 import org.apache.cxf.feature.Feature;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.provider.ServerConfigurableFactory;
 import org.apache.cxf.jaxrs.utils.ResourceUtils;
 
 /**
@@ -138,6 +140,11 @@ public class JAXRSCdiResourceExtension implements Extension {
                 busBean,
                 Bus.class,
                 beanManager.createCreationalContext(busBean));
+        
+        // Adding the extension for dynamic providers registration and instantiation
+        if (bus.getExtension(ServerConfigurableFactory.class) == null) {
+            bus.setExtension(new CdiServerConfigurableFactory(beanManager), ServerConfigurableFactory.class);
+        }
 
         for (final Bean< ? > application: applicationBeans) {
             final Application instance = (Application)beanManager.getReference(
@@ -182,11 +189,22 @@ public class JAXRSCdiResourceExtension implements Extension {
     }
 
     /**
+     * Registers created CreationalContext instances for disposal
+     */
+    public void registerCreationalContextForDisposal(@Observes final DisposableCreationalContext event) {
+        synchronized (disposableCreationalContexts) {
+            disposableCreationalContexts.add(event.getContext());
+        }
+    }
+    
+    /**
      * Releases created CreationalContext instances
      */
     public void release(@Observes final BeforeShutdown event) {
-        for (final CreationalContext<?> disposableCreationalContext: disposableCreationalContexts) {
-            disposableCreationalContext.release();
+        synchronized (disposableCreationalContexts) {
+            for (final CreationalContext<?> disposableCreationalContext: disposableCreationalContexts) {
+                disposableCreationalContext.release();
+            }
         }
     }
 
@@ -375,9 +393,13 @@ public class JAXRSCdiResourceExtension implements Extension {
      */
     private<T> CreationalContext< T > createCreationalContext(final BeanManager beanManager, Bean< T > bean) {
         final CreationalContext< T > creationalContext = beanManager.createCreationalContext(bean);
+        
         if (!(bean instanceof DefaultApplicationBean)) {
-            disposableCreationalContexts.add(creationalContext);
+            synchronized (disposableCreationalContexts) {
+                disposableCreationalContexts.add(creationalContext);
+            }
         }
+        
         return creationalContext;
     }
 
