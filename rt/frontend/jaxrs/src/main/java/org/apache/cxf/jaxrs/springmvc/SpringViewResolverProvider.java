779b131a466639ab66498828837dd5f6c44a4926
Sergey Beryozkin <sberyozkin@gmail.com>
2017-11-10 12:38:38 +0000

    [CXF-7555] Moving SpringViewResolverProvider to .springmvc. subpackage

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/springmvc/SpringViewResolverProvider.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/springmvc/SpringViewResolverProvider.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.cxf.jaxrs.spring;
+package org.apache.cxf.jaxrs.springmvc;
 
 import java.io.IOException;
 import java.io.OutputStream;
--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/springmvc/SpringViewResolverProviderTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/springmvc/SpringViewResolverProviderTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.cxf.jaxrs.spring;
+package org.apache.cxf.jaxrs.springmvc;
 
 import java.io.Closeable;
 import java.lang.annotation.Annotation;
