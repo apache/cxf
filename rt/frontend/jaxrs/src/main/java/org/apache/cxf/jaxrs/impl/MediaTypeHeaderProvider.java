0d3c73188b955ac4ec899fb4ed1d588abbfd0157
Andy McCright <j.andrew.mccright@gmail.com>
2017-11-29 13:25:16 -0600

    Wrap Integer.getInteger in a doPriv block
    (cherry picked from commit 9130f84fafca18620888aecfe1d0d8a0cbce1fc2)
    
    # Conflicts:
    #	rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/MediaTypeHeaderProvider.java
    #	rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/model/URITemplate.java

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/MediaTypeHeaderProvider.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/MediaTypeHeaderProvider.java
@@ -35,6 +35,7 @@ import javax.ws.rs.ext.RuntimeDelegate.HeaderDelegate;
 
 import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.common.util.StringUtils;
+import org.apache.cxf.common.util.SystemPropertyAction;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.message.MessageUtils;
 import org.apache.cxf.phase.PhaseInterceptorChain;
@@ -47,8 +48,8 @@ public class MediaTypeHeaderProvider implements HeaderDelegate<MediaType> {
         Pattern.compile("(([\\w-]+=\"[^\"]*\")|([\\w-]+=[\\w-/\\+]+))");
     
     private static Map<String, MediaType> map = new ConcurrentHashMap<String, MediaType>();
-    private static final int MAX_MT_CACHE_SIZE = 
-        Integer.getInteger("org.apache.cxf.jaxrs.max_mediatype_cache_size", 200);
+    private static final int MAX_MT_CACHE_SIZE =
+        SystemPropertyAction.getInteger("org.apache.cxf.jaxrs.max_mediatype_cache_size", 200);
 
     public MediaType fromString(String mType) {
         
