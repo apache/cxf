eae819ef21a53954f2112ea6f27e2e6964a7a7b2
reta <drreta@gmail.com>
2017-12-11 20:05:54 -0500

    Fixing all Checkstyle errors for samples

--- distribution/src/main/release/samples/jax_rs/odata/src/main/java/odata/server/DemoEdmProvider.java
+++ distribution/src/main/release/samples/jax_rs/odata/src/main/java/odata/server/DemoEdmProvider.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -41,112 +41,119 @@ import org.apache.olingo.commons.api.edm.provider.CsdlSchema;
  */
 public class DemoEdmProvider extends CsdlAbstractEdmProvider {
 
-  // Service Namespace
-  public static final String NAMESPACE = "OData.Demo";
+    // Service Namespace
+    public static final String NAMESPACE = "OData.Demo";
 
-  // EDM Container
-  public static final String CONTAINER_NAME = "Container";
-  public static final FullQualifiedName CONTAINER = new FullQualifiedName(NAMESPACE, CONTAINER_NAME);
+    // EDM Container
+    public static final String CONTAINER_NAME = "Container";
+    public static final FullQualifiedName CONTAINER = new FullQualifiedName(NAMESPACE, CONTAINER_NAME);
 
-  // Entity Types Names
-  public static final String ET_PRODUCT_NAME = "Product";
-  public static final FullQualifiedName ET_PRODUCT_FQN = new FullQualifiedName(NAMESPACE, ET_PRODUCT_NAME);
+    // Entity Types Names
+    public static final String ET_PRODUCT_NAME = "Product";
+    public static final FullQualifiedName ET_PRODUCT_FQN = new FullQualifiedName(NAMESPACE, ET_PRODUCT_NAME);
 
-  // Entity Set Names
-  public static final String ES_PRODUCTS_NAME = "Products";
-
-
-  @Override
-  public List<CsdlSchema> getSchemas() {
-
-    // create Schema
-    CsdlSchema schema = new CsdlSchema();
-    schema.setNamespace(NAMESPACE);
-
-    // add EntityTypes
-    List<CsdlEntityType> entityTypes = new ArrayList<CsdlEntityType>();
-    entityTypes.add(getEntityType(ET_PRODUCT_FQN));
-    schema.setEntityTypes(entityTypes);
-
-    // add EntityContainer
-    schema.setEntityContainer(getEntityContainer());
-
-    // finally
-    List<CsdlSchema> schemas = new ArrayList<CsdlSchema>();
-    schemas.add(schema);
-
-    return schemas;
-  }
+    // Entity Set Names
+    public static final String ES_PRODUCTS_NAME = "Products";
 
 
-  @Override
-  public CsdlEntityType getEntityType(FullQualifiedName entityTypeName) {
+    @Override
+    public List<CsdlSchema> getSchemas() {
 
-    // this method is called for one of the EntityTypes that are configured in the Schema
-    if(entityTypeName.equals(ET_PRODUCT_FQN)){
+        // create Schema
+        CsdlSchema schema = new CsdlSchema();
+        schema.setNamespace(NAMESPACE);
 
-      //create EntityType properties
-      CsdlProperty id = new CsdlProperty().setName("ID").setType(EdmPrimitiveTypeKind.Int32.getFullQualifiedName());
-      CsdlProperty name = new CsdlProperty().setName("Name").setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
-      CsdlProperty  description = new CsdlProperty().setName("Description").setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
+        // add EntityTypes
+        List<CsdlEntityType> entityTypes = new ArrayList<CsdlEntityType>();
+        entityTypes.add(getEntityType(ET_PRODUCT_FQN));
+        schema.setEntityTypes(entityTypes);
 
-      // create CsdlPropertyRef for Key element
-      CsdlPropertyRef propertyRef = new CsdlPropertyRef();
-      propertyRef.setName("ID");
+        // add EntityContainer
+        schema.setEntityContainer(getEntityContainer());
 
-      // configure EntityType
-      CsdlEntityType entityType = new CsdlEntityType();
-      entityType.setName(ET_PRODUCT_NAME);
-      entityType.setProperties(Arrays.asList(id, name , description));
-      entityType.setKey(Collections.singletonList(propertyRef));
+        // finally
+        List<CsdlSchema> schemas = new ArrayList<CsdlSchema>();
+        schemas.add(schema);
 
-      return entityType;
+        return schemas;
     }
 
-    return null;
-  }
 
-  @Override
-  public CsdlEntitySet getEntitySet(FullQualifiedName entityContainer, String entitySetName) {
+    @Override
+    public CsdlEntityType getEntityType(FullQualifiedName entityTypeName) {
 
-    if(entityContainer.equals(CONTAINER)){
-      if(entitySetName.equals(ES_PRODUCTS_NAME)){
-        CsdlEntitySet entitySet = new CsdlEntitySet();
-        entitySet.setName(ES_PRODUCTS_NAME);
-        entitySet.setType(ET_PRODUCT_FQN);
+        // this method is called for one of the EntityTypes that are configured in the Schema
+        if (entityTypeName.equals(ET_PRODUCT_FQN)) {
 
-        return entitySet;
-      }
+            //create EntityType properties
+            CsdlProperty id = new CsdlProperty()
+                .setName("ID")
+                .setType(EdmPrimitiveTypeKind.Int32.getFullQualifiedName());
+            CsdlProperty name = new CsdlProperty()
+                .setName("Name")
+                .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
+            CsdlProperty description = new CsdlProperty()
+                .setName("Description")
+                .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
+
+            // create CsdlPropertyRef for Key element
+            CsdlPropertyRef propertyRef = new CsdlPropertyRef();
+            propertyRef.setName("ID");
+
+            // configure EntityType
+            CsdlEntityType entityType = new CsdlEntityType();
+            entityType.setName(ET_PRODUCT_NAME);
+            entityType.setProperties(Arrays.asList(id, name, description));
+            entityType.setKey(Collections.singletonList(propertyRef));
+
+            return entityType;
+        }
+
+        return null;
     }
 
-    return null;
-  }
+    @Override
+    public CsdlEntitySet getEntitySet(FullQualifiedName entityContainer, String entitySetName) {
 
-  @Override
-  public CsdlEntityContainer getEntityContainer() {
+        if (entityContainer.equals(CONTAINER)) {
+            if (entitySetName.equals(ES_PRODUCTS_NAME)) {
+                CsdlEntitySet entitySet = new CsdlEntitySet();
+                entitySet.setName(ES_PRODUCTS_NAME);
+                entitySet.setType(ET_PRODUCT_FQN);
 
-    // create EntitySets
-    List<CsdlEntitySet> entitySets = new ArrayList<CsdlEntitySet>();
-    entitySets.add(getEntitySet(CONTAINER, ES_PRODUCTS_NAME));
+                return entitySet;
+            }
+        }
 
-    // create EntityContainer
-    CsdlEntityContainer entityContainer = new CsdlEntityContainer();
-    entityContainer.setName(CONTAINER_NAME);
-    entityContainer.setEntitySets(entitySets);
-
-    return entityContainer;
-  }
-
-  @Override
-  public CsdlEntityContainerInfo getEntityContainerInfo(FullQualifiedName entityContainerName) {
-
-    // This method is invoked when displaying the service document at e.g. http://localhost:8080/DemoService/DemoService.svc
-    if(entityContainerName == null || entityContainerName.equals(CONTAINER)){
-      CsdlEntityContainerInfo entityContainerInfo = new CsdlEntityContainerInfo();
-      entityContainerInfo.setContainerName(CONTAINER);
-      return entityContainerInfo;
+        return null;
     }
 
-    return null;
-  }
+    @Override
+    public CsdlEntityContainer getEntityContainer() {
+
+        // create EntitySets
+        List<CsdlEntitySet> entitySets = new ArrayList<CsdlEntitySet>();
+        entitySets.add(getEntitySet(CONTAINER, ES_PRODUCTS_NAME));
+
+        // create EntityContainer
+        CsdlEntityContainer entityContainer = new CsdlEntityContainer();
+        entityContainer.setName(CONTAINER_NAME);
+        entityContainer.setEntitySets(entitySets);
+
+        return entityContainer;
+    }
+
+    @Override
+    public CsdlEntityContainerInfo getEntityContainerInfo(FullQualifiedName entityContainerName) {
+
+        // This method is invoked when displaying the service document at 
+        // e.g. http://localhost:8080/DemoService/DemoService.svc
+        if (entityContainerName == null || entityContainerName.equals(CONTAINER)) {
+            CsdlEntityContainerInfo entityContainerInfo = new CsdlEntityContainerInfo();
+            entityContainerInfo.setContainerName(CONTAINER);
+            return entityContainerInfo;
+        }
+
+        return null;
+    }
 }
--- distribution/src/main/release/samples/jax_rs/odata/src/main/java/odata/server/DemoEntityCollectionProcessor.java
+++ distribution/src/main/release/samples/jax_rs/odata/src/main/java/odata/server/DemoEntityCollectionProcessor.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -48,102 +48,109 @@ import org.apache.olingo.server.api.uri.UriResource;
 import org.apache.olingo.server.api.uri.UriResourceEntitySet;
 
 /**
- * This class is invoked by the Olingo framework when the the OData service is invoked order to display a list/collection of data (entities).
+ * This class is invoked by the Olingo framework when the the OData service 
+ * is invoked order to display a list/collection of data (entities).
  * This is the case if an EntitySet is requested by the user.
  * Such an example URL would be:
  * http://localhost:8080/ExampleService1/ExampleService1.svc/Products
  */
 public class DemoEntityCollectionProcessor implements EntityCollectionProcessor {
 
-  private OData odata;
-  private ServiceMetadata serviceMetadata;
+    private OData odata;
+    private ServiceMetadata serviceMetadata;
 
-  // our processor is initialized with the OData context object
-  public void init(OData odata, ServiceMetadata serviceMetadata) {
-    this.odata = odata;
-    this.serviceMetadata = serviceMetadata;
-  }
+    // CHECKSTYLE:OFF
+    // our processor is initialized with the OData context object
+    public void init(OData odata, ServiceMetadata serviceMetadata) {
+        this.odata = odata;
+        this.serviceMetadata = serviceMetadata;
+    }
+    // CHECKSTYLE:ON
+
+    // the only method that is declared in the EntityCollectionProcessor interface
+    // this method is called, when the user fires a request to an EntitySet
+    // in our example, the URL would be:
+    // http://localhost:8080/ExampleService1/ExampleServlet1.svc/Products
+    public void readEntityCollection(ODataRequest request, ODataResponse response, 
+            UriInfo uriInfo, ContentType responseFormat) throws ODataApplicationException, SerializerException {
+
+        // 1st we have retrieve the requested EntitySet from the uriInfo object 
+        // (representation of the parsed service URI)
+        List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
+        // In our example, the first segment is the EntitySet
+        UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) resourcePaths.get(0); 
+        EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
+
+        // 2nd: fetch the data from backend for this requested EntitySetName 
+        // it has to be delivered as EntitySet object
+        EntityCollection entitySet = getData(edmEntitySet);
+
+        // 3rd: create a serializer based on the requested format (json)
+        ODataSerializer serializer = odata.createSerializer(responseFormat);
+        
+        // 4th: Now serialize the content: transform from the EntitySet object to InputStream
+        EdmEntityType edmEntityType = edmEntitySet.getEntityType();
+        ContextURL contextUrl = ContextURL.with().entitySet(edmEntitySet).build();
+
+        final String id = request.getRawBaseUri() + "/" + edmEntitySet.getName();
+        EntityCollectionSerializerOptions opts =
+                EntityCollectionSerializerOptions.with().id(id).contextURL(contextUrl).build();
+        SerializerResult serializedContent = serializer.entityCollection(serviceMetadata, 
+            edmEntityType, entitySet, opts);
+
+        // Finally: configure the response object: set the body, headers and status code
+        response.setContent(serializedContent.getContent());
+        response.setStatusCode(HttpStatusCode.OK.getStatusCode());
+        response.setHeader(HttpHeader.CONTENT_TYPE, responseFormat.toContentTypeString());
+    }
 
 
-  // the only method that is declared in the EntityCollectionProcessor interface
-  // this method is called, when the user fires a request to an EntitySet
-  // in our example, the URL would be:
-  // http://localhost:8080/ExampleService1/ExampleServlet1.svc/Products
-  public void readEntityCollection(ODataRequest request, ODataResponse response, UriInfo uriInfo, ContentType responseFormat) throws ODataApplicationException, SerializerException {
+    /**
+     * Helper method for providing some sample data
+     * @param edmEntitySet for which the data is requested
+     * @return data of requested entity set
+     */
+    private EntityCollection getData(EdmEntitySet edmEntitySet) {
 
-    // 1st we have retrieve the requested EntitySet from the uriInfo object (representation of the parsed service URI)
-    List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
-    UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) resourcePaths.get(0); // in our example, the first segment is the EntitySet
-    EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
+        EntityCollection productsCollection = new EntityCollection();
+        // check for which EdmEntitySet the data is requested
+        if (DemoEdmProvider.ES_PRODUCTS_NAME.equals(edmEntitySet.getName())) {
+            List<Entity> productList = productsCollection.getEntities();
 
-    // 2nd: fetch the data from backend for this requested EntitySetName // it has to be delivered as EntitySet object
-    EntityCollection entitySet = getData(edmEntitySet);
+            // add some sample product entities
+            final Entity e1 = new Entity()
+                    .addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 1))
+                    .addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Notebook Basic 15"))
+                    .addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
+                            "Notebook Basic, 1.7GHz - 15 XGA - 1024MB DDR2 SDRAM - 40GB"));
+            e1.setId(createId("Products", 1));
+            productList.add(e1);
 
-    // 3rd: create a serializer based on the requested format (json)
-    ODataSerializer serializer = odata.createSerializer(responseFormat);
+            final Entity e2 = new Entity()
+                    .addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 2))
+                    .addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "1UMTS PDA"))
+                    .addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
+                            "Ultrafast 3G UMTS/HSDPA Pocket PC, supports GSM network"));
+            e2.setId(createId("Products", 1));
+            productList.add(e2);
+
+            final Entity e3 = new Entity()
+                    .addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 3))
+                    .addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Ergo Screen"))
+                    .addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
+                            "19 Optimum Resolution 1024 x 768 @ 85Hz, resolution 1280 x 960"));
+            e3.setId(createId("Products", 1));
+            productList.add(e3);
+        }
+
+        return productsCollection;
+    }
     
-    // 4th: Now serialize the content: transform from the EntitySet object to InputStream
-    EdmEntityType edmEntityType = edmEntitySet.getEntityType();
-    ContextURL contextUrl = ContextURL.with().entitySet(edmEntitySet).build();
-
-    final String id = request.getRawBaseUri() + "/" + edmEntitySet.getName();
-    EntityCollectionSerializerOptions opts =
-        EntityCollectionSerializerOptions.with().id(id).contextURL(contextUrl).build();
-    SerializerResult serializedContent = serializer.entityCollection(serviceMetadata, edmEntityType, entitySet, opts);
-
-    // Finally: configure the response object: set the body, headers and status code
-    response.setContent(serializedContent.getContent());
-    response.setStatusCode(HttpStatusCode.OK.getStatusCode());
-    response.setHeader(HttpHeader.CONTENT_TYPE, responseFormat.toContentTypeString());
-  }
-
-
-  /**
-   * Helper method for providing some sample data
-   * @param edmEntitySet for which the data is requested
-   * @return data of requested entity set
-   */
-  private EntityCollection getData(EdmEntitySet edmEntitySet){
-
-    EntityCollection productsCollection = new EntityCollection();
-    // check for which EdmEntitySet the data is requested
-    if(DemoEdmProvider.ES_PRODUCTS_NAME.equals(edmEntitySet.getName())) {
-      List<Entity> productList = productsCollection.getEntities();
-
-      // add some sample product entities
-      final Entity e1 = new Entity()
-          .addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 1))
-          .addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Notebook Basic 15"))
-          .addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-              "Notebook Basic, 1.7GHz - 15 XGA - 1024MB DDR2 SDRAM - 40GB"));
-      e1.setId(createId("Products", 1));
-      productList.add(e1);
-
-      final Entity e2 = new Entity()
-          .addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 2))
-          .addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "1UMTS PDA"))
-          .addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-              "Ultrafast 3G UMTS/HSDPA Pocket PC, supports GSM network"));
-      e2.setId(createId("Products", 1));
-      productList.add(e2);
-
-      final Entity e3 = new Entity()
-          .addProperty(new Property(null, "ID", ValueType.PRIMITIVE, 3))
-          .addProperty(new Property(null, "Name", ValueType.PRIMITIVE, "Ergo Screen"))
-          .addProperty(new Property(null, "Description", ValueType.PRIMITIVE,
-              "19 Optimum Resolution 1024 x 768 @ 85Hz, resolution 1280 x 960"));
-      e3.setId(createId("Products", 1));
-      productList.add(e3);
+    private URI createId(String entitySetName, Object id) {
+        try {
+            return new URI(entitySetName + "(" + String.valueOf(id) + ")");
+        } catch (URISyntaxException e) {
+            throw new ODataRuntimeException("Unable to create id for entity: " + entitySetName, e);
+        }
     }
-
-    return productsCollection;
-  }
-  
-  private URI createId(String entitySetName, Object id) {
-    try {
-      return new URI(entitySetName + "(" + String.valueOf(id) + ")");
-    } catch (URISyntaxException e) {
-      throw new ODataRuntimeException("Unable to create id for entity: " + entitySetName, e);
-    }
-  }
 }
