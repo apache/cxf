c17b8f61ec863a253762bc14ad48f4415fa7dd8c
Dennis Kieselhorst <mail@dekies.de>
2017-12-25 12:50:52 +0100

    [CXF-7595] ensure InputStream gets closed after loading swagger.properties
    (cherry picked from commit d00d731682ef5707000a5186203ecf6b1cc1731c)

--- rt/rs/description-swagger/src/main/java/org/apache/cxf/jaxrs/swagger/Swagger2Feature.java
+++ rt/rs/description-swagger/src/main/java/org/apache/cxf/jaxrs/swagger/Swagger2Feature.java
@@ -246,8 +246,15 @@ public class Swagger2Feature extends AbstractSwaggerFeature {
                 props.load(is);
             } catch (IOException ex) {
                 props = null;
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException ignore) {
+                    // ignore
+                }
             }
         }
+
         return props;
     }
 
