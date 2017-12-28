c4fb6912abf68f83ace5e28a0e3b457e2125f447
Sergey Beryozkin <sberyozkin@gmail.com>
2017-12-28 16:26:20 +0000

    Adding application/pdf to the default binary types

--- core/src/main/java/org/apache/cxf/interceptor/AbstractLoggingInterceptor.java
+++ core/src/main/java/org/apache/cxf/interceptor/AbstractLoggingInterceptor.java
@@ -63,6 +63,7 @@ public abstract class AbstractLoggingInterceptor extends AbstractPhaseIntercepto
     static {
         BINARY_CONTENT_MEDIA_TYPES = new ArrayList<>();
         BINARY_CONTENT_MEDIA_TYPES.add("application/octet-stream");
+        BINARY_CONTENT_MEDIA_TYPES.add("application/pdf");
         BINARY_CONTENT_MEDIA_TYPES.add("image/png");
         BINARY_CONTENT_MEDIA_TYPES.add("image/jpeg");
         BINARY_CONTENT_MEDIA_TYPES.add("image/gif");
