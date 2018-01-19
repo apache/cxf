2bfbca85a60c135d817c8dc6aa51dc6f8e4cf92f
Colm O hEigeartaigh <coheigea@apache.org>
2018-01-19 18:03:13 +0000

    Adding programmatic example
    (cherry picked from commit eb190a0151ba1bf78d7e0e4e4067717b41606ff7)
    
    # Conflicts:
    #	systests/ws-security/src/test/java/org/apache/cxf/systest/ws/action/ActionTest.java

--- systests/ws-security/src/test/java/org/apache/cxf/systest/ws/action/ActionTest.java
+++ systests/ws-security/src/test/java/org/apache/cxf/systest/ws/action/ActionTest.java
@@ -56,6 +56,12 @@ public class ActionTest extends AbstractBusClientServerTestBase {
                 // set this to false to fork
                 launchServer(Server.class, true)
         );
+        assertTrue(
+                   "Server failed to launch",
+                   // run the server in the same process
+                   // set this to false to fork
+                   launchServer(UTServer.class, true)
+        );
     }
     
     @org.junit.AfterClass
@@ -102,7 +108,7 @@ public class ActionTest extends AbstractBusClientServerTestBase {
         DoubleItPortType port = 
                 service.getPort(portQName, DoubleItPortType.class);
         updateAddressPort(port, PORT);
-        
+
         // Successful call
         port.doubleIt(25);
         
@@ -117,8 +123,14 @@ public class ActionTest extends AbstractBusClientServerTestBase {
         } catch (javax.xml.ws.soap.SOAPFaultException ex) {
             assertTrue(ex.getMessage().equals(WSSecurityException.UNIFIED_SECURITY_ERR));
         }
-        
-        
+
+        // Here the Server is adding the WSS4JInInterceptor in code
+        portQName = new QName(NAMESPACE, "DoubleItUsernameTokenPort3");
+        port = service.getPort(portQName, DoubleItPortType.class);
+        updateAddressPort(port, UTServer.PORT);
+
+        port.doubleIt(25);
+
         ((java.io.Closeable)port).close();
         bus.shutdown(true);
     }
@@ -426,4 +438,5 @@ public class ActionTest extends AbstractBusClientServerTestBase {
         ((java.io.Closeable)port).close();
         bus.shutdown(true);
     }
+
 }
---
+++ systests/ws-security/src/test/java/org/apache/cxf/systest/ws/action/UTServer.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.systest.ws.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.systest.ws.common.DoubleItImpl;
+import org.apache.cxf.testutil.common.AbstractBusTestServerBase;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+
+/**
+ * Test adding the WSS4JInInterceptor in code.
+ */
+public class UTServer extends AbstractBusTestServerBase {
+
+    public static final String PORT = allocatePort(UTServer.class);
+
+    protected void run() {
+        Object implementor = new DoubleItImpl();
+        String address = "http://localhost:" + PORT + "/DoubleItUsernameToken3";
+        EndpointImpl jaxWsEndpoint = (EndpointImpl)Endpoint.publish(address, implementor);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("action", "UsernameToken");
+        properties.put("passwordCallbackClass", "org.apache.cxf.systest.ws.common.UTPasswordCallback");
+        WSS4JInInterceptor wss4jInInterceptor = new WSS4JInInterceptor(properties);
+
+        jaxWsEndpoint.getServer().getEndpoint().getInInterceptors().add(wss4jInInterceptor);
+    }
+}
