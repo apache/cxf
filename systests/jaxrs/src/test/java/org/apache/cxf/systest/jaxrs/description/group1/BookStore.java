60b98ee48fb0219b58be94b9bdbfcf1cb1e41d29
Francesco Chicchiriccò <ilgrosso@apache.org>
2018-01-31 13:57:31 +0100

    [CXF-7525] Fix test, broken due to swagger-jaxrs2 upgrade to 2.0.0-rc4 - see https://github.com/swagger-api/swagger-core/issues/2643

--- systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/description/group1/BookStore.java
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/description/group1/BookStore.java
@@ -35,9 +35,6 @@ import org.apache.cxf.systest.jaxrs.Book;
 
 import io.swagger.annotations.Api;
 
-//FIXME swagger-jaxrs 1.5.3 can't handle a self-recursive sub resource like Book. so hide Book for now
-//import org.apache.cxf.systest.jaxrs.Book;
-
 @Path("/bookstore")
 @Api("/bookstore")
 public class BookStore {
