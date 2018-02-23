4b08b5e92f4918a5ce2b849edd39f183931b5f06
Daniel Kulp <dkulp@apache.org>
2018-02-23 12:59:46 -0500

    [CXF-7653] Throw a more informative exception if the result could not be obtained from the response soap message
    (cherry picked from commit 1396f046333108ec0e7819bf4de154505ff4564c)

--- core/src/main/java/org/apache/cxf/endpoint/ClientImpl.java
+++ core/src/main/java/org/apache/cxf/endpoint/ClientImpl.java
@@ -659,6 +659,19 @@ public class ClientImpl
             throw ex;
         }
 
+        if (resList == null   
+            && oi != null && !oi.getOperationInfo().isOneWay()) {
+            
+            BindingOperationInfo boi = oi;
+            if (boi.isUnwrapped()) {
+                boi = boi.getWrappedOperation();
+            }
+            if (!boi.getOutput().getMessageParts().isEmpty()) {
+                //we were supposed to get some output, but didn't
+                throw new IllegalStateException("Response message did not contain proper response data. Expected: "
+                    + boi.getOutput().getMessageParts().get(0).getConcreteName());
+            }
+        }
         if (resList != null) {
             return resList.toArray();
         }
