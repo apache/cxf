5b7308408d8b0127a5ee4deea7bd66b44217dd58
Dennis Kieselhorst <deki@apache.org>
2018-03-11 13:51:47 +0100

    [CXF-7634] changed Spring Boot sample to use OpenApiFeature

--- distribution/src/main/release/samples/jax_rs/spring_boot/src/main/java/sample/rs/service/SampleRestApplication.java
+++ distribution/src/main/release/samples/jax_rs/spring_boot/src/main/java/sample/rs/service/SampleRestApplication.java
@@ -23,7 +23,7 @@ import org.apache.cxf.Bus;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.ext.logging.LoggingFeature;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
+import org.apache.cxf.jaxrs.openapi.OpenApiFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -47,19 +47,19 @@ public class SampleRestApplication {
         endpoint.setBus(bus);
         endpoint.setServiceBeans(Arrays.<Object>asList(new HelloServiceImpl1(), new HelloServiceImpl2()));
         endpoint.setAddress("/");
-        endpoint.setFeatures(Arrays.asList(createSwaggerFeature(), new LoggingFeature()));
+        endpoint.setFeatures(Arrays.asList(createOpenApiFeature(), new LoggingFeature()));
         return endpoint.create();
     }
 
-    public Swagger2Feature createSwaggerFeature() {
-        Swagger2Feature swagger2Feature = new Swagger2Feature();
-        swagger2Feature.setPrettyPrint(true);
-        swagger2Feature.setTitle("Spring Boot CXF REST Application");
-        swagger2Feature.setContact("The Apache CXF team");
-        swagger2Feature.setDescription("This sample project demonstrates how to use CXF JAX-RS services"
+    public OpenApiFeature createOpenApiFeature() {
+        OpenApiFeature openApiFeature = new OpenApiFeature();
+        openApiFeature.setPrettyPrint(true);
+        openApiFeature.setTitle("Spring Boot CXF REST Application");
+        openApiFeature.setContactName("The Apache CXF team");
+        openApiFeature.setDescription("This sample project demonstrates how to use CXF JAX-RS services"
                 + " with Spring Boot. This demo has two JAX-RS class resources being"
                 + " deployed in a single JAX-RS endpoint.");
-        swagger2Feature.setVersion("1.0.0");
-        return swagger2Feature;
+        openApiFeature.setVersion("1.0.0");
+        return openApiFeature;
     }
 }
