bae98200bb72a3610be44418525b21ab62f1f299
David J. M. Karlsen <david@davidkarlsen.com>
2018-06-22 13:12:15 +0200

    add skeleton

---
+++ rt/rs/security/http-signature/src/test/java/org/apache/cxf/rs/security/httpsignature/AppTest.java
@@ -0,0 +1,20 @@
+package org.apache.cxf.rs.security.httpsignature;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
