fc656dc1311963f5fc833856984b36b7ccd26042
Dennis Kieselhorst <mail@dekies.de>
2018-07-04 08:21:35 +0200

    update to Spring 5, Spring Security 5, Spring Boot 2
    The jax_rs_spring_security sample doesn't work for me, but this is also the case without the changes so I'll merge this.

--- distribution/src/main/release/samples/jaxws_spring_boot/src/test/java/sample/ws/service/SampleWsApplicationTest.java
+++ distribution/src/main/release/samples/jaxws_spring_boot/src/test/java/sample/ws/service/SampleWsApplicationTest.java
@@ -29,7 +29,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.rule.OutputCapture;
