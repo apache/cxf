f2756fc98c55bfa71a350f5bf8595806ed3269b8
Alexander Onnikov <Alexander.Onnikov@gmail.com>
2018-07-19 02:46:25 +0700

    [CXF-7799] Extract queryString from AtmosphereRequest path (#430)
    Thanks, @aonnikov !

--- rt/transports/websocket/src/main/java/org/apache/cxf/transport/websocket/atmosphere/DefaultProtocolInterceptor.java
+++ rt/transports/websocket/src/main/java/org/apache/cxf/transport/websocket/atmosphere/DefaultProtocolInterceptor.java
@@ -266,6 +266,13 @@ public class DefaultProtocolInterceptor extends AtmosphereInterceptorAdapter {
             throw new InvalidPathException();
         }
 
+        String queryString = "";
+        int index = path.indexOf("?");
+        if (index != -1) {
+            queryString = path.substring(index + 1);
+            path = path.substring(0, index);
+        }
+
         String requestURI = path;
         String requestURL = r.getRequestURL() + requestURI.substring(r.getRequestURI().length());
         String contentType = hdrs.get("Content-Type");
@@ -277,6 +284,7 @@ public class DefaultProtocolInterceptor extends AtmosphereInterceptorAdapter {
                 .method(method)
                 .requestURI(requestURI)
                 .requestURL(requestURL)
+                .queryString(queryString)
                 .request(r);
         // add the body only if it is present
         byte[] body = WebSocketUtils.readBody(in);
