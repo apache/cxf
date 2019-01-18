fb66e62ae9e2585263e62fca6bab6ad4da641fb9
Colm O hEigeartaigh <coheigea@apache.org>
2019-01-18 14:31:42 +0000

    CXF-7944 - OAuthClientUtils hides error message if it contains a comma
    (cherry picked from commit 907bfebcffb0d62304bb23a63a4e34540a7981de)
    
    # Conflicts:
    #	rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth2/provider/OAuthJSONProvider.java

--- rt/rs/security/oauth-parent/oauth2/src/test/java/org/apache/cxf/rs/security/oauth2/provider/OAuthJSONProviderTest.java
+++ rt/rs/security/oauth-parent/oauth2/src/test/java/org/apache/cxf/rs/security/oauth2/provider/OAuthJSONProviderTest.java
@@ -20,7 +20,9 @@ package org.apache.cxf.rs.security.oauth2.provider;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.lang.annotation.Annotation;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -36,6 +38,38 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class OAuthJSONProviderTest extends Assert {
+
+    @Test
+    public void testReadException() throws IOException {
+        String response =
+            "{"
+            + "\"error\":\"invalid_client\","
+            + "\"error_description\":\"Client authentication failed\""
+            + "}";
+
+        OAuthJSONProvider provider = new OAuthJSONProvider();
+        Map<String, String> responseMap =
+            provider.readJSONResponse(new ByteArrayInputStream(response.getBytes(StandardCharsets.UTF_8)));
+        assertEquals(responseMap.size(), 2);
+        assertEquals("invalid_client", responseMap.get("error"));
+        assertEquals("Client authentication failed", responseMap.get("error_description"));
+    }
+
+    @Test
+    public void testReadExceptionWithCommaInMessage() throws IOException {
+        String response =
+            "{"
+            + "\"error\":\"invalid_client\","
+            + "\"error_description\":\"Client authentication failed, due to xyz\""
+            + "}";
+
+        OAuthJSONProvider provider = new OAuthJSONProvider();
+        Map<String, String> responseMap =
+            provider.readJSONResponse(new ByteArrayInputStream(response.getBytes(StandardCharsets.UTF_8)));
+        assertEquals(responseMap.size(), 2);
+        assertEquals("invalid_client", responseMap.get("error"));
+        assertEquals("Client authentication failed, due to xyz", responseMap.get("error_description"));
+    }
 
     @Test
     public void testWriteBearerClientAccessToken() throws Exception {
