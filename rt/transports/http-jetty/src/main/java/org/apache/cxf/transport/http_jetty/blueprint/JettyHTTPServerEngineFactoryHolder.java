6e70d53a7b256c866efe9a5ec863cb998b0b9e58
Freeman Fang <freeman.fang@gmail.com>
2019-01-30 09:35:32 +0800

    [CXF-7956]add NPE guard for jetty and undertow threads configuration in blueprint

--- rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/blueprint/JettyHTTPServerEngineFactoryHolder.java
+++ rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/blueprint/JettyHTTPServerEngineFactoryHolder.java
@@ -89,8 +89,12 @@ public class JettyHTTPServerEngineFactoryHolder {
                 for (ThreadingParametersIdentifiedType threads : config.getIdentifiedThreadingParameters()) {
                     ThreadingParameters rThreads = new ThreadingParameters();
                     String id = threads.getId();
-                    rThreads.setMaxThreads(threads.getThreadingParameters().getMaxThreads());
-                    rThreads.setMinThreads(threads.getThreadingParameters().getMinThreads());
+                    if (threads.getThreadingParameters().getMaxThreads() != null) {
+                        rThreads.setMaxThreads(threads.getThreadingParameters().getMaxThreads());
+                    }
+                    if (threads.getThreadingParameters().getMinThreads() != null) {
+                        rThreads.setMinThreads(threads.getThreadingParameters().getMinThreads());
+                    }
                     rThreads.setThreadNamePrefix(threads.getThreadingParameters().getThreadNamePrefix());
                     threadingParametersMap.put(id, rThreads);
                 }
@@ -164,8 +168,12 @@ public class JettyHTTPServerEngineFactoryHolder {
                 if (engine.getThreadingParameters() != null) {
                     ThreadingParametersType threads = engine.getThreadingParameters();
                     ThreadingParameters rThreads = new ThreadingParameters();
-                    rThreads.setMaxThreads(threads.getMaxThreads());
-                    rThreads.setMinThreads(threads.getMinThreads());
+                    if (threads.getMaxThreads() != null) {
+                        rThreads.setMaxThreads(threads.getMaxThreads());
+                    }
+                    if (threads.getMinThreads() != null) {
+                        rThreads.setMinThreads(threads.getMinThreads());
+                    }
 
                     eng.setThreadingParameters(rThreads);
                 }
