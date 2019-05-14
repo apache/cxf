bca53bff89b0d18dea275847fcfa84b2a495acce
Andrei Shakirin <andrei.shakirin@gmail.com>
2019-05-14 23:23:16 +0200

    Removed unused import

--- services/xkms/xkms-x509-handlers/src/main/java/org/apache/cxf/xkms/x509/repo/file/FileCertificateRepo.java
+++ services/xkms/xkms-x509-handlers/src/main/java/org/apache/cxf/xkms/x509/repo/file/FileCertificateRepo.java
@@ -36,7 +36,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.apache.cxf.xkms.exception.XKMSConfigurationException;
 import org.apache.cxf.xkms.handlers.Applications;
