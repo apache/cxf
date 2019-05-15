d1ba0e2ecf652f963c5887a9680e0271ecf900b4
Colm O hEigeartaigh <coheigea@apache.org>
2019-05-15 12:09:20 +0100

    CXF-8043 - XKMS LdapCertificateRepo searching using DN doesn't work
    (cherry picked from commit 12f635546b2b2d84db78d07e20f58d23b04a8640)

--- services/xkms/xkms-x509-repo-ldap/src/main/java/org/apache/cxf/xkms/x509/repo/ldap/LdapCertificateRepo.java
+++ services/xkms/xkms-x509-repo-ldap/src/main/java/org/apache/cxf/xkms/x509/repo/ldap/LdapCertificateRepo.java
@@ -186,11 +186,13 @@ public class LdapCertificateRepo implements CertificateRepo {
         } catch (NamingException e) {
              // Not found
         }
-        // Try to find certificate by search for uid attribute
-        try {
-            cert = getCertificateForUIDAttr(id);
-        } catch (NamingException e) {
-            // Not found
+        if (cert == null) {
+            // Try to find certificate by search for uid attribute
+            try {
+                cert = getCertificateForUIDAttr(id);
+            } catch (NamingException e) {
+                // Not found
+            }
         }
         return cert;
     }
@@ -204,12 +206,14 @@ public class LdapCertificateRepo implements CertificateRepo {
         } catch (NamingException e) {
             // Not found
         }
-        // Try to find certificate by search for uid attribute
-        try {
-            String uidAttr = String.format(ldapConfig.getServiceCertUIDTemplate(), serviceName);
-            cert = getCertificateForUIDAttr(uidAttr);
-        } catch (NamingException e) {
-            // Not found
+        if (cert == null) {
+            // Try to find certificate by search for uid attribute
+            try {
+                String uidAttr = String.format(ldapConfig.getServiceCertUIDTemplate(), serviceName);
+                cert = getCertificateForUIDAttr(uidAttr);
+            } catch (NamingException e) {
+                // Not found
+            }
         }
         return cert;
     }
