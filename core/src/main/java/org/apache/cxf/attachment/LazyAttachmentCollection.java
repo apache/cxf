fd85803a73ad46f36816bcb55ed1c4f4b4c4312d
Colm O hEigeartaigh <coheigea@apache.org>
2019-09-03 15:45:20 +0100

    Restrict the number of message attachments
    (cherry picked from commit 6c3990144b56097502aa9f3ec9c06f3031109022)

--- core/src/main/java/org/apache/cxf/attachment/LazyAttachmentCollection.java
+++ core/src/main/java/org/apache/cxf/attachment/LazyAttachmentCollection.java
@@ -37,10 +37,12 @@ public class LazyAttachmentCollection
 
     private AttachmentDeserializer deserializer;
     private final List<Attachment> attachments = new ArrayList<>();
+    private final int maxAttachmentCount;
 
-    public LazyAttachmentCollection(AttachmentDeserializer deserializer) {
+    public LazyAttachmentCollection(AttachmentDeserializer deserializer, int maxAttachmentCount) {
         super();
         this.deserializer = deserializer;
+        this.maxAttachmentCount = maxAttachmentCount;
     }
 
     public List<Attachment> getLoadedAttachments() {
@@ -50,8 +52,13 @@ public class LazyAttachmentCollection
     private void loadAll() {
         try {
             Attachment a = deserializer.readNext();
+            int count = 0;
             while (a != null) {
                 attachments.add(a);
+                count++;
+                if (count > maxAttachmentCount) {
+                    throw new IOException("The message contains more attachments than are permitted");
+                }
                 a = deserializer.readNext();
             }
         } catch (IOException e) {
