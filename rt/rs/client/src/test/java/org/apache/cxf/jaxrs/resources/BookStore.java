26212d8581b27825fd9ccaf2894c1ce3c8854743
Alexey Markevich <buhhunyx@gmail.com>
2019-10-08 14:36:00 +0300

    cxf-rt-rs-client: set Accept header to wildcard for void response (#585)
    cxf-rt-rs-client: set Accept header to wildcard for void response

--- rt/rs/client/src/test/java/org/apache/cxf/jaxrs/resources/BookStore.java
+++ rt/rs/client/src/test/java/org/apache/cxf/jaxrs/resources/BookStore.java
@@ -20,11 +20,13 @@
 
 package org.apache.cxf.jaxrs.resources;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/bookstore/")
@@ -46,8 +48,8 @@ public class BookStore extends BookSuperClass implements BookInterface {
 
     @POST
     @Path("/books")
-    public Response addBook(Book book) {
-        return null;
+    @Consumes(MediaType.APPLICATION_XML)
+    public void addBook(Book book) {
     }
 
     @PUT
