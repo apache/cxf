67d670b34919dd2f64255505fcf9b3d222b56e7e
Colm O hEigeartaigh <coheigea@apache.org>
2019-11-07 17:10:38 +0000

    Make sure we're getting the Session NotOnOrAfter from the right assertion

--- rt/rs/security/sso/saml/src/test/java/org/apache/cxf/rs/security/saml/sso/SAMLSSOResponseValidatorTest.java
+++ rt/rs/security/sso/saml/src/test/java/org/apache/cxf/rs/security/saml/sso/SAMLSSOResponseValidatorTest.java
@@ -55,6 +55,7 @@ import org.opensaml.xmlsec.signature.KeyInfo;
 import org.opensaml.xmlsec.signature.Signature;
 import org.opensaml.xmlsec.signature.support.SignatureConstants;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
@@ -85,7 +86,12 @@ public class SAMLSSOResponseValidatorTest {
         validator.setClientAddress("http://apache.org");
         validator.setRequestId("12345");
         validator.setSpIdentifier("http://service.apache.org");
-        validator.validateSamlResponse(response, false);
+
+        SSOValidatorResponse validateSamlResponse = validator.validateSamlResponse(response, false);
+        assertEquals(response.getID(), validateSamlResponse.getResponseId());
+        assertNotNull(validateSamlResponse.getAssertionElement());
+        assertNotNull(validateSamlResponse.getCreated());
+        assertNotNull(validateSamlResponse.getSessionNotOnOrAfter());
     }
 
     @org.junit.Test
