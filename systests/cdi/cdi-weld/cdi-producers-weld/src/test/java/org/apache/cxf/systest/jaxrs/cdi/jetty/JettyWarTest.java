997eed0a4bc68445fe46dcf093548e04b17e1566
Colm O hEigeartaigh <coheigea@apache.org>
2019-11-19 13:19:05 +0000

    Updating CDI API + JBoss Weld versions

--- systests/cdi/cdi-weld/cdi-producers-weld/src/test/java/org/apache/cxf/systest/jaxrs/cdi/jetty/JettyWarTest.java
+++ systests/cdi/cdi-weld/cdi-producers-weld/src/test/java/org/apache/cxf/systest/jaxrs/cdi/jetty/JettyWarTest.java
@@ -21,8 +21,8 @@ package org.apache.cxf.systest.jaxrs.cdi.jetty;
 import org.apache.cxf.jaxrs.model.AbstractResourceInfo;
 import org.apache.cxf.systest.jaxrs.cdi.AbstractBookStoreCdiTest;
 import org.apache.cxf.systests.cdi.base.jetty.AbstractJettyServer;
-import org.jboss.weld.environment.Container;
 import org.jboss.weld.environment.jetty.JettyContainer;
+import org.jboss.weld.environment.servlet.Container;
 import org.jboss.weld.environment.servlet.Listener;
 
 import org.junit.BeforeClass;
