454a9110228faa8a8fb305c0ea07b5ea5b043867
Colm O hEigeartaigh <coheigea@apache.org>
2019-12-18 11:51:56 +0000

    Fixing bug in WrappedAttachments.containsAll
    (cherry picked from commit 43598810108b5b5f29caed64409cadfa874e09d0)
    (cherry picked from commit 73a8be32b5e143dcf1908bd4e73249c116b14f7c)
    
    # Conflicts:
    #	rt/frontend/jaxws/src/test/java/org/apache/cxf/jaxws/context/WrappedAttachmentsTest.java

--- rt/frontend/jaxws/src/test/java/org/apache/cxf/jaxws/context/WrappedAttachmentsTest.java
+++ rt/frontend/jaxws/src/test/java/org/apache/cxf/jaxws/context/WrappedAttachmentsTest.java
@@ -19,7 +19,9 @@
 
 package org.apache.cxf.jaxws.context;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.activation.DataHandler;
@@ -35,6 +37,7 @@ import org.junit.Test;
  *
  */
 public class WrappedAttachmentsTest extends Assert {
+
     @Test
     public void testCreateAndModify() {
         Map<String, DataHandler> content = new HashMap<>();
@@ -46,6 +49,11 @@ public class WrappedAttachmentsTest extends Assert {
 
         assertEquals(2, attachments.size());
         assertFalse(attachments.isEmpty());
+
+        assertTrue(attachments.containsAll(attachments));
+        List<String> testCollection = new ArrayList<>();
+        testCollection.add("Some value");
+        assertFalse(attachments.containsAll(testCollection));
 
         attachments.add(att3);
         assertEquals(3, attachments.size());
@@ -70,13 +78,13 @@ public class WrappedAttachmentsTest extends Assert {
         atts = attachments.toArray(new Attachment[attachments.size()]); //NOPMD - explicitly test this
         assertEquals(1, atts.length);
         assertEquals("att-1".equals(attx.getId()) ? "att-2" : "att-1", atts[0].getId());
-        
-        Object o[] = attachments.toArray(); //NOPMD - explicitly test this
+
+        Object[] o = attachments.toArray(); //NOPMD - explicitly test this
         assertEquals(1, o.length);
         Attachment a = (Attachment)o[0];
         assertEquals("att-1".equals(attx.getId()) ? "att-2" : "att-1", a.getId());
 
-        
+
         attachments.clear();
         assertTrue(attachments.isEmpty());
         assertTrue(content.isEmpty());
