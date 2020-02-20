8636c3760a97979f42e45080230a27b81df5d436
Jonathan Gallimore <jon@jrg.me.uk>
2020-02-20 10:25:11 +0000

    Prevent RMI registry rebind (#641)
    * Prevent registry rebind
    
    * Add the RMI server when not running threaded
    
    * Update MBServerConnectorFactory.java
    
    * Add compiler option for Java 11. Will look to see if there's a way to avoid the use of sun.rmi.registry altogether
    
    * Identify the lookup name from the JMX URL
    
    * Adding unit test for getBindingName()
    
    * Re-instating test removed by mistake
    
    Co-authored-by: Colm O hEigeartaigh <coheigea@users.noreply.github.com>
    (cherry picked from commit 543de65106515538ecaef8b57c554e88a7bda0d1)
    (cherry picked from commit 98ec361acd390483005de12ca3b10cf49cbdcf8a)
    
    # Conflicts:
    #	rt/management/pom.xml

--- rt/management/src/main/java/org/apache/cxf/management/jmx/MBServerConnectorFactory.java
+++ rt/management/src/main/java/org/apache/cxf/management/jmx/MBServerConnectorFactory.java
@@ -20,7 +20,13 @@
 package org.apache.cxf.management.jmx;
 
 import java.io.IOException;
-import java.rmi.registry.LocateRegistry;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.rmi.AccessException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,8 +34,9 @@ import java.util.logging.Logger;
 import javax.management.MBeanServer;
 import javax.management.MBeanServerFactory;
 import javax.management.remote.JMXConnectorServer;
-import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
+import javax.management.remote.rmi.RMIConnectorServer;
+import javax.management.remote.rmi.RMIJRMPServerImpl;
 
 import org.apache.cxf.common.logging.LogUtils;
 
@@ -57,6 +64,10 @@ public final class MBServerConnectorFactory {
 
     private static JMXConnectorServer connectorServer;
 
+    private static Remote remoteServerStub;
+
+    private static RMIJRMPServerImpl rmiServer;
+
     private static class MBServerConnectorFactoryHolder {
         private static final MBServerConnectorFactory INSTANCE =
             new MBServerConnectorFactory();
@@ -71,7 +82,7 @@ public final class MBServerConnectorFactory {
 
     }
 
-    private int getURLLocalHostPort(String url) {
+    static int getServerPort(final String url) {
         int portStart = url.indexOf("localhost") + 10;
         int portEnd;
         int port = 0;
@@ -127,29 +138,24 @@ public final class MBServerConnectorFactory {
         }
 
         // Create the JMX service URL.
-        JMXServiceURL url = new JMXServiceURL(serviceUrl);
+        final JMXServiceURL url = new JMXServiceURL(serviceUrl);
 
         // if the URL is localhost, start up an Registry
         if (serviceUrl.indexOf("localhost") > -1
             && url.getProtocol().compareToIgnoreCase("rmi") == 0) {
             try {
-                int port = getURLLocalHostPort(serviceUrl);
-                try {
-                    LocateRegistry.createRegistry(port);
-                } catch (Exception ex) {
-                    // the registry may had been created
-                    LocateRegistry.getRegistry(port);
-                }
+                int port = getRegistryPort(serviceUrl);
+                new JmxRegistry(port, getBindingName(url));
 
             } catch (Exception ex) {
                 LOG.log(Level.SEVERE, "CREATE_REGISTRY_FAULT_MSG", new Object[]{ex});
             }
         }
 
-        // Create the connector server now.
-        connectorServer =
-            JMXConnectorServerFactory.newJMXConnectorServer(url, environment, server);
+        rmiServer = new RMIJRMPServerImpl(getServerPort(serviceUrl), null, null, environment);
 
+        // Create the connector server now.
+        connectorServer = new RMIConnectorServer(url, environment, rmiServer, server);
 
         if (threaded) {
              // Start the connector server asynchronously (in a separate thread).
@@ -157,6 +163,7 @@ public final class MBServerConnectorFactory {
                 public void run() {
                     try {
                         connectorServer.start();
+                        remoteServerStub = rmiServer.toStub();
                     } catch (IOException ex) {
                         LOG.log(Level.SEVERE, "START_CONNECTOR_FAILURE_MSG", new Object[]{ex});
                     }
@@ -169,11 +176,44 @@ public final class MBServerConnectorFactory {
         } else {
              // Start the connector server in the same thread.
             connectorServer.start();
+            remoteServerStub = rmiServer.toStub();
         }
 
         if (LOG.isLoggable(Level.INFO)) {
             LOG.info("JMX connector server started: " + connectorServer);
         }
+    }
+
+    static int getRegistryPort(final String url) {
+        int serverStart = url.indexOf("/jndi/rmi://");
+        final String serverPart = url.substring(serverStart + 12);
+        int portStart = serverPart.indexOf(':') + 1;
+
+        int portEnd;
+        int port = 0;
+        if (portStart > 0) {
+            portEnd = indexNotOfNumber(serverPart, portStart);
+            if (portEnd > portStart) {
+                final String portString = serverPart.substring(portStart, portEnd);
+                port = Integer.parseInt(portString);
+            }
+        }
+        return port;
+    }
+
+    protected static String getBindingName(final JMXServiceURL jmxServiceURL) {
+        final String urlPath = jmxServiceURL.getURLPath();
+
+        try {
+            if (urlPath.startsWith("/jndi/")) {
+                return new URI(urlPath.substring(6)).getPath()
+                        .replaceAll("^/+", "").replaceAll("/+$", "");
+            }
+        } catch (URISyntaxException e) {
+            // ignore
+        }
+
+        return "jmxrmi"; // use the default
     }
 
     public void destroy() throws IOException {
@@ -183,4 +223,38 @@ public final class MBServerConnectorFactory {
         }
     }
 
+    /*
+     * Better to use the internal API than re-invent the wheel.
+     */
+    @SuppressWarnings("restriction")
+    private class JmxRegistry extends sun.rmi.registry.RegistryImpl {
+        private final String lookupName;
+
+        JmxRegistry(final int port, final String lookupName) throws RemoteException {
+            super(port);
+            this.lookupName = lookupName;
+        }
+
+        @Override
+        public Remote lookup(String s) throws RemoteException, NotBoundException {
+            return lookupName.equals(s) ? remoteServerStub : null;
+        }
+
+        @Override
+        public void bind(String s, Remote remote) throws RemoteException, AlreadyBoundException, AccessException {
+        }
+
+        @Override
+        public void unbind(String s) throws RemoteException, NotBoundException, AccessException {
+        }
+
+        @Override
+        public void rebind(String s, Remote remote) throws RemoteException, AccessException {
+        }
+
+        @Override
+        public String[] list() throws RemoteException {
+            return new String[] {lookupName};
+        }
+    }
 }
