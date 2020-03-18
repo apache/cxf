330f0f9d83c5e689a8db3c107b3d27a294161bbb
reta <drreta@gmail.com>
2020-03-17 21:00:23 -0400

    CXF-8238: Tracing spans need more tag to make it clearer

--- integration/tracing/tracing-opentracing/src/main/java/org/apache/cxf/tracing/opentracing/AbstractOpenTracingClientProvider.java
+++ integration/tracing/tracing-opentracing/src/main/java/org/apache/cxf/tracing/opentracing/AbstractOpenTracingClientProvider.java
@@ -99,6 +99,7 @@ public abstract class AbstractOpenTracingClientProvider extends AbstractTracingP
             }
 
             span.setTag(Tags.HTTP_STATUS.getKey(), responseStatus);
+            span.setTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CLIENT);
             span.finish();
             
             scope.close();
