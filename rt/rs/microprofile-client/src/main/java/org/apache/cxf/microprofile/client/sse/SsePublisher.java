cb0ece06e7252debc22b430872b5ef55a0fe315b
Andy McCright <j.andrew.mccright@gmail.com>
2020-04-17 14:19:57 -0500

    [CXF-8263] Support SSEs in MP Rest Client 2.0
    Signed-off-by: Andy McCright <j.andrew.mccright@gmail.com>

---
+++ rt/rs/microprofile-client/src/main/java/org/apache/cxf/microprofile/client/sse/SsePublisher.java
@@ -0,0 +1,98 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.microprofile.client.sse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.ws.rs.ext.Providers;
+import javax.ws.rs.sse.InboundSseEvent;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+public class SsePublisher implements Publisher<InboundSseEvent> {
+
+    final Executor executor;
+    final BufferedReader br;
+    final Providers providers;
+    final List<SseSubscription> subscriptions = new LinkedList<>();
+    AtomicBoolean isStarted = new AtomicBoolean(false);
+
+    SsePublisher(InputStream is, Executor executor, Providers providers) {
+        br = new BufferedReader(new InputStreamReader(is));
+        this.executor = executor;
+        this.providers = providers;
+    }
+
+    @Override
+    public void subscribe(Subscriber<? super InboundSseEvent> subscriber) {
+        SseSubscription subscription = new SseSubscription(this, subscriber);
+        subscriptions.add(subscription);
+        subscription.fireSubscribe();
+        start();
+    }
+
+    private void start() {
+        if (isStarted.compareAndSet(false, true)) {
+            executor.execute(() -> {
+                try (BufferedReader br2 = br) {
+                    SseEventBuilder builder = new SseEventBuilder(providers);
+                    String line = br.readLine();
+                    while (line != null && !subscriptions.isEmpty()) {
+                        if (line.startsWith("data: ")) {
+                            builder.data(line.substring(6));
+                        } else if (line.startsWith("id: ")) {
+                            builder.id(line.substring(4));
+                        } else if (line.startsWith("event: ")) {
+                            builder.name(line.substring(7));
+                        } else if (line.startsWith(": ")) {
+                            builder.comment(line.substring(2));
+                        } else if ("".equals(line)) {
+                            InboundSseEvent event = builder.build();
+                            for (SseSubscription subscription : subscriptions) {
+                                subscription.fireEvent(event);
+                            }
+                            builder = new SseEventBuilder(providers);
+                        }
+                        line = br.readLine();
+                    }
+                    for (SseSubscription subscription : subscriptions) {
+                        subscription.fireComplete();
+                    }
+                } catch (IOException ex) {
+                    for (SseSubscription subscription : subscriptions) {
+                        subscription.fireError(ex);
+                    }
+                }
+            });
+        }
+    }
+
+    void removeSubscription(SseSubscription subscription) {
+        subscriptions.remove(subscription);
+        subscription.fireComplete();
+    }
+}
---
+++ rt/rs/microprofile-client/src/main/java/org/apache/cxf/microprofile/client/sse/SseSubscription.java
@@ -0,0 +1,115 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.microprofile.client.sse;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.ws.rs.sse.InboundSseEvent;
+
+import org.apache.cxf.common.util.SystemPropertyAction;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public class SseSubscription implements Subscription {
+
+    private static final int DEFAULT_BUFFER_SIZE = 
+        SystemPropertyAction.getInteger("org.apache.cxf.microprofile.client.sse.bufferSize", 256);
+    private final SsePublisher publisher;
+    private final Subscriber<? super InboundSseEvent> subscriber;
+    private final AtomicLong requested = new AtomicLong();
+    private final AtomicLong delivered = new AtomicLong();
+    private final AtomicBoolean completed = new AtomicBoolean();
+    private final Queue<InboundSseEvent> buffer = new LinkedList<>();
+    private final AtomicInteger bufferSize = new AtomicInteger(DEFAULT_BUFFER_SIZE);
+
+    SseSubscription(SsePublisher publisher, Subscriber<? super InboundSseEvent> subscriber) {
+        this.publisher = publisher;
+        this.subscriber = subscriber;
+    }
+
+    @Override
+    public void request(long n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("Only postive values are valid - passed-in " + n);
+        }
+        requested.addAndGet(n);
+        synchronized (buffer) {
+            InboundSseEvent bufferedEvent = null;
+            while (delivered.get() < requested.get()
+                   && (bufferedEvent = ((LinkedList<InboundSseEvent>) buffer).pollFirst()) != null) {
+                InboundSseEvent finalEvent = bufferedEvent;
+                delivered.updateAndGet(l -> {
+                    subscriber.onNext(finalEvent);
+                    return l + 1;
+                });
+            }
+        }
+    }
+
+    @Override
+    public void cancel() {
+        publisher.removeSubscription(this);
+
+    }
+
+    void fireSubscribe() {
+        subscriber.onSubscribe(this);
+    }
+
+    void fireEvent(InboundSseEvent event) {
+        delivered.updateAndGet(l -> {
+            if (l < requested.get()) {
+                subscriber.onNext(event);
+                return l + 1;
+            } else {
+                buffer(event);
+            }
+            return l;
+        });
+    }
+
+    void fireComplete() {
+        if (completed.compareAndSet(false, true)) {
+            subscriber.onComplete();
+        }
+    }
+
+    void fireError(Throwable t) {
+        if (completed.compareAndSet(false, true)) {
+            subscriber.onError(t);
+        }
+    }
+
+    void setBufferSize(int newSize) {
+        bufferSize.set(newSize);
+    }
+
+    private void buffer(InboundSseEvent event) {
+        synchronized (buffer) {
+            ((LinkedList<InboundSseEvent>) buffer).addLast(event);
+            if (buffer.size() > bufferSize.get()) {
+                ((LinkedList<InboundSseEvent>) buffer).removeFirst();
+            }
+        }
+    }
+}
