9713d86acedbfa6be6375e7d494c2771cd438f05
Colm O hEigeartaigh <coheigea@apache.org>
2020-06-08 11:14:27 +0100

    CXF-8298 - An invalid package name is generated

--- core/src/main/java/org/apache/cxf/common/util/PackageUtils.java
+++ core/src/main/java/org/apache/cxf/common/util/PackageUtils.java
@@ -132,12 +132,14 @@ public final class PackageUtils {
             String token = tokens.get(i);
             token = removeIllegalIdentifierChars(token);
 
+            token = token.toLowerCase();
+
             // this will check for reserved keywords
             if (JavaUtils.isJavaKeyword(token)) {
                 token = '_' + token;
             }
 
-            tokens.set(i, token.toLowerCase());
+            tokens.set(i, token);
         }
 
         // concat all the pieces and return it
