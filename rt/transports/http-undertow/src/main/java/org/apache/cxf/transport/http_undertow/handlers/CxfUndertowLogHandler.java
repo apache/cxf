10e1322cfb148d8ebc37b43ef2d3afd74dcdd226
Freeman Fang <freeman.fang@gmail.com>
2021-01-22 11:36:06 -0500

    [CXF-8412]introduce EnableRecordRequestStartTime propery so that request process duration time can be logged for http-undertow transport
    (cherry picked from commit 356c0eca80991fa821e95326f6a8578fc018a017)

--- rt/transports/http-undertow/src/main/java/org/apache/cxf/transport/http_undertow/handlers/CxfUndertowLogHandler.java
+++ rt/transports/http-undertow/src/main/java/org/apache/cxf/transport/http_undertow/handlers/CxfUndertowLogHandler.java
@@ -16,8 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-package org.apache.cxf.systest.http_undertow;
+package org.apache.cxf.transport.http_undertow.handlers;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,14 +34,20 @@ import io.undertow.server.handlers.accesslog.AccessLogHandler;
 import io.undertow.server.handlers.accesslog.AccessLogReceiver;
 import io.undertow.server.handlers.accesslog.DefaultAccessLogReceiver;
 
-public class LogHandler implements CXFUndertowHttpHandler {
 
+
+
+public class CxfUndertowLogHandler implements CXFUndertowHttpHandler {
+    
     private HttpHandler next;
     private AccessLogHandler accessLogHandler;
-
+    private String pattern;
+    private String outPutDirectory;
+    private String baseName;
+    private String suffix;
+    
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-
         if (accessLogHandler == null) {
             buildLogHandler();
         }
@@ -52,20 +57,34 @@ public class LogHandler implements CXFUndertowHttpHandler {
     @Override
     public void setNext(HttpHandler nextHandler) {
         this.next = nextHandler;
-
+        
     }
-
+    
     private void buildLogHandler() {
         HttpHandler handler = this.next;
         XnioWorker xnioWorker = createWorker(this.getClass().getClassLoader());
-        AccessLogReceiver logReceiver = DefaultAccessLogReceiver.builder().setLogWriteExecutor(xnioWorker)
-            .setOutputDirectory(new File("target").toPath()).setLogBaseName("request.")
-            .setLogNameSuffix("log").setRotate(true).build();
-        this.accessLogHandler = new AccessLogHandler(handler, logReceiver, "combined",
-                                                     AccessLogHandler.class.getClassLoader());
+        if (this.getOutPutDirectory() == null) {
+            this.setOutPutDirectory("./data/log");
+        }
+        if (this.getBaseName() == null) {
+            this.setBaseName("request.");
+        }
+        if (this.getSuffix() == null) {
+            this.setSuffix("log");
+        }
+        if (this.getPattern() == null) {
+            this.setPattern("combined");
+        }
+        AccessLogReceiver logReceiver = DefaultAccessLogReceiver.builder()
+            .setLogWriteExecutor(xnioWorker)
+            .setOutputDirectory(new File(this.getOutPutDirectory()).toPath())
+            .setLogBaseName(this.getBaseName())
+            .setLogNameSuffix(this.getSuffix()).setRotate(true).build();
+        this.accessLogHandler = new AccessLogHandler(handler, logReceiver, this.getPattern(),
+                             AccessLogHandler.class.getClassLoader());
 
     }
-
+    
     public static XnioWorker createWorker(ClassLoader loader) {
         try {
             if (loader == null) {
@@ -78,4 +97,38 @@ public class LogHandler implements CXFUndertowHttpHandler {
             return null;
         }
     }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getOutPutDirectory() {
+        return outPutDirectory;
+    }
+
+    public void setOutPutDirectory(String outPutDirectory) {
+        this.outPutDirectory = outPutDirectory;
+    }
+
+    public String getBaseName() {
+        return baseName;
+    }
+
+    public void setBaseName(String baseName) {
+        this.baseName = baseName;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+
 }
