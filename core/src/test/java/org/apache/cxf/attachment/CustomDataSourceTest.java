28f48f967c1733afb2592d197dbd3e9de70eb378
AapooYYY <75163856+AapooYYY@users.noreply.github.com>
2021-04-10 16:16:07 +0300

    CustomDataSource unit testing second try
    Just try silmple unit testing

--- core/src/test/java/org/apache/cxf/attachment/CustomDataSourceTest.java
+++ core/src/test/java/org/apache/cxf/attachment/CustomDataSourceTest.java
@@ -11,42 +11,14 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class CustomDataSourceTest {
-
-    private static final String firstID = "id1";
-    private static final String secondID = "id2";
-	private static final String thirdID = "id3";
-
-    @Test
-    public void testNoDataSource() throws Exception {
-        DataSource ds = new CustomDataSource(firstID,
-                Collections.singleton(new AttachmentImpl(firstID, new DataHandler((DataSource) null) {
-                    @Override
-                    public DataSource getDataSource() {
-                        return null;
-                    }
-                })));
-        try {
-            ds.getNamehistory();
-            fail();
-        } catch (IllegalStateException e) {
-            String message = e.getMessage();
-            assertTrue(message, message.contains(firstID));
-        }
-    }
-	
-
-
-    @Test
-    public void testNoAttachment() throws Exception {
-        DataSource ds = new CustomDataSource(firstID, Collections.singleton(new AttachmentImpl(secondID)));
-        try {
-            ds.getName();
-            fail();
-        } catch (IllegalStateException e) {
-            String message = e.getMessage();
-            assertTrue(message, message.contains(firstID));
-            assertTrue(message, message.contains(secondID));
-        }
+    public void testGetName() {
+        DataSource ds = new CustomDataSource("esa","2t5");
+        assertEquals("esa",ds.getName());
     }
 
+    public void testSetName() {
+        DataSource ds = new CustomDataSource("esa","2t5");
+        ds.setName("Pekka")
+        assertEquals("Pekka",ds.getName());
+    }
 }
