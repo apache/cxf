c29b1c23a7e09f9fe2c5412d0b9ece1bafb7c89c
Andriy Redko <drreta@gmail.com>
2021-08-04 09:11:49 -0400

    CXF-8572: Incorrectly parsed ETag when contains 'W/' as part of the content (#830)

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/EntityTagHeaderProvider.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/EntityTagHeaderProvider.java
@@ -39,16 +39,17 @@ public class EntityTagHeaderProvider implements HeaderDelegate<EntityTag> {
 
         String tag;
         boolean weak = false;
-        int i = header.indexOf(WEAK_PREFIX);
-        if (i != -1) {
+        final String trimmed = header.trim();
+        // See please https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag for weak validator 
+        if (trimmed.startsWith(WEAK_PREFIX)) {
             weak = true;
-            if (i + 2 < header.length()) {
-                tag = header.substring(i + 2);
+            if (trimmed.length() > 2) {
+                tag = trimmed.substring(2);
             } else {
                 return new EntityTag("", weak);
             }
         }  else {
-            tag = header;
+            tag = trimmed;
         }
         if (tag.length() > 0 && !tag.startsWith("\"") && !tag.endsWith("\"")) {
             return new EntityTag(tag, weak);
