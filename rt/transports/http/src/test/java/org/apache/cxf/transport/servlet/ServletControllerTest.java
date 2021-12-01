6f02a585e82415826dc30c2b598df1ab1b187586
Alexey Markevich <buhhunyx@gmail.com>
2021-12-01 17:09:26 +0300

    cxf-rt-transports-http: allow to use service list endpoint as lightweight healthcheck endpoint using HTTP HEAD method

--- rt/transports/http/src/test/java/org/apache/cxf/transport/servlet/ServletControllerTest.java
+++ rt/transports/http/src/test/java/org/apache/cxf/transport/servlet/ServletControllerTest.java
@@ -30,9 +30,9 @@ import org.apache.cxf.message.Message;
 import org.apache.cxf.transport.MessageObserver;
 import org.apache.cxf.transport.http.AbstractHTTPDestination;
 import org.apache.cxf.transport.http.DestinationRegistry;
+import org.apache.cxf.transport.servlet.servicelist.ServiceListGeneratorServlet;
 
 import org.easymock.EasyMock;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -40,40 +40,23 @@ import static org.junit.Assert.assertTrue;
 
 public class ServletControllerTest {
 
-    private HttpServletRequest req;
-    private HttpServletResponse res;
-    private DestinationRegistry registry;
-    private HttpServlet serviceListGenerator;
-
-    @Before
-    public void setUp() {
-        req = EasyMock.createMock(HttpServletRequest.class);
-        res = EasyMock.createMock(HttpServletResponse.class);
-        registry = EasyMock.createMock(DestinationRegistry.class);
-        serviceListGenerator = EasyMock.createMock(HttpServlet.class);
-    }
+    private HttpServletRequest req = EasyMock.createMock(HttpServletRequest.class);
+    private HttpServletResponse res = EasyMock.createMock(HttpServletResponse.class);
+    private DestinationRegistry registry = EasyMock.createMock(DestinationRegistry.class);
+    private HttpServlet serviceListGenerator = EasyMock.createMock(HttpServlet.class);
 
     private void setReq(String pathInfo, String requestUri, String styleSheet, String formatted) {
-        req.getPathInfo();
-        EasyMock.expectLastCall().andReturn(pathInfo).anyTimes();
-        req.getContextPath();
-        EasyMock.expectLastCall().andReturn("").anyTimes();
-        req.getServletPath();
-        EasyMock.expectLastCall().andReturn("").anyTimes();
+        EasyMock.expect(req.getPathInfo()).andReturn(pathInfo).anyTimes();
+        EasyMock.expect(req.getContextPath()).andReturn("").anyTimes();
+        EasyMock.expect(req.getServletPath()).andReturn("").anyTimes();
         req.setAttribute(Message.BASE_PATH, "http://localhost:8080");
         EasyMock.expectLastCall().anyTimes();
-        req.getRequestURI();
-        EasyMock.expectLastCall().andReturn(requestUri).times(2);
-        req.getParameter("stylesheet");
-        EasyMock.expectLastCall().andReturn(styleSheet);
-        req.getParameter("formatted");
-        EasyMock.expectLastCall().andReturn(formatted);
-        req.getRequestURL();
-        EasyMock.expectLastCall().andReturn(new StringBuffer("http://localhost:8080" + requestUri)); //NOPMD
-        registry.getDestinationsPaths();
-        EasyMock.expectLastCall().andReturn(Collections.emptySet()).atLeastOnce();
-        registry.getDestinationForPath("", true);
-        EasyMock.expectLastCall().andReturn(null).anyTimes();
+        EasyMock.expect(req.getRequestURI()).andReturn(requestUri).times(2);
+        EasyMock.expect(req.getParameter("stylesheet")).andReturn(styleSheet);
+        EasyMock.expect(req.getParameter("formatted")).andReturn(formatted);
+        EasyMock.expect(req.getRequestURL()).andReturn(new StringBuffer("http://localhost:8080").append(requestUri));
+        EasyMock.expect(registry.getDestinationsPaths()).andReturn(Collections.emptySet()).atLeastOnce();
+        EasyMock.expect(registry.getDestinationForPath("", true)).andReturn(null).anyTimes();
     }
 
     private void expectServiceListGeneratorCalled() throws ServletException, IOException {
@@ -161,6 +144,19 @@ public class ServletControllerTest {
         sc.invoke(req, res);
         assertFalse(sc.invokeDestinationCalled());
     }
+
+    @Test
+    public void testHealthcheck() throws Exception {
+        setReq(null, "/services", null, "true");
+        EasyMock.expect(req.getAttribute(ServletController.AUTH_SERVICE_LIST)).andReturn(null);
+        EasyMock.expect(req.getMethod()).andReturn("HEAD");
+
+        EasyMock.replay(req, registry);
+        TestServletController sc = new TestServletController(registry, new ServiceListGeneratorServlet(registry, null));
+        sc.invoke(req, res);
+        assertFalse(sc.invokeDestinationCalled());
+    }
+
 
     public static class TestServletController extends ServletController {
         private boolean invokeDestinationCalled;
