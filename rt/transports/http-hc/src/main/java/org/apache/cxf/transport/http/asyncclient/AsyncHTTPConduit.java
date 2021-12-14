b42882889c4d0c7010c65d17bae50e86bb624675
Freeman Fang <freeman.fang@gmail.com>
2021-12-14 16:18:07 -0500

    [CXF-8626]async http client may handle response by multiple threads on work queue

--- rt/transports/http-hc/src/main/java/org/apache/cxf/transport/http/asyncclient/AsyncHTTPConduit.java
+++ rt/transports/http-hc/src/main/java/org/apache/cxf/transport/http/asyncclient/AsyncHTTPConduit.java
@@ -673,7 +673,7 @@ public class AsyncHTTPConduit extends URLConnectionHTTPConduit {
         }
 
         protected void handleResponseAsync() throws IOException {
-            isAsync = true;
+            
         }
 
         protected void closeInputStream() throws IOException {
