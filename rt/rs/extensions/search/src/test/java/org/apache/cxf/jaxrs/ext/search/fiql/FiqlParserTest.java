696cb81463ac9a314c5b09bedce8037d36a86845
Andriy Redko <drreta@gmail.com>
2022-02-22 10:09:42 -0500

    CXF-8659: Can't filter when attribute type is LocalDate or LocalDateTime (#911)
    (cherry picked from commit e0df5b52626680967c05a65ac3a8305270e99891)
    (cherry picked from commit 9f413c0ef37c798b9cf69a899e63d5cae0788573)

--- rt/rs/extensions/search/src/test/java/org/apache/cxf/jaxrs/ext/search/fiql/FiqlParserTest.java
+++ rt/rs/extensions/search/src/test/java/org/apache/cxf/jaxrs/ext/search/fiql/FiqlParserTest.java
@@ -21,6 +21,14 @@ package org.apache.cxf.jaxrs.ext.search.fiql;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -207,6 +215,151 @@ public class FiqlParserTest {
     }
 
     @Test
+    public void testParseLocalDateWithDefaultFormat() throws SearchParseException, ParseException {
+        SearchCondition<Condition> filter = parser.parse("localDate=le=2010-03-11");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(SearchUtils.DEFAULT_DATE_FORMAT);
+        assertTrue(filter.isMet(new Condition("whatever", 15, null, LocalDate.parse("2010-03-11", df))));
+        assertTrue(filter.isMet(new Condition(null, null, null, LocalDate.parse("2010-03-10", df))));
+        assertFalse(filter.isMet(new Condition("blah", null, null, LocalDate.parse("2010-03-12", df))));
+        assertFalse(filter.isMet(new Condition(null, 123, null, LocalDate.parse("2010-03-12", df))));
+    }
+
+    @Test
+    public void testParseInstantWithDefaultFormat() throws SearchParseException, ParseException {
+        SearchCondition<Condition> filter = parser.parse("instant=le=2010-03-11");
+        DateFormat df = new SimpleDateFormat(SearchUtils.DEFAULT_DATE_FORMAT);
+        assertTrue(filter.isMet(new Condition("whatever", 15, null, df.parse("2010-03-11").toInstant())));
+        assertTrue(filter.isMet(new Condition(null, null, null, df.parse("2010-03-10").toInstant())));
+        assertFalse(filter.isMet(new Condition("blah", null, null, df.parse("2010-03-12").toInstant())));
+        assertFalse(filter.isMet(new Condition(null, 123, null, df.parse("2010-03-12").toInstant())));
+    }
+
+    @Test
+    public void testParseInstantDuration() throws Exception {
+        SearchCondition<Condition> filter = parser.parse("localDateTime=gt=-PT1M");
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime tenMinutesAgo = now.minusMinutes(10);
+        assertTrue(filter.isMet(new Condition(null, null, null, now)));
+        assertFalse(filter.isMet(new Condition(null, null, null, tenMinutesAgo)));
+    }
+
+    @Test
+    public void testParseLocalTime() throws SearchParseException, ParseException {
+        SearchCondition<Condition> filter = parser.parse("localTime=le=03:11:22");
+        assertTrue(filter.isMet(new Condition("whatever", 15, null, LocalTime.parse("03:11:22"))));
+        assertTrue(filter.isMet(new Condition(null, null, null, LocalTime.parse("03:11:21"))));
+        assertFalse(filter.isMet(new Condition("blah", null, null, LocalTime.parse("03:12:21"))));
+        assertFalse(filter.isMet(new Condition(null, 123, null, LocalTime.parse("03:12:21"))));
+    }
+
+    @Test
+    public void testParseOffsetTime() throws SearchParseException, ParseException {
+        SearchCondition<Condition> filter = parser.parse("offsetTime=le=03:11:22+01:00");
+        assertTrue(filter.isMet(new Condition("whatever", 15, null, OffsetTime.parse("03:11:22+01:00"))));
+        assertTrue(filter.isMet(new Condition(null, null, null, OffsetTime.parse("03:11:21+01:00"))));
+        assertFalse(filter.isMet(new Condition("blah", null, null, OffsetTime.parse("03:12:21+01:00"))));
+        assertFalse(filter.isMet(new Condition(null, 123, null, OffsetTime.parse("03:12:21+01:00"))));
+    }
+
+    @Test
+    public void testParseLocalDateTimeWithDefaultFormat() throws SearchParseException, ParseException {
+        SearchCondition<Condition> filter = parser.parse("localDateTime=le=2010-03-11T18:00:00");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        assertTrue(filter.isMet(new Condition("whatever", 15, null,
+            LocalDateTime.parse("2010-03-11T18:00:00", df))));
+        assertTrue(filter.isMet(new Condition(null, null,  null,
+            LocalDateTime.parse("2010-03-10T22:22:00", df))));
+        assertFalse(filter.isMet(new Condition("blah", null, null,
+            LocalDateTime.parse("2010-03-12T00:00:00", df))));
+        assertFalse(filter.isMet(new Condition(null, 123, null,
+            LocalDateTime.parse("2010-03-12T00:00:00", df))));
+    }
+
+    @Test
+    public void testParseLocalDateTimeWithCustomFormat() throws SearchParseException, ParseException {
+        Map<String, String> props = new HashMap<>();
+        props.put(SearchUtils.DATE_FORMAT_PROPERTY, "yyyy-MM-dd'T'HH:mm:ss");
+        props.put(SearchUtils.TIMEZONE_SUPPORT_PROPERTY, "false");
+        parser = new FiqlParser<>(Condition.class, props);
+
+        SearchCondition<Condition> filter = parser.parse("localDateTime=le=2010-03-11T18:00:00");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        assertTrue(filter.isMet(new Condition("whatever", 15, null,
+            LocalDateTime.parse("2010-03-11T18:00:00", df))));
+        assertTrue(filter.isMet(new Condition(null, null,  null,
+            LocalDateTime.parse("2010-03-10T22:22:00", df))));
+        assertFalse(filter.isMet(new Condition("blah", null, null,
+            LocalDateTime.parse("2010-03-12T00:00:00", df))));
+        assertFalse(filter.isMet(new Condition(null, 123, null, 
+            LocalDateTime.parse("2010-03-12T00:00:00", df))));
+    }
+
+    @Test
+    public void testParseOffsetDateTimeWithDefaultFormat() throws SearchParseException, ParseException {
+        SearchCondition<Condition> filter = parser.parse("offsetDateTime=le=2010-03-11T18:00:00+01:00");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
+        assertTrue(filter.isMet(new Condition("whatever", 15, null,
+            OffsetDateTime.parse("2010-03-11T18:00:00+0100", df))));
+        assertTrue(filter.isMet(new Condition(null, null,  null,
+            OffsetDateTime.parse("2010-03-10T22:22:00+0100", df))));
+        assertFalse(filter.isMet(new Condition("blah", null, null,
+            OffsetDateTime.parse("2010-03-12T00:00:00+0100", df))));
+        assertFalse(filter.isMet(new Condition(null, 123, null, 
+            OffsetDateTime.parse("2010-03-12T00:00:00+0100", df))));
+    }
+
+    @Test
+    public void testParseOffsetDateTimeWithCustomFormat() throws SearchParseException, ParseException {
+        Map<String, String> props = new HashMap<>();
+        props.put(SearchUtils.DATE_FORMAT_PROPERTY, "yyyy-MM-dd'T'HH:mm:ssZ");
+        props.put(SearchUtils.TIMEZONE_SUPPORT_PROPERTY, "true");
+        parser = new FiqlParser<>(Condition.class, props);
+
+        SearchCondition<Condition> filter = parser.parse("offsetDateTime=le=2010-03-11T18:00:00+01:00");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
+        assertTrue(filter.isMet(new Condition("whatever", 15, null, 
+            OffsetDateTime.parse("2010-03-11T18:00:00+0100", df))));
+        assertTrue(filter.isMet(new Condition(null, null,  null, 
+            OffsetDateTime.parse("2010-03-10T22:22:00+0100", df))));
+        assertFalse(filter.isMet(new Condition("blah", null, null, 
+            OffsetDateTime.parse("2010-03-12T00:00:00+0100", df))));
+        assertFalse(filter.isMet(new Condition(null, 123, null, OffsetDateTime.parse("2010-03-12T00:00:00+0100", df))));
+    }
+
+    @Test
+    public void testParseZonedDateTimeWithCustomFormat() throws SearchParseException, ParseException {
+        Map<String, String> props = new HashMap<>();
+        props.put(SearchUtils.DATE_FORMAT_PROPERTY, "yyyy-MM-dd'T'HH:mm:ss '['z']'");
+        props.put(SearchUtils.TIMEZONE_SUPPORT_PROPERTY, "false");
+        parser = new FiqlParser<>(Condition.class, props);
+
+        SearchCondition<Condition> filter = parser.parse("zonedDateTime=le=2010-03-11T18:00:00 [Europe/Paris]");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss' ['z']'");
+        assertTrue(filter.isMet(new Condition("whatever", 15, null, 
+            ZonedDateTime.parse("2010-03-11T18:00:00 [Europe/Paris]", df))));
+        assertTrue(filter.isMet(new Condition(null, null,  null, 
+            ZonedDateTime.parse("2010-03-10T22:22:00 [Europe/Paris]", df))));
+        assertFalse(filter.isMet(new Condition("blah", null, null, 
+            ZonedDateTime.parse("2010-03-12T00:00:00 [Europe/Paris]", df))));
+        assertFalse(filter.isMet(new Condition(null, 123, null, 
+            ZonedDateTime.parse("2010-03-12T00:00:00 [Europe/Paris]", df))));
+    }
+    
+    @Test
+    public void testParseZonedDateTimeWithDefaultFormat() throws SearchParseException, ParseException {
+        SearchCondition<Condition> filter = parser.parse("zonedDateTime=le=2010-03-11T18:00:00+01:00[Europe/Paris]");
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ'['z']'");
+        assertTrue(filter.isMet(new Condition("whatever", 15, null,
+            ZonedDateTime.parse("2010-03-11T18:00:00+0100[Europe/Paris]", df))));
+        assertTrue(filter.isMet(new Condition(null, null,  null,
+            ZonedDateTime.parse("2010-03-10T22:22:00+0100[Europe/Paris]", df))));
+        assertFalse(filter.isMet(new Condition("blah", null, null,
+            ZonedDateTime.parse("2010-03-12T00:00:00+0100[Europe/Paris]", df))));
+        assertFalse(filter.isMet(new Condition(null, 123, null,
+            ZonedDateTime.parse("2010-03-12T00:00:00+0100[Europe/Paris]", df))));
+    }
+    
+    @Test
     public void testParseComplex1() throws SearchParseException {
         SearchCondition<Condition> filter = parser.parse("name==ami*;level=gt=10");
         assertEquals(ConditionType.AND, filter.getConditionType());
@@ -334,14 +487,68 @@ public class FiqlParserTest {
         private String name2;
         private Integer level;
         private Date time;
+        private LocalDate localDate;
+        private LocalTime localTime;
+        private LocalDateTime localDateTime;
+        private OffsetTime offsetTime;
+        private OffsetDateTime offsetDateTime;
+        private ZonedDateTime zonedDateTime;
+        private Instant instant;
 
         public Condition() {
         }
 
         public Condition(String name, Integer level, Date time) {
+            this(name, level, time, (LocalDate)null);
+        }
+
+        public Condition(String name, Integer level, Date time, ZonedDateTime zonedDateTime) {
             this.name = name;
             this.level = level;
             this.time = time;
+            this.zonedDateTime = zonedDateTime;
+        }
+
+        public Condition(String name, Integer level, Date time, OffsetDateTime offsetDateTime) {
+            this.name = name;
+            this.level = level;
+            this.time = time;
+            this.offsetDateTime = offsetDateTime;
+        }
+
+        public Condition(String name, Integer level, Date time, OffsetTime offsetTime) {
+            this.name = name;
+            this.level = level;
+            this.time = time;
+            this.offsetTime = offsetTime;
+        }
+
+        public Condition(String name, Integer level, Date time, LocalDateTime localDateTime) {
+            this.name = name;
+            this.level = level;
+            this.time = time;
+            this.localDateTime = localDateTime;
+        }
+
+        public Condition(String name, Integer level, Date time, LocalDate localDate) {
+            this.name = name;
+            this.level = level;
+            this.time = time;
+            this.localDate = localDate;
+        }
+
+        public Condition(String name, Integer level, Date time, LocalTime localTime) {
+            this.name = name;
+            this.level = level;
+            this.time = time;
+            this.localTime = localTime;
+        }
+
+        public Condition(String name, Integer level, Date time, Instant instant) {
+            this.name = name;
+            this.level = level;
+            this.time = time;
+            this.instant = instant;
         }
 
         public Condition(String name, Integer level, Date time, String name2) {
@@ -384,6 +591,61 @@ public class FiqlParserTest {
             return name2;
         }
 
+        public LocalDate getLocalDate() {
+            return localDate;
+        }
+        
+        public void setLocalDate(LocalDate localDate) {
+            this.localDate = localDate;
+        }
+        
+        public LocalTime getLocalTime() {
+            return localTime;
+        }
+        
+        public void setLocalTime(LocalTime localTime) {
+            this.localTime = localTime;
+        }
+        
+        public void setLocalDateTime(LocalDateTime localDateTime) {
+            this.localDateTime = localDateTime;
+        }
+        
+        public LocalDateTime getLocalDateTime() {
+            return localDateTime;
+        }
+        
+        public void setOffsetTime(OffsetTime offsetTime) {
+            this.offsetTime = offsetTime;
+        }
+        
+        public OffsetTime getOffsetTime() {
+            return offsetTime;
+        }
+        
+        public void setOffsetDateTime(OffsetDateTime offsetDateTime) {
+            this.offsetDateTime = offsetDateTime;
+        }
+        
+        public OffsetDateTime getOffsetDateTime() {
+            return offsetDateTime;
+        }
+        
+        public void setZonedDateTime(ZonedDateTime zonedDateTime) {
+            this.zonedDateTime = zonedDateTime;
+        }
+        
+        public ZonedDateTime getZonedDateTime() {
+            return zonedDateTime;
+        }
+        
+        public Instant getInstant() {
+            return instant;
+        }
+        
+        public void setInstant(Instant instant) {
+            this.instant = instant;
+        }
     }
 
 
