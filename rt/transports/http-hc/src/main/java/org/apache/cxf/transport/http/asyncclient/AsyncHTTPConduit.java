9d4e5fbb3706d5a30f18d0610b2b7620606b8d18
Andriy Redko <drreta@gmail.com>
2022-03-12 10:01:46 -0500

    CXF-8673: Bump httpasyncclient version to 4.1.5 (#922)
    (cherry picked from commit efa7f6d730fe6e1facf92d86821fa15bc123d201)
    (cherry picked from commit 2674fd03794efd3a01f9b6da883381953b99327a)

--- rt/transports/http-hc/src/main/java/org/apache/cxf/transport/http/asyncclient/AsyncHTTPConduit.java
+++ rt/transports/http-hc/src/main/java/org/apache/cxf/transport/http/asyncclient/AsyncHTTPConduit.java
@@ -562,7 +562,10 @@ public class AsyncHTTPConduit extends URLConnectionHTTPConduit {
                                     setSSLSession(sslsession);
                                 }
                             });
+                    // See please https://issues.apache.org/jira/browse/HTTPASYNC-168, the attribute names
+                    // are case-sensitive.
                     ctx.setAttribute("http.iosession-factory-registry", regBuilder.build());
+                    ctx.setAttribute("http.ioSession-factory-registry", regBuilder.build());
                 } catch (GeneralSecurityException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
