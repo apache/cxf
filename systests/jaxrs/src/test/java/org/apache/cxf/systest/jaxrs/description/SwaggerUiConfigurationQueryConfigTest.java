8c71f7330761d4b49a45b2d8c07e4a772ea6c7f8
Andriy Redko <drreta@gmail.com>
2022-05-31 21:23:29 -0400

    CXF-8683: Support SwaggerUI 4.10.3+ (#952)
    (cherry picked from commit df05725910b7e6b113faf214d787abd84c645bc7)
    (cherry picked from commit bc5d02a1823de215646508b22976358b1905adab)
    
    # Conflicts:
    #	distribution/src/main/release/samples/pom.xml
    #	parent/pom.xml

--- systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/description/SwaggerUiConfigurationQueryConfigTest.java
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/description/SwaggerUiConfigurationQueryConfigTest.java
@@ -94,7 +94,6 @@ public class SwaggerUiConfigurationQueryConfigTest extends AbstractBusClientServ
         try (Response response = uiClient.get()) {
             String html = response.readEntity(String.class);
             assertThat(html, containsString("<!-- HTML"));
-            assertThat(html, containsString("url: \"https://petstore.swagger.io/v2/swagger.json\","));
             assertThat(response.getMediaType(), equalTo(MediaType.TEXT_HTML_TYPE));
         }
     }
--- systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/description/SwaggerUiConfigurationTest.java
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/description/SwaggerUiConfigurationTest.java
@@ -112,7 +112,6 @@ public class SwaggerUiConfigurationTest extends AbstractBusClientServerTestBase 
         try (Response response = uiClient.get()) {
             String html = response.readEntity(String.class);
             assertThat(html, containsString("<!-- HTML"));
-            assertThat(html, containsString("url: \"/swagger.json\","));
             assertThat(response.getMediaType(), equalTo(MediaType.TEXT_HTML_TYPE));
         }
     }
@@ -129,7 +128,6 @@ public class SwaggerUiConfigurationTest extends AbstractBusClientServerTestBase 
         try (Response response = uiClient.get()) {
             String html = response.readEntity(String.class);
             assertThat(html, containsString("<!-- HTML"));
-            assertThat(html, containsString("url: \"/swagger.json\","));
             assertThat(response.getMediaType(), equalTo(MediaType.TEXT_HTML_TYPE));
         }
     }
