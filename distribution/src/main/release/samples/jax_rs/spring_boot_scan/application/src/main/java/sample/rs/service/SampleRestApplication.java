96a2e7f27fde6ebdffd511ce69120adfff7b9dcb
Andriy Redko <drreta@gmail.com>
2022-11-25 12:18:48 -0500

    Update Spring Framework to 6.0.2 and Spring Boot to 3.0.0 (#1036)

--- distribution/src/main/release/samples/jax_rs/spring_boot_scan/application/src/main/java/sample/rs/service/SampleRestApplication.java
+++ distribution/src/main/release/samples/jax_rs/spring_boot_scan/application/src/main/java/sample/rs/service/SampleRestApplication.java
@@ -20,11 +20,11 @@ package sample.rs.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SampleRestApplication {
 
     public static void main(String[] args) {
