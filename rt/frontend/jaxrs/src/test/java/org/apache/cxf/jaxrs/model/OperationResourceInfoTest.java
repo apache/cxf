9900e83b5970c32d40b797584d5cf6739952a97f
Andriy Redko <drreta@gmail.com>
2022-12-27 17:16:20 -0500

    CXF-8797: NameBinding ignored when implementing interface (#1045)

--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/model/OperationResourceInfoTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/model/OperationResourceInfoTest.java
@@ -19,13 +19,21 @@
 
 package org.apache.cxf.jaxrs.model;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.NameBinding;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.jaxrs.utils.AnnotationUtils;
 import org.apache.cxf.message.Exchange;
 import org.apache.cxf.message.ExchangeImpl;
 import org.apache.cxf.message.Message;
@@ -51,6 +59,30 @@ public class OperationResourceInfoTest {
         };
 
     }
+    
+    @Produces("text/xml")
+    @Consumes("application/xml")
+    interface TestInteface {
+        @Produces("text/plain")
+        void doIt();
+
+        @CustomNameBinding
+        @Consumes("application/atom+xml")
+        void doThat();
+    }
+
+    static class TestClass2 implements TestInteface {
+        @CustomNameBinding
+        @Override
+        public void doIt() {
+        }
+
+        @Override
+        public void doThat() {
+        }
+
+    }
+
 
     @Test
     public void testConsumeTypes() throws Exception {
@@ -136,6 +168,31 @@ public class OperationResourceInfoTest {
         assertEquals(0, result);
     }
 
+    @Test
+    public void testNameBindingsClass() throws NoSuchMethodException, SecurityException {
+        final Method method = TestClass2.class.getMethod("doIt", new Class[]{});
+
+        OperationResourceInfo ori = new OperationResourceInfo(
+                method,
+                AnnotationUtils.getAnnotatedMethod(TestClass2.class,  method),
+                new ClassResourceInfo(TestClass2.class));
+
+        final Set<String> names = ori.getNameBindings();
+        assertEquals(Collections.singleton(CustomNameBinding.class.getName()), names);
+    }
+
+    @Test
+    public void testNameBindingsInterface() throws NoSuchMethodException, SecurityException {
+        final Method method = TestClass2.class.getMethod("doThat", new Class[]{});
+
+        OperationResourceInfo ori = new OperationResourceInfo(
+                method,
+                AnnotationUtils.getAnnotatedMethod(TestClass2.class, method),
+                new ClassResourceInfo(TestClass2.class));
+
+        Set<String> names = ori.getNameBindings();
+        assertEquals(Collections.singleton(CustomNameBinding.class.getName()), names);
+    }
 
     private static Message createMessage() {
         Message m = new MessageImpl();
@@ -147,5 +204,11 @@ public class OperationResourceInfoTest {
         EasyMock.replay(endpoint);
         e.put(Endpoint.class, endpoint);
         return m;
+    }
+    
+    @Target({ ElementType.TYPE, ElementType.METHOD })
+    @Retention(value = RetentionPolicy.RUNTIME)
+    @NameBinding
+    public @interface CustomNameBinding {
     }
 }
