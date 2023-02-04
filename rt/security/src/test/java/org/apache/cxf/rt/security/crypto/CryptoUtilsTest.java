ba3e7f15d6bb9353b9ed2ffa240398783f1ae54d
epuzanov <epuzanov@users.noreply.github.com>
2023-02-05 00:10:50 +0100

    add CryptoUtils Compression test

---
+++ rt/security/src/test/java/org/apache/cxf/rt/security/crypto/CryptoUtilsTest.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.rt.security.crypto;
+
+import java.lang.SecurityException;
+import javax.crypto.SecretKey;
+
+import org.apache.cxf.rt.security.crypto.CryptoUtils;
+import org.apache.cxf.rt.security.crypto.KeyProperties;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
+
+public class CryptoUtilsTest {
+
+    @Test
+    public void testCompression() {
+        try {
+            byte[] bytes = "testString".getBytes("UTF-8");
+            KeyProperties keyProps = new KeyProperties("AES");
+            keyProps.setCompressionSupported(true);
+            SecretKey secretKey = CryptoUtils.getSecretKey(keyProps);
+            byte[] encryptedBytes = CryptoUtils.encryptBytes(bytes, secretKey, keyProps);
+            byte[] decryptedBytes = CryptoUtils.decryptBytes(encryptedBytes, secretKey, keyProps);
+            assertArrayEquals(bytes, decryptedBytes);
+        } catch (SecurityException ex) {
+            fail(ex.toString());
+        } catch (Exception ex) {
+        }
+    }
+}
