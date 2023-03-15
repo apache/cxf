7fe157b7b80ecd7c2c5003f6b2207db1b75306dc
Daniel Kulp <dan@kulp.com>
2023-03-15 14:30:09 -0400

    Move a test to inProcess on all platforms and make it quiet
    (cherry picked from commit 400b721be3d7d7b341a412722e20158b43a47f34)

--- systests/ws-specs/src/test/java/org/apache/cxf/systest/ws/addressing/NonDecoupledTest.java
+++ systests/ws-specs/src/test/java/org/apache/cxf/systest/ws/addressing/NonDecoupledTest.java
@@ -47,13 +47,9 @@ public class NonDecoupledTest extends MAPTestBase {
 
     @BeforeClass
     public static void startServers() throws Exception {
-        // special case handling for WS-Addressing system test to avoid
-        // UUID related issue when server is run as separate process
-        // via maven on Win2k
-        boolean inProcess = "Windows 2000".equals(System.getProperty("os.name"));
         assertTrue("server did not launch correctly",
                    launchServer(Server.class, null,
-                                new String[] {ADDRESS, GreeterImpl.class.getName()}, inProcess));
+                                new String[] {ADDRESS, GreeterImpl.class.getName()}, true));
     }
 
     @WebService(serviceName = "SOAPServiceAddressing",
@@ -62,7 +58,9 @@ public class NonDecoupledTest extends MAPTestBase {
                 targetNamespace = "http://apache.org/hello_world_soap_http",
                 wsdlLocation = "testutils/hello_world.wsdl")
     public static class GreeterImpl extends org.apache.cxf.systest.ws.addressing.AbstractGreeterImpl {
-
+        GreeterImpl() {
+            super(true);
+        }
     }
     public String getAddress() {
         return ADDRESS;
