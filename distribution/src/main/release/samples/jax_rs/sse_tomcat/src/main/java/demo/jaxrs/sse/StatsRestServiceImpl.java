78f90b6d6f0985ecc2277c37bbaaa594b0adb78d
Andriy Redko <drreta@gmail.com>
2023-07-29 11:43:55 -0400

    Minor distribution/samples fixes and dependency updates
    (cherry picked from commit 9727de7019a92c184a31e00d2ddffacef7b15d4d)
    
    # Conflicts:
    #	distribution/src/main/release/samples/jax_rs/sse_client/src/main/java/demo/jaxrs/sse/StatsRestServiceImpl.java
    #	distribution/src/main/release/samples/jax_rs/sse_tomcat/src/main/java/demo/jaxrs/sse/StatsRestServiceImpl.java
    #	distribution/src/main/release/samples/pom.xml

--- distribution/src/main/release/samples/jax_rs/sse_tomcat/src/main/java/demo/jaxrs/sse/StatsRestServiceImpl.java
+++ distribution/src/main/release/samples/jax_rs/sse_tomcat/src/main/java/demo/jaxrs/sse/StatsRestServiceImpl.java
@@ -28,7 +28,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.sse.OutboundSseEvent;
-import javax.ws.rs.sse.OutboundSseEvent.Builder;
 import javax.ws.rs.sse.Sse;
 import javax.ws.rs.sse.SseEventSink;
 
@@ -49,7 +48,7 @@ public class StatsRestServiceImpl {
         new Thread() {
             public void run() {
                 try {
-                    final Builder builder = sse.newEventBuilder();
+                    final OutboundSseEvent.Builder builder = sse.newEventBuilder();
                     sink.send(createStatsEvent(builder.name("stats"), 1));
                     Thread.sleep(1000);
                     sink.send(createStatsEvent(builder.name("stats"), 2));
