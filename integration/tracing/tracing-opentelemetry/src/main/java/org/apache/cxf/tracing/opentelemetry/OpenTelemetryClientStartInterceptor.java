af5919d1a99b295fb413ff3cb0b8143ac3acbfd1
Khacharia Dipesh <Dipesh.Khacharia@swisscom.com>
2023-10-09 14:50:08 +0200

    fix: oneWay client calls do not close the span causing bigger issues in e2e tracing

--- integration/tracing/tracing-opentelemetry/src/main/java/org/apache/cxf/tracing/opentelemetry/OpenTelemetryClientStartInterceptor.java
+++ integration/tracing/tracing-opentelemetry/src/main/java/org/apache/cxf/tracing/opentelemetry/OpenTelemetryClientStartInterceptor.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,16 +18,15 @@
  */
 package org.apache.cxf.tracing.opentelemetry;
 
-import java.util.List;
-import java.util.Map;
-
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
 import org.apache.cxf.helpers.CastUtils;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.Phase;
 
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.trace.Tracer;
+import java.util.List;
+import java.util.Map;
 
 public class OpenTelemetryClientStartInterceptor extends AbstractOpenTelemetryClientInterceptor {
     public OpenTelemetryClientStartInterceptor(final OpenTelemetry openTelemetry, final String instrumentationName) {
@@ -38,34 +37,39 @@ public class OpenTelemetryClientStartInterceptor extends AbstractOpenTelemetryCl
         this(Phase.PRE_STREAM, openTelemetry, tracer);
     }
 
-    public OpenTelemetryClientStartInterceptor(final String phase, final OpenTelemetry openTelemetry, 
-            final String instrumentationName) {
+    public OpenTelemetryClientStartInterceptor(final String phase, final OpenTelemetry openTelemetry,
+                                               final String instrumentationName) {
         super(phase, openTelemetry, instrumentationName);
     }
 
-    public OpenTelemetryClientStartInterceptor(final String phase, final OpenTelemetry openTelemetry, 
-            final Tracer tracer) {
+    public OpenTelemetryClientStartInterceptor(final String phase, final OpenTelemetry openTelemetry,
+                                               final Tracer tracer) {
         super(phase, openTelemetry, tracer);
     }
 
     @Override
     public void handleMessage(Message message) throws Fault {
         final Map<String, List<String>> headers = CastUtils
-            .cast((Map<?, ?>)message.get(Message.PROTOCOL_HEADERS));
+                .cast((Map<?, ?>) message.get(Message.PROTOCOL_HEADERS));
         final TraceScopeHolder<TraceScope> holder = super.startTraceSpan(headers, getUri(message),
-                                                                         (String)message
-                                                                             .get(Message.HTTP_REQUEST_METHOD));
+                (String) message
+                        .get(Message.HTTP_REQUEST_METHOD));
 
         if (holder != null) {
             message.getExchange().put(TRACE_SPAN, holder);
+        }
+
+        // STOP interceptor is not going to be invoked for oneWay calls. Need to close the span.
+        if (message.getExchange().isOneWay()) {
+            // set response status = 200 as default since the actual one will never come to this interceptor
+            super.stopTraceSpan(holder, 200);
         }
     }
 
     @Override
     public void handleFault(Message message) {
-        @SuppressWarnings("unchecked")
-        final TraceScopeHolder<TraceScope> holder = (TraceScopeHolder<TraceScope>)message.getExchange()
-            .get(TRACE_SPAN);
+        @SuppressWarnings("unchecked") final TraceScopeHolder<TraceScope> holder = (TraceScopeHolder<TraceScope>) message.getExchange()
+                .get(TRACE_SPAN);
 
         final Exception ex = message.getContent(Exception.class);
         super.stopTraceSpan(holder, ex);
