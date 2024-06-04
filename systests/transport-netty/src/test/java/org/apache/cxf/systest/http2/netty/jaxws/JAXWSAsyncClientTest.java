1a4c5067e22b83c6bd2989683b622916220f1afd
Andriy Redko <drreta@gmail.com>
2024-06-03 21:29:35 -0400

    Fix checkstyle violations (post-merge)

--- systests/transport-netty/src/test/java/org/apache/cxf/systest/http2/netty/jaxws/JAXWSAsyncClientTest.java
+++ systests/transport-netty/src/test/java/org/apache/cxf/systest/http2/netty/jaxws/JAXWSAsyncClientTest.java
@@ -27,6 +27,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
 import javax.jws.WebService;
 import javax.xml.ws.Response;
 import javax.xml.ws.soap.SOAPFaultException;
