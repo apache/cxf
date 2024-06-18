9d5224155ad93ab02e49d6a7e58cf86b4f1b82e2
Andriy Redko <drreta@gmail.com>
2024-06-18 17:21:25 -0400

    CXF-9007: NullPointerException in XMLStreamDataWriter.writeNode (#1861)

--- systests/transport-hc5/src/test/java/org/apache/cxf/systest/hc5/jaxws/JAXWSAsyncClientTest.java
+++ systests/transport-hc5/src/test/java/org/apache/cxf/systest/hc5/jaxws/JAXWSAsyncClientTest.java
@@ -19,10 +19,32 @@
 
 package org.apache.cxf.systest.hc5.jaxws;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.xml.namespace.QName;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import jakarta.jws.WebService;
+import jakarta.xml.soap.MessageFactory;
+import jakarta.xml.soap.SOAPException;
+import jakarta.xml.soap.SOAPMessage;
+import jakarta.xml.ws.Dispatch;
 import jakarta.xml.ws.Response;
+import jakarta.xml.ws.Service;
+import jakarta.xml.ws.soap.SOAPBinding;
 import jakarta.xml.ws.soap.SOAPFaultException;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.greeter_control.AbstractGreeterImpl;
@@ -37,12 +59,15 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class JAXWSAsyncClientTest  extends AbstractBusClientServerTestBase {
     static final String PORT = allocatePort(Server.class);
-
+    static ScheduledExecutorService executor;
+    
     public static class Server extends AbstractBusTestServerBase {
 
         protected void run()  {
@@ -88,11 +113,18 @@ public class JAXWSAsyncClientTest  extends AbstractBusClientServerTestBase {
     @BeforeClass
     public static void startServers() throws Exception {
         assertTrue("server did not launch correctly", launchServer(Server.class, true));
+        executor = Executors.newScheduledThreadPool(5);
     }
 
     @AfterClass
     public static void stopServers() throws Exception {
         stopAllServers();
+        if (executor != null) {
+            executor.shutdown();
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        }
     }
 
     @Test
@@ -135,5 +167,78 @@ public class JAXWSAsyncClientTest  extends AbstractBusClientServerTestBase {
                        ex.getCause() instanceof java.net.ConnectException
                        || ex.getCause() instanceof java.net.SocketTimeoutException);
         }
+    }
+    
+    /**
+     * Not 100% reproducible but used to sporadically fail with:
+     * 
+     * java.util.concurrent.ExecutionException: jakarta.xml.ws.soap.SOAPFaultException: 
+     *   Cannot invoke "org.w3c.dom.Node.getOwnerDocument()" because "nd" is null
+     *   at java.base/java.util.concurrent.CompletableFuture.reportGet(CompletableFuture.java:396)
+     *   at java.base/java.util.concurrent.CompletableFuture.get(CompletableFuture.java:2073)
+     *   at org.apache.cxf.endpoint.ClientCallback.get(ClientCallback.java:139)
+     *   at org.apache.cxf.jaxws.JaxwsResponseCallback.get(JaxwsResponseCallback.java:48)
+     *   at org.apache.cxf.systest.hc5.jaxws.JAXWSAsyncClientTest.testAsyncWsdl(JAXWSAsyncClientTest.java:193)
+     *
+     * @see https://issues.apache.org/jira/browse/CXF-9007
+     */
+    @Test
+    public void testAsyncWsdl() throws Exception {
+        final URL wsdlUrl = getClass().getClassLoader().getResource("greeting.wsdl");
+
+        final Service service = Service.create(wsdlUrl, new QName("http://apache.org/hello_world", "SOAPService"));
+
+        service.addPort(new QName("http://apache.org/hello_world", "Greeter"), SOAPBinding.SOAP11HTTP_BINDING,
+                "http://localhost:" + PORT + "/SoapContext/GreeterPort");
+
+        final Dispatch<SOAPMessage> client = service.createDispatch(
+                new QName("http://apache.org/hello_world", "Greeter"),
+                SOAPMessage.class,
+                Service.Mode.MESSAGE
+        );
+
+        final List<Future<Response<SOAPMessage>>> tasks = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            tasks.add(executor.submit(() -> client.invokeAsync(buildMessage())));
+        }
+
+        for (Future<Response<SOAPMessage>> task : tasks) {
+            final SOAPMessage result = task.get(5, TimeUnit.SECONDS).get();
+            verifyResult(result);
+        }
+    }
+
+    private static void verifyResult(SOAPMessage message) throws Exception {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        
+        String output =  null;
+        try (StringWriter writer = new StringWriter()) {
+            final DOMSource source = new DOMSource(message.getSOAPBody().extractContentAsDocument());
+            transformer.transform(source, new StreamResult(writer));
+            output = writer.getBuffer().toString().replaceAll("\n|\r", "");
+        }
+
+        assertThat(output, containsString("HELLO"));
+    }
+
+    private static SOAPMessage buildMessage() throws SOAPException, IOException {
+        String soapMessage = 
+            "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+            + "<soapenv:Header/>"
+            + "  <soapenv:Body>"
+            + "    <ns2:greetMe xmlns:ns2=\"http://cxf.apache.org/greeter_control/types\">"
+            + "        <ns2:requestType>Hello</ns2:requestType>"
+            + "    </ns2:greetMe>"
+            + "  </soapenv:Body>"
+            + "</soapenv:Envelope>";
+
+        SOAPMessage message = null;
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(soapMessage.getBytes())) {
+            message = MessageFactory.newInstance().createMessage(null, bis);
+            message.saveChanges();
+        }
+
+        return message;
     }
 }
