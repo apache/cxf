c899d7443b20e329ae5aabb37fd532332750936a
Daniel Holm <daniel.holm@trustly.com>
2024-10-15 18:59:58 +0200

    [CXF-9066] Fix MaskSensitiveHelper masking of multi-line content (#2105)

--- rt/features/logging/src/test/java/org/apache/cxf/ext/logging/MaskSensitiveHelperTest.java
+++ rt/features/logging/src/test/java/org/apache/cxf/ext/logging/MaskSensitiveHelperTest.java
@@ -56,6 +56,11 @@ public class MaskSensitiveHelperTest {
     private static final String MASKED_LOGGING_CONTENT_XML_WITH_ATTRIBUTE =
             "<user>testUser</user><password myAttribute=\"test\">XXX</password>";
 
+    private static final String SENSITIVE_LOGGING_CONTENT_XML_WITH_MULTILINE =
+        "<user>testUser</user><password>my \nsecret \npassword</password>";
+    private static final String MASKED_LOGGING_CONTENT_XML_WITH_MULTILINE =
+        "<user>testUser</user><password>XXX</password>";
+
     private static final String SENSITIVE_LOGGING_CONTENT_XML_WITH_WRAPPER =
         "<passwords><password>my secret password</password></passwords>";
     private static final String MASKED_LOGGING_CONTENT_XML_WITH_WITH_WRAPPER =
@@ -98,6 +103,7 @@ public class MaskSensitiveHelperTest {
         return Arrays.asList(new Object[][] {
             {SENSITIVE_LOGGING_CONTENT_XML, MASKED_LOGGING_CONTENT_XML, APPLICATION_XML},
             {SENSITIVE_LOGGING_CONTENT_XML_WITH_ATTRIBUTE, MASKED_LOGGING_CONTENT_XML_WITH_ATTRIBUTE, APPLICATION_XML},
+            {SENSITIVE_LOGGING_CONTENT_XML_WITH_MULTILINE, MASKED_LOGGING_CONTENT_XML_WITH_MULTILINE, APPLICATION_XML},
             {SENSITIVE_LOGGING_CONTENT_XML_WITH_WRAPPER, MASKED_LOGGING_CONTENT_XML_WITH_WITH_WRAPPER, APPLICATION_XML},
             {SENSITIVE_LOGGING_MULTIPLE_ELEMENT_XML, MASKED_LOGGING_MULTIPLE_ELEMENT_XML, APPLICATION_XML},
             {SENSITIVE_LOGGING_CONTENT_XML_WITH_NAMESPACE, MASKED_LOGGING_CONTENT_XML_WITH_NAMESPACE, APPLICATION_XML},
