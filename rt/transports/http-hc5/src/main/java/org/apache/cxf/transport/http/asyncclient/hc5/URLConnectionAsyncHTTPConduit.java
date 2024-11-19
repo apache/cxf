087af6c9856a816a88948303527d56380b89bbd9
Andriy Redko <drreta@gmail.com>
2024-11-19 17:21:29 -0500

    CXF-9078: AsyncHTTPConduit.java causing NullPointerException when closing (#2145)

--- rt/transports/http-hc5/src/main/java/org/apache/cxf/transport/http/asyncclient/hc5/URLConnectionAsyncHTTPConduit.java
+++ rt/transports/http-hc5/src/main/java/org/apache/cxf/transport/http/asyncclient/hc5/URLConnectionAsyncHTTPConduit.java
@@ -436,7 +436,11 @@ public class URLConnectionAsyncHTTPConduit extends URLConnectionHTTPConduit {
                     this.basicEntity.setContentLength(out.size());
                     wrappedStream = null;
                     handleHeadersTrustCaching();
-                    out.writeCacheTo(wrappedStream);
+                    // The wrappedStrem could be null for KNOWN_HTTP_VERBS_WITH_NO_CONTENT or empty
+                    // requests (org.apache.cxf.empty.request)
+                    if (wrappedStream != null) {
+                        out.writeCacheTo(wrappedStream);
+                    }
                 }
             }
             super.close();
