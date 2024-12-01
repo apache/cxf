3b41cec930395342caf1b12c491f9f6fdfcd6dc9
Andriy Redko <drreta@gmail.com>
2024-12-01 08:27:53 -0500

    CXF-9057: Chunked Stream is closed regularly when Exception is thrown (#2107)
    (cherry picked from commit ff6cf7b74bca04a1dfa98082d7d0fb115b64c123)
    (cherry picked from commit 20e65dcc26bbd20c6f61e9c9f9f207b957dc74c6)

--- rt/bindings/soap/src/main/java/org/apache/cxf/binding/soap/interceptor/Soap12FaultOutInterceptor.java
+++ rt/bindings/soap/src/main/java/org/apache/cxf/binding/soap/interceptor/Soap12FaultOutInterceptor.java
@@ -37,6 +37,7 @@ import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.common.util.StringUtils;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
+import org.apache.cxf.message.MessageUtils;
 import org.apache.cxf.phase.Phase;
 import org.apache.cxf.staxutils.StaxUtils;
 
@@ -64,9 +65,16 @@ public class Soap12FaultOutInterceptor extends AbstractSoapInterceptor {
         }
         public void handleMessage(SoapMessage message) throws Fault {
             LOG.info(getClass() + (String) message.get(Message.CONTENT_TYPE));
+            Fault f = (Fault)message.getContent(Exception.class);
+
+            // If only some attachments have been written (usually, using chunked transfer), we  could 
+            // have been streaming some data already and may not be able to inject a fault in the middle 
+            // of the data transfer.
+            if (MessageUtils.getContextualBoolean(message, Message.PARTIAL_ATTACHMENTS_MESSAGE, false)) {
+                throw f;
+            }
 
             XMLStreamWriter writer = message.getContent(XMLStreamWriter.class);
-            Fault f = (Fault)message.getContent(Exception.class);
             message.put(org.apache.cxf.message.Message.RESPONSE_CODE, f.getStatusCode());
 
             SoapFault fault = SoapFault.createFault(f, message.getVersion());
