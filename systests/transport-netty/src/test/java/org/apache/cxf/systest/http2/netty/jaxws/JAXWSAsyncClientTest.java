bc556641529cdd12193181af334466a81913c44d
Andriy Redko <drreta@gmail.com>
2025-04-05 10:31:23 -0400

    CXF-9121: LEAK: ByteBuf.release() was not called before it's garbage-collected (#2339)
    (cherry picked from commit b200e8ac644bfc41f3a2ec095e41e5afd774fc11)

--- systests/transport-netty/src/test/java/org/apache/cxf/systest/http2/netty/jaxws/JAXWSAsyncClientTest.java
+++ systests/transport-netty/src/test/java/org/apache/cxf/systest/http2/netty/jaxws/JAXWSAsyncClientTest.java
@@ -32,6 +32,7 @@ import jakarta.jws.WebService;
 import jakarta.xml.ws.Response;
 import jakarta.xml.ws.soap.SOAPFaultException;
 import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.ext.logging.LoggingFeature;
 import org.apache.cxf.greeter_control.AbstractGreeterImpl;
 import org.apache.cxf.greeter_control.Greeter;
 import org.apache.cxf.greeter_control.types.GreetMeResponse;
@@ -178,5 +179,24 @@ public class JAXWSAsyncClientTest  extends AbstractBusClientServerTestBase {
                        ex.getCause() instanceof IOException
                        || ex.getCause() instanceof ReadTimeoutException);
         }
+    }
+
+    @Test
+    public void testAsyncClientWithLogging() throws Exception {
+        // setup the feature by using JAXWS front-end API
+        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        factory.setAddress("http://localhost:" + PORT + "/SoapContext/GreeterPort");
+        factory.setServiceClass(Greeter.class);
+        factory.setFeatures(List.of(new LoggingFeature()));
+        Greeter proxy = factory.create(Greeter.class);
+
+        Response<GreetMeResponse>  response = proxy.greetMeAsync("cxf");
+        int waitCount = 0;
+        while (!response.isDone() && waitCount < 15) {
+            Thread.sleep(1000);
+            waitCount++;
+        }
+        
+        assertTrue("Response still not received.", response.isDone());
     }
 }
