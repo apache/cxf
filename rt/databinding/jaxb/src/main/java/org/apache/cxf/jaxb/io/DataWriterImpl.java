5b891e8d2834178f05daae8fcc86eaeb9f8bb43f
jspr-schlz <115313449+jspr-schlz@users.noreply.github.com>
2025-05-20 17:17:21 +0200

    Added null check to DataWriterImpl when calling getLastMTOMElementName (#2416)
    (cherry picked from commit 27deb8899755420557066ab9c3f6840ac5e1cbf0)

--- rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/io/DataWriterImpl.java
+++ rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/io/DataWriterImpl.java
@@ -101,7 +101,8 @@ public class DataWriterImpl<T> extends JAXBDataBase implements DataWriter<T> {
             // CXF-1194/CXF-7438 this hack is specific to MTOM, so pretty safe to leave in
             // here before calling the origHandler.
             String msg = event.getMessage();
-            if ((msg.startsWith("cvc-type.3.1.2") || msg.startsWith("cvc-complex-type.2.2"))
+            if (marshaller.getLastMTOMElementName() != null
+                && (msg.startsWith("cvc-type.3.1.2") || msg.startsWith("cvc-complex-type.2.2"))
                 && msg.contains(marshaller.getLastMTOMElementName().getLocalPart())) {
                 return true;
             }
