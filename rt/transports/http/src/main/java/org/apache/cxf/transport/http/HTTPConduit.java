2c5175b569f140665f832c799e28d5e9aa7cae8a
Eric Giese <eric6iese@googlemail.com>
2025-06-17 19:01:18 +0200

    CXF-9146 Fix MemoryLeak in HttpClientHttpConduit (#2467)
    * CXF-9146 if the wrappedStream corresponds to pout then it should be nulled as well. this fixes a memoryleak caused when pout is a PipedOutputStream, as the pipe holds a strong reference to the client thread which must be nulled to avoid MemoryLeaks in class like ThreadLocalClientState, which utilizes the same thread as a key for weakhashmap which in turn points to this very outputstream. this means that the thread is both the key and within the value of an entry of the weakhashamp, and as per the javadoc of said class, the GC cannot detect these cycles. to break this cycle, not only pout but also the wrappedStream needs to be nulled.
    
    * CXF-9146 both the cachedStream and the wrappedOutputStream references are cleared after closing them
    
    (cherry picked from commit 097b0a7c2cfc0743c05e66d92cb7bc988d618495)
    (cherry picked from commit 63653f0ed7cd2a2578dfd6cc3cad08323957dd55)

--- rt/transports/http/src/main/java/org/apache/cxf/transport/http/HTTPConduit.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/HTTPConduit.java
@@ -1421,6 +1421,7 @@ public abstract class HTTPConduit
                 } finally {
                     if (cachingForRetransmission && cachedStream != null) {
                         cachedStream.close();
+                        cachedStream = null;
                     }
                 }
             } catch (HttpRetryException e) {
