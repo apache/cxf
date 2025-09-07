5189063c59281bd725074a39d3f03cb8edb91e9a
Andriy Redko <drreta@gmail.com>
2025-09-07 13:43:07 -0400

    Fix OpenApi and SwaggerUi test cases

--- systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/description/openapi/SwaggerUiConfigurationQueryConfigTest.java
+++ systests/jaxrs/src/test/java/org/apache/cxf/systest/jaxrs/description/openapi/SwaggerUiConfigurationQueryConfigTest.java
@@ -87,7 +87,7 @@ public class SwaggerUiConfigurationQueryConfigTest extends AbstractClientServerT
         try (Response response = uiClient.get()) {
             String html = response.readEntity(String.class);
             assertThat(html, containsString("<!-- HTML"));
-            assertThat(response.getMediaType(), equalTo(MediaType.TEXT_HTML_TYPE));
+            assertThat(response.getMediaType(), equalTo(MediaType.TEXT_HTML_TYPE.withCharset("utf-8")));
         }
     }
 
