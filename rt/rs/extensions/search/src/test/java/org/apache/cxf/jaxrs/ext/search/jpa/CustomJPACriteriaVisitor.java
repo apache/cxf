95a5856dc223777f8c4390b8379277a605b008a2
Raoua <81809702+raoua-eng@users.noreply.github.com>
2026-03-11 13:30:11 +0100

    CXF-9174: Make doBuildPredicate and doBuildCollectionPredicate protected for extensibility (#2798)
    Allow subclasses of AbstractJPATypedQueryVisitor to customize JPA predicate
    building by changing doBuildPredicate() and doBuildCollectionPredicate() from
    private to protected visibility. This enables the Template Method pattern for
    custom FIQL-to-JPA predicate generation without modifying the core visitor.
    
    Includes tests demonstrating a custom visitor with case-insensitive LIKE
    matching and collection predicate override.
    
    Co-authored-by: Guillaume Nodet <gnodet@gmail.com>

---
+++ rt/rs/extensions/search/src/test/java/org/apache/cxf/jaxrs/ext/search/jpa/CustomJPACriteriaVisitor.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.jaxrs.ext.search.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.metamodel.Attribute;
+import jakarta.persistence.metamodel.Bindable;
+import org.apache.cxf.jaxrs.ext.search.ConditionType;
+import org.apache.cxf.jaxrs.ext.search.collections.CollectionCheckInfo;
+
+public class CustomJPACriteriaVisitor extends JPACriteriaQueryVisitor<Book, Book> {
+
+    CustomJPACriteriaVisitor(EntityManager em) {
+        super(em, Book.class, Book.class);
+    }
+
+    @Override
+    protected Predicate doBuildPredicate(ConditionType ct, Path<?> path, Class<?> valueClazz, Object value) {
+        String name = getAttributeName(path);
+        if ("bookTitle".equals(name)) {
+            return getCriteriaBuilder().like(
+                getCriteriaBuilder().lower(path.as(String.class)),
+                "%" + value.toString().toLowerCase() + "%");
+        }
+
+        return super.doBuildPredicate(ct, path, valueClazz, value);
+    }
+
+    @Override
+    protected Predicate doBuildCollectionPredicate(ConditionType ct, Path<?> path, CollectionCheckInfo collInfo) {
+        return getCriteriaBuilder().disjunction();
+    }
+
+    private String getAttributeName(Path<?> path) {
+        Bindable<?> model = path.getModel();
+        if (model instanceof Attribute) {
+            return ((Attribute<?, ?>)model).getName();
+        }
+        return null;
+    }
+}
