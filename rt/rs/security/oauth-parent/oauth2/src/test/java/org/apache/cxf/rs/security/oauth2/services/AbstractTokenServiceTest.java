413d75ec1d9bd1b6a2b50e72bf4e7f0e53258218
Colm O hEigeartaigh <coheigea@users.noreply.github.com>
2026-07-20 09:11:06 +0100

    Enforce TLS client certificate and store the application certificates for dynamic registration (#3316)

--- rt/rs/security/oauth-parent/oauth2/src/test/java/org/apache/cxf/rs/security/oauth2/services/AbstractTokenServiceTest.java
+++ rt/rs/security/oauth-parent/oauth2/src/test/java/org/apache/cxf/rs/security/oauth2/services/AbstractTokenServiceTest.java
@@ -18,12 +18,21 @@
  */
 package org.apache.cxf.rs.security.oauth2.services;
 
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+
+import javax.security.auth.x500.X500Principal;
+
 import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.SecurityContext;
+import org.apache.cxf.common.util.Base64Utility;
+import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.apache.cxf.jaxrs.impl.MetadataMap;
 import org.apache.cxf.rs.security.oauth2.common.Client;
 import org.apache.cxf.rs.security.oauth2.common.OAuthError;
 import org.apache.cxf.rs.security.oauth2.provider.OAuthServiceException;
 import org.apache.cxf.rs.security.oauth2.utils.OAuthConstants;
+import org.apache.cxf.security.transport.TLSSessionInfo;
 
 import org.junit.Test;
 
@@ -31,6 +40,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AbstractTokenServiceTest {
 
@@ -79,12 +90,91 @@ public class AbstractTokenServiceTest {
         assertSame(expected, actual);
     }
 
+    @Test
+    public void testCheckCertificateBindingAcceptsDnOnlyClient() {
+        TestTokenService service = new TestTokenService();
+        Client client = new Client("c1", null, true);
+        client.getProperties().put(OAuthConstants.TLS_CLIENT_AUTH_SUBJECT_DN, "CN=peer,O=Test,C=US");
+
+        service.callCheckCertificateBinding(client,
+            makeTlsSession("CN=peer,O=Test,C=US", "CN=issuer,O=Test,C=US"));
+    }
+
+    @Test
+    public void testCheckCertificateBindingRejectsDnOnlyClientMismatch() {
+        TestTokenService service = new TestTokenService();
+        Client client = new Client("c1", null, true);
+        client.getProperties().put(OAuthConstants.TLS_CLIENT_AUTH_SUBJECT_DN, "CN=peer,O=Test,C=US");
+
+        InvalidClientException ex = assertThrows(InvalidClientException.class,
+            () -> service.callCheckCertificateBinding(client,
+                makeTlsSession("CN=other,O=Test,C=US", "CN=issuer,O=Test,C=US")));
+
+        assertNotNull(ex.getError());
+        assertEquals(OAuthConstants.INVALID_CLIENT, ex.getError().getError());
+    }
+
+    @Test
+    public void testCheckCertificateBindingRejectsCertificateMismatch() {
+        TestTokenService service = new TestTokenService();
+        Client client = new Client("c2", null, true);
+        client.getApplicationCertificates().add(Base64Utility.encode(new byte[] {9, 9, 9}));
+
+        InvalidClientException ex = assertThrows(InvalidClientException.class,
+            () -> service.callCheckCertificateBinding(client,
+                makeTlsSession("CN=peer,O=Test,C=US", "CN=issuer,O=Test,C=US")));
+
+        assertNotNull(ex.getError());
+        assertEquals(OAuthConstants.INVALID_CLIENT, ex.getError().getError());
+    }
+
+    @Test
+    public void testCheckCertificateBindingAcceptsCertificateMatch() throws Exception {
+        TestTokenService service = new TestTokenService();
+        X509Certificate cert = createCertificate("CN=peer,O=Test,C=US", "CN=issuer,O=Test,C=US");
+
+        Client client = new Client("c3", null, true);
+        client.getApplicationCertificates().add(Base64Utility.encode(cert.getEncoded()));
+
+        service.callCheckCertificateBinding(client, makeTlsSession(cert));
+    }
+
+    private static TLSSessionInfo makeTlsSession(String subjectDn, String issuerDn) {
+        X509Certificate cert = createCertificate(subjectDn, issuerDn);
+        return makeTlsSession(cert);
+    }
+
+    private static TLSSessionInfo makeTlsSession(X509Certificate cert) {
+        return new TLSSessionInfo("TLS_FAKE", null, new Certificate[] {cert});
+    }
+
+    private static X509Certificate createCertificate(String subjectDn, String issuerDn) {
+        X509Certificate cert = mock(X509Certificate.class);
+        when(cert.getSubjectX500Principal()).thenReturn(new X500Principal(subjectDn));
+        when(cert.getIssuerX500Principal()).thenReturn(new X500Principal(issuerDn));
+        try {
+            when(cert.getEncoded()).thenReturn(new byte[] {1, 2, 3});
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        return cert;
+    }
+
     private static final class TestTokenService extends AbstractTokenService {
         private Client client;
         private OAuthServiceException exception;
 
         Client callGetClient(String clientId, String clientSecret, MultivaluedMap<String, String> params) {
             return getClient(clientId, clientSecret, params);
+        }
+
+        void callCheckCertificateBinding(Client boundClient, TLSSessionInfo tlsSessionInfo) {
+            MessageContext mc = mock(MessageContext.class);
+            SecurityContext sc = mock(SecurityContext.class);
+            when(sc.getAuthenticationScheme()).thenReturn("");
+            when(mc.getSecurityContext()).thenReturn(sc);
+            setMessageContext(mc);
+            checkCertificateBinding(boundClient, tlsSessionInfo);
         }
 
         void setClient(Client client) {
