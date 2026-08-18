66886ed954bcb83337170f026aae415609b5d3bc
vp340 <portavalentino00@gmail.com>
2026-08-19 00:25:04 +0200

    restore previous situation

--- core/src/main/java/org/apache/cxf/message/Message.java
+++ core/src/main/java/org/apache/cxf/message/Message.java
@@ -211,12 +211,6 @@ public interface Message extends StringMap {
      */
     String THREAD_SAFE_STAX_FACTORIES = Message.class.getName() + ".THREAD_SAFE_STAX_FACTORIES";
 
-
-    /**
-     * Property to disable the interceptor log of the Message
-     */
-    String LIVE_LOGGING_PROP = "org.apache.cxf.logging.enable";
-
     String getId();
     void setId(String id);
 
